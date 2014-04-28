0681ae27684d6605601a67bd5976307701b12391
Flaminyogurt <flaminyogurt@gmail.com>
2014-04-27 23:01:20 -0500

    Add build.gradle and mcmod.info

--- src/main/java/com/minecolonies/lib/Constants.java
+++ src/main/java/com/minecolonies/lib/Constants.java
@@ -3,7 +3,7 @@ package com.minecolonies.lib;
 public class Constants
 {
     public static final String MODID               = "minecolonies";
-    public static final String MODNAME             = "Mine Colonies";
+    public static final String MODNAME = "MineColonies";
     public static final String VERSION             = "0.0.1";
     public static final String CLIENTPROXYLOCATION = "com.minecolonies.proxy.ClientProxy";
     public static final String COMMONPROXYLOCATION = "com.minecolonies.proxy.CommonProxy";
