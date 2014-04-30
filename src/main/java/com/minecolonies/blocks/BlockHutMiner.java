9bda8eb658f43f21acf8587b12ac0dfa16e47bc3
NicoLinder <nicolinder@icloud.com>
2014-04-30 16:10:34 +0200

    fix Crash

--- src/main/java/com/minecolonies/blocks/BlockHutMiner.java
+++ src/main/java/com/minecolonies/blocks/BlockHutMiner.java
@@ -1,5 +1,6 @@
 package com.minecolonies.blocks;
 
+import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.block.material.Material;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
@@ -12,6 +13,7 @@ public class BlockHutMiner extends BlockInformator
     {
         super(Material.wood);
         setBlockName(getName());
+        GameRegistry.registerBlock(this, getName());
     }
 
     @Override
--- src/main/java/com/minecolonies/items/crafting/RecipeHandler.java
+++ src/main/java/com/minecolonies/items/crafting/RecipeHandler.java
@@ -3,11 +3,12 @@ package com.minecolonies.items.crafting;
 import com.minecolonies.blocks.ModBlocks;
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 
 public final class RecipeHandler
 {
     public static void init() {
-        GameRegistry.addRecipe(new ItemStack(ModBlocks.blockHutMiner), "XXX", "X#X", "XXX", Character.valueOf('X'), Blocks.planks, Character.valueOf('#'));
+        GameRegistry.addRecipe(new ItemStack(ModBlocks.blockHutMiner, 1), "XXX", "X#X", "XXX", Character.valueOf('X'), Blocks.planks, Character.valueOf('#'), Items.wooden_pickaxe);
     }
 }
