3a6b58af9e67701d906d9947105945b9fb0dbd69
MartijnWoudstra <martijnhenriwoudstra@gmail.com>
2014-04-30 15:25:59 +0200

    ChestDeployer Player properties

--- src/main/java/com/minecolonies/entity/PlayerProperties.java
+++ src/main/java/com/minecolonies/entity/PlayerProperties.java
@@ -23,11 +23,20 @@ public class PlayerProperties implements IExtendedEntityProperties
         this.player = player;
     }
 
+    /**
+     * Registers player property. Should be checked if already exists, and called in onEntityContruct event
+     * @param player player to create propery for
+     */
     public void register(EntityPlayer player)
     {
         player.registerExtendedProperties(Constants.PlayerPropertyName, new PlayerProperties(player));
     }
 
+    /**
+     * Gets the player properties for a player
+     * @param player player to get propery for
+     * @return PlayerProperties for the player.
+     */
     public PlayerProperties getPlayerProperties(EntityPlayer player)
     {
         return (PlayerProperties) player.getExtendedProperties(Constants.PlayerPropertyName);
@@ -53,21 +62,37 @@ public class PlayerProperties implements IExtendedEntityProperties
 
     }
 
-    public boolean HasPlacedTownHall()
+    /**
+     * Gets the property whether the player has placed a townhall
+     * @return whether the player has placed a townhall
+     */
+    public boolean hasPlacedTownHall()
     {
         return hasPlacedTownHall;
     }
 
+    /**
+     * Sets whether the player has placed a townhall
+     * @param hasPlacedTownHall boolean
+     */
     public void setHasPlacedTownHall(boolean hasPlacedTownHall)
     {
         this.hasPlacedTownHall = hasPlacedTownHall;
     }
 
+    /**
+     * Gets the property whether the player has placed a supply chest
+     * @return whether the player has placed a supply chest.
+     */
     public boolean hasPlacedSupplyChest()
     {
         return hasPlacedSupplyChest;
     }
 
+    /**
+     * Sets whether the player has placed a townhall
+     * @param hasPlacedSupplyChest boolean
+     */
     public void setHasPlacedSupplyChest(boolean hasPlacedSupplyChest)
     {
         this.hasPlacedSupplyChest = hasPlacedSupplyChest;
