17ad2c33a4545a9f9daf99d5df62f1f7e59a4ac0
NicoLinder <nicolinder@icloud.com>
2014-04-30 19:19:17 +0200

    places Supplychest facing away from water while using "SupplyChestDeployer"

--- src/main/java/com/minecolonies/items/ItemSupplyChestDeployer.java
+++ src/main/java/com/minecolonies/items/ItemSupplyChestDeployer.java
@@ -9,6 +9,7 @@ import com.minecolonies.util.Utils;
 import cpw.mods.fml.client.FMLClientHandler;
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.MovingObjectPosition;
@@ -17,6 +18,7 @@ import net.minecraft.world.World;
 public class ItemSupplyChestDeployer extends net.minecraft.item.Item implements IColony
 {
     private String name = "supplyChestDeployer";
+
     public ItemSupplyChestDeployer()
     {
         setUnlocalizedName(getName());
@@ -34,78 +36,83 @@ public class ItemSupplyChestDeployer extends net.minecraft.item.Item implements 
     @Override
     public ItemStack onItemRightClick(ItemStack itemStack, World world, EntityPlayer entityPlayer)
     {
-        if(world == null || entityPlayer == null || world.isRemote || itemStack.stackSize == 0)
-            return itemStack;
+        if(world == null || entityPlayer == null || world.isRemote || itemStack.stackSize == 0) return itemStack;
         MovingObjectPosition blockPos = getMovingObjectPositionFromPlayer(world, entityPlayer, false);
-        if(blockPos == null)
-            return itemStack;
+        if(blockPos == null) return itemStack;
         int x = blockPos.blockX;
         int y = blockPos.blockY;
         int z = blockPos.blockZ;
-        if(!canShipBePlaced(world, x, y, z, entityPlayer))
+        int chestFacing = getWaterSide(world, x, y, z, entityPlayer);
+        if(chestFacing == -1)
         {
             return itemStack;
         }
-        spawnShip(world, x, y, z, entityPlayer);
+        spawnShip(world, x, y, z, entityPlayer, chestFacing);
         return itemStack;
     }
 
     /**
-     * Checks if the ship can be placed, by checking an area for water
-     * @param world world
-     * @param x xCoord clicked
-     * @param y yCoord clicked
-     * @param z zCoord clicked
+     * Checks if the ship can be placed, by checking an area for water and returns metadata/facing of chest
+     *
+     * @param world        world
+     * @param x            xCoord clicked
+     * @param y            yCoord clicked
+     * @param z            zCoord clicked
      * @param entityPlayer Player
-     * @return true if ship can be placed, false otherwise
+     * @return -1 if ship can't be placed, 0-3 if ship can be placed
      */
-    public boolean canShipBePlaced(World world, int x, int y, int z, EntityPlayer entityPlayer)
+    public int getWaterSide(World world, int x, int y, int z, EntityPlayer entityPlayer)
     {
-        if (!isFirstPlacing(world, entityPlayer)) {
+        if(!isFirstPlacing(world, entityPlayer))
+        {
             FMLClientHandler.instance().getClient().ingameGUI.getChatGUI().printChatMessage(new ChatComponentText("Supply Chest Already Placed"));
 
-            return false;
+            return -1;
         }
-        if (Utils.isWater(world.getBlock(x - 1, y, z)) && Utils.isWater(world.getBlock(x - 15, y, z)) && Utils.isWater(world.getBlock(x - 15, y, z + 23)))
-            return true;
-        if (Utils.isWater(world.getBlock(x + 1, y, z)) && Utils.isWater(world.getBlock(x + 15, y, z)) && Utils.isWater(world.getBlock(x + 15, y, z - 23)))
-            return true;
-        if (Utils.isWater(world.getBlock(x, y, z - 1)) && Utils.isWater(world.getBlock(x, y, z - 15)) && Utils.isWater(world.getBlock(x - 23, y, z - 15)))
-            return true;
-        if (Utils.isWater(world.getBlock(x, y, z + 1)) && Utils.isWater(world.getBlock(x, y, z + 15)) && Utils.isWater(world.getBlock(x + 23, y, z + 15)))
-            return true;
+        if(Utils.isWater(world.getBlock(x - 1, y, z)) && Utils.isWater(world.getBlock(x - 15, y, z)) && Utils.isWater(world.getBlock(x - 15, y, z + 23)))
+            return 5; //Chest: East
+        if(Utils.isWater(world.getBlock(x + 1, y, z)) && Utils.isWater(world.getBlock(x + 15, y, z)) && Utils.isWater(world.getBlock(x + 15, y, z - 23)))
+            return 4; //Chest: West
+        if(Utils.isWater(world.getBlock(x, y, z - 1)) && Utils.isWater(world.getBlock(x, y, z - 15)) && Utils.isWater(world.getBlock(x - 23, y, z - 15)))
+            return 3; //Chest: South
+        if(Utils.isWater(world.getBlock(x, y, z + 1)) && Utils.isWater(world.getBlock(x, y, z + 15)) && Utils.isWater(world.getBlock(x + 23, y, z + 15)))
+            return 2; //Chest: North
 
         FMLClientHandler.instance().getClient().ingameGUI.getChatGUI().printChatMessage(new ChatComponentText("Supply Chest Must Be next to a large body of water"));
 
-        return false;
+        return -1;
     }
 
     /**
      * Checks if the player already placed a supply chest
-     * @param world World obj
+     *
+     * @param world        World obj
      * @param entityPlayer The player
      * @return boolean, returns true when player hasnt placed before, or when infinite placing is on.
      */
     boolean isFirstPlacing(World world, EntityPlayer entityPlayer)
     {
-        if(Configurations.allowInfinitePlacing)
-            return true;
-        PlayerProperties playerProperties = (PlayerProperties)entityPlayer.getExtendedProperties(Constants.PlayerPropertyName);
+        if(Configurations.allowInfinitePlacing) return true;
+        PlayerProperties playerProperties = (PlayerProperties) entityPlayer.getExtendedProperties(Constants.PlayerPropertyName);
         return !playerProperties.hasPlacedSupplyChest();
     }
 
     /**
      * Spawns the ship and supply chest
-     * @param world world obj
-     * @param x xCoord clicked
-     * @param y yCoord clicked
-     * @param z zCoord clicked
+     *
+     * @param world        world obj
+     * @param x            xCoord clicked
+     * @param y            yCoord clicked
+     * @param z            zCoord clicked
      * @param entityPlayer the player
      */
-    private void spawnShip(World world, int x, int y, int z, EntityPlayer entityPlayer)
+    private void spawnShip(World world, int x, int y, int z, EntityPlayer entityPlayer, int chestFacing)
     {
-      //TODO Spawn ship, spawn chest, fill chest, save new ship.
+        //TODO Spawn ship, spawn chest, fill chest, save new ship.
         PlayerProperties playerProperties = (PlayerProperties) entityPlayer.getExtendedProperties(Constants.PlayerPropertyName);
         playerProperties.setHasPlacedSupplyChest(true);
+
+        world.setBlock(x, y + 1, z, Blocks.chest);
+        world.setBlockMetadataWithNotify(x, y + 1, z, chestFacing, 2);
     }
 }
