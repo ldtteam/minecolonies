a80e5303a23470b29da7d901611ed1eba9094663
Colton Schlosser <cltnschlosser@gmail.com>
2014-05-09 05:00:51 +0000

    IProxy.java edited online with Bitbucket

--- src/main/java/com/minecolonies/proxy/IProxy.java
+++ src/main/java/com/minecolonies/proxy/IProxy.java
@@ -3,13 +3,4 @@ package com.minecolonies.proxy;
 public interface IProxy
 {
     void registerTileEntities();
-
-    void registerEntities();
-
-    void registerEntityRendering();
-
-
-    void registerKeybindings();
- 
-    void registerEvents();
 }
