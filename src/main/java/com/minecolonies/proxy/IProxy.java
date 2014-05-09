711c619bb4d4d7b485a8299ef1b1b89636b6ca5f
MartijnWoudstra <martijnhenriwoudstra@gmail.com>
2014-05-09 07:07:58 +0200

    Revert "IProxy.java edited online with Bitbucket"
    This reverts commit a80e5303a23470b29da7d901611ed1eba9094663.

--- src/main/java/com/minecolonies/proxy/IProxy.java
+++ src/main/java/com/minecolonies/proxy/IProxy.java
@@ -3,4 +3,13 @@ package com.minecolonies.proxy;
 public interface IProxy
 {
     void registerTileEntities();
+
+    void registerEntities();
+
+    void registerEntityRendering();
+
+
+    void registerKeybindings();
+ 
+    void registerEvents();
 }
