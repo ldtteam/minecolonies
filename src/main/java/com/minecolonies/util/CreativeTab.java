c7fcc08302354b034cbfec40f37c5555be24fe97
cltnschlosser <cltnschlosser@gmail.com>
2014-05-11 20:38:35 -0500

    Put Townhall on creative tab

--- src/main/java/com/minecolonies/util/CreativeTab.java
+++ src/main/java/com/minecolonies/util/CreativeTab.java
@@ -1,5 +1,6 @@
 package com.minecolonies.util;
 
+import com.minecolonies.blocks.ModBlocks;
 import com.minecolonies.lib.Constants;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.init.Items;
@@ -7,12 +8,12 @@ import net.minecraft.item.Item;
 
 public class CreativeTab
 {
-    public static final CreativeTabs mineColoniesTab = new CreativeTabs(Constants.MODID.toLowerCase())
+    public static final CreativeTabs mineColoniesTab = new CreativeTabs(Constants.MODID)
     {
         @Override
         public Item getTabIconItem()
         {
-            return Items.apple; // TODO
+            return Item.getItemFromBlock(ModBlocks.blockHutTownhall);
         }
     };
 }
