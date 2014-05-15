8aa81e2e5ca6353387db2bcf55955d0b8f1cbe6f
MrIbby <siribby@outlook.com>
2014-05-14 19:37:58 -0700

    Close GUI when inventory button is pressed, closes #10

--- src/main/java/com/minecolonies/client/gui/GuiBase.java
+++ src/main/java/com/minecolonies/client/gui/GuiBase.java
@@ -128,4 +128,13 @@ public class GuiBase extends GuiScreen
         addElements();
         super.initGui();
     }
+
+    @Override
+    protected void keyTyped(char par1, int par2)
+    {
+        if (par2 == 1 || par2 == this.mc.gameSettings.keyBindInventory.getKeyCode())
+        {
+            this.mc.thePlayer.closeScreen();
+        }
+    }
 }
