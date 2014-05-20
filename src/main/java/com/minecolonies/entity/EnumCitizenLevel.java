200bfd2ffdfa20c318e015ba62f1dd3207bc797a
Colton Schlosser <cltnschlosser@gmail.com>
2014-05-19 20:42:09 -0500

    Server doesn't have model

--- src/main/java/com/minecolonies/entity/EnumCitizenLevel.java
+++ src/main/java/com/minecolonies/entity/EnumCitizenLevel.java
@@ -8,33 +8,26 @@ import net.minecraft.client.model.ModelBiped;
 
 public enum EnumCitizenLevel
 {
-    SETTLERMALE(new ModelBiped(), 0, "Male", "/textures/entity/EntitySettler"),
-    CITIZENMALE(new ModelBiped(), 1, "Male", "/textures/entity/EntityCitizen"),
-    NOBLEMALE(new ModelBiped(), 2, "Male", "/textures/entity/EntityNoble"),
-    ARISTOCRATMALE(new ModelBiped(), 3, "Male", "/textures/entity/EntityAristocrat"),
-    SETTLERFEMALE(new ModelEntityCitizenFemaleCitizen(), 0, "Female", "/textures/entity/EntitySettler"),
-    CITIZENFEMALE(new ModelEntityCitizenFemaleCitizen(), 1, "Female", "/textures/entity/EntityCitizen"),
-    NOBLEFEMALE(new ModelEntityCitizenFemaleNoble(), 2, "Female", "/textures/entity/EntityNoble"), //TODO ADD MODEL
-    ARISTOCRATFEMALE(new ModelEntityCitizenFemaleAristocrat(), 3, "Female", "/textures/entity/EntityAristocrat"); //TODO ADD MODEL
+    SETTLERMALE(0, "Male", "/textures/entity/EntitySettler"),
+    CITIZENMALE(1, "Male", "/textures/entity/EntityCitizen"),
+    NOBLEMALE(2, "Male", "/textures/entity/EntityNoble"),
+    ARISTOCRATMALE(3, "Male", "/textures/entity/EntityAristocrat"),
+    SETTLERFEMALE(0, "Female", "/textures/entity/EntitySettler"),
+    CITIZENFEMALE(1, "Female", "/textures/entity/EntityCitizen"),
+    NOBLEFEMALE(2, "Female", "/textures/entity/EntityNoble"),
+    ARISTOCRATFEMALE(3, "Female", "/textures/entity/EntityAristocrat");
 
-    private final ModelBiped model;
     private final int        level;
     private final String     partialTextureString;
     private final int        sexInt;
     private final String     sexString;
 
-    EnumCitizenLevel(ModelBiped modelBiped, int level, String sex, String textureLocationPart)
+    EnumCitizenLevel(int level, String sex, String textureLocationPart)
     {
-        this.model = modelBiped;
         this.level = level;
         this.sexInt = sex.equalsIgnoreCase("Male") ? 0 : 1;
         this.sexString = sex;
         this.partialTextureString = setTexture(textureLocationPart);
-    }
-
-    public ModelBiped getModel()
-    {
-        return this.model;
     }
 
     public int getLevel()
