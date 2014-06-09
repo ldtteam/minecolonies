87447914109df105562cc4cd5a44c5b05ebf3327
Colton Schlosser <cltnschlosser@gmail.com>
2014-06-08 23:57:04 -0500

    Create method instead of just having TODO in ItemBuildTool

--- src/main/java/com/minecolonies/items/ItemBuildTool.java
+++ src/main/java/com/minecolonies/items/ItemBuildTool.java
@@ -1,5 +1,9 @@
 package com.minecolonies.items;
 
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+
 public class ItemBuildTool extends ItemMinecolonies
 {
     public ItemBuildTool()
@@ -14,5 +18,10 @@ public class ItemBuildTool extends ItemMinecolonies
         return "scepterGold";
     }
 
-    //TODO onItemUse
+    @Override
+    public boolean onItemUse(ItemStack par1ItemStack, EntityPlayer par2EntityPlayer, World par3World, int par4, int par5, int par6, int par7, float par8, float par9, float par10)
+    {
+        //TODO open build GUI
+        return super.onItemUse(par1ItemStack, par2EntityPlayer, par3World, par4, par5, par6, par7, par8, par9, par10);
+    }
 }
