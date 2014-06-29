7a1ca272cbb2ea7d9d36dc114028c4cc85dcd860
Colton Schlosser <cltnschlosser@gmail.com>
2014-06-29 15:55:26 -0500

    Fix schematica bug - was completely my fault.
    https://github.com/Lunatrius/Schematica/commit/ef1655dddd5ae42f6a8002e8dcab19e2c3449fff

--- src/main/java/com/github/lunatrius/schematica/world/schematic/SchematicAlpha.java
+++ src/main/java/com/github/lunatrius/schematica/world/schematic/SchematicAlpha.java
@@ -155,7 +155,7 @@ public class SchematicAlpha extends SchematicFormat
                         extra = true;
                     }
 
-                    String name = GameData.getBlockRegistry().getNameForObject(blockId);
+                    String name = GameData.getBlockRegistry().getNameForObject(world.getBlockRaw(x, y, z));
                     if(!mapping.hasKey(name))
                     {
                         mapping.setShort(name, (short) blockId);
