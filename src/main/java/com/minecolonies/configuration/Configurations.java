49e0d84b2c21cc621c07fc9f208d847022fec953
Colton Schlosser <cltnschlosser@gmail.com>
2014-07-04 22:48:47 -0500

    Citizen names. Closes #21

--- src/main/java/com/minecolonies/configuration/Configurations.java
+++ src/main/java/com/minecolonies/configuration/Configurations.java
@@ -11,6 +11,15 @@ public class Configurations
     public static final int     DEFAULT_CITIZENRESPAWNINTERVAL   = 30;
     public static final boolean DEFAULT_BUILDERINFINITERESOURCES = true;//TODO change to false when material handling is implemented
 
+    public static final String[] DEFAULT_MALE_FIRST_NAMES = new String[]{
+            "Jim", "John", "James", "Robert", "Thomas", "Michael", "William", "David", "Richard", "Charles", "Joseph", "Christopher", "Paul", "Mark", "George", "Steven", "Peter", "Henrik", "Rory"};
+
+    public static final String[] DEFAULT_FEMALE_FIRST_NAMES = new String[]{
+            "Mary", "Patricia", "Linda", "Barbara", "Elizabeth", "Jennifer", "Maria", "Susan", "Margaret", "Dorothy", "Lisa", "Nancy", "Karen", "Betty", "Helen", "Natasha"};
+
+    public static final String[] DEFAULT_LAST_NAMES = new String[]{
+            "Smith", "Johnson", "Jones", "Williams", "Brown", "Miller", "Wilson", "Taylor", "Jackson", "White", "Harris", "Robinson", "Clark", "Wallgreen", "Allen", "Mardle"};
+
     /*
     Holders
      */
@@ -22,4 +31,8 @@ public class Configurations
      */
     public static int     citizenRespawnInterval;
     public static boolean builderInfiniteResources;
+
+    public static String[] maleFirstNames;
+    public static String[] femaleFirstNames;
+    public static String[] lastNames;
 }
