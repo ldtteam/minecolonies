6844707b93c93ac853fdb86c6429880fd306537e
Colton Schlosser <cltnschlosser@gmail.com>
2014-08-07 14:11:35 -0500

    Revert "Group lines. Does this increase readability?"
    This reverts commit 630242b8f53a35f2c6988012fee47795a2e6664d.

--- src/main/java/com/minecolonies/tileentities/TileEntityHutWarehouse.java
+++ src/main/java/com/minecolonies/tileentities/TileEntityHutWarehouse.java
@@ -50,26 +50,22 @@ public class TileEntityHutWarehouse extends TileEntityHutWorker
         super.readFromNBT(compound);
 
         NBTTagCompound deliveryCompound = compound.getCompoundTag("delivery");
-        {
-            NBTTagCompound blacksmithCompound = deliveryCompound.getCompoundTag("blacksmith");
-            {
-                blacksmithGold = blacksmithCompound.getBoolean("gold");
-                blacksmithDiamond = blacksmithCompound.getBoolean("diamond");
-            }
-            NBTTagCompound stonemasonCompound = deliveryCompound.getCompoundTag("stonemason");
-            {
-                stonemasonStone = stonemasonCompound.getBoolean("stone");
-                stonemasonStone = stonemasonCompound.getBoolean("sand");
-                stonemasonNetherrack = stonemasonCompound.getBoolean("netherrack");
-                stonemasonQuartz = stonemasonCompound.getBoolean("quartz");
-            }
-            NBTTagCompound guardCompound = deliveryCompound.getCompoundTag("guard");
-            {
-                guardArmor = guardCompound.getBoolean("armor");
-                guardWeapon = guardCompound.getBoolean("weapon");
-            }
-            citizenVisit = deliveryCompound.getBoolean("citizen");
-        }
+
+        NBTTagCompound blacksmithCompound = deliveryCompound.getCompoundTag("blacksmith");
+        blacksmithGold = blacksmithCompound.getBoolean("gold");
+        blacksmithDiamond = blacksmithCompound.getBoolean("diamond");
+
+        NBTTagCompound stonemasonCompound = deliveryCompound.getCompoundTag("stonemason");
+        stonemasonStone = stonemasonCompound.getBoolean("stone");
+        stonemasonStone = stonemasonCompound.getBoolean("sand");
+        stonemasonNetherrack = stonemasonCompound.getBoolean("netherrack");
+        stonemasonQuartz = stonemasonCompound.getBoolean("quartz");
+
+        NBTTagCompound guardCompound = deliveryCompound.getCompoundTag("guard");
+        guardArmor = guardCompound.getBoolean("armor");
+        guardWeapon = guardCompound.getBoolean("weapon");
+
+        citizenVisit = deliveryCompound.getBoolean("citizen");
     }
 
     @Override
@@ -78,32 +74,26 @@ public class TileEntityHutWarehouse extends TileEntityHutWorker
         super.writeToNBT(compound);
 
         NBTTagCompound deliveryCompound = new NBTTagCompound();
-        {
-            NBTTagCompound blacksmithCompound = new NBTTagCompound();
-            {
-                blacksmithCompound.setBoolean("gold", blacksmithGold);
-                blacksmithCompound.setBoolean("diamond", blacksmithDiamond);
-            }
-            deliveryCompound.setTag("blacksmith", blacksmithCompound);
 
-            NBTTagCompound stonemasonCompound = new NBTTagCompound();
-            {
-                stonemasonCompound.setBoolean("stone", stonemasonStone);
-                stonemasonCompound.setBoolean("sand", stonemasonStone);
-                stonemasonCompound.setBoolean("netherrack", stonemasonNetherrack);
-                stonemasonCompound.setBoolean("quartz", stonemasonQuartz);
-            }
-            deliveryCompound.setTag("stonemason", stonemasonCompound);
+        NBTTagCompound blacksmithCompound = new NBTTagCompound();
+        blacksmithCompound.setBoolean("gold", blacksmithGold);
+        blacksmithCompound.setBoolean("diamond", blacksmithDiamond);
+        deliveryCompound.setTag("blacksmith", blacksmithCompound);
 
-            NBTTagCompound guardCompound = new NBTTagCompound();
-            {
-                guardCompound.setBoolean("armor", guardArmor);
-                guardCompound.setBoolean("weapon", guardWeapon);
-            }
-            deliveryCompound.setTag("guard", guardCompound);
+        NBTTagCompound stonemasonCompound = new NBTTagCompound();
+        stonemasonCompound.setBoolean("stone", stonemasonStone);
+        stonemasonCompound.setBoolean("sand", stonemasonStone);
+        stonemasonCompound.setBoolean("netherrack", stonemasonNetherrack);
+        stonemasonCompound.setBoolean("quartz", stonemasonQuartz);
+        deliveryCompound.setTag("stonemason", stonemasonCompound);
 
-            deliveryCompound.setBoolean("citizen", citizenVisit);
-        }
+        NBTTagCompound guardCompound = new NBTTagCompound();
+        guardCompound.setBoolean("armor", guardArmor);
+        guardCompound.setBoolean("weapon", guardWeapon);
+        deliveryCompound.setTag("guard", guardCompound);
+
+        deliveryCompound.setBoolean("citizen", citizenVisit);
+
         compound.setTag("delivery", deliveryCompound);
     }
 }
