4b114889c2668f69871dd8763305bcf0e0f2d55a
Chris Jacobson <mainecoon@gmail.com>
2014-11-16 22:38:58 -0800

    Rename GuiEntityCitizenWindow to GuiTestWindow; and windowEntityCitizen.xml to windowTest.xml;

--- src/main/java/com/minecolonies/client/gui/GuiTestWindow.java
+++ src/main/java/com/minecolonies/client/gui/GuiTestWindow.java
@@ -11,15 +11,15 @@ import com.minecolonies.lib.Constants;
 import com.minecolonies.network.messages.OpenInventoryMessage;
 import com.minecolonies.util.LanguageHandler;
 
-public class GuiEntityCitizenWindow extends Window implements Button.Handler
+public class GuiTestWindow extends Window implements Button.Handler
 {
     private static String INVENTORY_BUTTON_ID = "inventory";
 
     private CitizenData.View citizen;
 
-    public GuiEntityCitizenWindow(CitizenData.View citizen)
+    public GuiTestWindow(CitizenData.View citizen)
     {
-        super(Constants.MODID + ":" + "gui/windowEntityCitizen.xml");
+        super(Constants.MODID + ":" + "gui/windowTestGUI.xml");
         this.citizen = citizen;
     }
 
@@ -30,8 +30,8 @@ public class GuiEntityCitizenWindow extends Window implements Button.Handler
             findPaneOfTypeByID("strength", Label.class).setLabel(LanguageHandler.format("com.minecolonies.gui.citizen.skills.strength", citizen.strength));
             findPaneOfTypeByID("stamina", Label.class).setLabel(LanguageHandler.format("com.minecolonies.gui.citizen.skills.stamina", citizen.stamina));
             findPaneOfTypeByID("wisdom", Label.class).setLabel(LanguageHandler.format("com.minecolonies.gui.citizen.skills.wisdom", citizen.wisdom));
-            findPaneOfTypeByID("intelligence", Label.class).setLabel(LanguageHandler.format("com.minecolonies.gui.citizen.skills.intelligence", citizen.intelligence));
-            findPaneOfTypeByID("charisma", Label.class).setLabel(LanguageHandler.format("com.minecolonies.gui.citizen.skills.charisma", citizen.charisma));
+            //findPaneOfTypeByID("intelligence", Label.class).setLabel(LanguageHandler.format("com.minecolonies.gui.citizen.skills.intelligence", citizen.intelligence));
+            //findPaneOfTypeByID("charisma", Label.class).setLabel(LanguageHandler.format("com.minecolonies.gui.citizen.skills.charisma", citizen.charisma));
 
             findPaneOfTypeByID("scrollgroup", ScrollingList.class).setDataProvider(
                 new ScrollingList.DataProvider() {
@@ -42,9 +42,23 @@ public class GuiEntityCitizenWindow extends Window implements Button.Handler
                     public void updateElement(int index, Pane pane)
                     {
                         pane.findPaneOfTypeByID("listlabel", Label.class).setLabel(String.format("#%d", index));
+                        ScrollingList scrollList = pane.findPaneOfTypeByID("scrollgroup2", ScrollingList.class);
+                        if (scrollList != null)
+                        {
+                            scrollList.setDataProvider(
+                                new ScrollingList.DataProvider() {
+                                    @Override
+                                    public int getElementCount() { return 5; }
+
+                                    @Override
+                                    public void updateElement(int index2, Pane pane)
+                                    {
+                                        pane.findPaneOfTypeByID("listlabel2", Label.class).setLabel(String.format("%c", 'A' + index2));
+                                    }
+                                });
+                        }
                     }
-                }
-            );
+                });
         }
         catch (NullPointerException exc) {}
     }
