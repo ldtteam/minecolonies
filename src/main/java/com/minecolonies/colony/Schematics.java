4b972ac0c230873eb92ba71ea0f69885b13706ff
Marvin Hofmann <marvin@marv-productions.de>
2016-02-01 21:24:51 +0100

    Fixed nested auto-closable issue

--- src/main/java/com/minecolonies/colony/Schematics.java
+++ src/main/java/com/minecolonies/colony/Schematics.java
@@ -9,7 +9,10 @@ import net.minecraft.block.Block;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.nio.file.*;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
 import java.util.stream.Stream;
 
@@ -18,12 +21,12 @@ import java.util.stream.Stream;
  */
 public class Schematics
 {
+    private static Map<String, List<String>> hutStyleMap = new HashMap<>();//Hut,Styles
+
     public static void init()
     {
         loadHutStyleMap();
     }
-
-    private static Map<String, List<String>> hutStyleMap = new HashMap<>();//Hut,Styles
 
     private static void loadHutStyleMap()
     {
@@ -33,39 +36,39 @@ public class Schematics
             System.out.println(uri.toString());
             Path basePath;
 
-            if(uri.getScheme().equals("jar"))
+            if (uri.getScheme().equals("jar"))
             {
-                try(FileSystem fileSystem = FileSystems.newFileSystem(uri, Collections.<String, Object>emptyMap()))
-                {
-                    basePath = fileSystem.getPath("/assets/minecolonies/schematics/");
-                }
+                basePath = FileSystems.newFileSystem(uri, Collections.<String, Object>emptyMap()).getPath(
+                        "/assets/minecolonies/schematics/");
             }
             else
             {
                 basePath = Paths.get(uri);
             }
-            try(Stream<Path> walk = Files.walk(basePath))
+            try (Stream<Path> walk = Files.walk(basePath))
             {
 
                 Iterator<Path> it = walk.iterator();
 
-                while(it.hasNext())
+                while (it.hasNext())
                 {
                     Path path = it.next();
 
-                    if(path.toString().endsWith("1.schematic"))
+                    if (path.toString().endsWith("1.schematic"))
                     {
                         String hutpath = path.getFileName().toString();
                         String hut = hutpath.substring(0, hutpath.length() - 11);
                         String style = path.getParent().getFileName().toString();
 
-                        if(Block.getBlockFromName(Constants.MOD_ID + ":blockHut" + hut) == null)
+                        if (Block.getBlockFromName(Constants.MOD_ID + ":blockHut" + hut) == null)
                         {
-                            MineColonies.logger.warn(String.format("Malformed schematic name: %s/%s ignoring file", style, hut));
+                            MineColonies.logger.warn(String.format("Malformed schematic name: %s/%s ignoring file",
+                                                                   style,
+                                                                   hut));
                             continue;
                         }
 
-                        if(!hutStyleMap.containsKey(hut))
+                        if (!hutStyleMap.containsKey(hut))
                         {
                             hutStyleMap.put(hut, new ArrayList<String>());
                         }
@@ -75,7 +78,7 @@ public class Schematics
             }
 
         }
-        catch(IOException | URISyntaxException e)
+        catch (IOException | URISyntaxException e)
         {
             MineColonies.logger.error("Error loading Schematic directory. Things will break!");
         }
