851e320fc45f208d4aa6b812da1e247e2974c9d4
Marvin Hofmann <marvin@marv-productions.de>
2016-02-01 15:27:32 +0100

    added @Override and removed needless nested ifs to reduce complexity

--- src/main/java/com/minecolonies/entity/ai/EntityAICitizenWander.java
+++ src/main/java/com/minecolonies/entity/ai/EntityAICitizenWander.java
@@ -9,10 +9,10 @@ import net.minecraft.util.Vec3;
 public class EntityAICitizenWander extends EntityAIBase
 {
     private EntityCitizen citizen;
-    private double xPosition;
-    private double yPosition;
-    private double zPosition;
-    private double speed;
+    private double        xPosition;
+    private double        yPosition;
+    private double        zPosition;
+    private double        speed;
 
     public EntityAICitizenWander(EntityCitizen citizen, double speed)
     {
@@ -24,59 +24,49 @@ public class EntityAICitizenWander extends EntityAIBase
     /**
      * Returns whether the EntityAIBase should begin execution.
      */
+    @Override
     public boolean shouldExecute()
     {
-        if (citizen.getAge() >= 100)
+        if(citizen.getAge() >= 100)
         {
             return false;
         }
-        else if (citizen.getRNG().nextInt(120) != 0)
+        if(citizen.getRNG().nextInt(120) != 0)
         {
             return false;
         }
-        else
+        Vec3 vec3 = RandomPositionGenerator.findRandomTarget(citizen, 10, 7);
+        if(vec3 == null)
         {
-            Vec3 vec3 = RandomPositionGenerator.findRandomTarget(citizen, 10, 7);
-
-            if (vec3 == null)
-            {
-                return false;
-            }
-            else
-            {
-                if (vec3.yCoord < 0)
-                {
-                    vec3.yCoord = 0;
-                }
-
-                while (vec3.yCoord >= 1 && citizen.worldObj.isAirBlock(
-                        MathHelper.floor_double(vec3.xCoord),
-                        (int)vec3.yCoord - 1,
-                        MathHelper.floor_double(vec3.zCoord)))
-                {
-                    vec3.yCoord -= 1.0D;
-                }
-
-                while (!citizen.worldObj.isAirBlock(
-                        MathHelper.floor_double(vec3.xCoord),
-                        (int)vec3.yCoord,
-                        MathHelper.floor_double(vec3.zCoord)))
-                {
-                    vec3.yCoord += 1.0D;
-                }
-
-                this.xPosition = vec3.xCoord;
-                this.yPosition = vec3.yCoord;
-                this.zPosition = vec3.zCoord;
-
-                return true;
-            }
+            return false;
         }
+
+        if(vec3.yCoord < 0)
+        {
+            vec3.yCoord = 0;
+        }
+
+        while(vec3.yCoord >= 1 && citizen.worldObj.isAirBlock(MathHelper.floor_double(vec3.xCoord), (int) vec3.yCoord - 1, MathHelper.floor_double(vec3.zCoord)))
+        {
+            vec3.yCoord -= 1.0D;
+        }
+
+        while(!citizen.worldObj.isAirBlock(MathHelper.floor_double(vec3.xCoord), (int) vec3.yCoord, MathHelper.floor_double(vec3.zCoord)))
+        {
+            vec3.yCoord += 1.0D;
+        }
+
+        this.xPosition = vec3.xCoord;
+        this.yPosition = vec3.yCoord;
+        this.zPosition = vec3.zCoord;
+
+        return true;
     }
 
     /**
      * Returns whether an in-progress EntityAIBase should continue executing
      */
+    @Override
     public boolean continueExecuting()
     {
         return !citizen.getNavigator().noPath();
@@ -85,6 +75,7 @@ public class EntityAICitizenWander extends EntityAIBase
     /**
      * Execute a one shot task or start executing a continuous task
      */
+    @Override
     public void startExecuting()
     {
         citizen.getNavigator().tryMoveToXYZ(this.xPosition, this.yPosition, this.zPosition, this.speed);
