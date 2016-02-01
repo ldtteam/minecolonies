1bcedced0c4651c3211749ac6063b6ff96cb371f
Marvin Hofmann <marvin@marv-productions.de>
2016-02-01 14:39:06 +0100

    made ModBlocks final

--- src/main/java/com/minecolonies/blocks/ModBlocks.java
+++ src/main/java/com/minecolonies/blocks/ModBlocks.java
@@ -4,19 +4,20 @@ import net.minecraft.block.Block;
 
 public final class ModBlocks
 {
-    public static Block blockHutTownhall, blockHutCitizen, blockHutMiner, blockHutLumberjack, blockHutBaker, blockHutBuilder, blockHutWarehouse, blockHutBlacksmith, blockHutStonemason, blockHutFarmer;
+    public static final Block
+            blockHutTownhall = new BlockHutTownHall(),
+            blockHutCitizen = new BlockHutCitizen(),
+            blockHutMiner = new BlockHutMiner(),
+            blockHutLumberjack = new BlockHutLumberjack(),
+            blockHutBaker = new BlockHutBaker(),
+            blockHutBuilder = new BlockHutBuilder(),
+            blockHutWarehouse = new BlockHutWarehouse(),
+            blockHutBlacksmith = new BlockHutBlacksmith(),
+            blockHutStonemason = new BlockHutStonemason(),
+            blockHutFarmer = new BlockHutFarmer();
 
     public static void init()
     {
-        blockHutTownhall = new BlockHutTownHall();
-        blockHutCitizen = new BlockHutCitizen();
-        blockHutMiner = new BlockHutMiner();
-        blockHutLumberjack = new BlockHutLumberjack();
-        blockHutBaker = new BlockHutBaker();
-        blockHutBuilder = new BlockHutBuilder();
-        blockHutWarehouse = new BlockHutWarehouse();
-        blockHutBlacksmith = new BlockHutBlacksmith();
-        blockHutStonemason = new BlockHutStonemason();
-        blockHutFarmer = new BlockHutFarmer();
+        //TODO remove later when checked safe
     }
 }
