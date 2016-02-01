fdf26911196dbcc941d2b9ae1415ed0702de0436
Marvin Hofmann <marvin@marv-productions.de>
2016-02-01 13:44:15 +0100

    try with resource for FileSystem to auto-close it

--- src/main/java/com/minecolonies/colony/Schematics.java
+++ src/main/java/com/minecolonies/colony/Schematics.java
@@ -34,38 +34,38 @@ public class Schematics
             Path basePath;
             if (uri.getScheme().equals("jar"))
             {
-                FileSystem fileSystem = FileSystems.newFileSystem(uri, Collections.<String, Object>emptyMap());
-                basePath = fileSystem.getPath("/assets/minecolonies/schematics/");
+                try(FileSystem fileSystem = FileSystems.newFileSystem(uri, Collections.<String, Object>emptyMap()))
+                {
+                    basePath = fileSystem.getPath("/assets/minecolonies/schematics/");
+                }
             }
             else
             {
                 basePath = Paths.get(uri);
             }
 
-            Iterator<Path> it = Files.walk(basePath).iterator();
-            while (it.hasNext())
-            {
+            Iterator<Path> it =Files.walk(basePath).iterator();
+
+            while (it.hasNext()) {
                 Path path = it.next();
 
-                if(path.toString().endsWith("1.schematic"))
-                {
+                if (path.toString().endsWith("1.schematic")) {
                     String hutpath = path.getFileName().toString();
                     String hut = hutpath.substring(0, hutpath.length() - 11);
                     String style = path.getParent().getFileName().toString();
 
-                    if(Block.getBlockFromName(Constants.MOD_ID + ":blockHut" + hut) == null)
-                    {
+                    if (Block.getBlockFromName(Constants.MOD_ID + ":blockHut" + hut) == null) {
                         MineColonies.logger.warn(String.format("Malformed schematic name: %s/%s ignoring file", style, hut));
                         continue;
                     }
 
-                    if(!hutStyleMap.containsKey(hut))
-                    {
+                    if (!hutStyleMap.containsKey(hut)) {
                         hutStyleMap.put(hut, new ArrayList<String>());
                     }
                     hutStyleMap.get(hut).add(style);
                 }
             }
+
         }
         catch(IOException | URISyntaxException e)
         {
