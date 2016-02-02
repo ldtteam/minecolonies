f8f04a17a17a58531d25c10a878dedff221def8d
Marvin Hofmann <marvin@marv-productions.de>
2016-02-02 13:55:13 +0100

    MINECOL-22 #resolved #comment colony name added to text pushed IColony::getName up to interface to use it in eventhandler

--- src/main/java/com/minecolonies/event/EventHandler.java
+++ src/main/java/com/minecolonies/event/EventHandler.java
@@ -138,7 +138,8 @@ public class EventHandler
                     else if (!colony.getPermissions().hasPermission(player, Permissions.Action.PLACE_HUTS))
                     {
                         //  No permission to place hut in colony
-                        LanguageHandler.sendPlayerLocalizedMessage(player, "tile.blockHut.messageNoPermission");
+                        LanguageHandler.sendPlayerLocalizedMessage(player, "tile.blockHut.messageNoPermission",
+                                                                   colony.getName());
                         return false;
                     }
                 }
