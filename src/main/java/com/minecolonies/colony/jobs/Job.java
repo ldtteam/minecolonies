848e5671861cb3462d8c579b28d4d197675d79ea
Marvin Hofmann <marvin@marv-productions.de>
2016-02-02 15:08:58 +0100

    formatter ran on Job.java

--- src/main/java/com/minecolonies/colony/jobs/Job.java
+++ src/main/java/com/minecolonies/colony/jobs/Job.java
@@ -15,15 +15,11 @@ import java.util.*;
 
 public abstract class Job
 {
-    private final CitizenData citizen;
-    private List<ItemStack> itemsNeeded = new ArrayList<>();
-
-    //  Job and View Class Mapping
-    private static Map<String, Class<? extends Job>> nameToClassMap = new HashMap<String, Class<? extends Job>>();
-    private static Map<Class<? extends Job>, String> classToNameMap = new HashMap<Class<? extends Job>, String>();
-
-    private static final String TAG_TYPE         = "type";
+    private static final String TAG_TYPE = "type";
     private static final String TAG_ITEMS_NEEDED = "itemsNeeded";
+    //  Job and View Class Mapping
+    private static Map<String, Class<? extends Job>> nameToClassMap = new HashMap<>();
+    private static Map<Class<? extends Job>, String> classToNameMap = new HashMap<>();
 
     static
     {
@@ -33,6 +29,14 @@ public abstract class Job
         addMapping("Miner", JobMiner.class);
         addMapping("Lumberjack", JobLumberjack.class);
         addMapping("Farmer", JobFarmer.class);
+    }
+
+    private final CitizenData citizen;
+    private List<ItemStack> itemsNeeded = new ArrayList<>();
+
+    public Job(CitizenData entity)
+    {
+        citizen = entity;
     }
 
     /**
@@ -59,46 +63,17 @@ public abstract class Job
             }
             catch (NoSuchMethodException exception)
             {
-                throw new IllegalArgumentException("Missing constructor for type '" + name + "' when adding Job class mapping");
+                throw new IllegalArgumentException("Missing constructor for type '"
+                                                   + name
+                                                   + "' when adding Job class mapping");
             }
         }
     }
 
-    public Job(CitizenData entity)
-    {
-        citizen = entity;
-    }
-
-    /**
-     * Return a Localization label for the Job
-     * @return localization label String
-     */
-    public abstract String getName();
-
-    /**
-     * Get the RenderBipedCitizen.Model to use when the Citizen performs this job role.
-     * @return
-     */
-    public RenderBipedCitizen.Model getModel()
-    {
-        return RenderBipedCitizen.Model.CITIZEN;
-    }
-
-    /**
-     * Get the CitizenData that this Job belongs to
-     * @return CitizenData that owns this Job
-     */
-    public CitizenData getCitizen() { return citizen; }
-
-    /**
-     * Get the Colony that this Job is associated with (shortcut for getCitizen().getColony())
-     * @return
-     */
-    public Colony getColony() { return citizen.getColony(); }
-
     /**
      * Create a Job from saved NBTTagCompound data
-     * @param citizen The citizen that owns the Job
+     *
+     * @param citizen  The citizen that owns the Job
      * @param compound The NBTTagCompound containing the saved Job data
      * @return new Job created from the data, or null
      */
@@ -114,7 +89,7 @@ public abstract class Job
             if (oclass != null)
             {
                 Constructor<?> constructor = oclass.getDeclaredConstructor(CitizenData.class);
-                job = (Job)constructor.newInstance(citizen);
+                job = (Job) constructor.newInstance(citizen);
             }
         }
         catch (Exception exception)
@@ -130,20 +105,72 @@ public abstract class Job
             }
             catch (Exception ex)
             {
-                MineColonies.logger.error(String.format("A Job %s(%s) has thrown an exception during loading, its state cannot be restored. Report this to the mod author", compound.getString(TAG_TYPE), oclass.getName()), ex);
+                MineColonies.logger.error(String.format(
+                        "A Job %s(%s) has thrown an exception during loading, its state cannot be restored. Report "
+                        + "this to the mod author",
+                        compound.getString(TAG_TYPE),
+                        oclass.getName()), ex);
                 job = null;
             }
         }
         else
         {
-            MineColonies.logger.warn(String.format("Unknown Job type '%s' or missing constructor of proper format.", compound.getString(TAG_TYPE)));
+            MineColonies.logger.warn(String.format("Unknown Job type '%s' or missing constructor of proper format.",
+                                                   compound.getString(TAG_TYPE)));
         }
 
         return job;
     }
 
     /**
+     * Restore the Job from an NBTTagCompound
+     *
+     * @param compound NBTTagCompound containing saved Job data
+     */
+    public void readFromNBT(NBTTagCompound compound)
+    {
+        NBTTagList itemsNeededTag = compound.getTagList(TAG_ITEMS_NEEDED, Constants.NBT.TAG_COMPOUND);
+        for (int i = 0; i < itemsNeededTag.tagCount(); i++)
+        {
+            NBTTagCompound itemCompound = itemsNeededTag.getCompoundTagAt(i);
+            itemsNeeded.add(ItemStack.loadItemStackFromNBT(itemCompound));
+        }
+    }
+
+    /**
+     * Return a Localization label for the Job
+     *
+     * @return localization label String
+     */
+    public abstract String getName();
+
+    /**
+     * Get the RenderBipedCitizen.Model to use when the Citizen performs this job role.
+     *
+     * @return
+     */
+    public RenderBipedCitizen.Model getModel()
+    {
+        return RenderBipedCitizen.Model.CITIZEN;
+    }
+
+    /**
+     * Get the CitizenData that this Job belongs to
+     *
+     * @return CitizenData that owns this Job
+     */
+    public CitizenData getCitizen(){ return citizen; }
+
+    /**
+     * Get the Colony that this Job is associated with (shortcut for getCitizen().getColony())
+     *
+     * @return
+     */
+    public Colony getColony(){ return citizen.getColony(); }
+
+    /**
      * Save the Job to an NBTTagCompound
+     *
      * @param compound NBTTagCompound to save the Job to
      */
     public void writeToNBT(NBTTagCompound compound)
@@ -167,20 +194,6 @@ public abstract class Job
                 itemsNeededTag.appendTag(itemCompound);
             }
             compound.setTag(TAG_ITEMS_NEEDED, itemsNeededTag);
-        }
-    }
-
-    /**
-     * Restore the Job from an NBTTagCompound
-     * @param compound NBTTagCompound containing saved Job data
-     */
-    public void readFromNBT(NBTTagCompound compound)
-    {
-        NBTTagList itemsNeededTag = compound.getTagList(TAG_ITEMS_NEEDED, Constants.NBT.TAG_COMPOUND);
-        for(int i = 0; i < itemsNeededTag.tagCount(); i++)
-        {
-            NBTTagCompound itemCompound = itemsNeededTag.getCompoundTagAt(i);
-            itemsNeeded.add(ItemStack.loadItemStackFromNBT(itemCompound));
         }
     }
 
@@ -216,9 +229,9 @@ public abstract class Job
      */
     public void addItemNeeded(ItemStack stack)
     {
-        for(ItemStack neededItem : itemsNeeded)
+        for (ItemStack neededItem : itemsNeeded)
         {
-            if(stack.isItemEqual(neededItem))
+            if (stack.isItemEqual(neededItem))
             {
                 neededItem.stackSize += stack.stackSize;
                 return;
@@ -237,15 +250,15 @@ public abstract class Job
     public ItemStack removeItemNeeded(ItemStack stack)
     {
         ItemStack stackCopy = stack.copy();
-        for(ItemStack neededItem : itemsNeeded)
+        for (ItemStack neededItem : itemsNeeded)
         {
-            if(stackCopy.isItemEqual(neededItem))
+            if (stackCopy.isItemEqual(neededItem))
             {
                 int itemsToRemove = Math.min(neededItem.stackSize, stackCopy.stackSize);
                 neededItem.stackSize -= itemsToRemove;
                 stackCopy.stackSize -= itemsToRemove;
 
-                if(neededItem.stackSize == 0)
+                if (neededItem.stackSize == 0)
                 {
                     itemsNeeded.remove(neededItem);
                 }
@@ -262,14 +275,16 @@ public abstract class Job
      *
      * @param tasks EntityAITasks list to add tasks to
      */
-    public void addTasks(EntityAITasks tasks) {}
+    public void addTasks(EntityAITasks tasks){}
 
     /**
      * This method can be used to display the current status.
      * That a citizen is having.
+     *
      * @return Small string to display info in name tag
      */
-    public String getNameTagDescription() {
+    public String getNameTagDescription()
+    {
         return "";
     }
 }
