d034844eb926cb63d2b1850ab0ac3b7e6c9a0538
Colton Schlosser <cltnschlosser@gmail.com>
2016-03-20 16:32:46 -0500

    Revert "Fixes workers stuck in walls on callback"
    This reverts commit 1465f75f6a6f56a2ce063a731e226feaee67c4bb.

--- src/main/java/com/minecolonies/network/messages/RecallCitizenMessage.java
+++ src/main/java/com/minecolonies/network/messages/RecallCitizenMessage.java
@@ -5,17 +5,11 @@ import com.minecolonies.colony.ColonyManager;
 import com.minecolonies.colony.buildings.BuildingWorker;
 import com.minecolonies.entity.EntityCitizen;
 import com.minecolonies.util.ChunkCoordUtils;
-import com.minecolonies.util.Utils;
 import cpw.mods.fml.common.network.simpleimpl.IMessage;
 import cpw.mods.fml.common.network.simpleimpl.IMessageHandler;
 import cpw.mods.fml.common.network.simpleimpl.MessageContext;
 import io.netty.buffer.ByteBuf;
-import net.minecraft.block.Block;
-import net.minecraft.init.Blocks;
 import net.minecraft.util.ChunkCoordinates;
-import net.minecraft.world.World;
-
-import java.awt.geom.Point2D;
 
 /**
  * Recalls the citizen to the hut
@@ -63,15 +57,7 @@ public class RecallCitizenMessage implements IMessage, IMessageHandler<RecallCit
                 EntityCitizen citizen = building.getWorkerEntity();
                 if(citizen != null)
                 {
-                    World world = colony.getWorld();
-                    ChunkCoordinates spawnPoint = Utils.scanForBlockNearPoint(world, loc.posX, loc.posY+1, loc.posZ, 1, 0, 1, 2, Blocks.air, Blocks.snow_layer);
-
-                    //Search a close Block next to the spawnPoint to teleport in between these two blocks
-                    Point2D.Double point = Utils.getClearSpace(world, spawnPoint.posX, spawnPoint.posY, spawnPoint.posZ);
-
-                    double x = point.getX();
-                    double z = point.getY();
-                    citizen.setLocationAndAngles(x, spawnPoint.posY, z, citizen.rotationYaw, citizen.rotationPitch);
+                    citizen.setLocationAndAngles(loc.posX, loc.posY + 1, loc.posZ, citizen.rotationYaw, citizen.rotationPitch);//May need a different spot if this isn't safe
                 }
             }
         }
