ff0add7e3686bb66f24ecd167291fd6aaf9c8123
martijnwoudstra <martijnhenriwoudstra@gmail.com>
2016-03-25 12:54:59 +0100

    part 7

--- src/main/java/com/minecolonies/colony/permissions/IPermissions.java
+++ src/main/java/com/minecolonies/colony/permissions/IPermissions.java
@@ -6,7 +6,18 @@ import java.util.UUID;
 
 public interface IPermissions
 {
+    /**
+     * Returns whether the player has the permission for an action
+     * @param player    {@link EntityPlayer} player
+     * @param action    {@link com.minecolonies.colony.permissions.Permissions.Action} action
+     * @return          true if has permission, otherwise false
+     */
     boolean hasPermission(EntityPlayer player, Permissions.Action action);
 
-    Permissions.Rank getRank(UUID rank);
+    /**
+     * Get the rank of a UUID
+     * @param player    UUID to check rank of
+     * @return          Rank of the player
+     */
+    Permissions.Rank getRank(UUID player);
 }
