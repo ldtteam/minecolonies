9d6fb2b5fc3e343461e29264da85b97d12bc2340
Patrick <lige2@gmx.de>
2016-05-08 17:18:17 +0200

    check for Forge & Minecraft version

--- src/main/java/com/minecolonies/lib/Constants.java
+++ src/main/java/com/minecolonies/lib/Constants.java
@@ -5,6 +5,8 @@ public class Constants
     public static final String MOD_ID                = "minecolonies";
     public static final String MOD_NAME              = "MineColonies";
     public static final String VERSION               = "@VERSION@";
+    public static final String FORGE_VERSION 		 = "required-after:Forge@[11.15.1.1855,)";
+    public static final String MC_VERSION			 = "[1.8.9]";
     public static final String FINGERPRINT           = "@FINGERPRINT@";
     public static final String CLIENT_PROXY_LOCATION = "com.minecolonies.proxy.ClientProxy";
     public static final String SERVER_PROXY_LOCATION = "com.minecolonies.proxy.ServerProxy";
