23a84c114ddb420668eea3d0c7845dd3ed11bb82
Raycoms <ray.neiheiser@gmail.com>
2016-05-17 16:59:50 -0300

    Some changes for citizen fence gate opening

--- src/main/java/com/minecolonies/entity/ai/EntityAIGateInteract.java
+++ src/main/java/com/minecolonies/entity/ai/EntityAIGateInteract.java
@@ -37,10 +37,12 @@ public class EntityAIGateInteract extends EntityAIBase {
      * Constructor called to register the AI class with an entity
      * @param entityIn the registering entity
      */
-    public EntityAIGateInteract(EntityLiving entityIn) {
+    public EntityAIGateInteract(EntityLiving entityIn)
+    {
         this.gatePosition = BlockPos.ORIGIN;
         this.theEntity = entityIn;
-        if (!(entityIn.getNavigator() instanceof PathNavigateGround)) {
+        if (!(entityIn.getNavigator() instanceof PathNavigateGround))
+        {
             throw new IllegalArgumentException("Unsupported mob type for DoorInteractGoal");
         }
     }
@@ -49,20 +51,31 @@ public class EntityAIGateInteract extends EntityAIBase {
      * Checks if the Interaction should be executed
      * @return true or false depending on the conditions
      */
-    public boolean shouldExecute() {
-        if (!this.theEntity.isCollidedHorizontally) {
+    public boolean shouldExecute()
+    {
+        if (!this.theEntity.isCollidedHorizontally)
+        {
             return false;
-        } else {
+        }
+        else
+        {
             PathNavigateGround pathnavigateground = (PathNavigateGround) this.theEntity.getNavigator();
             PathEntity pathentity = pathnavigateground.getPath();
-            if (pathentity != null && !pathentity.isFinished() && pathnavigateground.getEnterDoors()) {
-                for (int i = 0; i < Math.min(pathentity.getCurrentPathIndex() + 2, pathentity.getCurrentPathLength()); ++i) {
+            if (pathentity != null && !pathentity.isFinished()  && pathnavigateground.getEnterDoors())
+            {
+                for (int i = 0; i < Math.min(pathentity.getCurrentPathIndex() + 2, pathentity.getCurrentPathLength()); ++i)
+                {
                     PathPoint pathpoint = pathentity.getPathPointFromIndex(i);
-                    this.gatePosition = new BlockPos(pathpoint.xCoord, pathpoint.yCoord + 1, pathpoint.zCoord);
-                    if (this.theEntity.getDistanceSq((double) this.gatePosition.getX(), this.theEntity.posY, (double) this.gatePosition.getZ()) <= 2.25D) {
-                        this.gateBlock = this.getBlockFence(this.gatePosition);
-                        if (this.gateBlock != null) {
-                            return true;
+                    for(int j=0;i<2;i++)
+                    {
+                        this.gatePosition = new BlockPos(pathpoint.xCoord, pathpoint.yCoord + j, pathpoint.zCoord);
+                        if (this.theEntity.getDistanceSq((double) this.gatePosition.getX(), this.theEntity.posY, (double) this.gatePosition.getZ()) <= 2.25D)
+                        {
+                            this.gateBlock = this.getBlockFence(this.gatePosition);
+                            if (this.gateBlock != null)
+                            {
+                                return true;
+                            }
                         }
                     }
                 }
@@ -70,7 +83,9 @@ public class EntityAIGateInteract extends EntityAIBase {
                 this.gatePosition = (new BlockPos(this.theEntity)).up();
                 this.gateBlock = this.getBlockFence(this.gatePosition);
                 return this.gateBlock != null;
-            } else {
+            }
+            else
+            {
                 return false;
             }
         }
@@ -80,14 +95,16 @@ public class EntityAIGateInteract extends EntityAIBase {
      * Checks if the execution is still ongoing
      * @return true or false
      */
-    public boolean continueExecuting() {
+    public boolean continueExecuting()
+    {
         return !this.hasStoppedFenceInteraction;
     }
 
     /**
      * Starts the execution
      */
-    public void startExecuting() {
+    public void startExecuting()
+    {
         this.hasStoppedFenceInteraction = false;
         this.entityPositionX = (float) ((double) ((float) this.gatePosition.getX() + 0.5F) - this.theEntity.posX);
         this.entityPositionZ = (float) ((double) ((float) this.gatePosition.getZ() + 0.5F) - this.theEntity.posZ);
@@ -96,11 +113,13 @@ public class EntityAIGateInteract extends EntityAIBase {
     /**
      * Updates the task
      */
-    public void updateTask() {
+    public void updateTask()
+    {
         float f = (float) ((double) ((float) this.gatePosition.getX() + 0.5F) - this.theEntity.posX);
         float f1 = (float) ((double) ((float) this.gatePosition.getZ() + 0.5F) - this.theEntity.posZ);
         float f2 = this.entityPositionX * f + this.entityPositionZ * f1;
-        if (f2 < 0.0F) {
+        if (f2 < 0.0F)
+        {
             this.hasStoppedFenceInteraction = true;
         }
 
@@ -111,8 +130,9 @@ public class EntityAIGateInteract extends EntityAIBase {
      * @param pos the position to be searched
      * @return fenceBlock or null
      */
-    private BlockFenceGate getBlockFence(BlockPos pos) {
-        Block block = this.theEntity.worldObj.getBlockState(pos).getBlock();
-        return block instanceof BlockFenceGate && block.getMaterial() == Material.wood ? (BlockFenceGate) block : null;
+    private BlockFenceGate getBlockFence(BlockPos pos)
+    {
+        Block block  = this.theEntity.worldObj.getBlockState(pos).getBlock();
+        return (block instanceof BlockFenceGate && block.getMaterial() == Material.wood ? (BlockFenceGate) block : null);
     }
 }
