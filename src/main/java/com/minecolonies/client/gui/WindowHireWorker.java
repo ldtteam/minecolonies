10b864109c4387f1a43f3d2fb309ac7237d5f330
Marvin Hofmann <marvin@marv-productions.de>
2016-05-25 11:43:45 +0200

    fixed some issues

--- src/main/java/com/minecolonies/client/gui/WindowHireWorker.java
+++ src/main/java/com/minecolonies/client/gui/WindowHireWorker.java
@@ -98,7 +98,6 @@ public class WindowHireWorker extends Window implements Button.Handler
     {
         citizens.clear();
         citizens.addAll(colony.getCitizens().values());
-        ArrayList<CitizenData.View> list = new ArrayList<>(citizens);
 
         //Removes all citizens which already have a job.
         citizens = colony.getCitizens().values().stream()
