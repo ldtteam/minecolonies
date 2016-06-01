e1cb69c0e0bc7a29e902b562d78cf721a4758732
Marvin Hofmann <marvin@marv-productions.de>
2016-06-01 14:51:22 +0200

    fixed unneeded import

--- src/main/java/com/minecolonies/entity/ai/citizen/fisherman/EntityAIWorkFisherman.java
+++ src/main/java/com/minecolonies/entity/ai/citizen/fisherman/EntityAIWorkFisherman.java
@@ -4,7 +4,6 @@ import com.minecolonies.colony.buildings.BuildingFisherman;
 import com.minecolonies.colony.jobs.JobFisherman;
 import com.minecolonies.entity.EntityCitizen;
 import com.minecolonies.entity.EntityFishHook;
-import com.minecolonies.entity.ai.basic.AbstractEntityAIBasic;
 import com.minecolonies.entity.ai.basic.AbstractEntityAIInteract;
 import com.minecolonies.entity.ai.util.AIState;
 import com.minecolonies.entity.ai.util.AITarget;
