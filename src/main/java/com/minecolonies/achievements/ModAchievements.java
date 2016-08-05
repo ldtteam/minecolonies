d0394600e6adeda8aad3943496f5f5057e4800a4
Isfirs <isfirs.pb@gmail.com>
2016-08-05 14:32:15 +0200

    Register Achievements

--- src/main/java/com/minecolonies/achievements/ModAchievements.java
+++ src/main/java/com/minecolonies/achievements/ModAchievements.java
@@ -1,7 +1,5 @@
 package com.minecolonies.achievements;
 
-import com.minecolonies.blocks.ModBlocks;
-import com.minecolonies.items.ModItems;
 import net.minecraft.stats.Achievement;
 import net.minecraftforge.common.AchievementPage;
 
@@ -15,11 +13,11 @@ public class ModAchievements
 {
 
     // Achievements
-    public static Achievement achGetSupply      = new AchGetSupply("get.supply", "get.supply", -2, -2);
-    public static Achievement achWandOfbuilding = new AchWandOfBuilding("wandofbuilding", "wandofbuilding", 0, -2);
-    public static Achievement achTownhall       = new AchTownhall("townhall", "townhall", -1, 0);
-    public static Achievement achBuilder        = new AchBuilder("builder", "builder", 1, 0);
-    public static Achievement achPioneers = new AchPioneers("size.pioneer", "", 0, 0);
+    public static Achievement achGetSupply      = new AchGetSupply("get.supply", "get.supply", -2, -2).registerStat();
+    public static Achievement achWandOfbuilding = new AchWandOfBuilding("wandofbuilding", "wandofbuilding", 0, -2).registerStat();
+    public static Achievement achTownhall       = new AchTownhall("townhall", "townhall", -1, 0).registerStat();
+    public static Achievement achBuilder        = new AchBuilder("builder", "builder", 1, 0).registerStat();
+    public static Achievement achPioneers = new AchPioneers("size.pioneer", "", 0, 0).registerStat();
     public static Achievement achHdideout;
     public static Achievement achTown;
     public static Achievement achCity;
@@ -37,5 +35,9 @@ public class ModAchievements
     private ModAchievements()
     {
     }
+
+	public static void init() {
+		AchievementPage.registerAchievementPage(pageMineColonies);
+	}
 
 }
