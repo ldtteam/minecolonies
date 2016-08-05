35bce3158e35f9bfb96e48bde2b1ba3c432e4b9a
Isfirs <isfirs.pb@gmail.com>
2016-08-05 22:47:20 +0200

    Subclass for colony size achievements

---
+++ src/main/java/com/minecolonies/achievements/AbstractSizeAchievement.java
@@ -0,0 +1,33 @@
+package com.minecolonies.achievements;
+
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.stats.Achievement;
+
+public class AbstractSizeAchievement extends AbstractAchievement {
+	
+	public final int size;
+
+	public AbstractSizeAchievement(String id, String name, int offsetX, int offsetY, Item itemIcon,
+			Achievement parent, int size) {
+		super(id, name, offsetX, offsetY, itemIcon, parent);
+		
+		this.size = size;
+	}
+
+	public AbstractSizeAchievement(String id, String name, int offsetX, int offsetY, Block blockIcon,
+			Achievement parent, int size) {
+		super(id, name, offsetX, offsetY, blockIcon, parent);
+		
+		this.size = size;
+	}
+
+	public AbstractSizeAchievement(String id, String name, int offsetX, int offsetY, ItemStack itemStackIcon,
+			Achievement parent, int size) {
+		super(id, name, offsetX, offsetY, itemStackIcon, parent);
+		
+		this.size = size;
+	}
+
+}
