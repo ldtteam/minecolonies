3e90f9878ec0d1c39c4260e88e57e66836551c7d
Isfirs <isfirs.pb@gmail.com>
2016-08-05 15:26:35 +0200

    Renamed to match name policy

---
+++ src/main/java/com/minecolonies/blocks/AbstractBlockMineColonies.java
@@ -0,0 +1,59 @@
+package com.minecolonies.blocks;
+
+import com.minecolonies.achievements.ModAchievements;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.MapColor;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.BlockPos;
+import net.minecraft.world.World;
+
+/**
+ * Abstract {@link Block} wrapper.
+ * 
+ * @author Isfirs
+ * @since 0.1
+ */
+public abstract class AbstractBlockMineColonies extends Block {
+
+	/**
+	 * Parent constructor
+	 * 
+	 * @param materialIn
+	 * @see Block#Block(Material)
+	 */
+	public AbstractBlockMineColonies(Material materialIn) {
+		super(materialIn);
+	}
+	
+	/**
+	 * Parent constructor
+	 * 
+	 * @param blockMaterialIn
+	 * @param blockMapColorIn
+	 * @see Block#Block(Material, MapColor)
+	 */
+	public AbstractBlockMineColonies(Material blockMaterialIn, MapColor blockMapColorIn) {
+		super(blockMaterialIn, blockMapColorIn);
+	}
+	
+	@Override
+	public void onBlockPlacedBy(World worldIn, BlockPos pos, IBlockState state, EntityLivingBase placer,
+			ItemStack stack) {
+		if (placer instanceof EntityPlayer) {
+			EntityPlayer player = (EntityPlayer) placer;
+			Block block = state.getBlock();
+			
+			if (block == ModBlocks.blockHutTownHall) {
+				player.triggerAchievement(ModAchievements.achTownhall);
+			}
+		}
+		
+		super.onBlockPlacedBy(worldIn, pos, state, placer, stack);
+	}
+
+}
