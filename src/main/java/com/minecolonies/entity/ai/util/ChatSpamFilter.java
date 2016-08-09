7aa234795bd8834e03667fc3371053dedc7715c5
Marvin Hofmann <marvin@marv-productions.de>
2016-08-09 15:12:05 +0200

    added convenience methods for ticks passed

--- src/main/java/com/minecolonies/entity/ai/util/ChatSpamFilter.java
+++ src/main/java/com/minecolonies/entity/ai/util/ChatSpamFilter.java
@@ -32,11 +32,28 @@ public class ChatSpamFilter
      * BASE_TIMEOUT * 2^times
      */
     private static final   int POWER_TIMEOUT = 2;
+    /**
+     * The worker we send chats from.
+     */
     private final EntityCitizen worker;
+    /**
+     * Ticks to wait till we let a new chat through.
+     */
     private int    speechDelay       = 0;
+    /**
+     * The current chat message to compare incoming messages to.
+     */
     private String speechDelayString = "";
+    /**
+     * The number of times the current message was already send.
+     */
     private int    speechRepeat      = 0;
 
+    /**
+     * Create a new ChatSpamFilter for the worker.
+     *
+     * @param worker the worker who will sends chats through this filter.
+     */
     public ChatSpamFilter(final EntityCitizen worker)
     {
         this.worker = worker;
@@ -49,7 +66,18 @@ public class ChatSpamFilter
      */
     public void requestWithoutSpam(@NotNull String chat)
     {
-        talkWithoutSpam("entity.miner.messageNeedBlockAndItem", chat);
+        requestWithoutSpam(chat, 1);
+    }
+
+    /**
+     * Request an Item without spamming the chat.
+     *
+     * @param chat  the Item Name
+     * @param ticks the amount of ticks waited
+     */
+    public void requestWithoutSpam(@NotNull String chat, int ticks)
+    {
+        talkWithoutSpam("entity.miner.messageNeedBlockAndItem", ticks, chat);
     }
 
     /**
@@ -57,17 +85,18 @@ public class ChatSpamFilter
      * It will be shown in certain delays.
      * Helpful for requesting items.
      *
-     * @param key  the translation key
-     * @param chat the chat message
+     * @param key   the translation key
+     * @param ticks the amount of ticks waited
+     * @param chat  the chat message
      */
-    public void talkWithoutSpam(String key, String... chat)
+    public void talkWithoutSpam(String key, int ticks, String... chat)
     {
         String curstring = key + Arrays.toString(chat);
         if (Objects.equals(speechDelayString, curstring))
         {
             if (speechDelay > 0)
             {
-                speechDelay--;
+                speechDelay -= ticks;
                 return;
             }
             speechRepeat++;
@@ -80,5 +109,18 @@ public class ChatSpamFilter
         worker.sendLocalizedChat(key, (Object[]) chat);
         speechDelayString = key + Arrays.toString(chat);
         speechDelay = Math.min((int) (BASE_TIMEOUT * Math.pow(POWER_TIMEOUT, speechRepeat)), MAX_TIMEOUT);
+    }
+
+    /**
+     * Send a chat message as often as you like.
+     * It will be shown in certain delays.
+     * Helpful for requesting items.
+     *
+     * @param key  the translation key
+     * @param chat the chat message
+     */
+    public void talkWithoutSpam(String key, String... chat)
+    {
+        talkWithoutSpam(key, 1, chat);
     }
 }
