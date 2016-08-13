a722de2148b3da036e8f96762f6c946556b2e025
Marvin Hofmann <marvin@marv-productions.de>
2016-08-13 17:31:34 +0200

    Update dropbox.sh

--- dropbox.sh
+++ dropbox.sh
@@ -1,9 +1,5 @@
 cat > ~/.dropbox_uploader <<EOL
-APPKEY=${APPKEY}
-APPSECRET=${APPSECRET}
-ACCESS_LEVEL=${ACCESS_LEVEL}
 OAUTH_ACCESS_TOKEN=${OAUTH_ACCESS_TOKEN}
-OAUTH_ACCESS_TOKEN_SECRET=${OAUTH_ACCESS_TOKEN_SECRET}
 EOL
 
 curl "https://raw.githubusercontent.com/andreafabrizi/Dropbox-Uploader/master/dropbox_uploader.sh" -o ~/dropbox_uploader.sh
