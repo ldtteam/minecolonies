92617f8ba36990bc4f7c110662d51fac8d6e9648
Marvin Hofmann <marvin@marv-productions.de>
2016-08-13 17:49:38 +0200

    Update dropbox.sh

--- dropbox.sh
+++ dropbox.sh
@@ -1,3 +1,4 @@
+rm -f ~/.dropbox_uploader
 echo "OAUTH_ACCESS_TOKEN=$OAUTH_ACCESS_TOKEN" > ~/.dropbox_uploader
 
 curl "https://raw.githubusercontent.com/andreafabrizi/Dropbox-Uploader/master/dropbox_uploader.sh" -o ~/dropbox_uploader.sh
