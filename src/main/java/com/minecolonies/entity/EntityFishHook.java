99ea05d7489df5e358d557152fe6a67a77c05e29
Marvin Hofmann <marvin@marv-productions.de>
2016-08-18 15:11:53 +0200

    Update EntityFishHook.java

--- src/main/java/com/minecolonies/entity/EntityFishHook.java
+++ src/main/java/com/minecolonies/entity/EntityFishHook.java
@@ -736,7 +736,6 @@ public final class EntityFishHook extends Entity
      * @param citizen the fisherman getting the loot
      * @return an ItemStack randomly from the loot table
      */
-    @SuppressWarnings("unchecked")
     private ItemStack getFishingLoot(final EntityCitizen citizen)
     {
         //Reduce random to get more fish drops
