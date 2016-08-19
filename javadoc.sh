1e2e5cd45e0ba53fd92670c1c24d21298a2199dc
Marvin Hofmann <marvin@marv-productions.de>
2016-08-19 11:10:37 +0200

    Update javadoc.sh

--- javadoc.sh
+++ javadoc.sh
@@ -1,5 +1,7 @@
 
-
+curl -H "Content-Type: application/json" -H "authToken: $GITHUB_TOKEN" \
+      "https://api.github.com/repos/Minecolonies/minecolonies/commits/$TRAVIS_COMMIT/statuses"
+      
 
 ./gradlew javadoc
 
