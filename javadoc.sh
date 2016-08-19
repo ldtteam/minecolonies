5aae234a34bc65a51a073e6a99e12e890e61d574
Marvin Hofmann <marvin@marv-productions.de>
2016-08-19 12:01:52 +0200

    updated auth tokes

--- javadoc.sh
+++ javadoc.sh
@@ -1,8 +1,9 @@
+#!/bin/bash
 
 echo "Javadoc-testing commit $TRAVIS_COMMIT"
 
-curl -H "Content-Type: application/json" -H "authToken: $GITHUB_TOKEN" \
-      --request POST --user "MinecoloniesSonar" \
+curl -H "Content-Type: application/json" -H "Authorization: token $GITHUB_TOKEN" \
+      --request POST \
       --data '{"state":"pending",
       "target_url":"https://travis-ci.org/Minecolonies/minecolonies/builds/$TRAVIS_BUILD_ID",
       "description":"Javadoc build status",
