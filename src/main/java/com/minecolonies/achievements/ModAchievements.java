dcaa69b0f26664d8727a5ccff3f8a0ef57e9f080
Isfirs <isfirs.pb@gmail.com>
2016-08-20 12:23:04 +0200

    Added Dots to comments

--- src/main/java/com/minecolonies/achievements/ModAchievements.java
+++ src/main/java/com/minecolonies/achievements/ModAchievements.java
@@ -17,29 +17,29 @@ public final class ModAchievements
 {
 
     /**
-     * Size to achieve {@link ModAchievements#achievementSizeSettlement}
+     * Size to achieve {@link ModAchievements#achievementSizeSettlement}.
      */
     public static final int ACHIEVEMENT_SIZE_SETTLEMENT = 5;
 
     /**
-     * Size to achieve {@link ModAchievements#achievementSizeTown}
+     * Size to achieve {@link ModAchievements#achievementSizeTown}.
      */
     public static final int ACHIEVEMENT_SIZE_TOWN = 10;
 
     /**
-     * Size to achieve {@link ModAchievements#achievementSizeCity}
+     * Size to achieve {@link ModAchievements#achievementSizeCity}.
      */
     public static final int ACHIEVEMENT_SIZE_CITY = 20;
 
     // Achievements
     /**
-     * Place a supply chest
+     * Place a supply chest.
      */
     public static final Achievement achievementGetSupply      = new MineColoniesAchievement("supply", "supply", -2, -2,
                                                                                             ModItems.supplyChest, null)
             .registerStat();
     /**
-     * Use the building tool
+     * Use the building tool.
      */
     public static final Achievement achievementWandOfbuilding = new MineColoniesAchievement("wandofbuilding", "wandofbuilding", 0, -2,
                                                                                             ModItems.buildTool, achievementGetSupply)
@@ -47,72 +47,72 @@ public final class ModAchievements
 
     // Buildings
     /**
-     * Place a townhall
+     * Place a townhall.
      */
     public static final Achievement achievementBuildingTownhall = new MineColoniesAchievement("townhall", "townhall", -2, 0,
                                                                                               ModBlocks.blockHutTownHall, achievementGetSupply)
             .registerStat();
 
     /**
-     * Upgrade a builder to lv 1
+     * Upgrade a builder to lv 1.
      */
     public static final Achievement achievementBuildingBuilder   = new MineColoniesAchievement("upgrade.builder.first", "upgrade.builder.first", 0, 1,
                                                                                                ModBlocks.blockHutBuilder, achievementBuildingTownhall)
             .registerStat();
     /**
-     * Max out a builder
+     * Max out a builder.
      */
     public static final Achievement achievementUpgradeBuilderMax = new MineColoniesAchievement("upgrade.builder.max", "upgrade.builder.max", 2, 1,
                                                                                                ModBlocks.blockHutBuilder, achievementBuildingBuilder)
             .registerStat();
 
     /**
-     * Upgrade a builder to lv 1
+     * Upgrade a builder to lv 1.
      */
     public static final Achievement achievementBuildingColonist   = new MineColoniesAchievement("upgrade.colonist.first", "upgrade.colonist.first", 0, 2,
                                                                                                 ModBlocks.blockHutCitizen, achievementGetSupply)
             .registerStat();
     /**
-     * Max out a builder
+     * Max out a builder.
      */
     public static final Achievement achievementUpgradeColonistMax = new MineColoniesAchievement("upgrade.colonist.max", "upgrade.colonist.max", 2, 2,
                                                                                                 ModBlocks.blockHutCitizen, achievementBuildingColonist)
             .registerStat();
 
     /**
-     * Upgrade a lumberjack to lv 1
+     * Upgrade a lumberjack to lv 1.
      */
     public static final Achievement achievementBuildingLumberjack   = new MineColoniesAchievement("upgrade.lumberjack.first", "upgrade.lumberjack.first", 0, 3,
                                                                                                   ModBlocks.blockHutLumberjack, achievementGetSupply)
             .registerStat();
     /**
-     * Max out a lumberjack
+     * Max out a lumberjack.
      */
     public static final Achievement achievementUpgradeLumberjackMax = new MineColoniesAchievement("upgrade.lumberjack.max", "upgrade.lumberjack.max", 2, 3,
                                                                                                   ModBlocks.blockHutLumberjack, achievementBuildingLumberjack)
             .registerStat();
 
     /**
-     * Upgrade a miner to lv 1
+     * Upgrade a miner to lv 1.
      */
     public static final Achievement achievementBuildingMiner   = new MineColoniesAchievement("upgrade.miner.first", "upgrade.miner.first", 0, 4,
                                                                                              ModBlocks.blockHutMiner, achievementGetSupply)
             .registerStat();
     /**
-     * Max out a miner
+     * Max out a miner.
      */
     public static final Achievement achievementUpgradeMinerMax = new MineColoniesAchievement("upgrade.miner.max", "upgrade.miner.max", 2, 4,
                                                                                              ModBlocks.blockHutMiner, achievementBuildingMiner)
             .registerStat();
 
     /**
-     * Upgrade a fisher to lv 1
+     * Upgrade a fisher to lv 1.
      */
     public static final Achievement achievementBuildingFisher   = new MineColoniesAchievement("upgrade.fisher.first", "upgrade.fisher.first", 0, 5,
                                                                                               ModBlocks.blockHutFisherman, achievementGetSupply)
             .registerStat();
     /**
-     * Max out a fisher
+     * Max out a fisher.
      */
     public static final Achievement achievementUpgradeFisherMax = new MineColoniesAchievement("upgrade.fisher.max", "upgrade.fisher.max", 2, 5,
                                                                                               ModBlocks.blockHutFisherman, achievementBuildingFisher)
@@ -120,19 +120,19 @@ public final class ModAchievements
 
     // Sizes
     /**
-     * Reach {@link ModAchievements#ACHIEVEMENT_SIZE_SETTLEMENT} citizens
+     * Reach {@link ModAchievements#ACHIEVEMENT_SIZE_SETTLEMENT} citizens.
      */
     public static final Achievement achievementSizeSettlement = new MineColoniesAchievement("size.pioneer", "size.settlement", 2, -2,
                                                                                             Items.iron_ingot, null)
             .registerStat();
     /**
-     * Reach {@link ModAchievements#ACHIEVEMENT_SIZE_TOWN} citizens
+     * Reach {@link ModAchievements#ACHIEVEMENT_SIZE_TOWN} citizens.
      */
     public static final Achievement achievementSizeTown       = new MineColoniesAchievement("size.town", "size.town", 4, -2,
                                                                                             Items.gold_ingot, achievementSizeSettlement)
             .registerStat();
     /**
-     * Reach {@link ModAchievements#ACHIEVEMENT_SIZE_CITY} citizens
+     * Reach {@link ModAchievements#ACHIEVEMENT_SIZE_CITY} citizens.
      */
     public static final Achievement achievementSizeCity       = new MineColoniesAchievement("size.city", "size.city", 6, -2,
                                                                                             Items.diamond, achievementSizeTown)
@@ -140,7 +140,7 @@ public final class ModAchievements
 
     // Achievement pages#+
     /**
-     * The MineColonies achievement page
+     * The MineColonies achievement page.
      */
     public static final AchievementPage achievementPageMineColonies = new MineColoniesAchievementPage(
             Constants.MOD_NAME,
@@ -160,7 +160,7 @@ public final class ModAchievements
     /**
      * Init.
      * 
-     * Registers the page
+     * Registers the page.
      */
     public static void init()
     {
