1f386d4a9bc7d2c53616bea602304fe56928250b
Marvin Hofmann <marvin@marv-productions.de>
2016-08-26 10:50:43 +0200

    Update solder.sh

--- travis/solder.sh
+++ travis/solder.sh
@@ -4,7 +4,8 @@
 if [ "$TRAVIS_BRANCH" = "develop" ] || [ "$TRAVIS_BRANCH" = "master" ] || [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
     mkdir solder
     mv ./build/libs/*univ*.jar solder
-    ls -al solder
+    jarfile=$(ls -1 solder)
+    echo $jarfile
 fi
 
 exit 0
