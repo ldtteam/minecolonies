fd5f870af9152f6947e7112e9a396002bae4eff3
Marvin Hofmann <marvin@marv-productions.de>
2016-08-26 13:04:26 +0200

    Update solder.sh

--- travis/solder.sh
+++ travis/solder.sh
@@ -23,7 +23,15 @@ if [ "$TRAVIS_BRANCH" = "develop" ] || [ "$TRAVIS_BRANCH" = "master" ] || [ "$TR
         --data "mod-id=1&add-version=$JAR_VERSION&add-md5=" -H 'X-Requested-With: XMLHttpRequest' \
         $SOLDER_URL/mod/add-version -o /dev/null
         
-    MODPACK_VERSION=$(curl -sL -w "%{url_effective}\\n" -b cookies.txt -c cookies.txt --data "version=$JAR_VERSION&minecraft=1.8.9&clone=33&java-version=1.8" -H 'X-Requested-With: XMLHttpRequest' $SOLDER_URL/modpack/add-build/1)
+    curl -sL -w "%{url_effective}\\n" -b cookies.txt -c cookies.txt \
+        --data "version=$JAR_VERSION&minecraft=1.8.9&clone=33&java-version=1.8" -H 'X-Requested-With: XMLHttpRequest' \
+        $SOLDER_URL/modpack/add-build/1 -o /dev/null | cut -d'/' -f6
+    
+    INSERT_TOKEN=$(curl -sL -b cookies.txt -c cookies.txt $SOLDER_URL/modpack/add-build/1 | grep token | cut -d'"' -f12)
+    
+    echo $INSERT_TOKEN
+    
+    MODPACK_VERSION=$(curl -sL -w "%{url_effective}\\n" -b cookies.txt -c cookies.txt --data "_token=$INSERT_TOKEN&version=$JAR_VERSION&minecraft=1.8.9&clone=33&java-version=1.8" -H 'X-Requested-With: XMLHttpRequest' $SOLDER_URL/modpack/add-build/1  -o /dev/null | cut -d'/' -f6)
         
     echo $MODPACK_VERSION
 fi
