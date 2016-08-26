e3ed41a99880783108dc0a46a4266387dbc3b5e4
Marvin Hofmann <marvin@marv-productions.de>
2016-08-26 12:22:15 +0200

    Update solder.sh

--- travis/solder.sh
+++ travis/solder.sh
@@ -13,7 +13,15 @@ if [ "$TRAVIS_BRANCH" = "develop" ] || [ "$TRAVIS_BRANCH" = "master" ] || [ "$TR
     zip -r minecolonies.zip mods
     echo $(unzip -l minecolonies.zip)
     
-    curl -sL -w "CODE: %{http_code}\\n" -c cookies.txt -d "password=$SOLDER_WEB_PASS&email=$SOLDER_WEB_USER&login=Log In" $SOLDER_URL -o /dev/null
+    JAR_VERSION=$(echo $JAR_FILE | cut -d'-' -f4 | rev | cut -c5- | rev)
+    echo $JAR_VERSION
+    
+    curl -sL -w "CODE: %{http_code}\\n" -c cookies.txt \
+        -d "password=$SOLDER_WEB_PASS&email=$SOLDER_WEB_USER&login=Log In" $SOLDER_URL -o /dev/null
+    
+    curl -s -w "%{http_code}\\n" -b cookies.txt -c cookies.txt \
+        --data "mod-id=1&add-version=$JAR_VERSION&add-md5=" -H 'X-Requested-With: XMLHttpRequest' \
+        $SOLDER_URL/mod/add-version -o /dev/null
 fi
 
 exit 0
