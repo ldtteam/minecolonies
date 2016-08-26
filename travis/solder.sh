8e377db482563d45f46278cd06fe6fac1c5ebc6c
Marvin Hofmann <marvin@marv-productions.de>
2016-08-26 11:52:12 +0200

    Update solder.sh

--- travis/solder.sh
+++ travis/solder.sh
@@ -8,10 +8,9 @@ if [ "$TRAVIS_BRANCH" = "develop" ] || [ "$TRAVIS_BRANCH" = "master" ] || [ "$TR
     cd $SOLDER_DIR
     JAR_FILE=$(ls -1 .)
     echo $JAR_FILE
-    mkdir modpack
-    mkdir modpack/mods
-    cp $JAR_FILE modpack/mods/$JAR_FILE
-    zip -r minecolonies.zip modpack
+    mkdir mods
+    cp $JAR_FILE mods/$JAR_FILE
+    zip -r minecolonies.zip mods
     echo $(unzip -l minecolonies.zip)
     
     curl -sL -w "CODE: %{http_code}\\n" -c cookies.txt -d "password=$SOLDER_WEB_PASS&email=$SOLDER_WEB_USER&login=Log In" $SOLDER_URL -o /dev/null
