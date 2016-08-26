d0caf6a28f9871d18d10aed11e372876a3472b93
Raycoms <ray.neiheiser@gmail.com>
2016-08-26 16:36:28 -0300

    Hotfix builder.

--- src/main/java/com/minecolonies/entity/ai/citizen/builder/EntityAIStructureBuilder.java
+++ src/main/java/com/minecolonies/entity/ai/citizen/builder/EntityAIStructureBuilder.java
@@ -98,7 +98,7 @@ public class EntityAIStructureBuilder extends AbstractEntityAIStructure<JobBuild
 
         if(wo == null)
         {
-            resetTask();
+            cancelTask();
             return true;
         }
 
@@ -845,10 +845,8 @@ public class EntityAIStructureBuilder extends AbstractEntityAIStructure<JobBuild
     /**
      * Resets the builders current task.
      */
-    @Override
-    public void resetTask()
+    public void cancelTask()
     {
-        super.resetTask();
         job.setWorkOrder(null);
         workFrom = null;
         job.setSchematic(null);
