ef7b29c226868110607123126fc8056eec190a2d
Marvin Hofmann <marvin@marv-productions.de>
2016-08-26 11:27:50 +0200

    Update solder.sh

--- travis/solder.sh
+++ travis/solder.sh
@@ -5,13 +5,16 @@ SOLDER_DIR="solder"
 if [ "$TRAVIS_BRANCH" = "develop" ] || [ "$TRAVIS_BRANCH" = "master" ] || [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
     mkdir $SOLDER_DIR
     mv ./build/libs/*univ*.jar $SOLDER_DIR
-    JAR_FILE=$(ls -1 $SOLDER_DIR)
+    cd $SOLDER_DIR
+    JAR_FILE=$(ls -1 .)
     echo $JAR_FILE
-    mkdir $SOLDER_DIR/modpack
-    mkdir $SOLDER_DIR/modpack/mods
-    cp $SOLDER_DIR/$JAR_FILE $SOLDER_DIR/modpack/mods/$JAR_FILE
-    zip -r $SOLDER_DIR/minecolonies.zip $SOLDER_DIR/modpack
-    echo $(unzip -l $SOLDER_DIR/minecolonies.zip)
+    mkdir modpack
+    mkdir modpack/mods
+    cp $JAR_FILE modpack/mods/$JAR_FILE
+    zip -r minecolonies.zip modpack
+    echo $(unzip -l minecolonies.zip)
+    
+    curl -sL -w "CODE: %{http_code}\\n" -c cookies.txt -d "password=$SOLDER_WEB_PASS&email=$SOLDER_WEB_USER&login=Log In" $SOLDER_URL -o /dev/null
 fi
 
 exit 0
