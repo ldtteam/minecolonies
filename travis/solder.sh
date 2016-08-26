774ee102651ab252ee822144628598829fa70579
Marvin Hofmann <marvin@marv-productions.de>
2016-08-26 11:10:29 +0200

    Update solder.sh

--- travis/solder.sh
+++ travis/solder.sh
@@ -7,7 +7,7 @@ if [ "$TRAVIS_BRANCH" = "develop" ] || [ "$TRAVIS_BRANCH" = "master" ] || [ "$TR
     mv ./build/libs/*univ*.jar $SOLDER_DIR
     JAR_FILE=$(ls -1 $SOLDER_DIR)
     echo $JAR_FILE
-    mkdir $SOLDER_DIR/moddpack
+    mkdir $SOLDER_DIR/modpack
     mkdir $SOLDER_DIR/modpack/mods
     cp $SOLDER_DIR/$JAR_FILE $SOLDER_DIR/modpack/mods/$JAR_FILE
     zip -r $SOLDER_DIR/minecolonies.zip $SOLDER_DIR/modpack
