3d380045ce7b563e90d085aa2c2b150c95f165c7
Colton Schlosser <cltnschlosser@gmail.com>
2016-08-31 18:57:05 -0500

    Comment out blank lines

--- tools/fix-lang.sh
+++ tools/fix-lang.sh
@@ -1,1 +1,1 @@
-sed -i.bak 's/ = /=/g;s/\\//g' ../src/main/resources/assets/minecolonies/lang/*.lang && rm ../src/main/resources/assets/minecolonies/lang/*.bak
+sed -i.bak 's/ = /=/g;s/\\//g;s/^\(.*\)=$/#\1=/g;' ../src/main/resources/assets/minecolonies/lang/*.lang && rm ../src/main/resources/assets/minecolonies/lang/*.bak
