2ab2c36f3c3e18660322af32e2b59ca827428ac4
Raycoms <ray.neiheiser@gmail.com>
2016-09-06 12:46:21 -0300

    Check permission message.

--- src/main/java/com/minecolonies/network/messages/AssignFieldMessage.java
+++ src/main/java/com/minecolonies/network/messages/AssignFieldMessage.java
@@ -3,6 +3,7 @@ package com.minecolonies.network.messages;
 import com.minecolonies.colony.Colony;
 import com.minecolonies.colony.ColonyManager;
 import com.minecolonies.colony.buildings.BuildingFarmer;
+import com.minecolonies.colony.permissions.Permissions;
 import com.minecolonies.util.BlockPosUtil;
 import io.netty.buffer.ByteBuf;
 import net.minecraft.util.BlockPos;
@@ -69,6 +70,12 @@ public class AssignFieldMessage implements IMessage, IMessageHandler<AssignField
         final Colony colony = ColonyManager.getColony(message.colonyId);
         if (colony != null)
         {
+            //Verify player has permission to do edit permissions
+            if(!colony.getPermissions().hasPermission(ctx.getServerHandler().playerEntity, Permissions.Action.SEND_MESSAGES))
+            {
+                return null;
+            }
+
             final BuildingFarmer building = colony.getBuilding(message.buildingId, BuildingFarmer.class);
             if (building != null)
             {
