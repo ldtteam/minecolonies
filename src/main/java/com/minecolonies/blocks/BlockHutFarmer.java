9064f9121f61a64b66b11c92d7af86ab37100535
Raycoms <ray.neiheiser@gmail.com>
2016-09-06 15:08:37 -0300

    Remove unused imports

--- src/main/java/com/minecolonies/blocks/BlockHutFarmer.java
+++ src/main/java/com/minecolonies/blocks/BlockHutFarmer.java
@@ -1,16 +1,5 @@
 package com.minecolonies.blocks;
 
-import com.minecolonies.colony.Colony;
-import com.minecolonies.colony.ColonyManager;
-import com.minecolonies.tileentities.TileEntityColonyBuilding;
-import net.minecraft.block.state.IBlockState;
-import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.ItemStack;
-import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.BlockPos;
-import net.minecraft.world.World;
-
 /**
  * Hut for the Farmer.
  * No different from {@link AbstractBlockHut}
