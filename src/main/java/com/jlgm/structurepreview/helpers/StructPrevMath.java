9221582ca488d6e81704b7c3b514cd449451b5b2
JoseluGames <joselugamesofficial@gmail.com>
2016-09-29 16:05:08 +0200

    Fix structure rotation

--- src/main/java/com/jlgm/structurepreview/helpers/StructPrevMath.java
+++ src/main/java/com/jlgm/structurepreview/helpers/StructPrevMath.java
@@ -8,13 +8,13 @@ public class StructPrevMath {
     public static Rotation getRotationFromYaw(){
     	EnumFacing facing = Minecraft.getMinecraft().thePlayer.getHorizontalFacing();
     	if(facing == EnumFacing.NORTH)
-    		return Rotation.COUNTERCLOCKWISE_90;
-    	if(facing == EnumFacing.SOUTH)
-    		return Rotation.CLOCKWISE_90;
-    	if(facing == EnumFacing.WEST)
     		return Rotation.CLOCKWISE_180;
-    	if(facing == EnumFacing.EAST)
+    	if(facing == EnumFacing.SOUTH)
     		return Rotation.NONE;
+    	if(facing == EnumFacing.WEST)
+    		return Rotation.CLOCKWISE_90;
+    	if(facing == EnumFacing.EAST)
+    		return Rotation.COUNTERCLOCKWISE_90;
     	return Rotation.NONE;
     }
 }
