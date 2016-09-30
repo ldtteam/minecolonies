4ac262671a1db778e415c8d69569198a4ed09b99
Raycoms <ray.neiheiser@gmail.com>
2016-09-30 19:30:49 -0300

    Improves and facilitates the sound handling.

--- src/main/java/com/minecolonies/sounds/FishermanSounds.java
+++ src/main/java/com/minecolonies/sounds/FishermanSounds.java
@@ -20,7 +20,7 @@ public class FishermanSounds
     /**
      * Number of different sounds in this class.
      */
-    private static final int NUMBER_OF_SOUNDS = 19;
+    private static final int NUMBER_OF_SOUNDS = 2;
 
     /**
      * Chance to say a phrase.
@@ -37,28 +37,12 @@ public class FishermanSounds
      */
     public static class Female
     {
-        public static SoundEvent callingItADay;
-        public static SoundEvent cheers;
-        public static SoundEvent coughs;
-        public static SoundEvent weWillGo;
-        public static SoundEvent goodDayToYou;
-        public static SoundEvent grumbles;
-        public static SoundEvent haveAGoodDay;
-        public static SoundEvent hey;
-        public static SoundEvent ho_hum;
+        public static SoundEvent generalPhrases;
+        public static SoundEvent noises;
+
         public static SoundEvent iGotOne;
-        public static SoundEvent lousyWheather;
-        public static SoundEvent lovelyDay;
         public static SoundEvent needFishingRod;
-        public static SoundEvent notBitingToday;
         public static SoundEvent offToBed;
-        public static SoundEvent offToFish;
-        public static SoundEvent rowYourBoat;
-        public static SoundEvent sighs;
-        public static SoundEvent sniffles;
-        public static SoundEvent theresFishToCatch;
-        public static SoundEvent workWorkWork;
-        public static SoundEvent yawns;
     }
 
     /**
@@ -79,64 +63,14 @@ public class FishermanSounds
     {
         if(isFemale)
         {
+            //Leaving it as switch-case we may add further random sound categories here (Whistling, singing, etc).
             switch(rand.nextInt(NUMBER_OF_SOUNDS+1))
             {
                 case 1:
-                    SoundUtils.playSoundAtCitizenWithChance(worldIn, position, FishermanSounds.Female.callingItADay, PHRASE_CHANCE);
+                    SoundUtils.playSoundAtCitizenWithChance(worldIn, position, Female.generalPhrases, PHRASE_CHANCE);
                     break;
                 case 2:
-                    SoundUtils.playSoundAtCitizenWithChance(worldIn, position, Female.cheers, BASIC_SOUND_CHANCE);
-                    break;
-                case 3:
-                    SoundUtils.playSoundAtCitizenWithChance(worldIn, position, Female.coughs, BASIC_SOUND_CHANCE);
-                    break;
-                case 4:
-                    SoundUtils.playSoundAtCitizenWithChance(worldIn, position, Female.weWillGo, PHRASE_CHANCE);
-                    break;
-                case 5:
-                    SoundUtils.playSoundAtCitizenWithChance(worldIn, position, Female.goodDayToYou, PHRASE_CHANCE);
-                    break;
-                case 6:
-                    SoundUtils.playSoundAtCitizenWithChance(worldIn, position, Female.grumbles, BASIC_SOUND_CHANCE);
-                    break;
-                case 7:
-                    SoundUtils.playSoundAtCitizenWithChance(worldIn, position, Female.haveAGoodDay, PHRASE_CHANCE);
-                    break;
-                case 8:
-                    SoundUtils.playSoundAtCitizenWithChance(worldIn, position, Female.hey, BASIC_SOUND_CHANCE);
-                    break;
-                case 9:
-                    SoundUtils.playSoundAtCitizenWithChance(worldIn, position, Female.ho_hum, BASIC_SOUND_CHANCE);
-                    break;
-                case 10:
-                    SoundUtils.playSoundAtCitizenWithChance(worldIn, position, Female.lousyWheather, PHRASE_CHANCE);
-                    break;
-                case 11:
-                    SoundUtils.playSoundAtCitizenWithChance(worldIn, position, Female.lovelyDay, PHRASE_CHANCE);
-                    break;
-                case 12:
-                    SoundUtils.playSoundAtCitizenWithChance(worldIn, position, Female.notBitingToday, PHRASE_CHANCE);
-                    break;
-                case 13:
-                    SoundUtils.playSoundAtCitizenWithChance(worldIn, position, Female.offToFish, BASIC_SOUND_CHANCE);
-                    break;
-                case 14:
-                    SoundUtils.playSoundAtCitizenWithChance(worldIn, position, Female.rowYourBoat, PHRASE_CHANCE);
-                    break;
-                case 15:
-                    SoundUtils.playSoundAtCitizenWithChance(worldIn, position, Female.sighs, BASIC_SOUND_CHANCE);
-                    break;
-                case 16:
-                    SoundUtils.playSoundAtCitizenWithChance(worldIn, position, Female.sniffles, BASIC_SOUND_CHANCE);
-                    break;
-                case 17:
-                    SoundUtils.playSoundAtCitizenWithChance(worldIn, position, Female.theresFishToCatch, PHRASE_CHANCE);
-                    break;
-                case 18:
-                    SoundUtils.playSoundAtCitizenWithChance(worldIn, position, Female.workWorkWork, PHRASE_CHANCE);
-                    break;
-                case 19:
-                    SoundUtils.playSoundAtCitizenWithChance(worldIn, position, Female.yawns, BASIC_SOUND_CHANCE);
+                    SoundUtils.playSoundAtCitizenWithChance(worldIn, position, Female.noises, BASIC_SOUND_CHANCE);
                     break;
             }
 
