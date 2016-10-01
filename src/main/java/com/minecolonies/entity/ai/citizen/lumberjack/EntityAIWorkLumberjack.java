2577a8b530dff8c11d56afe8295f2797e3059b7f
Raycoms <ray.neiheiser@gmail.com>
2016-10-01 12:41:14 -0300

    backport 7 - lumberjack fixes

--- src/main/java/com/minecolonies/entity/ai/citizen/lumberjack/EntityAIWorkLumberjack.java
+++ src/main/java/com/minecolonies/entity/ai/citizen/lumberjack/EntityAIWorkLumberjack.java
@@ -114,7 +114,7 @@ public class EntityAIWorkLumberjack extends AbstractEntityAIInteract<JobLumberja
     /**
      * Vertical range in which the lumberjack picks up items
      */
-    private static final float RANGE_VERTICAL_PICKUP   = 15.0F;
+    private static final float RANGE_VERTICAL_PICKUP   = 3.0F;
     /**
      * How often should strength factor into the lumberjacks skill modifier.
      */
