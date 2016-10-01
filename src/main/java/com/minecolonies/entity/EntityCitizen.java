2ea0e3f66d263b32789f42511a4f885d7dc7ec1b
Raycoms <ray.neiheiser@gmail.com>
2016-10-01 16:27:32 -0300

    Fixes rants about rainy weather.

--- src/main/java/com/minecolonies/entity/EntityCitizen.java
+++ src/main/java/com/minecolonies/entity/EntityCitizen.java
@@ -630,11 +630,11 @@ public class EntityCitizen extends EntityAgeable implements INpc
             pickupItems();
             cleanupChatMessages();
             updateColonyServer();
-            if(worldObj.isDaytime())
+            if(worldObj.isDaytime() && !worldObj.isRaining())
             {
                 SoundUtils.playRandomSound(worldObj, this);
             }
-            else if(worldObj.isRaining() && 1 <=rand.nextInt(RANT_ABOUT_WEATHER_CHANCE))
+            else if(worldObj.isRaining() && 1 >=rand.nextInt(RANT_ABOUT_WEATHER_CHANCE))
             {
                 //todo add sounds of other workers as well.
                 final SoundEvent badWeather = isFemale() ? FishermanSounds.Female.badWeather : FishermanSounds.Male.badWeather;
