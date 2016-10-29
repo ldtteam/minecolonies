6a5a6b1a2a2b56bae00fc39c6409d6ec1b51ea8b
Marvin Hofmann <marvin@marv-productions.de>
2016-10-29 10:59:22 +0200

    added all schematics

--- src/main/java/com/minecolonies/colony/buildings/BuildingTownHall.java
+++ src/main/java/com/minecolonies/colony/buildings/BuildingTownHall.java
@@ -26,7 +26,7 @@ public class BuildingTownHall extends AbstractBuildingHut
     @Override
     public int getMaxBuildingLevel()
     {
-        return 4;
+        return 2;
     }
 
     public static class View extends AbstractBuildingHut.View
