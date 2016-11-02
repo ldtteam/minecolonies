b8d90dcd82874e1d87a69fe483a81438147d1361
Marvin Hofmann <marvin@marv-productions.de>
2016-11-02 10:48:37 +0100

    NotNull additions

--- src/main/java/com/crafting/Knowledge.java
+++ src/main/java/com/crafting/Knowledge.java
@@ -1,11 +1,12 @@
 package com.crafting;
 
-import com.minecolonies.util.InventoryFunctions;
 import com.minecolonies.util.InventoryUtils;
 import net.minecraft.inventory.InventoryCrafting;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.IRecipe;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 
@@ -14,22 +15,29 @@ import java.util.List;
  */
 public class Knowledge
 {
+    @NotNull
     private final IRecipe         recipe;
+    @NotNull
     private final List<ItemStack> requirements;
+    @NotNull
     private final ItemStack output;
 
 
 
-    public Knowledge(final InventoryCrafting craftingGrid, final IRecipe recipe, World world)
+    public Knowledge(@NotNull final InventoryCrafting craftingGrid, @NotNull final IRecipe recipe, @NotNull World world)
     {
         this(recipe, InventoryUtils.getInventoryAsList(craftingGrid), recipe.getRecipeOutput());
         if(!recipe.matches(craftingGrid, world)){
             throw new IllegalStateException("Recipe did not match!");
         }
+
     }
 
-    private Knowledge(final IRecipe recipe, final List<ItemStack> requirements, final ItemStack output)
+    private Knowledge(@NotNull final IRecipe recipe, @NotNull final List<ItemStack> requirements, @Nullable final ItemStack output)
     {
+        if(output == null){
+            throw new IllegalStateException("Recipe did not output anything!");
+        }
         this.recipe = recipe;
         this.requirements = requirements;
         this.output = output;
