4a252e3d7e72c5f0e59a18f8a7fff863f8331169
Marvin Hofmann <marvin@marv-productions.de>
2016-11-13 22:21:11 +0100

    next fix

--- src/main/java/com/minecolonies/entity/pathfinding/PathNavigate.java
+++ src/main/java/com/minecolonies/entity/pathfinding/PathNavigate.java
@@ -123,9 +123,10 @@ public class PathNavigate extends PathNavigateGround
 
         final Vec3d moveVector = getEntityPosition().addVector(newX, newY, newZ).subtract(getEntityPosition());
         final double moveLength = moveVector.lengthVector();
-        if (moveLength >= MAX_PATHING_LENGTH)
+        if (moveLength >= MAX_PATHING_LENGTH && !this.isUnableToReachDestination())
         {
             final Vec3d newMove = moveVector.scale(PATHING_INTERMEDIARY_LENGTH / moveLength).add(getEntityPosition());
+            originalDestination = new BlockPos(newX, newY, newZ);
             newX = MathHelper.floor_double(newMove.xCoord);
             newY = MathHelper.floor_double(newMove.yCoord);
             newZ = MathHelper.floor_double(newMove.zCoord);
