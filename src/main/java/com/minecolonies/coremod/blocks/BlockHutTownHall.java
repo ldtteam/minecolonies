584abb0dd2f92fef174e928231283631aae40abf
Raycoms <ray.neiheiser@gmail.com>
2016-12-12 10:24:20 -0200

    Fix placement bug

--- src/main/java/com/minecolonies/coremod/blocks/BlockHutTownHall.java
+++ src/main/java/com/minecolonies/coremod/blocks/BlockHutTownHall.java
@@ -7,6 +7,7 @@ import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.NotNull;
@@ -35,17 +36,19 @@ public class BlockHutTownHall extends AbstractBlockHut
     public void onBlockPlacedBy(
             @NotNull final World worldIn, @NotNull final BlockPos pos, final IBlockState state, final EntityLivingBase placer, final ItemStack stack)
     {
-        super.onBlockPlacedBy(worldIn, pos, state, placer, stack);
-
         if(worldIn.isRemote)
         {
             return;
         }
 
-        final Colony colony = ColonyManager.getClosestColony(worldIn, pos);
-        if (colony == null && placer instanceof EntityPlayer)
+        if(placer.getActiveHand().equals(EnumHand.MAIN_HAND))
         {
-            ColonyManager.createColony(worldIn, pos, (EntityPlayer) placer);
+            Colony colony = ColonyManager.getClosestColony(worldIn, pos);
+            if (colony == null && placer instanceof EntityPlayer)
+            {
+                ColonyManager.createColony(worldIn, pos, (EntityPlayer) placer);
+            }
         }
+        super.onBlockPlacedBy(worldIn, pos, state, placer, stack);
     }
 }
