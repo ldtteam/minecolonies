5a40f815667229930549e5c29df72123ebfdc5b4
Mário Fernandes <mgustavofernandes@gmail.com>
2016-12-17 18:24:00 +0000

    Update Utils.java

--- src/main/java/com/minecolonies/coremod/util/Utils.java
+++ src/main/java/com/minecolonies/coremod/util/Utils.java
@@ -1,8 +1,8 @@
 package com.minecolonies.coremod.util;
 
 import com.minecolonies.compatibility.Compatibility;
-import com.minecolonies.entity.coremod.EntityCitizen;
-import com.minecolonies.inventory.coremod.InventoryCitizen;
+import com.minecolonies.coremod.entity.EntityCitizen;
+import com.minecolonies.coremod.inventory.InventoryCitizen;
 import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.init.Blocks;
