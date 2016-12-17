6ca281edbd7d1dc89c972d0aa4cf810044678218
Mário Fernandes <mgustavofernandes@gmail.com>
2016-12-17 18:30:44 +0000

    remove import

--- src/main/java/com/minecolonies/coremod/util/Utils.java
+++ src/main/java/com/minecolonies/coremod/util/Utils.java
@@ -9,7 +9,6 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemSword;
 import net.minecraft.item.ItemTool;
 import net.minecraft.nbt.NBTTagList;
-import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.NotNull;
