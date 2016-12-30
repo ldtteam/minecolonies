c4e29f0f0a7ffe26f442ecea5ac9c1852f640eb5
nojo121 <nojo121@gmail.com>
2016-12-30 20:53:41 +0100

    get colony and citizen id now in a different class

--- src/main/java/com/minecolonies/coremod/commands/AbstractSingleCommand.java
+++ src/main/java/com/minecolonies/coremod/commands/AbstractSingleCommand.java
@@ -1,6 +1,8 @@
 package com.minecolonies.coremod.commands;
 
+import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommandSender;
+import net.minecraft.server.MinecraftServer;
 import org.jetbrains.annotations.NotNull;
 
 /**
