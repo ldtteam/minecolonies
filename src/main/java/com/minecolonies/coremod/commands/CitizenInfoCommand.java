12068869c539a1fd5cad2062c5e5abb9c84896a1
nojo121 <nojo121@gmail.com>
2016-12-30 21:58:49 +0100

    added test if colony id is made of characters

--- src/main/java/com/minecolonies/coremod/commands/CitizenInfoCommand.java
+++ src/main/java/com/minecolonies/coremod/commands/CitizenInfoCommand.java
@@ -4,18 +4,15 @@ import com.minecolonies.coremod.colony.CitizenData;
 import com.minecolonies.coremod.colony.Colony;
 import com.minecolonies.coremod.colony.ColonyManager;
 import com.minecolonies.coremod.entity.EntityCitizen;
-import com.minecolonies.coremod.commands.GetColonyAndCitizen;
 import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TextComponentString;
 import org.jetbrains.annotations.NotNull;
-
 import javax.annotation.Nullable;
 import java.util.Collections;
 import java.util.List;
-import java.util.UUID;
 
 /**
  * Show info of one citizen.
