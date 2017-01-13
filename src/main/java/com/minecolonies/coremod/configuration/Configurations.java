0495f07ea63ee30064283069c3d54713a5ebdeb5
xavierh <xavier.hervy@gmail.com>
2017-01-13 23:42:24 +0000

    Speed configuration for placing and mining blocks #455
    Allow to change the mining speed from the configuration file (blockMiningDelayModifier default to 1000)
    Allow to add a delay between placing block for the builder from the config file (to give a more natural feel) (builderBuildBlockDelay default to 0 i.e. no delay)

--- src/main/java/com/minecolonies/coremod/configuration/Configurations.java
+++ src/main/java/com/minecolonies/coremod/configuration/Configurations.java
@@ -10,6 +10,8 @@ public class Configurations
     public static       boolean allowInfiniteSupplyChests    = false;
     public static       int     citizenRespawnInterval       = 30;
     public static       boolean builderInfiniteResources     = false;
+    public static       int     builderBuildBlockDelay       = 0;
+    public static       int     blockMiningDelayModifier     = 1000;
 
     public static boolean enableColonyProtection = true;
     public static boolean turnOffExplosionsInColonies = true;
