3537f51f6abd7f017bbeb69f0c52077b6316cdde
Raycoms <ray.neiheiser@gmail.com>
2017-01-29 19:33:12 -0200

    Fix entity problem and fix workOrder log message

--- src/main/java/com/minecolonies/coremod/colony/workorders/WorkOrderBuild.java
+++ src/main/java/com/minecolonies/coremod/colony/workorders/WorkOrderBuild.java
@@ -66,7 +66,7 @@ public class WorkOrderBuild extends AbstractWorkOrder
 
         if (MinecraftServer.class.getResourceAsStream("/assets/" + Constants.MOD_ID + "/schematics/" + building.getStyle() + '/' + this.getUpgradeName() + ".nbt") == null)
         {
-            Log.getLogger().warn(String.format("StructureProxy in Style (%s) does not exist - switching to default", building.getStyle()));
+            Log.getLogger().warn(String.format("StructureProxy in Style (%s) does not exist - switching to wooden", building.getStyle()));
             this.structureName = DEFAULT_STYLE + '/' + this.getUpgradeName();
             return;
         }
