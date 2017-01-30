8062227e0718cb6e451567a45e7bb59cfd95e09c
xavierh <xavier.hervy@gmail.com>
2017-01-30 13:44:43 +0000

    Removed unused import

--- src/main/java/com/minecolonies/coremod/network/messages/MarkBuildingDirtyMessage.java
+++ src/main/java/com/minecolonies/coremod/network/messages/MarkBuildingDirtyMessage.java
@@ -1,26 +1,14 @@
 package com.minecolonies.coremod.network.messages;
 
-import com.minecolonies.coremod.MineColonies;
 import com.minecolonies.coremod.colony.Colony;
 import com.minecolonies.coremod.colony.ColonyManager;
 import com.minecolonies.coremod.colony.buildings.AbstractBuilding;
-import com.minecolonies.coremod.colony.permissions.Permissions;
 import com.minecolonies.coremod.util.BlockPosUtil;
-import com.minecolonies.coremod.util.InventoryUtils;
-import com.minecolonies.coremod.util.Log;
 import io.netty.buffer.ByteBuf;
 import net.minecraft.entity.player.EntityPlayerMP;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.tileentity.TileEntity;
-import net.minecraft.tileentity.TileEntityChest;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
-import net.minecraftforge.fml.common.network.ByteBufUtils;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
 import org.jetbrains.annotations.NotNull;
-
-
 import com.minecolonies.coremod.util.Log;
 
 /**
@@ -49,7 +37,7 @@ public class MarkBuildingDirtyMessage  extends AbstractMessage<MarkBuildingDirty
     }
 
     /**
-     * Creates a build request message.
+     * Creates a mark building dirty request message.
      *
      * @param building AbstractBuilding of the request.
      */
