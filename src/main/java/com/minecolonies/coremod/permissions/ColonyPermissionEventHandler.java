5b4241e2ae395db4983475e8307ce2a594d752aa
Raycoms <ray.neiheiser@gmail.com>
2017-02-04 19:56:59 -0200

    Hotfix/some bugs (#545)
    * Fix game crashing bug and let players attack mobs
    
    * Check if file exists
    
    * remove unused imports
    
    * change order of if
    
    * Coltons remark

--- src/main/java/com/minecolonies/coremod/permissions/ColonyPermissionEventHandler.java
+++ src/main/java/com/minecolonies/coremod/permissions/ColonyPermissionEventHandler.java
@@ -8,6 +8,7 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.monster.EntityMob;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemMonsterPlacer;
 import net.minecraft.item.ItemPotion;
@@ -373,7 +374,7 @@ public class ColonyPermissionEventHandler
     public void on(final AttackEntityEvent event)
     {
         final EntityPlayer playerIn = event.getEntityPlayer();
-        if (Configurations.enableColonyProtection && colony.isCoordInColony(playerIn.getEntityWorld(), playerIn.getPosition()))
+        if (Configurations.enableColonyProtection && !(event.getEntity() instanceof EntityMob) && colony.isCoordInColony(playerIn.getEntityWorld(), playerIn.getPosition()))
         {
             final Permissions.Rank rank = colony.getPermissions().getRank(playerIn);
 
