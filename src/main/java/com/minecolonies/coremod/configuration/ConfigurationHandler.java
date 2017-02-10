125cf0d83b32d0895e01ae23d32cb48e73c7d18b
nojo121 <nojo121@gmail.com>
2017-02-10 16:47:17 +0100

    Added configuration if builder should place it

--- src/main/java/com/minecolonies/coremod/configuration/ConfigurationHandler.java
+++ src/main/java/com/minecolonies/coremod/configuration/ConfigurationHandler.java
@@ -38,6 +38,7 @@ public final class ConfigurationHandler
         try
         {
             config.load();
+            builderPlaceConstructionTape = config.get(CATEGORY_GAMEPLAY, "placeConstructionTape", builderPlaceConstructionTape, "Should builder place construction tape").getBoolean();
             workingRangeTownHall = config.get(CATEGORY_GAMEPLAY, "workingRangeTownHall", workingRangeTownHall, "Colony size (radius)").getInt();
             townHallPadding = config.get(CATEGORY_GAMEPLAY, "townHallPadding", townHallPadding, "Empty space between town hall boundaries").getInt();
             supplyChests = config.get(CATEGORY_GAMEPLAY, "supplyChests", supplyChests, "Allow crafting of a Supply Chest").getBoolean();
