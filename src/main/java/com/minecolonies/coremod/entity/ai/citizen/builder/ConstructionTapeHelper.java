e3e90893ee7b832478e8afb7fa7fc3398b5b1e7c
nojo121 <nojo121@gmail.com>
2017-02-11 18:51:20 +0100

    lang + removed unused import

--- src/main/java/com/minecolonies/coremod/entity/ai/citizen/builder/ConstructionTapeHelper.java
+++ src/main/java/com/minecolonies/coremod/entity/ai/citizen/builder/ConstructionTapeHelper.java
@@ -17,7 +17,6 @@ import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.NotNull;
-import sun.font.TrueTypeFont;
 
 /**
  * Helper class to place and remove constructionTapes from the buildings.
