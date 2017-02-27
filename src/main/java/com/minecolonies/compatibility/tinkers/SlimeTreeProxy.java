e84c6a0be2353b6f354ff7ec30e4f4a8724ee0f5
nojo121 <nojo121@gmail.com>
2017-02-27 17:52:29 +0100

    sonar

--- src/main/java/com/minecolonies/compatibility/tinkers/SlimeTreeProxy.java
+++ src/main/java/com/minecolonies/compatibility/tinkers/SlimeTreeProxy.java
@@ -4,13 +4,13 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * This is the fallback for when tinkers is not present!
+ */
 public class SlimeTreeProxy
 {
-
-    private static final double SLIME_BLOCK_SLIPPERINESS = 0.5D;
-
     /**
-     * Check if tree is Slime Tree.
+     * This is the fallback for when tinkers is not present!
      *
      * @param world the world.
      * @param pos the position
@@ -18,9 +18,6 @@ public class SlimeTreeProxy
      */
     protected boolean checkForTinkersSlimeBlock(@NotNull final IBlockAccess world, @NotNull final BlockPos pos)
     {
-        /*
-        This is the fallback for when tinkers is not present!
-         */
         return false;
 
     }
