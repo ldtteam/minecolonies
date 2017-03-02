08b4b65f47b7734dc07effc44bf6bbfdccb04928
Colton Schlosser <cltnschlosser@gmail.com>
2017-03-02 11:27:27 -0600

    Add forgotten method

--- src/test/java/com/minecolonies/coremod/test/ReflectionUtil.java
+++ src/test/java/com/minecolonies/coremod/test/ReflectionUtil.java
@@ -21,4 +21,17 @@ public class ReflectionUtil
 
         field.set(object, newValue);
     }
+
+    public static void setStaticFinalField(Class clazz, String fieldName, Object newValue)
+            throws NoSuchFieldException, IllegalAccessException
+    {
+        Field field = clazz.getField(fieldName);
+        field.setAccessible(true);
+
+        Field modifiersField = Field.class.getDeclaredField("modifiers");
+        modifiersField.setAccessible(true);
+        modifiersField.setInt(field, field.getModifiers() & ~Modifier.FINAL);
+
+        field.set(null, newValue);
+    }
 }
