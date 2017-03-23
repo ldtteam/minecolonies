394d2a2b68dab6b765c8668a8493abc6458371a0
Raycoms <ray.neiheiser@gmail.com>
2017-03-23 10:34:47 -0300

    fix sonar issues

--- src/main/java/com/minecolonies/coremod/commands/HomeTeleportCommand.java
+++ src/main/java/com/minecolonies/coremod/commands/HomeTeleportCommand.java
@@ -1,10 +1,8 @@
 package com.minecolonies.coremod.commands;
 
-import com.minecolonies.coremod.configuration.Configurations;
 import com.minecolonies.coremod.util.TeleportToColony;
 import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommandSender;
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TextComponentString;
