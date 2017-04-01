73056059b0793f8a967c237bd0b22468f7e49922
Raycoms <ray.neiheiser@gmail.com>
2017-03-31 22:51:24 -0300

    Fix commands

--- src/main/java/com/minecolonies/coremod/commands/RandomTeleportCommand.java
+++ src/main/java/com/minecolonies/coremod/commands/RandomTeleportCommand.java
@@ -19,6 +19,7 @@ import java.util.List;
 import java.util.Random;
 
 import static com.minecolonies.coremod.commands.AbstractSingleCommand.Commands.COLONYTP;
+import static com.minecolonies.coremod.commands.AbstractSingleCommand.Commands.RTP;
 
 /**
  * this command is made to TP a player to a safe random spot that is not to close to another colony.
@@ -62,7 +63,8 @@ public class RandomTeleportCommand extends AbstractSingleCommand
             sender.getCommandSenderEntity().addChatMessage(new TextComponentString("Please have an admin raise the maxDistanceFromWorldSpawn number in config."));
             return;
         }
-        if (!canCommandSenderUseCommand(COLONYTP))
+
+        if (!canCommandSenderUseCommand(RTP))
         {
             sender.getCommandSenderEntity().addChatMessage(new TextComponentString("Not happenin bro!!, ask an OP to TP you."));
             return;
