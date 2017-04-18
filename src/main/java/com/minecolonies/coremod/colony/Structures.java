cc4a48ac5c927121d965f1d7516dafab7cb41581
xavierh <xavier.hervy@gmail.com>
2017-04-18 12:05:11 +0100

    missing separator constant

--- src/main/java/com/minecolonies/coremod/colony/Structures.java
+++ src/main/java/com/minecolonies/coremod/colony/Structures.java
@@ -204,11 +204,11 @@ public final class Structures
                 if (path.toString().endsWith(SCHEMATIC_EXTENSION))
                 {
                     String relativePath = path.toString().substring(basePath.toString().length()).split("\\" + SCHEMATIC_EXTENSION)[0];
-                    if (!path.getFileSystem().getSeparator().equals("/"))
+                    if (!path.getFileSystem().getSeparator().equals(SCHEMATICS_SEPARATOR))
                     {
-                        relativePath = relativePath.replace(path.getFileSystem().getSeparator(), "/");
+                        relativePath = relativePath.replace(path.getFileSystem().getSeparator(), SCHEMATICS_SEPARATOR);
                     }
-                    if (relativePath.startsWith("/"))
+                    if (relativePath.startsWith(SCHEMATICS_SEPARATOR))
                     {
                         relativePath = relativePath.substring(1);
                     }
