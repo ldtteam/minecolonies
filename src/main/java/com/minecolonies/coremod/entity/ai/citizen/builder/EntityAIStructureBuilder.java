b7673d73c96d104c41cba15a7d053a4e5557cafd
Raycoms <ray.neiheiser@gmail.com>
2017-04-30 16:09:46 -0300

    Hotfix/fix happiness (#905)
    * save happiness and don't devidi through 0
    
    * add constants
    
    * check if citizenData != null first
    
    * fix assets
    
    * another hotfix, new citizen cause crash if happiness 1
    
    * don't double request resources
    
    * Fix prboblem with incorrect amount of entities requesting

--- src/main/java/com/minecolonies/coremod/entity/ai/citizen/builder/EntityAIStructureBuilder.java
+++ src/main/java/com/minecolonies/coremod/entity/ai/citizen/builder/EntityAIStructureBuilder.java
@@ -187,17 +187,25 @@ public class EntityAIStructureBuilder extends AbstractEntityAIStructure<JobBuild
     @SuppressWarnings("squid:S135")
     private void requestMaterials()
     {
+        if (job.getWorkOrder().isRequested())
+        {
+            return;
+        }
+        
         while (job.getStructure().findNextBlock())
         {
             @Nullable final Template.BlockInfo blockInfo = job.getStructure().getBlockInfo();
             @Nullable final Template.EntityInfo entityInfo = job.getStructure().getEntityinfo();
 
+            if (entityInfo != null)
+            {
+                requestEntityToBuildingIfRequired(entityInfo);
+            }
+
             if (blockInfo == null)
             {
                 continue;
             }
-
-            requestEntityToBuildingIfRequired(entityInfo);
 
             @Nullable IBlockState blockState = blockInfo.blockState;
             @Nullable Block block = blockState.getBlock();
