e06fb65a5440ddb7a1737fb2030433e0094d8edf
Raycoms <ray.neiheiser@gmail.com>
2017-05-17 01:42:35 -0300

    Feature/builder pickup to 1.11 (#1000)
    * make builder pickup things
    
    extract from lj to interact
    
    * sonar fix cast to double
    
    * remove unused import
    
    * remove double guard code
    
    * unused import
    
    * Update doc
    
    * Refactor even smaller

--- src/main/java/com/minecolonies/coremod/entity/ai/basic/AbstractEntityAIInteract.java
+++ src/main/java/com/minecolonies/coremod/entity/ai/basic/AbstractEntityAIInteract.java
@@ -5,15 +5,19 @@ import com.minecolonies.coremod.configuration.Configurations;
 import com.minecolonies.coremod.util.*;
 import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.item.EntityItem;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.common.ForgeHooks;
 import net.minecraftforge.items.wrapper.InvWrapper;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * This is the base class of all worker AIs.
@@ -32,18 +36,58 @@ public abstract class AbstractEntityAIInteract<J extends AbstractJob> extends Ab
      * The amount of xp the entity gains per block mined.
      */
     private static final double XP_PER_BLOCK         = 0.05D;
+
     /**
      * The percentage of time needed if we are one level higher.
      */
     private static final double LEVEL_MODIFIER       = 0.85D;
-    /**
-     * The Multiplier to add to hand mining speed.
-     */
-    private static final int    HAND_MINING_MODIFIER = 10;
+
     /**
      * The minimum range the builder has to reach in order to construct or clear.
      */
     private static final int    MIN_WORKING_RANGE    = 12;
+
+    /**
+     * Range around the worker to pickup items.
+     */
+    private static final int   ITEM_PICKUP_RANGE       = 3;
+
+    /**
+     * Ticks to wait until discovering that is stuck.
+     */
+    private static final int   STUCK_WAIT_TICKS        = 20;
+
+    /**
+     * The amount of time to wait while walking to items.
+     */
+    private static final int   WAIT_WHILE_WALKING      = 5;
+
+    /**
+     * Horizontal range in which the worker picks up items.
+     */
+    private static final float RANGE_HORIZONTAL_PICKUP = 45.0F;
+
+    /**
+     * Vertical range in which the worker picks up items.
+     */
+    private static final float RANGE_VERTICAL_PICKUP   = 3.0F;
+
+    /**
+     * Number of ticks the worker is standing still.
+     */
+    private              int   stillTicks              = 0;
+
+    /**
+     * Used to store the path index
+     * to check if the worker is still walking.
+     */
+    private              int   previousIndex           = 0;
+
+    /**
+     * Positions of all items that have to be collected.
+     */
+    @Nullable
+    private List<BlockPos> items;
 
     /**
      * Creates the abstract part of the AI.
@@ -114,7 +158,7 @@ public abstract class AbstractEntityAIInteract<J extends AbstractJob> extends Ab
         final int fortune = Utils.getFortuneOf(tool);
 
         //get all item drops
-        final List<ItemStack> items = BlockPosUtil.getBlockDrops(world, blockToMine, fortune);
+        final List<ItemStack> localItems = BlockPosUtil.getBlockDrops(world, blockToMine, fortune);
 
         //if block in statistic then increment that statistic.
         triggerMinedBlock(blockToMine);
@@ -123,7 +167,7 @@ public abstract class AbstractEntityAIInteract<J extends AbstractJob> extends Ab
         worker.breakBlockWithToolInHand(blockToMine);
 
         //add the drops to the citizen
-        for (final ItemStack item : items)
+        for (final ItemStack item : localItems)
         {
             InventoryUtils.addItemStackToItemHandler(new InvWrapper(worker.getInventoryCitizen()), item);
         }
@@ -224,5 +268,108 @@ public abstract class AbstractEntityAIInteract<J extends AbstractJob> extends Ab
                         / (double) (worker.getHeldItemMainhand().getItem()
                                       .getStrVsBlock(worker.getHeldItemMainhand(),
                                         block.getDefaultState())));
+    }
+
+    /**
+     * Fill the list of the item positions to gather.
+     */
+    public void fillItemsList()
+    {
+        searchForItems(worker.getEntityBoundingBox().expand(RANGE_HORIZONTAL_PICKUP, RANGE_VERTICAL_PICKUP, RANGE_HORIZONTAL_PICKUP));
+    }
+
+    /**
+     * Search for all items around the worker.
+     * and store them in the items list.
+     * @param boundingBox the area to search.
+     */
+    public void searchForItems(final AxisAlignedBB boundingBox)
+    {
+        items = world.getEntitiesWithinAABB(EntityItem.class, boundingBox)
+                .stream()
+                .filter(item -> item != null && !item.isDead)
+                .map(BlockPosUtil::fromEntity)
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * Collect one item by walking to it.
+     */
+    public void gatherItems()
+    {
+        worker.setCanPickUpLoot(true);
+        if (worker.getNavigator().noPath())
+        {
+            final BlockPos pos = getAndRemoveClosestItemPosition();
+            worker.isWorkerAtSiteWithMove(pos, ITEM_PICKUP_RANGE);
+            return;
+        }
+        if (worker.getNavigator().getPath() == null)
+        {
+            setDelay(WAIT_WHILE_WALKING);
+            return;
+        }
+
+        final int currentIndex = worker.getNavigator().getPath().getCurrentPathIndex();
+        //We moved a bit, not stuck
+        if (currentIndex != previousIndex)
+        {
+            stillTicks = 0;
+            previousIndex = currentIndex;
+            return;
+        }
+
+        stillTicks++;
+        //Stuck for too long
+        if (stillTicks > STUCK_WAIT_TICKS)
+        {
+            //Skip this item
+            worker.getNavigator().clearPathEntity();
+            if(items != null && !items.isEmpty())
+            {
+                items.remove(0);
+            }
+        }
+    }
+
+    /**
+     * Reset the gathering items to null.
+     */
+    public void resetGatheringItems()
+    {
+        items = null;
+    }
+
+    /**
+     * Get the items to gather list.
+     * @return a copy of it.
+     */
+    @Nullable
+    public List<BlockPos> getItemsForPickUp()
+    {
+        return items == null ? null : new ArrayList<>(items);
+    }
+
+    /**
+     * Find the closest item and remove it from the list.
+     *
+     * @return the closest item
+     */
+    private BlockPos getAndRemoveClosestItemPosition()
+    {
+        int index = 0;
+        double distance = Double.MAX_VALUE;
+
+        for (int i = 0; i < items.size(); i++)
+        {
+            final double tempDistance = items.get(i).distanceSq(worker.getPosition());
+            if (tempDistance < distance)
+            {
+                index = i;
+                distance = tempDistance;
+            }
+        }
+
+        return items.remove(index);
     }
 }
--- src/main/java/com/minecolonies/coremod/entity/ai/citizen/builder/EntityAIStructureBuilder.java
+++ src/main/java/com/minecolonies/coremod/entity/ai/citizen/builder/EntityAIStructureBuilder.java
@@ -21,6 +21,7 @@ import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.item.EntityArmorStand;
 import net.minecraft.entity.item.EntityItemFrame;
+import net.minecraft.entity.monster.EntityMob;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
@@ -288,6 +289,10 @@ public class EntityAIStructureBuilder extends AbstractEntityAIStructure<JobBuild
                 {
                     request.add(entity.getPickedResult(new RayTraceResult(worker)));
                     entity.getArmorInventoryList().forEach(request::add);
+                }
+                else if(entity instanceof EntityMob)
+                {
+                    //Don't try to request the monster.
                 }
                 else
                 {
