630f53c76396de8b536c51afb17b0ba685057581
Colton Schlosser <cltnschlosser@gmail.com>
2017-05-22 12:53:07 -0500

    Change founds to expected lowercase names

---
+++ tools/lowercase_resources.sh
@@ -0,0 +1,5 @@
+# Change this folder as needed
+find ../src/main/resources/assets/minecolonies/sounds/mob/deliveryman/female -regex ".*[A-Z].*" | while read old; do
+    new="$(echo "$old" | tr '[A-Z]' '[a-z]')"
+    git mv -v "$old" "$new"
+  done
