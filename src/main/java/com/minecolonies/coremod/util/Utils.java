30738d1d0a03d1f87fabb750df734d589f471651
Marvin Hofmann <marvin@marv-productions.de>
2017-05-24 10:57:11 +0200

    move compatibility

--- src/main/java/com/minecolonies/coremod/util/Utils.java
+++ src/main/java/com/minecolonies/coremod/util/Utils.java
@@ -1,6 +1,6 @@
 package com.minecolonies.coremod.util;
 
-import com.minecolonies.compatibility.Compatibility;
+import com.minecolonies.api.compatibility.Compatibility;
 import net.minecraft.block.Block;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemHoe;
