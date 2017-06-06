de30bfd0c8b893646ae9a3e2f7c3e0af598988b4
xavierh <xavier.hervy@gmail.com>
2017-06-06 17:01:42 +0100

    remove unused import

--- src/main/java/com/minecolonies/coremod/colony/buildings/BuildingFarmer.java
+++ src/main/java/com/minecolonies/coremod/colony/buildings/BuildingFarmer.java
@@ -1,8 +1,7 @@
 package com.minecolonies.coremod.colony.buildings;
 
-import com.minecolonies.api.util.InventoryUtils;
-import com.minecolonies.api.util.LanguageHandler;
 import com.minecolonies.api.util.ItemStackUtils;
+import com.minecolonies.api.util.LanguageHandler;
 import com.minecolonies.api.util.constant.ToolType;
 import com.minecolonies.blockout.views.Window;
 import com.minecolonies.coremod.MineColonies;
