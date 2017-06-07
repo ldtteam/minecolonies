90d2a1eca38d16a2890855d77125fc52baeb6af9
Asherslab <Asher.parry@icloud.com>
2017-06-07 17:00:13 +1000

    Changed Some Speeds

--- src/main/java/com/minecolonies/coremod/entity/ai/mobs/AbstractArcherBarbarian.java
+++ src/main/java/com/minecolonies/coremod/entity/ai/mobs/AbstractArcherBarbarian.java
@@ -68,19 +68,15 @@ public abstract class AbstractArcherBarbarian extends EntityMob implements IRang
         this.setCombatTask();
     }
 
-    @Override
-    protected void initEntityAI() {
-        this.tasks.addTask(0, new EntityAISwimming(this));
-        this.tasks.addTask(5, new EntityAIMoveTowardsRestriction(this, 1.0D));
-        this.tasks.addTask(3, new EntityAIAttackMelee(this, 2.3D, true));
-        this.tasks.addTask(3, new EntityAIWalkToRandomHuts(this, 3.0D));
-        this.tasks.addTask(8, new EntityAIWatchClosest(this, EntityPlayer.class, 8.0F));
-        this.tasks.addTask(8, new EntityAILookIdle(this));
-        this.applyEntityAI();
-    }
-
-    private void applyEntityAI() {
-        this.tasks.addTask(6, new EntityAIMoveThroughVillage(this, 1.0D, false));
+    protected void initEntityAI()
+    {
+        this.tasks.addTask(1, new EntityAISwimming(this));
+        this.tasks.addTask(2, new EntityAIRestrictSun(this));
+        this.tasks.addTask(3, new EntityAIAvoidEntity(this, EntityWolf.class, 6.0F, 1.0D, 1.2D));
+        this.tasks.addTask(5, new EntityAIWalkToRandomHuts(this, 1.0D));
+        this.tasks.addTask(6, new EntityAIWatchClosest(this, EntityPlayer.class, 8.0F));
+        this.tasks.addTask(6, new EntityAILookIdle(this));
+        this.targetTasks.addTask(1, new EntityAIHurtByTarget(this, false, new Class[0]));
         this.targetTasks.addTask(2, new EntityAINearestAttackableTarget(this, EntityPlayer.class, true));
         this.targetTasks.addTask(3, new EntityAINearestAttackableTarget(this, EntityCitizen.class, true));
     }
