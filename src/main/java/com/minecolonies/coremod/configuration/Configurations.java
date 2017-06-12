0e9b2d16bf3f89a70d322a379fafec5a29d3c7c6
Asherslab <Asher.parry@icloud.com>
2017-06-13 01:31:06 +1000

    Added config option for wether the worker will work during rain (#1159)
    (it does NOT affect wether they will go inside when it thunders)

--- src/main/java/com/minecolonies/coremod/configuration/Configurations.java
+++ src/main/java/com/minecolonies/coremod/configuration/Configurations.java
@@ -14,6 +14,7 @@ public class Configurations
     public static       int     builderBuildBlockDelay       = 0;
     public static       int     blockMiningDelayModifier     = 1000;
     public static       boolean limitToOneWareHousePerColony = true;
+    public static       boolean workersAlwaysWorkInRain      = false;
 
     public static boolean enableColonyProtection      = true;
     public static boolean turnOffExplosionsInColonies = true;
