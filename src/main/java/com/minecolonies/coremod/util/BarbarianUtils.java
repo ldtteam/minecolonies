e2122ce90655e9f7f2854eb57d340010f535179d
Asherslab <Asher.parry@icloud.com>
2017-06-20 00:07:22 +1000

    Step 4: Raid Level

--- src/main/java/com/minecolonies/coremod/util/BarbarianUtils.java
+++ src/main/java/com/minecolonies/coremod/util/BarbarianUtils.java
@@ -1,6 +1,8 @@
 package com.minecolonies.coremod.util;
 
 import com.minecolonies.api.util.constant.Constants;
+import com.minecolonies.coremod.colony.CitizenData;
+import com.minecolonies.coremod.colony.Colony;
 import com.minecolonies.coremod.entity.EntityCitizen;
 import com.minecolonies.coremod.entity.ai.mobs.*;
 import com.minecolonies.coremod.items.ModItems;
@@ -12,6 +14,10 @@ import net.minecraft.init.Items;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Utils for the Barbarians
@@ -135,6 +141,37 @@ public class BarbarianUtils
         else
         {
             return null;
+        }
+    }
+
+    /**
+     * Takes a colony and spits out that colony's RaidLevel
+     * @param colony The colony to use
+     * @return an int describing the raid level
+     */
+    public static int getColonyRaidLevel(final Colony colony)
+    {
+        int levels = 0;
+
+        @NotNull final List<CitizenData> citizensList = new ArrayList<>();
+        citizensList.addAll(colony.getCitizens().values());
+
+        for (@NotNull final CitizenData citizen : citizensList)
+        {
+            if (citizen.getJob() != null && citizen.getWorkBuilding() != null)
+            {
+                final int buildingLevel = citizen.getWorkBuilding().getBuildingLevel();
+                levels += buildingLevel;
+            }
+        }
+
+        if (colony.getTownHall() != null)
+        {
+            return (levels + colony.getTownHall().getBuildingLevel());
+        }
+        else
+        {
+            return 0;
         }
     }
 }
