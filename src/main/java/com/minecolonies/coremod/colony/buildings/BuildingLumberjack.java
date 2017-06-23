3ea5e7c5a5da5aaf271bb102a79b08ab921bca76
xavierh <xavier.hervy@gmail.com>
2017-06-23 21:22:16 +0100

    Update BuildingLumberjack.java

--- src/main/java/com/minecolonies/coremod/colony/buildings/BuildingLumberjack.java
+++ src/main/java/com/minecolonies/coremod/colony/buildings/BuildingLumberjack.java
@@ -2,7 +2,6 @@ package com.minecolonies.coremod.colony.buildings;
 
 import com.minecolonies.api.util.constant.ToolType;
 import com.minecolonies.api.util.ItemStackUtils;
-import com.minecolonies.blockout.Log;
 import com.minecolonies.blockout.views.Window;
 import com.minecolonies.coremod.MineColonies;
 import com.minecolonies.coremod.achievements.ModAchievements;
