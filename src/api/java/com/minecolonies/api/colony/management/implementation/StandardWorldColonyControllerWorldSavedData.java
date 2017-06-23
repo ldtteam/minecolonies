ea282e4ad202dfeea0cf2b22160c51c8f2025486
OrionDevelopment <oriondevelopment@outlook.com>
2017-06-23 13:28:51 +0200

    Enabling delayed loading of the WorldSaveData and added documentation.

--- src/api/java/com/minecolonies/api/colony/management/implementation/StandardWorldColonyControllerWorldSavedData.java
+++ src/api/java/com/minecolonies/api/colony/management/implementation/StandardWorldColonyControllerWorldSavedData.java
@@ -1,12 +1,16 @@
 package com.minecolonies.api.colony.management.implementation;
 
 import com.minecolonies.api.colony.management.IWorldColonyController;
+import com.minecolonies.api.util.Log;
+import net.minecraft.nbt.NBTBase;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.world.WorldSavedData;
+import org.jetbrains.annotations.NotNull;
 
 /**
- * ----------------------- Not Documented Object ---------------------
- * TODO: Document Object
+ * A class that functions as a Wrapper around a {@link IWorldColonyController} data.
+ * It is used as a {@link WorldSavedData} to store the data of said {@link IWorldColonyController}
+ * into the {@link net.minecraft.world.World} internal Data store.
  */
 public class StandardWorldColonyControllerWorldSavedData extends WorldSavedData
 {
@@ -18,7 +22,24 @@ public class StandardWorldColonyControllerWorldSavedData extends WorldSavedData
     public static final String TAG_CONTROLLER = "Controller";
     ////// --------------------------- String Constants --------------------------- \\\\\\
 
-    private IWorldColonyController controller;
+    /**
+     * The {@link IWorldColonyController} for which this {@link StandardWorldColonyControllerWorldSavedData}
+     * wraps and stores the data into the {@link net.minecraft.world.World}.
+     */
+    private IWorldColonyController<?, ?> controller;
+
+    /**
+     * The NBTTagCompound that was loaded from the NBT of the {@link net.minecraft.world.World}.
+     * It is set by MC by calling the {@link #deserializeNBT(NBTBase)} method.
+     *
+     * Once a Controller is being set after the loading, by calling the {@link #setController(IWorldColonyController)}
+     * method this Tag is read into the first given controller and cleared.
+     *
+     * As such it only functions as a temporary storage of the data stored in the NBT to hold it until MC finishes the loading
+     * of this {@link StandardWorldColonyControllerWorldSavedData} until Minecolonies sets the {@link IWorldColonyController}
+     * for this {@link StandardWorldColonyControllerWorldSavedData}.
+     */
+    private NBTTagCompound loadCompound;
 
     public StandardWorldColonyControllerWorldSavedData(final String name)
     {
@@ -33,7 +54,7 @@ public class StandardWorldColonyControllerWorldSavedData extends WorldSavedData
     @Override
     public void readFromNBT(final NBTTagCompound nbt)
     {
-        getController().deserializeNBT(nbt.getCompoundTag(TAG_CONTROLLER));
+        loadCompound = nbt;
     }
 
     @Override
@@ -43,13 +64,32 @@ public class StandardWorldColonyControllerWorldSavedData extends WorldSavedData
         return compound;
     }
 
-    public IWorldColonyController getController()
+    /**
+     * Method to ge the current {@link IWorldColonyController} for which this {@link StandardWorldColonyControllerWorldSavedData}
+     * wraps and stores the Data.
+     * @return The {@link IWorldColonyController} that is linked to this {@link StandardWorldColonyControllerWorldSavedData}
+     */
+    public IWorldColonyController<?, ?> getController()
     {
         return controller;
     }
 
-    public void setController(final IWorldColonyController controller)
+    /**
+     * Method to set the {@link IWorldColonyController} of which this {@link StandardWorldColonyControllerWorldSavedData}
+     * holds the data.
+     *
+     * If the field {@link #loadCompound} is not null, this {@link StandardWorldColonyControllerWorldSavedData}
+     * will load the data into the given {@link IWorldColonyController}.
+     *
+     * @param controller The {@link IWorldColonyController} for which this {@link StandardWorldColonyControllerWorldSavedData} is supposed to hold data.
+     */
+    public void setController(@NotNull final IWorldColonyController<?, ?> controller)
     {
         this.controller = controller;
+        if (loadCompound != null) {
+            Log.getLogger().debug("First time loading for this World. Reading from NBT.");
+            getController().deserializeNBT(loadCompound);
+            loadCompound = null;
+        }
     }
 }
