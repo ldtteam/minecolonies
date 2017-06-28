a431ca93bcf4505879e2d3223de13a9185128e13
Asherslab <Asher.parry@icloud.com>
2017-06-28 14:28:38 +1000

    AI

--- src/main/java/com/minecolonies/coremod/entity/ai/mobs/barbarians/package-info.java
+++ src/main/java/com/minecolonies/coremod/entity/ai/mobs/barbarians/package-info.java
@@ -1,4 +1,4 @@
 /**
- * Classes describing the Barbarian entities.
+ * Classes describing the Barbarian entities and their AI.
  */
 package com.minecolonies.coremod.entity.ai.mobs.barbarians;
--- src/main/java/com/minecolonies/coremod/entity/ai/mobs/util/BarbarianSpawnUtils.java
+++ src/main/java/com/minecolonies/coremod/entity/ai/mobs/util/BarbarianSpawnUtils.java
@@ -3,16 +3,10 @@ package com.minecolonies.coremod.entity.ai.mobs.util;
 import com.minecolonies.api.util.constant.Constants;
 import com.minecolonies.coremod.colony.Colony;
 import com.minecolonies.coremod.entity.EntityCitizen;
-import com.minecolonies.coremod.entity.ai.mobs.barbarians.AbstractEntityBarbarian;
-import com.minecolonies.coremod.entity.ai.mobs.barbarians.EntityArcherBarbarian;
-import com.minecolonies.coremod.entity.ai.mobs.barbarians.EntityBarbarian;
-import com.minecolonies.coremod.entity.ai.mobs.barbarians.EntityChiefBarbarian;
+import com.minecolonies.coremod.entity.ai.mobs.barbarians.*;
 import com.minecolonies.coremod.items.ModItems;
 import net.minecraft.entity.SharedMonsterAttributes;
-import net.minecraft.entity.ai.EntityAINearestAttackableTarget;
-import net.minecraft.entity.ai.EntityAISwimming;
-import net.minecraft.entity.ai.EntityAIWander;
-import net.minecraft.entity.ai.EntityAIWatchClosest;
+import net.minecraft.entity.ai.*;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
 import net.minecraft.inventory.EntityEquipmentSlot;
@@ -27,31 +21,36 @@ public class BarbarianSpawnUtils
     /**
      * Loot tables for Barbarians.
      */
-    public static final ResourceLocation BarbarianLootTable = new ResourceLocation(Constants.MOD_ID, "EntityBarbarianDrops");
-    public static final ResourceLocation ArcherLootTable = new ResourceLocation(Constants.MOD_ID, "EntityArcherBarbarianDrops");
-    public static final ResourceLocation ChiefLootTable = new ResourceLocation(Constants.MOD_ID, "EntityChiefBarbarianDrops");
+    public static final  ResourceLocation BarbarianLootTable    = new ResourceLocation(Constants.MOD_ID, "EntityBarbarianDrops");
+    public static final  ResourceLocation ArcherLootTable       = new ResourceLocation(Constants.MOD_ID, "EntityArcherBarbarianDrops");
+    public static final  ResourceLocation ChiefLootTable        = new ResourceLocation(Constants.MOD_ID, "EntityChiefBarbarianDrops");
+
+    /**
+     * Barbarian Attack Damage.
+     */
+    public static final  double           ATTACK_DAMAGE         = 2.0D;
+
     /**
      * Values used for AI Task's Priorities.
      */
-    private static final int PRIORITY_ZERO  = 0;
-    private static final int PRIORITY_ONE   = 1;
-    private static final int PRIORITY_TWO   = 2;
-    private static final int PRIORITY_THREE = 3;
-    private static final int PRIORITY_FOUR  = 4;
-    private static final int PRIORITY_FIVE  = 5;
+    private static final int              PRIORITY_ZERO         = 0;
+    private static final int              PRIORITY_ONE          = 1;
+    private static final int              PRIORITY_TWO          = 2;
+    private static final int              PRIORITY_THREE        = 3;
+    private static final int              PRIORITY_FOUR         = 4;
+    private static final int              PRIORITY_FIVE         = 5;
     /**
      * Other various values used for AI Tasks.
      */
-    private static final double AI_MOVE_SPEED      = 2.0D;
-    private static final float  MAX_WATCH_DISTANCE = 8.0F;
+    private static final double           AI_MOVE_SPEED         = 2.0D;
+    private static final float            MAX_WATCH_DISTANCE    = 8.0F;
     /**
      * Values used for mob attributes.
      */
-    private static final double FOLLOW_RANGE          = 35.0D;
-    private static final double MOVEMENT_SPEED        = 0.2D;
-    private static final double ATTACK_DAMAGE         = 2.0D;
-    private static final double ARMOR                 = 2.0D;
-    private static final double BARBARIAN_BASE_HEALTH = 20;
+    private static final double           FOLLOW_RANGE          = 35.0D;
+    private static final double           MOVEMENT_SPEED        = 0.2D;
+    private static final double           ARMOR                 = 2.0D;
+    private static final double           BARBARIAN_BASE_HEALTH = 20;
 
     /**
      * Set barbarian attributes.
@@ -79,8 +78,18 @@ public class BarbarianSpawnUtils
         barbarian.tasks.addTask(PRIORITY_ZERO, new EntityAISwimming(barbarian));
         barbarian.tasks.addTask(PRIORITY_TWO, new EntityAINearestAttackableTarget<>(barbarian, EntityPlayer.class, true, true));
         barbarian.tasks.addTask(PRIORITY_THREE, new EntityAINearestAttackableTarget<>(barbarian, EntityCitizen.class, true, true));
-        barbarian.tasks.addTask(PRIORITY_FOUR, new EntityAIWander(barbarian, AI_MOVE_SPEED));
+        barbarian.tasks.addTask(PRIORITY_FOUR, new EntityAIWalkToRandomHuts(barbarian, AI_MOVE_SPEED));
         barbarian.tasks.addTask(PRIORITY_FIVE, new EntityAIWatchClosest(barbarian, EntityPlayer.class, MAX_WATCH_DISTANCE));
+
+        if (barbarian instanceof EntityArcherBarbarian)
+        {
+            barbarian.tasks.addTask(PRIORITY_ONE, new EntityAIAttackArcher(barbarian));
+        }
+        else
+        {
+            barbarian.tasks.addTask(PRIORITY_ONE, new EntityAIBarbarianAttackMelee(barbarian));
+        }
+
     }
 
     public static void setBarbarianEquipment(AbstractEntityBarbarian barbarian)
