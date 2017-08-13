9441b489caf1de576a175ff111d7fa58c1fc64cd
Asherslab <Asher.parry@icloud.com>
2017-08-13 22:45:23 +1000

    Done

--- src/main/java/com/minecolonies/coremod/commands/generalcommands/BackupCommand.java
+++ src/main/java/com/minecolonies/coremod/commands/generalcommands/BackupCommand.java
@@ -20,8 +20,8 @@ import static com.minecolonies.coremod.commands.AbstractSingleCommand.Commands.M
  */
 public class BackupCommand extends AbstractSingleCommand
 {
-    public static final String DESC = "backup";
-    public static final String NO_PERMISSION_MESSAGE = "You do not have permission to backup colony data!";
+    public static final String DESC                   = "backup";
+    public static final String NO_PERMISSION_MESSAGE  = "You do not have permission to backup colony data!";
     public static final String BACKUP_SUCCESS_MESSAGE = "Successfully backed up colony data!";
     public static final String BACKUP_FAILURE_MESSAGE = "Failed to back up colony data!";
 
@@ -41,7 +41,8 @@ public class BackupCommand extends AbstractSingleCommand
 
         if (isPlayerOpped(sender))
         {
-            server.addScheduledTask(() -> {
+            server.addScheduledTask(() ->
+            {
                 if (ColonyManager.backupColonyData())
                 {
                     sender.sendMessage(new TextComponentString(BACKUP_SUCCESS_MESSAGE));
@@ -49,7 +50,6 @@ public class BackupCommand extends AbstractSingleCommand
                 else
                 {
                     sender.sendMessage(new TextComponentString(BACKUP_FAILURE_MESSAGE));
-
                 }
             });
         }
