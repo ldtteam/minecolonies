d7117d793d181317a96caf8b82cc0f468e83e919
Raycoms <ray.neiheiser@gmail.com>
2017-08-21 21:29:01 +0200

    Port fixes to 1.12 (#1373)
    Merge several fixes from 1.10 to 1.12

--- src/main/java/com/minecolonies/coremod/entity/EntityCitizen.java
+++ src/main/java/com/minecolonies/coremod/entity/EntityCitizen.java
@@ -1707,10 +1707,12 @@ public class EntityCitizen extends EntityAgeable implements INpc
             }
 
             final ItemStack itemStack = entityItem.getItem();
+            final ItemStack compareStack = itemStack.copy();
 
-            final ItemStack resultStack = InventoryUtils.addItemStackToItemHandlerWithResult(new InvWrapper(getInventoryCitizen()), itemStack.copy());
+            final ItemStack resultStack = InventoryUtils.addItemStackToItemHandlerWithResult(new InvWrapper(getInventoryCitizen()), itemStack);
             final int resultingStackSize = ItemStackUtils.isEmpty(resultStack) ? 0 : ItemStackUtils.getSize(resultStack);
-            if (ItemStackUtils.isEmpty(resultStack) || ItemStackUtils.compareItemStacksIgnoreStackSize(itemStack, resultStack))
+
+            if (ItemStackUtils.isEmpty(resultStack) || ItemStackUtils.getSize(resultStack) != ItemStackUtils.getSize(compareStack))
             {
                 CompatibilityUtils.getWorld(this).playSound((EntityPlayer) null,
                         this.getPosition(),
