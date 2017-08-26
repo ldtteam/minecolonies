3e410b555ad06846e20b612c61ef5815476fd5a5
Raycoms <ray.neiheiser@gmail.com>
2017-08-26 14:25:06 +0100

    import

--- src/main/java/com/minecolonies/coremod/network/messages/BlockParticleEffectMessage.java
+++ src/main/java/com/minecolonies/coremod/network/messages/BlockParticleEffectMessage.java
@@ -5,7 +5,6 @@ import io.netty.buffer.ByteBuf;
 import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.Minecraft;
-import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.fml.client.FMLClientHandler;
