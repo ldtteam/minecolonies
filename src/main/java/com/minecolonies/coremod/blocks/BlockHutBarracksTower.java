c69b2d5612e40f67a4a1128281bf8442e454c988
Raycoms <ray.neiheiser@gmail.com>
2017-09-06 15:47:32 +0100

    make player unable to get barracksTower block

--- src/main/java/com/minecolonies/coremod/blocks/BlockHutBarracksTower.java
+++ src/main/java/com/minecolonies/coremod/blocks/BlockHutBarracksTower.java
@@ -1,6 +1,14 @@
 package com.minecolonies.coremod.blocks;
 
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Block of the BarracksTower.
@@ -21,5 +29,20 @@ public class BlockHutBarracksTower extends AbstractBlockHut
     public String getName()
     {
         return "blockHutBarracksTower";
+    }
+
+    /**
+     * This returns a complete list of items dropped from this block.
+     *
+     * @param world   The current world
+     * @param pos     Block position in world
+     * @param state   Current state
+     * @param fortune Breakers fortune level
+     * @return A ArrayList containing all items this block drops
+     */
+    @Override
+    public List<ItemStack> getDrops(final IBlockAccess world, final BlockPos pos, final IBlockState state, final int fortune)
+    {
+        return Collections.emptyList();
     }
 }
