b9038719882184647b24bd17be6f6c3840a465dd
Raycoms <ray.neiheiser@gmail.com>
2017-09-08 21:27:01 +0100

    Port improved rendering to 1.12

--- src/main/java/com/minecolonies/structures/fake/FakeWorld.java
+++ src/main/java/com/minecolonies/structures/fake/FakeWorld.java
@@ -1,6 +1,7 @@
 package com.minecolonies.structures.fake;
 
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.init.Blocks;
 import net.minecraft.profiler.Profiler;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.BlockPos;
@@ -11,13 +12,25 @@ import net.minecraft.world.storage.ISaveHandler;
 import net.minecraft.world.storage.WorldInfo;
 import org.jetbrains.annotations.NotNull;
 
+import javax.annotation.Nullable;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Holds a fake world.
  */
 public class FakeWorld extends World
 {
 
+    /**
+     * Map of all states.
+     */
+    private final Map<BlockPos, IBlockState> stateHashMap  = new HashMap<>();
+    private final Map<BlockPos, TileEntity>  entityHashMap = new HashMap<>();
+
     private final IBlockState blockState;
+    private final TileEntity entity;
+    private final boolean simulateWorld;
 
     /**
      * Creates a fake world.
@@ -39,6 +52,35 @@ public class FakeWorld extends World
     {
         super(saveHandlerIn, info, providerIn, profilerIn, client);
         this.blockState = blockState;
+        this.entity = null;
+        simulateWorld = false;
+    }
+
+    /**
+     * Creates a fake world.
+     *
+     * @param blockState    with the blockState.
+     * @param saveHandlerIn a saveHandler.
+     * @param info          additional info.
+     * @param providerIn    worldProvider.
+     * @param profilerIn    profiler.
+     * @param client        and if is client.
+     * @param entity        the tileEntity.
+     */
+    public FakeWorld(
+            final IBlockState blockState,
+            final ISaveHandler saveHandlerIn,
+            final WorldInfo info,
+            final WorldProvider providerIn,
+            final Profiler profilerIn,
+            final boolean client,
+            final TileEntity entity,
+            final boolean simulateWorld)
+    {
+        super(saveHandlerIn, info, providerIn, profilerIn, client);
+        this.blockState = blockState;
+        this.entity = entity;
+        this.simulateWorld = simulateWorld;
     }
 
     @NotNull
@@ -60,16 +102,41 @@ public class FakeWorld extends World
         return false;
     }
 
+    @Override
+    public boolean setBlockState(final BlockPos pos, final IBlockState state)
+    {
+        stateHashMap.put(pos, state);
+        return true;
+    }
+
+    @Override
+    public void setTileEntity(final BlockPos pos, @Nullable final TileEntity tileEntityIn)
+    {
+        entityHashMap.put(pos, tileEntityIn);
+    }
+
     @NotNull
     @Override
     public IBlockState getBlockState(final BlockPos pos)
     {
+        if(simulateWorld)
+        {
+            if(stateHashMap.containsKey(pos))
+            {
+                return stateHashMap.get(pos);
+            }
+            return Blocks.AIR.getDefaultState();
+        }
         return this.blockState;
     }
 
     @Override
     public TileEntity getTileEntity(final BlockPos pos)
     {
-        return null;
+        if(simulateWorld)
+        {
+            return entityHashMap.get(pos);
+        }
+        return entity;
     }
 }
