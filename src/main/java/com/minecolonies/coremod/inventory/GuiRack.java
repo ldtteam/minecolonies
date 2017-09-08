1278299040cf7506f89ad6c50050e3dda16d6e64
Raycoms <ray.neiheiser@gmail.com>
2017-09-08 17:16:57 -0300

    Feature/increase storage (#1416)
    Offer to increase the warehouse in the GUI (from level 5 on)

--- src/main/java/com/minecolonies/coremod/inventory/GuiRack.java
+++ src/main/java/com/minecolonies/coremod/inventory/GuiRack.java
@@ -1,5 +1,6 @@
 package com.minecolonies.coremod.inventory;
 
+import com.minecolonies.api.util.constant.Constants;
 import com.minecolonies.coremod.tileentities.TileEntityRack;
 import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.client.renderer.GlStateManager;
@@ -16,9 +17,14 @@ import net.minecraftforge.items.wrapper.CombinedInvWrapper;
 public class GuiRack extends GuiContainer
 {
     /**
-     * The resource location of the texture.
+     * The resource LOCATION of the texture.
      */
-    private static final ResourceLocation CHEST_GUI_TEXTURE = new ResourceLocation("textures/gui/container/generic_54.png");
+    private static final ResourceLocation CHEST_GUI_TEXTURE = new ResourceLocation(Constants.MOD_ID, "textures/gui/generic_108.png");
+
+    /**
+     * The LOCATION of the additional styles.
+     */
+    private static final String LOCATION = "textures/gui/gui%s.png";
 
     /**
      * Amount of slots each row.
@@ -26,14 +32,29 @@ public class GuiRack extends GuiContainer
     private static final int SLOTS_EACH_ROW = 9;
 
     /**
+     * Size of the custom texture.
+     */
+    private static final int TEXTURE_SIZE = 350;
+
+    /**
      * Offset of each slot.
      */
-    private static final int SLOT_OFFSET    = 18;
+    private static final int SLOT_OFFSET = 18;
+
+    /**
+     * Size at which the normal GUI texture still works.
+     */
+    private static final int GOOD_SIZE = 8;
+
+    /**
+     * Multiply the current size by this amount.
+     */
+    private static final int SIZE_MULTIPLIER = 3;
 
     /**
      * General y offset.
      */
-    private static final int Y_OFFSET       = 114;
+    private static final int Y_OFFSET = 114;
 
     /**
      * Offet of the screen for the texture.
@@ -43,8 +64,17 @@ public class GuiRack extends GuiContainer
     /**
      * Offset inside the texture to use.
      */
-    private static final int TEXTURE_OFFSET = 126;
+    private static final int TEXTURE_OFFSET = 126 * 2 - 17;
 
+    /**
+     * Extra offset to move increase the texture if the inventory is huge.
+     */
+    private static final int EXTRA_OFFSET = 56;
+
+    /**
+     * Extra height to show the whole texture for big inventories.
+     */
+    private static final int EXTRA_HEIGHT = 50;
 
     /**
      * The upper chest inventory.
@@ -58,11 +88,11 @@ public class GuiRack extends GuiContainer
 
     public GuiRack(final InventoryPlayer parInventoryPlayer, final TileEntityRack tileEntity, final TileEntityRack neighborRack, final World world, final BlockPos location)
     {
-        super(new ContainerRack(tileEntity, neighborRack, parInventoryPlayer, world, location));
+        super(new ContainerRack(tileEntity, neighborRack, parInventoryPlayer, location));
 
-        if(neighborRack != null)
+        if (neighborRack != null)
         {
-            if(tileEntity.isMain())
+            if (tileEntity.isMain())
             {
                 this.jointChestInventory = new CombinedInvWrapper(tileEntity.getInventory(), neighborRack.getInventory());
             }
@@ -79,7 +109,11 @@ public class GuiRack extends GuiContainer
         this.inventoryRows = jointChestInventory.getSlots() / SLOTS_EACH_ROW;
 
         this.allowUserInput = false;
-        this.ySize = Y_OFFSET + this.inventoryRows * SLOT_OFFSET;
+        this.ySize = Y_OFFSET + Math.min(SLOTS_EACH_ROW, this.inventoryRows) * SLOT_OFFSET;
+        if (this.inventoryRows > SLOTS_EACH_ROW - 1)
+        {
+            this.xSize = this.xSize + (this.inventoryRows - SLOTS_EACH_ROW) * (SLOTS_EACH_ROW + 1);
+        }
     }
 
     /**
@@ -88,10 +122,41 @@ public class GuiRack extends GuiContainer
     protected void drawGuiContainerBackgroundLayer(final float partialTicks, final int mouseX, final int mouseY)
     {
         GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
-        this.mc.getTextureManager().bindTexture(CHEST_GUI_TEXTURE);
+        this.mc.getTextureManager().bindTexture(getCorrectTextureForSlots(inventoryRows));
         final int i = (this.width - this.xSize) / 2;
         final int j = (this.height - this.ySize) / 2;
-        this.drawTexturedModalRect(i, j, 0, 0, this.xSize, this.inventoryRows * SLOT_OFFSET + SLOT_OFFSET-1);
-        this.drawTexturedModalRect(i, j + this.inventoryRows * SLOT_OFFSET + SLOT_OFFSET-1, 0, TEXTURE_OFFSET, this.xSize, TEXTURE_HEIGHT);
+
+        if (inventoryRows < SLOTS_EACH_ROW)
+        {
+            drawModalRectWithCustomSizedTexture(i, j, 0, 0, this.xSize, this.inventoryRows * SLOT_OFFSET + SLOT_OFFSET - 1, TEXTURE_SIZE, TEXTURE_SIZE);
+            drawModalRectWithCustomSizedTexture(i, j + this.inventoryRows * SLOT_OFFSET + SLOT_OFFSET - 1, 0,
+                    TEXTURE_OFFSET, this.xSize, TEXTURE_HEIGHT, TEXTURE_SIZE, TEXTURE_SIZE);
+        }
+        else
+        {
+            final int textureOffset = TEXTURE_OFFSET - EXTRA_OFFSET;
+            drawModalRectWithCustomSizedTexture(i, j, 0, 0, (this.xSize * SIZE_MULTIPLIER) / 2, this.inventoryRows * SLOT_OFFSET + SLOT_OFFSET - 1, TEXTURE_SIZE, TEXTURE_SIZE);
+            drawModalRectWithCustomSizedTexture(i,
+                    j + Math.min(SLOTS_EACH_ROW, this.inventoryRows) * SLOT_OFFSET + SLOT_OFFSET - 1, 0,
+                    textureOffset, (this.xSize * SIZE_MULTIPLIER) / 2, TEXTURE_HEIGHT + EXTRA_HEIGHT, TEXTURE_SIZE, TEXTURE_SIZE);
+        }
+    }
+
+    /**
+     * Get the correct resource LOCATION for this amount of rows.
+     *
+     * @param inventoryRows the amount of rows.
+     * @return the correct LOCATION.
+     */
+    private static ResourceLocation getCorrectTextureForSlots(final int inventoryRows)
+    {
+        if (inventoryRows <= GOOD_SIZE)
+        {
+            return CHEST_GUI_TEXTURE;
+        }
+        else
+        {
+            return new ResourceLocation(Constants.MOD_ID, String.format(LOCATION, Integer.toString(inventoryRows * SLOTS_EACH_ROW)));
+        }
     }
 }
