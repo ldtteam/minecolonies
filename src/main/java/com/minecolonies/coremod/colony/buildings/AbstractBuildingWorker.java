7136c7d7d7aecc88ec86ba1ea6c5d1a96af8e919
Raycoms <ray.neiheiser@gmail.com>
2017-10-29 22:11:16 -0200

    Feature/better avoid (#1558)
    * avoid in random direction
    
    * some small adaptions
    
    * move away from location fix
    
    * fix up all the going home and sleep stuff
    
    * codacity issues
    
    * fix problem with missing bed
    
    * not necessary if
    
    * hotfix bugfix NPE

--- src/main/java/com/minecolonies/coremod/colony/buildings/AbstractBuildingWorker.java
+++ src/main/java/com/minecolonies/coremod/colony/buildings/AbstractBuildingWorker.java
@@ -254,14 +254,6 @@ public abstract class AbstractBuildingWorker extends AbstractBuildingHut
     }
 
     /**
-     * executed when a new day start.
-     */
-    public void onWakeUp()
-    {
-
-    }
-
-    /**
      * Returns whether or not the building has a worker.
      *
      * @return true if building has worker, otherwise false.
