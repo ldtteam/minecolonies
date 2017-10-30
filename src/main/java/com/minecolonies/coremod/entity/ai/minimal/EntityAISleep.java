287bfe053eda0912cc1ef0d37cf0ae80c0182357
Raycoms <ray.neiheiser@gmail.com>
2017-10-30 15:17:34 -0200

    Port/standing avoid (#1563)
    * Improving the standing logic of the Builder by adding some offsets.
    
    * Feature/better avoid (#1558)
    
    * avoid in random direction
    
    * some small adaptions
    
    * move away from location fix
    
    * fix up all the going home and sleep stuff
    
    * codacity issues
    
    * fix problem with missing bed
    
    * not necessary if
    
    * hotfix bugfix NPE
    
    * fix loc

--- src/main/java/com/minecolonies/coremod/entity/ai/minimal/EntityAISleep.java
+++ src/main/java/com/minecolonies/coremod/entity/ai/minimal/EntityAISleep.java
@@ -65,7 +65,7 @@ public class EntityAISleep extends EntityAIBase
         if (citizen.getDesiredActivity() == EntityCitizen.DesiredActivity.SLEEP)
         {
             final Colony colony = citizen.getColony();
-            if(colony == null || colony.getBuilding(citizen.getHomePosition()) != null)
+            if(colony == null || colony.getBuilding(citizen.getHomePosition()) == null)
             {
                 return true;
             }
@@ -80,14 +80,18 @@ public class EntityAISleep extends EntityAIBase
                         final World world = citizen.world;
                         IBlockState state = world.getBlockState(pos);
                         state = state.getBlock().getActualState(state, world, pos);
-                        if (state.getBlock() instanceof BlockBed && !state.getValue(BlockBed.OCCUPIED))
+                        if (state.getBlock() instanceof BlockBed
+                                && !state.getValue(BlockBed.OCCUPIED)
+                                && state.getValue(BlockBed.PART).equals(BlockBed.EnumPartType.HEAD))
                         {
                             usedBed = pos;
-                            citizen.world.notifyBlockUpdate(pos, state, state.withProperty(BlockBed.OCCUPIED, true), 0);
+                            citizen.world.setBlockState(pos, state.withProperty(BlockBed.OCCUPIED, true), 0x03);
                             return true;
                         }
                     }
                 }
+
+                usedBed = citizen.getHomePosition();
             }
             else
             {
@@ -105,7 +109,7 @@ public class EntityAISleep extends EntityAIBase
             final IBlockState state = citizen.world.getBlockState(usedBed);
             if(state.getBlock() instanceof BlockBed)
             {
-                citizen.world.notifyBlockUpdate(usedBed, state, state.withProperty(BlockBed.OCCUPIED, false), 0);
+                citizen.world.setBlockState(usedBed, state.withProperty(BlockBed.OCCUPIED, false), 0x03);
             }
             usedBed = null;
         }
