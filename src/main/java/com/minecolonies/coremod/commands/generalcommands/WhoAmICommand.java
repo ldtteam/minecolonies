5de14b88f5db12a880e8dba4085c9114d86ef249
Raycoms <ray.neiheiser@gmail.com>
2017-10-31 12:40:11 -0200

    Port1.12/colony commands (#1578)
    * Create new colony commands to check for a close colony (#1572)
    
    * Create new colony commands to check for a close colony
    
    * fix desc
    
    * fixes
    
    * fix crash
    
    * 1.12 fixes

---
+++ src/main/java/com/minecolonies/coremod/commands/generalcommands/WhoAmICommand.java
@@ -0,0 +1,78 @@
+package com.minecolonies.coremod.commands.generalcommands;
+
+import com.minecolonies.api.colony.IColony;
+import com.minecolonies.blockout.Log;
+import com.minecolonies.coremod.colony.ColonyManager;
+import com.minecolonies.coremod.commands.AbstractSingleCommand;
+import net.minecraft.command.CommandException;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.TextComponentString;
+import org.jetbrains.annotations.NotNull;
+
+import javax.annotation.Nullable;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by asie on 2/16/17.
+ */
+public class WhoAmICommand extends AbstractSingleCommand
+{
+    /**
+     * Command description.
+     */
+    public static final String DESC = "whoami";
+
+    /**
+     * Player description string.
+     */
+    public static final String TELL_HIM = "You are %s, your colony is %s at position %s";
+
+
+    /**
+     * Initialize this SubCommand with it's parents.
+     *
+     * @param parents an array of all the parents.
+     */
+    public WhoAmICommand(@NotNull final String... parents)
+    {
+        super(parents);
+    }
+
+    @Override
+    public void execute(@NotNull final MinecraftServer server, @NotNull final ICommandSender sender, @NotNull final String... args) throws CommandException
+    {
+        if(!(sender instanceof EntityPlayer))
+        {
+            Log.getLogger().info("Very funny, you're a console!");
+            return;
+        }
+
+        final IColony colony = ColonyManager.getIColonyByOwner(server.getEntityWorld(), ((EntityPlayer) sender).getUniqueID());
+        final BlockPos pos = colony.getCenter();
+        final String colonyName = colony.getName();
+        final String playerName = sender.getDisplayName().getFormattedText();
+        final String posString = "x: " + pos.getX() + " y: " + pos.getY() + " z: " + pos.getZ();
+        sender.sendMessage(new TextComponentString(String.format(TELL_HIM, playerName, colonyName, posString)));
+    }
+
+    @NotNull
+    @Override
+    public List<String> getTabCompletionOptions(
+                                                 @NotNull final MinecraftServer server,
+                                                 @NotNull final ICommandSender sender,
+                                                 @NotNull final String[] args,
+                                                 @Nullable final BlockPos pos)
+    {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public boolean isUsernameIndex(@NotNull final String[] args, final int index)
+    {
+        return false;
+    }
+}
