92ef80c090a19f60e57d8ff13767e8bb69244c96
Raycoms <ray.neiheiser@gmail.com>
2017-10-31 08:56:57 -0200

    Create new colony commands to check for a close colony (#1572)
    * Create new colony commands to check for a close colony
    
    * fix desc
    
    * fixes
    
    * fix crash

--- src/main/java/com/minecolonies/coremod/commands/MinecoloniesCommand.java
+++ src/main/java/com/minecolonies/coremod/commands/MinecoloniesCommand.java
@@ -34,6 +34,8 @@ public class MinecoloniesCommand extends AbstractSplitCommand
         .put(RaidAllTonightCommand.DESC, new RaidAllTonightCommand(DESC))
         .put(RaidAllNowCommand.DESC, new RaidAllNowCommand(DESC))
         .put(CheckForAutoDeletesCommand.DESC, new CheckForAutoDeletesCommand(DESC))
+        .put(WhereAmICommand.DESC, new WhereAmICommand(DESC))
+        .put(WhoAmICommand.DESC, new WhoAmICommand(DESC))
         .build();
 
     /**
