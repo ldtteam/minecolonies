2af98ed54fda38dc1fae9bdd8e392decbf91e9d4
Raycoms <ray.neiheiser@gmail.com>
2017-11-04 15:58:58 +0000

    several bug fixes (#1582)
    * several bug fixes
    
    * explain the death
    
    * npe fix. (#1588)

--- src/main/java/com/minecolonies/coremod/entity/EntityCitizen.java
+++ src/main/java/com/minecolonies/coremod/entity/EntityCitizen.java
@@ -857,10 +857,10 @@ public class EntityCitizen extends EntityAgeable implements INpc
     /**
      * Called when the mob's health reaches 0.
      *
-     * @param par1DamageSource the attacking entity.
+     * @param damageSource the attacking entity.
      */
     @Override
-    public void onDeath(final DamageSource par1DamageSource)
+    public void onDeath(final DamageSource damageSource)
     {
         double penalty = CITIZEN_DEATH_PENALTY;
         if (par1DamageSource.getTrueSource() instanceof EntityPlayer)
@@ -881,24 +881,24 @@ public class EntityCitizen extends EntityAgeable implements INpc
         if (colony != null)
         {
             colony.decreaseOverallHappiness(penalty);
-            triggerDeathAchievement(par1DamageSource, getColonyJob());
+            triggerDeathAchievement(damageSource, getColonyJob());
             if (getColonyJob() instanceof JobGuard)
             {
                 LanguageHandler.sendPlayersMessage(
                         colony.getMessageEntityPlayers(),
                         "tile.blockHutTownHall.messageGuardDead",
-                        citizenData.getName(), (int) posX, (int) posY, (int) posZ);
+                        citizenData.getName(), (int) posX, (int) posY, (int) posZ, damageSource.damageType);
             }
             else
             {
                 LanguageHandler.sendPlayersMessage(
                         colony.getMessageEntityPlayers(),
                         "tile.blockHutTownHall.messageColonistDead",
-                        citizenData.getName(), (int) posX, (int) posY, (int) posZ);
+                        citizenData.getName(), (int) posX, (int) posY, (int) posZ, damageSource.damageType);
             }
             colony.removeCitizen(getCitizenData());
         }
-        super.onDeath(par1DamageSource);
+        super.onDeath(damageSource);
     }
 
     @Override
