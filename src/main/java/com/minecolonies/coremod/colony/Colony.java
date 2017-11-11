a7e2d9d1bffa0feae12cef9599c8b1f7d3046301
Raycoms <ray.neiheiser@gmail.com>
2017-11-11 15:41:58 -0200

    Delete event handler on colony deletion (#1611)
    Correctly delete colonies

--- src/main/java/com/minecolonies/coremod/colony/Colony.java
+++ src/main/java/com/minecolonies/coremod/colony/Colony.java
@@ -241,6 +241,11 @@ public class Colony implements IColony
     private final Set<Block> freeBlocks = new HashSet<>();
 
     /**
+     * Colony permission event handler.
+     */
+    private final ColonyPermissionEventHandler eventHandler;
+
+    /**
      * Amount of ticks passed.
      */
     private int ticksPassed = 0;
@@ -274,7 +279,8 @@ public class Colony implements IColony
         this.colonyAchievements = new ArrayList<>();
 
         // Register a new event handler
-        MinecraftForge.EVENT_BUS.register(new ColonyPermissionEventHandler(this));
+        eventHandler = new ColonyPermissionEventHandler(this);
+        MinecraftForge.EVENT_BUS.register(eventHandler);
 
         for (final String s : Configurations.freeToInteractBlocks)
         {
@@ -430,6 +436,15 @@ public class Colony implements IColony
         lastContactInHours = compound.getInteger(TAG_ABANDONED);
         manualHousing = compound.getBoolean(TAG_MANUAL_HOUSING);
         canColonyBeAutoDeleted = compound.getBoolean(TAG_DELETABLE);
+    }
+
+    /**
+     * Get the event handler assigned to the colony.
+     * @return the ColonyPermissionEventHandler.
+     */
+    public ColonyPermissionEventHandler getEventHandler()
+    {
+        return eventHandler;
     }
 
     /**
