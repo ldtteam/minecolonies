39e5f483f7c232aff637178043e155a989fec6b4
Raycoms <ray.neiheiser@gmail.com>
2017-12-02 21:03:24 +0000

    fix port errors

--- src/main/java/com/minecolonies/coremod/blocks/AbstractBlockMinecoloniesStairs.java
+++ src/main/java/com/minecolonies/coremod/blocks/AbstractBlockMinecoloniesStairs.java
@@ -2,21 +2,14 @@ package com.minecolonies.coremod.blocks;
 
 import com.minecolonies.api.util.constant.Suppression;
 import net.minecraft.block.Block;
-import net.minecraft.block.BlockPane;
 import net.minecraft.block.BlockStairs;
-import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
-import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
-import net.minecraftforge.fml.common.registry.IForgeRegistry;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
-import org.jetbrains.annotations.NotNull;
-import slimeknights.mantle.block.BlockStairsBase;
+import net.minecraftforge.registries.IForgeRegistry;
 
 import static com.minecolonies.api.util.constant.Suppression.DEPRECATION;
 
