42fafa95fb845a22baf02dffdb5b852d295ac3b2
Raycoms <ray.neiheiser@gmail.com>
2017-12-04 21:20:10 +0000

    Fixing the classloading issue when running a dedi server.

--- src/main/java/com/minecolonies/coremod/proxy/CommonProxy.java
+++ src/main/java/com/minecolonies/coremod/proxy/CommonProxy.java
@@ -19,15 +19,18 @@ import net.minecraft.item.Item;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import net.minecraft.world.storage.loot.LootTableList;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.network.NetworkRegistry;
+import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
 import net.minecraftforge.fml.common.registry.EntityRegistry;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import org.jetbrains.annotations.NotNull;
 
+import javax.annotation.Nullable;
 import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
@@ -254,6 +257,13 @@ public class CommonProxy implements IProxy
     public File getSchematicsFolder()
     {
         return null;
+    }
+
+    @Nullable
+    @Override
+    public World getWorldFromMessage(@NotNull final MessageContext context)
+    {
+        return context.getServerHandler().playerEntity.getServerWorld();
     }
 
     /**
