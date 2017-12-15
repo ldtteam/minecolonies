9fc7df2227863497eeebfa2816ee2a398a0c49a3
Marc Hermans <OrionDevelopment@outlook.com>
2017-12-15 17:51:12 +0100

    Limiting stack sizes.

--- src/api/java/com/minecolonies/api/colony/requestsystem/requestable/Stack.java
+++ src/api/java/com/minecolonies/api/colony/requestsystem/requestable/Stack.java
@@ -44,7 +44,8 @@ public class Stack implements IDeliverable
 
         setMatchMeta(true).setMatchNBT(true);
 
-        this.stack = stack;
+        this.stack = stack.copy();
+        this.stack.setCount(this.stack.getMaxStackSize());
     }
 
     public Stack setMatchNBT(final boolean match)
