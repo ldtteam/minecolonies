7ae0ffe295e5723f66c16b3319f788542929aa80
Raycoms <ray.neiheiser@gmail.com>
2017-12-19 19:01:37 -0200

    Fix several issues relate to building with the new RS (#1877) (#1880)
    * Fix several issues relate to building with the new RS
    
    * Fix provider removal and label color.
    
    * fix crash on kill
    
    * isEmpty and not == 0
    
    * Fix requesting entities
    
    * More tries and fix javadoc
    
    * Fixing the unit test.

--- src/main/java/com/minecolonies/coremod/entity/ai/citizen/guard/AbstractEntityAIGuard.java
+++ src/main/java/com/minecolonies/coremod/entity/ai/citizen/guard/AbstractEntityAIGuard.java
@@ -76,7 +76,7 @@ public abstract class AbstractEntityAIGuard extends AbstractEntityAIInteract<Job
     /**
      * Max tries to find a position to path to.
      */
-    private static final int MAX_TRIES = 4;
+    private static final int MAX_TRIES = 20;
 
     /**
      * Path that close to the patrol target.
