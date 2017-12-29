5d3c8e5f55b20f5e236f585e0128543a505348fa
Raycoms <ray.neiheiser@gmail.com>
2017-12-29 21:52:19 +0000

    finish the blockszaze

--- src/main/java/com/minecolonies/coremod/blocks/BlockShingleSlab.java
+++ src/main/java/com/minecolonies/coremod/blocks/BlockShingleSlab.java
@@ -2,16 +2,19 @@ package com.minecolonies.coremod.blocks;
 
 import com.minecolonies.api.util.constant.Constants;
 import com.minecolonies.coremod.creativetab.ModCreativeTabs;
+import net.minecraft.block.BlockStairs;
 import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.IProperty;
 import net.minecraft.block.properties.PropertyEnum;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.init.Blocks;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Locale;
 
@@ -62,14 +65,13 @@ public class BlockShingleSlab extends AbstractBlockMinecoloniesDirectional<Block
         setHardness(BLOCK_HARDNESS);
         setResistance(RESISTANCE);
         this.setLightOpacity(255);
-        this.fullBlock = false;
     }
 
     /**
      * Convert the BlockState into the correct metadata value
      */
     @Override
-    public int getMetaFromState(final IBlockState state)
+    public int getMetaFromState(@NotNull final IBlockState state)
     {
         return state.getValue(FACING).getHorizontalIndex();
     }
@@ -84,9 +86,112 @@ public class BlockShingleSlab extends AbstractBlockMinecoloniesDirectional<Block
     }
 
     @Override
-    public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos)
+    public AxisAlignedBB getBoundingBox(@NotNull final IBlockState state, @NotNull final IBlockAccess source, @NotNull final BlockPos pos)
     {
         return AABB_BOTTOM_HALF;
+    }
+
+    @Override
+    public IBlockState getActualState(@NotNull final IBlockState state, @NotNull final IBlockAccess worldIn, @NotNull final BlockPos pos)
+    {
+        return getStairsShape(state, worldIn, pos);
+    }
+
+    private static IBlockState getStairsShape(@NotNull final IBlockState state, @NotNull final IBlockAccess world, @NotNull final BlockPos position)
+    {
+        final boolean isAirDir1 = world.getBlockState(position.east()).getBlock() == Blocks.AIR;
+        final boolean isAirDir2 = world.getBlockState(position.west()).getBlock() == Blocks.AIR;
+        final boolean isAirDir3 = world.getBlockState(position.north()).getBlock() == Blocks.AIR;
+        final boolean isAirDir4 = world.getBlockState(position.south()).getBlock() == Blocks.AIR;
+
+        int amount = 0;
+        if(isAirDir1)
+        {
+            amount++;
+        }
+        if(isAirDir2)
+        {
+            amount++;
+        }
+        if(isAirDir3)
+        {
+            amount++;
+        }
+        if(isAirDir4)
+        {
+            amount++;
+        }
+
+        if(amount == 0)
+        {
+            return state.withProperty(VARIANT, ShingleSlabType.TOP);
+        }
+        if(amount == 1)
+        {
+            if (isAirDir1)
+            {
+                return state.withProperty(VARIANT, ShingleSlabType.ONE_WAY).withProperty(FACING, EnumFacing.SOUTH);
+            }
+            else if (isAirDir2)
+            {
+                return state.withProperty(VARIANT, ShingleSlabType.ONE_WAY).withProperty(FACING, EnumFacing.NORTH);
+            }
+            else if (isAirDir3)
+            {
+                return state.withProperty(VARIANT, ShingleSlabType.ONE_WAY).withProperty(FACING, EnumFacing.EAST);
+            }
+            else
+            {
+                return state.withProperty(VARIANT, ShingleSlabType.ONE_WAY).withProperty(FACING, EnumFacing.WEST);
+            }
+        }
+        else if(amount == 2)
+        {
+            if (isAirDir1 && isAirDir2 && !isAirDir3 && !isAirDir4)
+            {
+                return state.withProperty(VARIANT, ShingleSlabType.TWO_WAY).withProperty(FACING, EnumFacing.EAST);
+            }
+            else if (!isAirDir1 && !isAirDir2 && isAirDir3 && isAirDir4)
+            {
+                return state.withProperty(VARIANT, ShingleSlabType.TWO_WAY).withProperty(FACING, EnumFacing.NORTH);
+            }
+            else if(!isAirDir1 && isAirDir2 && isAirDir3 && !isAirDir4)
+            {
+                return state.withProperty(VARIANT, ShingleSlabType.CURVED).withProperty(FACING, EnumFacing.WEST);
+            }
+            else if(isAirDir1 && !isAirDir2 && !isAirDir3 && isAirDir4)
+            {
+                return state.withProperty(VARIANT, ShingleSlabType.CURVED).withProperty(FACING, EnumFacing.EAST);
+            }
+            else if(!isAirDir1 && isAirDir2 && !isAirDir3 && isAirDir4)
+            {
+                return state.withProperty(VARIANT, ShingleSlabType.CURVED).withProperty(FACING, EnumFacing.SOUTH);
+            }
+            if(isAirDir1 && !isAirDir2 && isAirDir3 && !isAirDir4)
+            {
+                return state.withProperty(VARIANT, ShingleSlabType.CURVED).withProperty(FACING, EnumFacing.NORTH);
+            }
+        }
+        else if(amount == 3)
+        {
+            if (!isAirDir1)
+            {
+                return state.withProperty(VARIANT, ShingleSlabType.THREE_WAY).withProperty(FACING, EnumFacing.NORTH);
+            }
+            else if (!isAirDir2)
+            {
+                return state.withProperty(VARIANT, ShingleSlabType.THREE_WAY).withProperty(FACING, EnumFacing.SOUTH);
+            }
+            else if (!isAirDir3)
+            {
+                return state.withProperty(VARIANT, ShingleSlabType.THREE_WAY).withProperty(FACING, EnumFacing.WEST);
+            }
+            else
+            {
+                return state.withProperty(VARIANT, ShingleSlabType.THREE_WAY).withProperty(FACING, EnumFacing.EAST);
+            }
+        }
+        return state.withProperty(VARIANT, ShingleSlabType.FOUR_WAY);
     }
 
     @Override
@@ -95,11 +200,20 @@ public class BlockShingleSlab extends AbstractBlockMinecoloniesDirectional<Block
         return new BlockStateContainer(this, new IProperty[] {FACING, VARIANT});
     }
 
-    /**
-     * Used to determine ambient occlusion and culling when rebuilding chunks for render
-     */
     @Override
-    public boolean isOpaqueCube(IBlockState state)
+    public boolean isOpaqueCube(@NotNull final IBlockState state)
+    {
+        return false;
+    }
+
+    @Override
+    public boolean isFullCube(@NotNull final IBlockState state)
+    {
+        return false;
+    }
+
+    @Override
+    public boolean doesSideBlockRendering(@NotNull final IBlockState state, @NotNull final IBlockAccess world, @NotNull final BlockPos pos, @NotNull final EnumFacing face)
     {
         return false;
     }
