d6ae4772fb906adfd023ad7fe3b7141522c89b1c
Raycoms <ray.neiheiser@gmail.com>
2017-12-31 00:48:51 +0000

    add some tests

--- src/test/java/com/minecolonies/coremod/colony/ColonyTest.java
+++ src/test/java/com/minecolonies/coremod/colony/ColonyTest.java
@@ -1,7 +1,15 @@
 package com.minecolonies.coremod.colony;
 
+import com.minecolonies.api.colony.permissions.Player;
+import com.minecolonies.api.colony.permissions.Rank;
+import com.minecolonies.api.util.Log;
+import com.minecolonies.coremod.colony.permissions.Permissions;
 import com.minecolonies.coremod.colony.requestsystem.init.StandardFactoryControllerInitializer;
 import com.minecolonies.coremod.test.ReflectionUtil;
+import com.mojang.authlib.GameProfile;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldProvider;
@@ -15,8 +23,11 @@ import org.mockito.runners.MockitoJUnitRunner;
 
 import java.util.Iterator;
 import java.util.List;
+import java.util.UUID;
 
 import static org.junit.Assert.*;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -31,50 +42,70 @@ public class ColonyTest
     private BlockPos center;
 
     @Mock
-    private Colony colony1;
-
-    @Mock
-    private Colony colony1Copy;
-
-    @Mock
-    private Colony colony2;
-
-    @Mock
     private WorldProvider worldProvider;
 
     @Mock
     private World world;
 
     @Mock
+    private EntityPlayer player;
+
+    @Mock
     private EventBus eventBus;
 
+    @Mock
+    private Colony colony1;
+
     private ColonyList<Colony> list;
+
+    private Colony colony;
+
+    private final UUID id = UUID.randomUUID();
+
 
     @Before
     public void setup() throws NoSuchFieldException, IllegalAccessException
     {
         list = new ColonyList<>();
-
-        when(colony1.getID()).thenReturn(1);
-        when(colony1Copy.getID()).thenReturn(1);
-        when(colony2.getID()).thenReturn(2);
-        when(colony1.getCenter()).thenReturn(center);
-        when(colony1Copy.getCenter()).thenReturn(center);
-        when(colony2.getCenter()).thenReturn(center);
-        when(colony1.getWorld()).thenReturn(world);
-        when(colony1Copy.getWorld()).thenReturn(world);
-        when(colony2.getWorld()).thenReturn(world);
-
         when(worldProvider.getDimension()).thenReturn(1);
+        when(player.getGameProfile()).thenReturn(new GameProfile(id, "ray"));
         ReflectionUtil.setFinalField(world, "provider", worldProvider);
         ReflectionUtil.setStaticFinalField(MinecraftForge.class, "EVENT_BUS", eventBus);
         StandardFactoryControllerInitializer.onPreInit();
+        colony = list.create(world, center);
     }
 
     @Test
-    public void testCreate()
+    public void testToggleAnything()
     {
-        colony1.setName("blahsColony");
-        assertNotEquals(null, colony1.getT);
+        colony.setName("blahsColony");
+        assertNotEquals(null, colony.getColonyTag());
+    }
+
+    @Test
+    public void testToggleOverManager()
+    {
+        colony.getBarbManager().setCanHaveBarbEvents(true);
+        assertNotEquals(null, colony.getColonyTag());
+    }
+
+    @Test
+    public void testToggleInLoad()
+    {
+        colony.markDirty();
+        NBTTagCompound compound = colony.getColonyTag();
+        Colony test = Colony.loadColony(compound, world);
+        assertNotEquals(null, test.getColonyTag());
+    }
+
+    @Test
+    public void testLoadDifferentThanActual()
+    {
+        colony.markDirty();
+        NBTTagCompound compound = colony.getColonyTag();
+        Colony test = Colony.loadColony(compound, world);
+        assertNotEquals(null, test.getColonyTag());
+        test.setName("blahColony");
+        assertNotEquals(compound, test.getColonyTag());
     }
 }
