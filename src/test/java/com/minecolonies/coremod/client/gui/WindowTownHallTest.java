55c619f02925776782cabcf230c18c77538cebac
East <eastmaels@gmail.com>
2018-01-05 14:16:54 +0800

    fix for JMX java.lang.LinkageError

--- src/test/java/com/minecolonies/coremod/client/gui/WindowTownHallTest.java
+++ src/test/java/com/minecolonies/coremod/client/gui/WindowTownHallTest.java
@@ -9,6 +9,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 import org.powermock.reflect.Whitebox;
@@ -21,6 +22,7 @@ import static org.mockito.Mockito.*;
 //@RunWith(MockitoJUnitRunner.class)
 @RunWith(PowerMockRunner.class)
 @PrepareForTest({ColonyView.class})
+@PowerMockIgnore( {"javax.management.*"})
 public class WindowTownHallTest {
 
     @Mock
