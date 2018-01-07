0575302f71b57f1085f4d962075acd58f45ff9ee
Raycoms <ray.neiheiser@gmail.com>
2018-01-07 20:35:48 +0000

    Some fixes (#1979)
    * fix shingles resistance
    
    * fixes #1954
    
    * fix #1928
    
    * fix herder blocks
    
    * fix scrollbar, who did this? =D

--- src/api/java/com/minecolonies/api/colony/requestsystem/requestable/Tool.java
+++ src/api/java/com/minecolonies/api/colony/requestsystem/requestable/Tool.java
@@ -164,6 +164,12 @@ public class Tool implements IDeliverable
     private Set<String> getToolClasses(final ItemStack stack)
     {
         final Set set = new HashSet();
+
+        if(ItemStackUtils.isEmpty(stack))
+        {
+            return set;
+        }
+
         set.addAll(stack.getItem().getToolClasses(stack));
 
         if(stack.getItem() instanceof ItemBow)
