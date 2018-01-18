2f2e8b934e60c483bb237113c0bdc472a30635b8
Raycoms <ray.neiheiser@gmail.com>
2018-01-18 22:11:13 +0000

    Feature/colony friends (#2050)
    * Add an allies and feuds list to the colony
    * Fix a bug with the smelter

---
+++ src/api/java/com/minecolonies/api/util/constant/WindowConstants.java
@@ -0,0 +1,343 @@
+package com.minecolonies.api.util.constant;
+
+/**
+ * Class which contains all constants required for windows.
+ */
+public final class WindowConstants
+{
+    /**
+     * Id of the info button in the GUI.
+     */
+    public static final String BUTTON_INFO = "info";
+
+    /**
+     * Id of the action button in the GUI.
+     */
+    public static final String BUTTON_ACTIONS = "actions";
+
+    /**
+     * Id of the settings button in the GUI.
+     */
+    public static final String BUTTON_SETTINGS = "settings";
+
+    /**
+     * Id of the permissions button in the GUI.
+     */
+    public static final String BUTTON_PERMISSIONS = "permissions";
+
+    /**
+     * Id of the citizens button in the GUI.
+     */
+    public static final String BUTTON_CITIZENS = "citizens";
+
+    /**
+     * Id of the citizens button in the GUI.
+     */
+    public static final String BUTTON_WORKORDER = "workOrder";
+
+    /**
+     * Id of the recall button in the GUI.
+     */
+    public static final String BUTTON_RECALL = "recall";
+
+    /**
+     * Id of the change specialization button in the GUI.
+     */
+    public static final String BUTTON_CHANGE_SPEC = "changeSpec";
+
+    /**
+     * Id of the rename button in the GUI.
+     */
+    public static final String BUTTON_RENAME = "rename";
+
+    /**
+     * Id of the add player button in the GUI.
+     */
+    public static final String BUTTON_ADD_PLAYER = "addPlayer";
+
+    /**
+     * Id of the toggle job button in the GUI.
+     */
+    public static final String BUTTON_TOGGLE_JOB = "toggleJob";
+
+    /**
+     * Id of the toggle job button in the GUI.
+     */
+    public static final String BUTTON_TOGGLE_HOUSING = "toggleHousing";
+
+    /**
+     * Id of the remove player button in the GUI..
+     */
+    public static final String BUTTON_REMOVE_PLAYER = "removePlayer";
+
+    /**
+     * Id of the promote player button in the GUI..
+     */
+    public static final String BUTTON_PROMOTE = "promote";
+
+    /**
+     * Id of the demote player button in the GUI..
+     */
+    public static final String BUTTON_DEMOTE = "demote";
+
+    /**
+     * Id of the up button in the GUI.
+     */
+    public static final String BUTTON_UP = "up";
+
+    /**
+     * Id of the teleport to colony button in the GUI.
+     */
+    public static final String BUTTON_TP = "tp";
+
+    /**
+     * Id of the up button in the GUI.
+     */
+    public static final String BUTTON_DOWN = "down";
+
+    /**
+     * The id of the delete button in the GUI.
+     */
+    public static final String BUTTON_DELETE = "delete";
+
+    /**
+     * Id of the input bar to add players. in the GUI.
+     */
+    public static final String INPUT_ADDPLAYER_NAME = "addPlayerName";
+
+    /**
+     * Id of the page view in the GUI.
+     */
+    public static final String VIEW_PAGES = "pages";
+
+    /**
+     * Id of the info page in the GUI.
+     */
+    public static final String PAGE_INFO = "pageInfo";
+
+    /**
+     * Id of the actions page in the GUI.
+     */
+    public static final String PAGE_ACTIONS = "pageActions";
+
+    /**
+     * Id of the settings page in the GUI.
+     */
+    public static final String PAGE_SETTINGS = "pageSettings";
+
+    /**
+     * Id of the permissions page in the GUI.
+     */
+    public static final String PAGE_PERMISSIONS = "pagePermissions";
+
+    /**
+     * Id of the citizens page in the GUI.
+     */
+    public static final String PAGE_CITIZENS = "pageCitizens";
+
+    /**
+     * Id of the citizens page in the GUI.
+     */
+    public static final String PAGE_WORKORDER = "pageWorkOrder";
+
+    /**
+     * Id of the user list in the GUI.
+     */
+    public static final String LIST_USERS = "users";
+
+    /**
+     * Id of the citizens list in the GUI.
+     */
+    public static final String LIST_CITIZENS = "citizenList";
+
+    /**
+     * Id of the allies list in the GUI.
+     */
+    public static final String LIST_ALLIES = "allies";
+
+    /**
+     * Id of the feuds list in the GUI.
+     */
+    public static final String LIST_FEUDS = "feuds";
+
+    /**
+     * Id of the workOrder list in the GUI.
+     */
+    public static final String LIST_WORKORDER  = "workOrderList";
+
+    /**
+     * Id of the current specializations label in the GUI.
+     */
+    public static final String HAPPINESS_LABEL = "happiness";
+
+    /**
+     * Id of the name label in the GUI.
+     */
+    public static final String NAME_LABEL = "name";
+
+    /**
+     * Id of the distance label in the GUI.
+     */
+    public static final String DIST_LABEL = "dist";
+
+    /**
+     * Id of the total citizens label in the GUI.
+     */
+    public static final String TOTAL_CITIZENS_LABEL = "totalCitizens";
+
+    /**
+     * Statistics label width.
+     */
+    public static final int STATISTICS_LABEL_WIDTH = 100;
+
+    /**
+     * Statistics label height.
+     */
+    public static final int STATISTICS_LABEL_HEIGHT = 11;
+
+    /**
+     * Id of the total assignee label in the GUI.
+     */
+    public static final String ASSIGNEE_LABEL = "assignee";
+
+    /**
+     * Id of the total work label in the GUI.
+     */
+    public static final String WORK_LABEL = "work";
+
+    /**
+     * Id of the hidden workorder id in the GUI.
+     */
+    public static final String HIDDEN_WORKORDER_ID = "hiddenId";
+
+    /**
+     * The position of the hidden id in the workOrder window.
+     */
+    public static final int HIDDEN_ID_POSITION = 5;
+
+    /**
+     * The distance to move the ribbon.
+     */
+    public static final int RIBBON_OFFSET = 5;
+
+    /**
+     * Link to the xml file of the window.
+     */
+    public static final String TOWNHALL_RESOURCE_SUFFIX = ":gui/windowtownhall.xml";
+
+    /**
+     * The button to go to the previous permission settings page.
+     */
+    public static final String BUTTON_PREV_PAGE_PERM = "prevPagePerm";
+
+    /**
+     * The button to go to the next permission settings page.
+     */
+    public static final String BUTTON_NEXT_PAGE_PERM = "nextPagePerm";
+
+    /**
+     * The button to go to the officer permission settings page.
+     */
+    public static final String BUTTON_MANAGE_OFFICER = "officerPage";
+
+    /**
+     * The button to go to the friend permission settings page.
+     */
+    public static final String BUTTON_MANAGE_FRIEND = "friendPage";
+
+    /**
+     * The button to go to the neutral permission settings page.
+     */
+    public static final String BUTTON_MANAGE_NEUTRAL = "neutralPage";
+
+    /**
+     * The button to go to the hostile permission settings page.
+     */
+    public static final String BUTTON_MANAGE_HOSTILE = "hostilePage";
+
+    /**
+     * Id of the switch view of the perm pages.
+     */
+    public static final String VIEW_PERM_PAGES = "permPages";
+
+    /**
+     * Id of the switch view of the different groups.
+     */
+    public static final String VIEW_PERM_GROUPS = "userGroups";
+
+    /**
+     * The view of the permission management.
+     */
+    public static final String PERMISSION_VIEW = "managePermissions";
+
+    /**
+     * Button to trigger the permission changes.
+     */
+    public static final String BUTTON_TRIGGER = "trigger";
+
+    /**
+     * The id of the officer permission view.
+     */
+    public static final String VIEW_OFFICER = "officer";
+
+    /**
+     * The id of the officer permission view.
+     */
+    public static final String VIEW_FRIEND = "friend";
+
+    /**
+     * The id of the officer permission view.
+     */
+    public static final String VIEW_NEUTRAL = "neutral";
+
+    /**
+     * The id of the officer permission view.
+     */
+    public static final String VIEW_HOSTILE = "hostile";
+
+    /**
+     * The list of actions for a certain permission group.
+     */
+    public static final String LIST_ACTIONS = "actions";
+
+    /**
+     * The list of free blocks to be interacted with.
+     */
+    public static final String LIST_FREE_BLOCKS = "blocks";
+
+    /**
+     * Key to get readable permission values.
+     */
+    public static final String KEY_TO_PERMISSIONS = "com.minecolonies.coremod.permission.";
+
+    /**
+     * Ignored index starts at this line, ignore this amount after this index.
+     */
+    public static final int IGNORE_INDEX = 3;
+
+    /**
+     * Button clicked to add a block to the colony to be freely interacted with.
+     */
+    public static final String BUTTON_ADD_BLOCK = "addBlock";
+
+    /**
+     * Input field with the blockName or position to add.
+     */
+    public static final String INPUT_BLOCK_NAME = "addBlockName";
+
+    /**
+     * Button to remove a block or position of the list.
+     */
+    public static final String BUTTON_REMOVE_BLOCK = "removeBlock";
+
+
+    /**
+     * private constructor to hide implicit public one.
+     */
+    private WindowConstants()
+    {
+        /**
+         * Intentionally left empty.
+         */
+    }
+}
