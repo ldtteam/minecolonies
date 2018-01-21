21a7f0933f845457dd01bbac5bd4634749d13079
Raycoms <ray.neiheiser@gmail.com>
2018-01-21 19:28:52 +0000

    fix corners after new style

--- src/main/java/com/minecolonies/coremod/colony/buildings/AbstractBuilding.java
+++ src/main/java/com/minecolonies/coremod/colony/buildings/AbstractBuilding.java
@@ -1036,6 +1036,7 @@ public abstract class AbstractBuilding implements IRequestResolverProvider, IReq
     {
         this.style = style;
         this.markDirty();
+        this.onUpgradeComplete(this.getBuildingLevel());
     }
 
     /**
