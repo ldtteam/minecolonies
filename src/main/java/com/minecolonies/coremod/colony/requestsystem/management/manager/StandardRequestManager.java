1ad8837afcceab1b2102abb18a65bd3297d64f35
Marc Hermans <OrionDevelopment@outlook.com>
2018-01-29 14:13:24 +0100

    Fix duplication of the SPRR and the SRRR

--- src/main/java/com/minecolonies/coremod/colony/requestsystem/management/manager/StandardRequestManager.java
+++ src/main/java/com/minecolonies/coremod/colony/requestsystem/management/manager/StandardRequestManager.java
@@ -359,9 +359,6 @@ public class StandardRequestManager implements IStandardRequestManager
         this.playerResolver.onSystemReset();
         this.retryingResolver.onSystemReset();
 
-        ResolverHandler.registerResolver(this, this.playerResolver);
-        ResolverHandler.registerResolver(this, this.retryingResolver);
-
         version = -1;
         UpdateHandler.handleUpdate(this);
     }
