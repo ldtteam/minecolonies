3f60714113b28f94d14ac5bfa9b1a44bd56b3726
Raycoms <ray.neiheiser@gmail.com>
2018-03-19 14:36:44 +0000

    remove that logging

--- src/main/java/com/minecolonies/coremod/colony/ColonyManager.java
+++ src/main/java/com/minecolonies/coremod/colony/ColonyManager.java
@@ -280,7 +280,6 @@ public final class ColonyManager
     {
         final IColonyTagCapability cap = chunk.getCapability(CLOSE_COLONY_CAP, null);
         storage.applyToCap(cap);
-        Log.getLogger().warn("Loading Chunk: " + chunk.x + " " + chunk.z);
         chunk.markDirty();
         MineColonies.getNetwork().sendToAll(new UpdateChunkCapabilityMessage(cap, chunk.x, chunk.z));
     }
