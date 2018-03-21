68cf81dc1c48395ebaa3f5060a131de62829dc33
Asherslab <Asher.parry@icloud.com>
2018-03-22 00:22:29 +1000

    Port 1.12 Turn of Millking (#2327)
    * Allows users to turn Of and On cowboy milking of cows

--- src/main/java/com/minecolonies/coremod/colony/buildings/BuildingCowboy.java
+++ src/main/java/com/minecolonies/coremod/colony/buildings/BuildingCowboy.java
@@ -1,12 +1,16 @@
 package com.minecolonies.coremod.colony.buildings;
 
 import com.minecolonies.blockout.views.Window;
-import com.minecolonies.coremod.client.gui.WindowHutWorkerPlaceholder;
+import com.minecolonies.coremod.MineColonies;
+import com.minecolonies.coremod.client.gui.WindowHutCowboy;
 import com.minecolonies.coremod.colony.CitizenData;
 import com.minecolonies.coremod.colony.Colony;
 import com.minecolonies.coremod.colony.ColonyView;
 import com.minecolonies.coremod.colony.jobs.AbstractJob;
 import com.minecolonies.coremod.colony.jobs.JobCowboy;
+import com.minecolonies.coremod.network.messages.CowboySetMilkCowsMessage;
+import io.netty.buffer.ByteBuf;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.math.BlockPos;
 import org.jetbrains.annotations.NotNull;
 
@@ -21,14 +25,19 @@ public class BuildingCowboy extends AbstractBuildingWorker
     private static final String COWBOY          = "Cowboy";
 
     /**
-     * Description of the block used to set this block.
+     * NBT Tag for milkCows boolean.
      */
-    private static final String COWBOY_HUT_NAME = "cowboyHut";
+    private static final String NBT_MILK_COWS = "MILK_COWS";
 
     /**
      * Max building level of the hut.
      */
     private static final int MAX_BUILDING_LEVEL = 5;
+
+    /**
+     * Milk Cows or not.
+     */
+    private boolean milkCows = true;
 
     /**
      * Instantiates the building.
@@ -67,11 +76,48 @@ public class BuildingCowboy extends AbstractBuildingWorker
         return new JobCowboy(citizen);
     }
 
+    @Override
+    public void serializeToView(@NotNull final ByteBuf buf)
+    {
+        super.serializeToView(buf);
+        buf.writeBoolean(milkCows);
+    }
+
+    @Override
+    public void writeToNBT(@NotNull final NBTTagCompound compound)
+    {
+        super.writeToNBT(compound);
+        compound.setBoolean(NBT_MILK_COWS, this.milkCows);
+    }
+
+    @Override
+    public void readFromNBT(@NotNull final NBTTagCompound compound)
+    {
+        super.readFromNBT(compound);
+        this.milkCows = compound.getBoolean(NBT_MILK_COWS);
+    }
+
+    public boolean isMilkCows()
+    {
+        return milkCows;
+    }
+
+    public void setMilkCows(final boolean milkCows)
+    {
+        this.milkCows = milkCows;
+        markDirty();
+    }
+
     /**
      * ClientSide representation of the building.
      */
     public static class View extends AbstractBuildingWorker.View
     {
+        /**
+         * Milk Cows or not.
+         */
+        private boolean milkCows = true;
+
         /**
          * Instantiates the view of the building.
          * @param c the colonyView.
@@ -86,7 +132,7 @@ public class BuildingCowboy extends AbstractBuildingWorker
         @Override
         public Window getWindow()
         {
-            return new WindowHutWorkerPlaceholder<AbstractBuildingWorker.View>(this, COWBOY_HUT_NAME);
+            return new WindowHutCowboy(this);
         }
 
         @NotNull
@@ -101,6 +147,24 @@ public class BuildingCowboy extends AbstractBuildingWorker
         public Skill getSecondarySkill()
         {
             return Skill.STRENGTH;
+        }
+
+        public void setMilkCows(final boolean milkCows)
+        {
+            MineColonies.getNetwork().sendToServer(new CowboySetMilkCowsMessage(this, milkCows));
+            this.milkCows = milkCows;
+        }
+
+        public boolean isMilkCows()
+        {
+            return milkCows;
+        }
+
+        @Override
+        public void deserialize(@NotNull final ByteBuf buf)
+        {
+            super.deserialize(buf);
+            milkCows = buf.readBoolean();
         }
     }
 }
