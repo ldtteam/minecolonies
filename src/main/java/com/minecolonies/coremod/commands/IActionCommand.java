5f9f7bb9ece5d54dcb44896fdba792cb13b9584a
Mike Kienenberger <mkienenb@gmail.com>
2018-04-02 09:23:10 -0400

    Fix broken javadoc

--- src/main/java/com/minecolonies/coremod/commands/IActionCommand.java
+++ src/main/java/com/minecolonies/coremod/commands/IActionCommand.java
@@ -16,7 +16,7 @@ public interface IActionCommand
      *
      * @param server the server this is executed on.
      * @param sender this commands executor.
-     * @param ActionMenuState actionMenuState for this execution containing arguments
+     * @param actionMenuState for this execution containing arguments
      * @throws CommandException if something goes wrong (like wrong syntax).
      */
     void execute(@NotNull MinecraftServer server, @NotNull ICommandSender sender, @NotNull ActionMenuState actionMenuState) throws CommandException;
