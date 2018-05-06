c14eede68e70f73ab05970875a2bbff8726ed6d9
Raycoms <ray.neiheiser@gmail.com>
2018-05-06 02:32:05 +0100

    disable changing on gamemode0

--- src/main/java/com/minecolonies/coremod/client/gui/WindowScan.java
+++ src/main/java/com/minecolonies/coremod/client/gui/WindowScan.java
@@ -23,6 +23,7 @@ import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.TextComponentString;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -70,6 +71,20 @@ public class WindowScan extends AbstractWindowSkeleton
     private BlockPos pos2;
 
     /**
+     * Pos 1 text fields.
+     */
+    private final TextField pos1x;
+    private final TextField pos1y;
+    private final TextField pos1z;
+
+    /**
+     * Pos 2 text fields.
+     */
+    private final TextField pos2x;
+    private final TextField pos2y;
+    private final TextField pos2z;
+
+    /**
      * Constructor for when the player wants to scan something.
      * @param pos1 the first pos.
      * @param pos2 the second pos.
@@ -82,19 +97,39 @@ public class WindowScan extends AbstractWindowSkeleton
         registerButton(BUTTON_CONFIRM, this::confirmClicked);
         registerButton(BUTTON_CANCEL, this::discardClicked);
         registerButton(BUTTON_SHOW_RES, this::updateResources);
+
+        pos1x = findPaneOfTypeByID(POS1X_LABEL, TextField.class);
+        pos1y = findPaneOfTypeByID(POS1Y_LABEL, TextField.class);
+        pos1z = findPaneOfTypeByID(POS1Z_LABEL, TextField.class);
+
+        pos2x = findPaneOfTypeByID(POS2X_LABEL, TextField.class);
+        pos2y = findPaneOfTypeByID(POS2Y_LABEL, TextField.class);
+        pos2z = findPaneOfTypeByID(POS2Z_LABEL, TextField.class);
     }
 
     @Override
     public void onOpened()
     {
         super.onOpened();
-        findPaneOfTypeByID(POS1X_LABEL, TextField.class).setText(String.valueOf(pos1.getX()));
-        findPaneOfTypeByID(POS1Y_LABEL, TextField.class).setText(String.valueOf(pos1.getY()));
-        findPaneOfTypeByID(POS1Z_LABEL, TextField.class).setText(String.valueOf(pos1.getZ()));
 
-        findPaneOfTypeByID(POS2X_LABEL, TextField.class).setText(String.valueOf(pos2.getX()));
-        findPaneOfTypeByID(POS2Y_LABEL, TextField.class).setText(String.valueOf(pos2.getY()));
-        findPaneOfTypeByID(POS2Z_LABEL, TextField.class).setText(String.valueOf(pos2.getZ()));
+        if (!Minecraft.getMinecraft().player.capabilities.isCreativeMode)
+        {
+            pos1x.disable();
+            pos1y.disable();
+            pos1z.disable();
+
+            pos2x.disable();
+            pos2y.disable();
+            pos2z.disable();
+        }
+
+        pos1x.setText(String.valueOf(pos1.getX()));
+        pos1y.setText(String.valueOf(pos1.getY()));
+        pos1z.setText(String.valueOf(pos1.getZ()));
+
+        pos2x.setText(String.valueOf(pos2.getX()));
+        pos2y.setText(String.valueOf(pos2.getY()));
+        pos2z.setText(String.valueOf(pos2.getZ()));
     }
 
     /**
@@ -112,13 +147,13 @@ public class WindowScan extends AbstractWindowSkeleton
     {
         final String name = findPaneOfTypeByID(NAME_LABEL, TextField.class).getText();
 
-        final int x1 = Integer.parseInt(findPaneOfTypeByID(POS1X_LABEL, TextField.class).getText());
-        final int y1 = Integer.parseInt(findPaneOfTypeByID(POS1Y_LABEL, TextField.class).getText());
-        final int z1 = Integer.parseInt(findPaneOfTypeByID(POS1Z_LABEL, TextField.class).getText());
+        final int x1 = Integer.parseInt(pos1x.getText());
+        final int y1 = Integer.parseInt(pos1y.getText());
+        final int z1 = Integer.parseInt(pos1z.getText());
 
-        final int x2 = Integer.parseInt(findPaneOfTypeByID(POS2X_LABEL, TextField.class).getText());
-        final int y2 = Integer.parseInt(findPaneOfTypeByID(POS2Y_LABEL, TextField.class).getText());
-        final int z2 = Integer.parseInt(findPaneOfTypeByID(POS2Z_LABEL, TextField.class).getText());
+        final int x2 = Integer.parseInt(pos2x.getText());
+        final int y2 = Integer.parseInt(pos2y.getText());
+        final int z2 = Integer.parseInt(pos2z.getText());
 
         MineColonies.getNetwork().sendToServer(new ScanOnServerMessage(new BlockPos(x1, y1, z1), new BlockPos(x2, y2, z2), name));
         close();
@@ -139,15 +174,23 @@ public class WindowScan extends AbstractWindowSkeleton
      */
     private void updateResources()
     {
-        final int x1 = Integer.parseInt(findPaneOfTypeByID(POS1X_LABEL, TextField.class).getText());
-        final int y1 = Integer.parseInt(findPaneOfTypeByID(POS1Y_LABEL, TextField.class).getText());
-        final int z1 = Integer.parseInt(findPaneOfTypeByID(POS1Z_LABEL, TextField.class).getText());
-        pos1 = new BlockPos(x1, y1, z1);
+        try
+        {
+            final int x1 = Integer.parseInt(pos1x.getText());
+            final int y1 = Integer.parseInt(pos1y.getText());
+            final int z1 = Integer.parseInt(pos1z.getText());
+            pos1 = new BlockPos(x1, y1, z1);
 
-        final int x2 = Integer.parseInt(findPaneOfTypeByID(POS2X_LABEL, TextField.class).getText());
-        final int y2 = Integer.parseInt(findPaneOfTypeByID(POS2Y_LABEL, TextField.class).getText());
-        final int z2 = Integer.parseInt(findPaneOfTypeByID(POS2Z_LABEL, TextField.class).getText());
-        pos2 = new BlockPos(x2, y2, z2);
+            final int x2 = Integer.parseInt(pos2x.getText());
+            final int y2 = Integer.parseInt(pos2y.getText());
+            final int z2 = Integer.parseInt(pos2z.getText());
+            pos2 = new BlockPos(x2, y2, z2);
+        }
+        catch(final NumberFormatException e)
+        {
+            Minecraft.getMinecraft().player.sendMessage(new TextComponentString("Invalid Number - Closing!"));
+            close();
+        }
         
         final World world = Minecraft.getMinecraft().world;
         resources.clear();
