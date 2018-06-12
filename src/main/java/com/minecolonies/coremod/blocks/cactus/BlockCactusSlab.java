ff3d8fcd713a49a69901cce0b949bc01f2651f69
Raycoms <ray.neiheiser@gmail.com>
2018-06-12 18:30:13 -0300

    unabstract the class

--- src/main/java/com/minecolonies/coremod/blocks/cactus/BlockCactusSlab.java
+++ src/main/java/com/minecolonies/coremod/blocks/cactus/BlockCactusSlab.java
@@ -5,10 +5,11 @@ import com.minecolonies.coremod.blocks.AbstractBlockSlab;
 import com.minecolonies.coremod.creativetab.ModCreativeTabs;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.item.ItemStack;
 
 import java.util.Locale;
 
-public abstract class BlockCactusSlab extends AbstractBlockSlab<BlockCactusSlab> {
+public class BlockCactusSlab extends AbstractBlockSlab<BlockCactusSlab> {
 
     public BlockCactusSlab() {
         super(Material.WOOD);
@@ -16,5 +17,23 @@ public abstract class BlockCactusSlab extends AbstractBlockSlab<BlockCactusSlab>
         setUnlocalizedName(Constants.MOD_ID.toLowerCase(Locale.ENGLISH) + "." + "blockcactusslab");
         setCreativeTab(ModCreativeTabs.MINECOLONIES);
         setSoundType(SoundType.WOOD);
+    }
+
+    @Override
+    public String getUnlocalizedName(final int i)
+    {
+        return null;
+    }
+
+    @Override
+    public boolean isDouble()
+    {
+        return false;
+    }
+
+    @Override
+    public Comparable<?> getTypeForItem(final ItemStack itemStack)
+    {
+        return null;
     }
 }
