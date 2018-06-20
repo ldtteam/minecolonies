e853db276d96b89e0cc0450f5ad81a31a1cee437
Cazsius <cazsius0518@gmail.com>
2018-06-20 08:47:04 -0400

    Feature/#1871 cactus plank (#2546)
    Adds Cactus Blocks: Slab, Door, Trapdoor, Stair, Plank.

---
+++ src/main/java/com/minecolonies/coremod/blocks/huts/BlockHutBlacksmith.java
@@ -0,0 +1,24 @@
+package com.minecolonies.coremod.blocks.huts;
+
+import com.minecolonies.coremod.blocks.AbstractBlockHut;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Hut for the blacksmith.
+ * No different from {@link AbstractBlockHut}
+ */
+public class BlockHutBlacksmith extends AbstractBlockHut<BlockHutBlacksmith>
+{
+    public BlockHutBlacksmith()
+    {
+        //No different from Abstract parent
+        super();
+    }
+
+    @NotNull
+    @Override
+    public String getName()
+    {
+        return "blockHutBlacksmith";
+    }
+}
---
+++ src/main/java/com/minecolonies/coremod/blocks/huts/BlockHutCowboy.java
@@ -0,0 +1,24 @@
+package com.minecolonies.coremod.blocks.huts;
+
+import com.minecolonies.coremod.blocks.AbstractBlockHut;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Hut for the shepherd.
+ * No different from {@link AbstractBlockHut}
+ */
+public class BlockHutCowboy extends AbstractBlockHut<BlockHutCowboy>
+{
+    public BlockHutCowboy()
+    {
+        //No different from Abstract parent
+        super();
+    }
+
+    @NotNull
+    @Override
+    public String getName()
+    {
+        return "blockHutCowboy";
+    }
+}
