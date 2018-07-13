663a9b919d28d304fbe6047541f273bf2f96a0f4
Unknown <kevin@freemailclient.com>
2018-07-12 21:45:21 -0500

    formatting changes.
    making changes to make it more simplified code.

--- src/main/java/com/minecolonies/coremod/colony/buildings/workerbuildings/BuildingLumberjack.java
+++ src/main/java/com/minecolonies/coremod/colony/buildings/workerbuildings/BuildingLumberjack.java
@@ -212,37 +212,19 @@ public class BuildingLumberjack extends AbstractBuildingWorker
     public void readFromNBT(@NotNull final NBTTagCompound compound)
     {
         super.readFromNBT(compound);
-        if (treesToFell.isEmpty())
+        final NBTTagList saplingTagList = compound.getTagList(TAG_SAPLINGS, Constants.NBT.TAG_COMPOUND);
+        for (int i = 0; i < saplingTagList.tagCount(); ++i)
         {
-            final NBTTagList saplingTagList = compound.getTagList(TAG_SAPLINGS, Constants.NBT.TAG_COMPOUND);
-            for (int i = 0; i < saplingTagList.tagCount(); ++i)
-            {
-                final NBTTagCompound saplingCompound = saplingTagList.getCompoundTagAt(i);
-                final ItemStack stack = new ItemStack(saplingCompound);
-                final boolean cut = saplingCompound.getBoolean(TAG_CUT);
-                treesToFell.put(new ItemStorage(stack), cut);
-            }
-        }
-		else 
-		{
-			// We have a list of sapplings already, but we nee to load in the
-			// NBT file.
-			// check to see if it exist and if so then update it. Otherwise skip
-			// it,
-			// as it doesn't exist in minecraft anymore.
-			final NBTTagList saplingTagList = compound.getTagList(TAG_SAPLINGS, Constants.NBT.TAG_COMPOUND);
-			for (int i = 0; i < saplingTagList.tagCount(); ++i) {
-				final NBTTagCompound saplingCompound = saplingTagList.getCompoundTagAt(i);
-				final ItemStack stack = new ItemStack(saplingCompound);
-				final boolean cut = saplingCompound.getBoolean(TAG_CUT);
-				final ItemStorage storage = new ItemStorage(stack);
-				if (treesToFell.containsKey(storage)) {
-					treesToFell.put(new ItemStorage(stack), cut);
-				}
+            final NBTTagCompound saplingCompound = saplingTagList.getCompoundTagAt(i);
+            final ItemStack stack = new ItemStack(saplingCompound);
+            final boolean cut = saplingCompound.getBoolean(TAG_CUT);
+            final ItemStorage storage = new ItemStorage(stack); 
+            if (treesToFell.containsKey(storage)) {
+				treesToFell.put(new ItemStorage(stack), cut);
 			}
+        }
 
-		}
-    	if (compound.hasKey(TAG_REPLANT))
+        if (compound.hasKey(TAG_REPLANT))
         {
             replant = compound.getBoolean(TAG_REPLANT);
         }
