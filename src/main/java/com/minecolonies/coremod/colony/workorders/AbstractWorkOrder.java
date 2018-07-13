fe25d4fcfd0f5f4f1d36b8686aab4b7c5890aadc
Unknown <kevin@freemailclient.com>
2018-07-12 21:42:25 -0500

    Formatting changes
    fixing some formatting issues.

--- src/main/java/com/minecolonies/coremod/colony/workorders/AbstractWorkOrder.java
+++ src/main/java/com/minecolonies/coremod/colony/workorders/AbstractWorkOrder.java
@@ -22,7 +22,7 @@ import static com.minecolonies.api.util.constant.Suppression.UNUSED_METHOD_PARAM
 public abstract class AbstractWorkOrder
 {
     private static final String                                          TAG_TYPE       = "type";
-    private static final String                                          TAG_PRIORITY   = "priority";
+    private static final String                                          TAG_TH_PRIORITY   = "priority";
     private static final String                                          TAG_ID         = "id";
     private static final String                                          TAG_CLAIMED_BY = "claimedBy";
     //  Job and View Class Mapping
@@ -113,7 +113,7 @@ public abstract class AbstractWorkOrder
         }
         try
         {
-        	order.setPriority(compound.getInteger(TAG_PRIORITY));
+        	order.setPriority(compound.getInteger(TAG_TH_PRIORITY));
             order.readFromNBT(compound);
         }
         catch (final RuntimeException ex)
@@ -134,7 +134,7 @@ public abstract class AbstractWorkOrder
     public void readFromNBT(@NotNull final NBTTagCompound compound)
     {
         id = compound.getInteger(TAG_ID);
-        priority = compound.getInteger(TAG_PRIORITY);
+        priority = compound.getInteger(TAG_TH_PRIORITY);
         claimedBy = compound.getInteger(TAG_CLAIMED_BY);
     }
 
@@ -281,7 +281,7 @@ public abstract class AbstractWorkOrder
             throw new IllegalStateException(this.getClass() + " is missing a mapping! This is a bug!");
         }
 
-        compound.setInteger(TAG_PRIORITY, priority);
+        compound.setInteger(TAG_TH_PRIORITY, priority);
         compound.setString(TAG_TYPE, s);
         compound.setInteger(TAG_ID, id);
         if (claimedBy != 0)
