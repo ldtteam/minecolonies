a1e5ce7b126742d2e39fe1df07fae4d6fd9e4e63
Raycoms <ray.neiheiser@gmail.com>
2018-07-22 17:34:58 -0300

    fix the code

--- src/api/java/com/minecolonies/api/compatibility/CompatibilityManager.java
+++ src/api/java/com/minecolonies/api/compatibility/CompatibilityManager.java
@@ -2,6 +2,8 @@ package com.minecolonies.api.compatibility;
 
 import com.google.common.collect.BiMap;
 import com.google.common.collect.HashBiMap;
+import com.google.common.collect.ImmutableList;
+import com.minecolonies.api.configuration.Configurations;
 import com.minecolonies.api.crafting.ItemStorage;
 import com.minecolonies.api.util.ItemStackUtils;
 import com.minecolonies.api.util.Log;
@@ -12,6 +14,7 @@ import net.minecraft.block.BlockOre;
 import net.minecraft.block.BlockRedstoneOre;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
@@ -25,6 +28,10 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Spliterator;
+import java.util.Spliterators;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 import static com.minecolonies.api.util.constant.Constants.ORE_STRING;
 import static com.minecolonies.api.util.constant.Constants.SAPLINGS;
@@ -53,6 +60,11 @@ public class CompatibilityManager implements ICompatibilityManager
     private final List<Block> ores = new ArrayList<>();
 
     /**
+     * List of all the items that can be composted
+     */
+    private final List<ItemStorage> compostableItems = new ArrayList<>();
+
+    /**
      * Instantiates the compatibilityManager.
      */
     public CompatibilityManager()
@@ -71,6 +83,55 @@ public class CompatibilityManager implements ICompatibilityManager
                 discoverOres(string);
             }
         }
+        discoverCompostableItems();
+    }
+
+    private void discoverCompostableItems()
+    {
+        //Todo: implement this method
+        if (compostableItems.isEmpty())
+        {
+            compostableItems.addAll(
+              ImmutableList.copyOf(StreamSupport.stream(Spliterators.spliteratorUnknownSize(Item.REGISTRY.iterator(), Spliterator.ORDERED), false).flatMap(item -> {
+                  final NonNullList<ItemStack> stacks = NonNullList.create();
+                  try
+                  {
+                      item.getSubItems(CreativeTabs.SEARCH, stacks);
+                  }
+                  catch (Exception ex)
+                  {
+                      Log.getLogger().warn("Failed to get sub items from: " + item.getRegistryName());
+                  }
+
+
+                  return stacks.stream().filter(CompatabilityManager::isCompost); // Here where we do ::isOre you could add the check from the TileEntity if the stack matches the requirements
+
+              }).map(ItemStorage::new).collect(Collectors.toList())));
+        }
+    }
+
+    public static boolean isCompost(final ItemStack itemStack)
+    {
+        if(itemStack.isEmpty())
+        {
+            return false;
+        }
+
+        for(final String string : Configurations.gameplay.listOfCompostableItems)
+        {
+            if(itemStack.getItem().getRegistryName().toString().equals(string))
+            {
+                return true;
+            }
+            for(final int id: OreDictionary.getOreIDs(itemStack))
+            {
+                if (OreDictionary.getOreName(id).equals(string))
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
     @Override
