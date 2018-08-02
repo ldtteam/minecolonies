34d662fca9aa2a0462558b619e981e4ecb1bbc34
bob0bob <30982580+bob0bob@users.noreply.github.com>
2018-08-02 07:27:46 -0500

    Happinesss - possible changes. enhancement in progress #2600 (#2691)
    * Happinesss - possible changes. enhancement in progress #2600
    
    This is an update on some of the new features of the new happiness system.
    Features of the new happiness system:
    
    Each citizen has their own happiness indicator.
    You can view the citizen happiness indicator by right clicking on citizen.
    Also, can view citizen happiness through Town Hall
    Colony has its own happiness indicator.
    
    Total happiness on the Town Gui is a combination of the two items from above.
    
    Colony Happiness:
    Citizen to Guard Ratio will modify the happiness
    Average food Saturation for all citizens will modify the happiness
    Citizen to Housing ration will modify the happiness
    When a citizen dies it will modify the happiness
    Death modifier will stay around for a period of days before it drops off.
    It keeps a list of all death in a certain period of time and each death modifies happiness
    
    Citizen Happiness:
    base happiness
    Food saturation will modify the happiness
    If player is unable to eat when they go to eat will affect happiness
    Citizen getting hit will modify happiness
    Citizen not having a house will modify happiness
    Citizen will start demanding a house to live in after a period of time without a house.
    Citizen not have a job will modify happiness
    will start demanding a job to work at after a period of time without a job.
    A farmer having fields they are not able to farm will modify happiness
    A farmer having no fields will modify happiness
    Citizen not having a required tool to do their job will modify happiness
    will start demanding the tool after a period of day without requesting being fulfilled.

--- src/main/java/com/minecolonies/coremod/entity/EntityCitizen.java
+++ src/main/java/com/minecolonies/coremod/entity/EntityCitizen.java
@@ -325,7 +325,11 @@ public class EntityCitizen extends AbstractEntityCitizen
         }
 
         citizenItemHandler.updateArmorDamage(damage);
-
+        if (citizenData != null) 
+        { 
+            getCitizenData().getCitizenHappinessHandler().setDamageModifier(); 
+        } 
+        
         return result;
     }
 
@@ -408,7 +412,7 @@ public class EntityCitizen extends AbstractEntityCitizen
 
             citizenExperienceHandler.dropExperience();
             this.setDead();
-            citizenColonyHandler.getColony().decreaseOverallHappiness(penalty);
+            citizenColonyHandler.getColony().getHappinessData().setDeathModifier(penalty,citizenJobHandler.getColonyJob() instanceof AbstractJobGuard); 
             triggerDeathAchievement(damageSource, citizenJobHandler.getColonyJob());
             citizenChatHandler.notifyDeath(damageSource);
             citizenColonyHandler.getColony().getCitizenManager().removeCitizen(getCitizenData());
@@ -605,7 +609,11 @@ public class EntityCitizen extends AbstractEntityCitizen
 
             if (citizenData.getSaturation() < HIGH_SATURATION)
             {
-                tryToEat();
+                citizenData.getCitizenHappinessHandler().setFoodModifier(tryToEat()); 
+            } 
+            else  
+            { 
+                citizenData.getCitizenHappinessHandler().setSaturated();
             }
 
             if((distanceWalkedModified + 1.0) % ACTIONS_EACH_BLOCKS_WALKED == 0)
@@ -670,16 +678,18 @@ public class EntityCitizen extends AbstractEntityCitizen
     }
 
     /**
-     * Lets the citizen tryToEat to replentish saturation.
+     * Lets the citizen tryToEat to replenish saturation. 
+     *  
+     * @return return true or not if citizen eat food. 
      */
-    private void tryToEat()
+    private boolean tryToEat()
     {
         final int slot = InventoryUtils.findFirstSlotInProviderWith(this,
           itemStack -> !ItemStackUtils.isEmpty(itemStack) && itemStack.getItem() instanceof ItemFood);
 
         if (slot == -1)
         {
-            return;
+            return false;
         }
 
         final ItemStack stack = getCitizenData().getInventory().getStackInSlot(slot);
@@ -690,6 +700,7 @@ public class EntityCitizen extends AbstractEntityCitizen
             getCitizenData().getInventory().decrStackSize(slot, 1);
             citizenData.markDirty();
         }
+        return true;
     }
 
     /**
