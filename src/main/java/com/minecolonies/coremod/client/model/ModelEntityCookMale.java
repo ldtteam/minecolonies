48c1213ee24ed8d768e54c16876301ec444f844e
Raycoms <ray.neiheiser@gmail.com>
2018-10-15 23:13:39 -0300

    Cook and cow and chicken farmer models

---
+++ src/main/java/com/minecolonies/coremod/client/model/ModelEntityCookMale.java
@@ -0,0 +1,101 @@
+package com.minecolonies.coremod.client.model;
+
+import net.minecraft.client.model.ModelBiped;
+import net.minecraft.client.model.ModelRenderer;
+import net.minecraft.entity.Entity;
+import org.jetbrains.annotations.NotNull;
+
+public class ModelEntityCookMale extends ModelBiped
+{
+    //fields
+
+    public ModelEntityCookMale()
+    {
+        ModelRenderer prism;
+        ModelRenderer plane;
+
+        textureWidth = 256;
+        textureHeight = 128;
+
+        bipedRightArm = new ModelRenderer(this, 40, 16);
+        bipedRightArm.addBox(-3F, -2F, -2F, 4, 12, 4);
+        bipedRightArm.setRotationPoint(-5F, 2F, 0F);
+        bipedRightArm.setTextureSize(256, 128);
+        bipedRightArm.mirror = true;
+        setRotation(bipedRightArm, 0F, 0F, 0F);
+
+        bipedLeftArm.mirror = true;
+        bipedLeftArm = new ModelRenderer(this, 40, 16);
+        bipedLeftArm.addBox(-1F, -2F, -2F, 4, 12, 4);
+        bipedLeftArm.setRotationPoint(5F, 2F, 0F);
+        bipedLeftArm.setTextureSize(256, 128);
+        bipedLeftArm.mirror = true;
+        setRotation(bipedLeftArm, 0F, 0F, 0F);
+
+        bipedRightLeg = new ModelRenderer(this, 0, 16);
+        bipedRightLeg.addBox(-2F, 0F, -2F, 4, 12, 4);
+        bipedRightLeg.setRotationPoint(-2F, 12F, 0F);
+        bipedRightLeg.setTextureSize(256, 128);
+        bipedRightLeg.mirror = true;
+        setRotation(bipedRightLeg, 0F, 0F, 0F);
+
+        bipedLeftLeg = new ModelRenderer(this, 0, 16);
+        bipedLeftLeg.addBox(-2F, 0F, -2F, 4, 12, 4);
+        bipedLeftLeg.setRotationPoint(2F, 12F, 0F);
+        bipedLeftLeg.setTextureSize(256, 128);
+        bipedLeftLeg.mirror = true;
+        setRotation(bipedLeftLeg, 0F, 0F, 0F);
+
+        bipedHead = new ModelRenderer(this, 0, 0);
+        bipedHead.addBox(-4F, -8F, -4F, 8, 8, 8);
+        bipedHead.setRotationPoint(0F, 0F, 0F);
+        bipedHead.setTextureSize(256, 128);
+        bipedHead.mirror = true;
+        setRotation(bipedHead, 0F, 0F, 0F);
+
+        bipedBody = new ModelRenderer(this, 16, 16);
+        bipedBody.addBox(-4F, 0F, -2F, 8, 12, 4);
+        bipedBody.setRotationPoint(0F, 0F, 0F);
+        bipedBody.setTextureSize(256, 128);
+        bipedBody.mirror = true;
+        setRotation(bipedBody, 0F, 0F, 0F);
+
+        prism = new ModelRenderer(this, 0, 0);
+        prism.addBox(2F, -12F, 0F, 3, 1, 1);
+        prism.setRotationPoint(-3F, 11F, 2F);
+        prism.setTextureSize(256, 128);
+        prism.mirror = true;
+        setRotation(prism, 0F, 0F, 0F);
+
+        plane = new ModelRenderer(this, 0, 2);
+        plane.addBox(2F, -12F, 0F, 3, 4, 0);
+        plane.setRotationPoint(-3F, 12F, 3F);
+        plane.setTextureSize(256, 128);
+        plane.mirror = true;
+        setRotation(plane, 0F, 0F, 0F);
+
+        bipedRightLeg.addChild(prism);
+        bipedRightLeg.addChild(plane);
+    }
+
+    @Override
+    public void render(
+      @NotNull final Entity entity,
+                       final float limbSwing,
+                       final float limbSwingAmount,
+                       final float ageInTicks,
+                       final float netHeadYaw,
+                       final float headPitch,
+                       final float scaleFactor)
+    {
+        super.render(entity, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scaleFactor);
+        setRotationAngles(limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scaleFactor, entity);
+    }
+
+    private void setRotation(@NotNull final ModelRenderer model, final float x, final float y, final float z)
+    {
+        model.rotateAngleX = x;
+        model.rotateAngleY = y;
+        model.rotateAngleZ = z;
+    }
+}
