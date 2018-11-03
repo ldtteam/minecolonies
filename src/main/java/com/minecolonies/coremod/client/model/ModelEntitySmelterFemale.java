a349e0ee5b29e4c056072de1e4c300130437d1a5
Raycoms <ray.neiheiser@gmail.com>
2018-11-03 11:51:39 -0300

    Feature/more models (#3032)
    Adds female models for all 4 herders
    Adds chest texture for baker chest
    Makes worker teleport down but not up
    Add smelter model

---
+++ src/main/java/com/minecolonies/coremod/client/model/ModelEntitySmelterFemale.java
@@ -0,0 +1,146 @@
+package com.minecolonies.coremod.client.model;
+
+import net.minecraft.client.model.ModelBiped;
+import net.minecraft.client.model.ModelRenderer;
+import net.minecraft.entity.Entity;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * The smelter female model.
+ */
+public class ModelEntitySmelterFemale extends ModelBiped
+{
+    /**
+     * Constructor which generates the model.
+     */
+    public ModelEntitySmelterFemale()
+    {
+        ModelRenderer toolHandle1;
+        ModelRenderer toolHandle2;
+        ModelRenderer pocket;
+
+        ModelRenderer bipedChest;
+
+        ModelRenderer ponytailB;
+        ModelRenderer ponytailT;
+
+        textureWidth = 128;
+        textureHeight = 64;
+
+        toolHandle1 = new ModelRenderer(this, 0, 32);
+        toolHandle1.addBox(0F, 0F, 0F, 4, 3, 1);
+        toolHandle1.setRotationPoint(-2F, 8F, -3F);
+        toolHandle1.setTextureSize(128, 64);
+        toolHandle1.mirror = true;
+        setRotation(toolHandle1, 0F, 0F, 0F);
+
+        toolHandle2 = new ModelRenderer(this, 10, 32);
+        toolHandle2.addBox(0F, 0F, 0F, 1, 2, 1);
+        toolHandle2.setRotationPoint(-1F, 6F, -3F);
+        toolHandle2.setTextureSize(128, 64);
+        toolHandle2.mirror = true;
+        setRotation(toolHandle2, 0F, 0F, 0F);
+
+        pocket = new ModelRenderer(this, 10, 32);
+        pocket.addBox(0F, 0F, 0F, 1, 2, 1);
+        pocket.setRotationPoint(1F, 6F, -3F);
+        pocket.setTextureSize(128, 64);
+        pocket.mirror = true;
+        setRotation(pocket, 0F, 0F, 0F);
+
+        bipedRightArm = new ModelRenderer(this, 40, 16);
+        bipedRightArm.addBox(-3F, -2F, -2F, 4, 12, 4);
+        bipedRightArm.setRotationPoint(-5F, 2F, 0F);
+        bipedRightArm.setTextureSize(128, 64);
+        bipedRightArm.mirror = true;
+        setRotation(bipedRightArm, 0F, 0F, 0F);
+
+        bipedLeftArm = new ModelRenderer(this, 40, 16);
+        bipedLeftArm.addBox(-1F, -2F, -2F, 4, 12, 4);
+        bipedLeftArm.setRotationPoint(5F, 2F, 0F);
+        bipedLeftArm.setTextureSize(128, 64);
+        bipedLeftArm.mirror = true;
+        setRotation(bipedLeftArm, 0F, 0F, 0F);
+        bipedLeftArm.mirror = false;
+
+        bipedChest = new ModelRenderer(this, 0, 55);
+        bipedChest.addBox(-3.5F, 2.7F, -0.6F, 7, 3, 4);
+        bipedChest.setRotationPoint(0F, 0F, 0F);
+        bipedChest.setTextureSize(128, 64);
+        bipedChest.mirror = true;
+        setRotation(bipedChest, -0.5934119F, 0F, 0F);
+
+        bipedRightLeg = new ModelRenderer(this, 0, 16);
+        bipedRightLeg.addBox(-2F, 0F, -2F, 4, 12, 4);
+        bipedRightLeg.setRotationPoint(-2F, 12F, 0F);
+        bipedRightLeg.setTextureSize(128, 64);
+        bipedRightLeg.mirror = true;
+        setRotation(bipedRightLeg, 0F, 0F, 0F);
+
+        bipedLeftLeg = new ModelRenderer(this, 0, 16);
+        bipedLeftLeg.addBox(-2F, 0F, -2F, 4, 12, 4);
+        bipedLeftLeg.setRotationPoint(2F, 12F, 0F);
+        bipedLeftLeg.setTextureSize(128, 64);
+        bipedLeftLeg.mirror = true;
+        setRotation(bipedLeftLeg, 0F, 0F, 0F);
+
+        bipedBody = new ModelRenderer(this, 16, 16);
+        bipedBody.addBox(-4F, 0F, -2F, 8, 12, 4);
+        bipedBody.setRotationPoint(0F, 0F, 0F);
+        bipedBody.setTextureSize(128, 64);
+        bipedBody.mirror = true;
+        setRotation(bipedBody, 0F, 0F, 0F);
+
+        bipedHead = new ModelRenderer(this, 0, 0);
+        bipedHead.addBox(-4F, -7F, -4F, 8, 8, 8);
+        bipedHead.setRotationPoint(0F, -1F, 0F);
+        bipedHead.setTextureSize(128, 64);
+        bipedHead.mirror = true;
+        setRotation(bipedHead, 0F, 0F, 0F);
+
+        ponytailB = new ModelRenderer(this, 80, 40);
+        ponytailB.addBox(-0.5F, 2.4F, 3.7F, 1, 5, 1);
+        ponytailB.setRotationPoint(0F, 0F, 0F);
+        ponytailB.setTextureSize(128, 64);
+        ponytailB.mirror = true;
+        setRotation(ponytailB, 0.1047198F, 0F, 0F);
+
+        ponytailT = new ModelRenderer(this, 79, 33);
+        ponytailT.addBox(-1F, -2F, 3.4F, 2, 5, 1);
+        ponytailT.setRotationPoint(0F, 0F, 0F);
+        ponytailT.setTextureSize(128, 64);
+        ponytailT.mirror = true;
+        setRotation(ponytailT, 0.2268928F, 0F, 0F);
+
+        this.bipedBody.addChild(toolHandle1);
+        this.bipedBody.addChild(toolHandle2);
+        this.bipedBody.addChild(pocket);
+        this.bipedBody.addChild(bipedChest);
+
+        this.bipedHead.addChild(ponytailB);
+        this.bipedHead.addChild(ponytailT);
+
+        bipedHeadwear.isHidden = true;
+    }
+
+    @Override
+    public void render(
+      @NotNull final Entity entity,
+      final float limbSwing,
+      final float limbSwingAmount,
+      final float ageInTicks,
+      final float netHeadYaw,
+      final float headPitch,
+      final float scaleFactor)
+    {
+        super.render(entity, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scaleFactor);
+        setRotationAngles(limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scaleFactor, entity);
+    }
+
+    private void setRotation(@NotNull final ModelRenderer model, final float x, final float y, final float z)
+    {
+        model.rotateAngleX = x;
+        model.rotateAngleY = y;
+        model.rotateAngleZ = z;
+    }
+}
