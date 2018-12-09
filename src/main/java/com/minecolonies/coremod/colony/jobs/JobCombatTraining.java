55df0d4c392b29a6f22217394c856b341896cb44
Raycoms <ray.neiheiser@gmail.com>
2018-12-09 11:57:10 -0200

    Feature/knights camp (#3144)
    Adds a knights camp for knights training
    Adds correct rendering of bow/shield handling
    Adds nice chests for both the training camps (Archery and Combat Academy)

---
+++ src/main/java/com/minecolonies/coremod/colony/jobs/JobCombatTraining.java
@@ -0,0 +1,47 @@
+package com.minecolonies.coremod.colony.jobs;
+
+import com.minecolonies.coremod.client.render.RenderBipedCitizen;
+import com.minecolonies.coremod.colony.CitizenData;
+import com.minecolonies.coremod.entity.ai.basic.AbstractAISkeleton;
+import com.minecolonies.coremod.entity.ai.citizen.trainingcamps.EntityAIArcherTraining;
+import com.minecolonies.coremod.entity.ai.citizen.trainingcamps.EntityAICombatTraining;
+
+/**
+ * The Knight's Training Job class
+ */
+public class JobCombatTraining extends AbstractJob
+{
+    /**
+     * Initialize citizen data.
+     *
+     * @param entity the citizen data.
+     */
+    public JobCombatTraining(final CitizenData entity)
+    {
+        super(entity);
+    }
+
+    @Override
+    public String getName()
+    {
+        return "CombatTraining";
+    }
+
+    @Override
+    public String getExperienceTag()
+    {
+        return JobKnight.DESC;
+    }
+
+    @Override
+    public RenderBipedCitizen.Model getModel()
+    {
+        return RenderBipedCitizen.Model.KNIGHT_GUARD;
+    }
+
+    @Override
+    public AbstractAISkeleton<? extends AbstractJob> generateAI()
+    {
+        return new EntityAICombatTraining(this);
+    }
+}
---
+++ src/main/java/com/minecolonies/coremod/entity/ai/citizen/trainingcamps/AbstractEntityAITraining.java
@@ -0,0 +1,166 @@
+package com.minecolonies.coremod.entity.ai.citizen.trainingcamps;
+
+import com.minecolonies.api.util.BlockPosUtil;
+import com.minecolonies.coremod.colony.jobs.AbstractJob;
+import com.minecolonies.coremod.entity.ai.basic.AbstractEntityAIBasic;
+import com.minecolonies.coremod.entity.ai.util.AIState;
+import com.minecolonies.coremod.entity.ai.util.AITarget;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
+import org.jetbrains.annotations.NotNull;
+
+import static com.minecolonies.coremod.entity.ai.util.AIState.*;
+
+/**
+ * Abstract class for all training AIs.
+ */
+@SuppressWarnings("squid:MaximumInheritanceDepth")
+public abstract class AbstractEntityAITraining<J extends AbstractJob> extends AbstractEntityAIBasic<AbstractJob>
+{
+    /**
+     * Percentual chance for target search being chosen as target job.
+     */
+    private static final int TARGET_SEARCH_CHANCE = 10;
+
+    /**
+     * 100% chance to compare it with smaller percentages.
+     */
+    protected static final int ONE_HUNDRED_PERCENT = 100;
+
+    /**
+     * The building range.
+     */
+    private AxisAlignedBB range;
+
+    /**
+     * The current pathing target to walk to.
+     */
+    protected BlockPos currentPathingTarget;
+
+    /**
+     * State to go to after pathing.
+     */
+    protected AIState stateAfterPathing;
+
+    /**
+     * How many more ticks we have until next attack.
+     */
+    protected int currentAttackDelay = 0;
+
+    /**
+     * Creates the abstract part of the AI.inte
+     * Always use this constructor!
+     *
+     * @param job the job to fulfill
+     */
+    public AbstractEntityAITraining(@NotNull final AbstractJob job)
+    {
+        //Tasks: Wander around, Find shooting position, go to shooting position, shoot, verify shot
+        super(job);
+        super.registerTargets(
+          new AITarget(IDLE, true, () -> START_WORKING),
+          new AITarget(START_WORKING, true, () -> DECIDE),
+          new AITarget(DECIDE, true, this::decide),
+          new AITarget(TRAINING_WANDER, true, this::wander),
+          new AITarget(GO_TO_TARGET, true, this::pathToTarget)
+        );
+        worker.setCanPickUpLoot(true);
+    }
+
+    /**
+     * Decide on which state to go to.
+     *
+     * @return the next state to go to.
+     */
+    public AIState decide()
+    {
+        setDelay(STANDARD_DELAY);
+        if (!isSetup())
+        {
+            return DECIDE;
+        }
+
+        if (worker.getRandom().nextInt(ONE_HUNDRED_PERCENT) < TARGET_SEARCH_CHANCE)
+        {
+            return COMBAT_TRAINING;
+        }
+        return TRAINING_WANDER;
+    }
+
+    /**
+     * Method to check if the worker is ready to start.
+     * @return true if so.
+     */
+    protected abstract boolean isSetup();
+
+    /**
+     * Wander randomly around within the premises of the building.
+     *
+     * @return the next state to go to.
+     */
+    private AIState wander()
+    {
+        setDelay(STANDARD_DELAY);
+        if (currentPathingTarget == null)
+        {
+            currentPathingTarget = getWanderPosition();
+        }
+
+        if (!walkToBlock(currentPathingTarget) || worker.getCitizenStuckHandler().isStuck())
+        {
+            currentPathingTarget = null;
+            return DECIDE;
+        }
+
+        return TRAINING_WANDER;
+    }
+
+    /**
+     * Walk to the shooting stand position.
+     *
+     * @return the next state to go to.
+     */
+    private AIState pathToTarget()
+    {
+        setDelay(STANDARD_DELAY);
+        if (walkToBlock(currentPathingTarget, 2))
+        {
+            return getState();
+        }
+        return stateAfterPathing;
+    }
+
+    /**
+     * Get a wander position within the archer training camp to walk to.
+     *
+     * @return the position or the location of the hut chest if not found.
+     */
+    private BlockPos getWanderPosition()
+    {
+        final BlockPos pos = BlockPosUtil.getRandomPosition(world, worker.getPosition(), getOwnBuilding().getLocation());
+
+        if (range == null)
+        {
+            range = getOwnBuilding().getTargetableArea(world);
+        }
+
+        if (range.intersectsWithXZ(new Vec3d(pos)))
+        {
+            return pos;
+        }
+
+        return getOwnBuilding().getLocation();
+    }
+
+    /**
+     * Reduces the attack delay by the given Tickrate
+     */
+    protected void reduceAttackDelay()
+    {
+        if (currentAttackDelay > 0)
+        {
+            currentAttackDelay--;
+        }
+    }
+}
