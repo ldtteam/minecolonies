12a4bc94245dbe82941725d62354a6db35bb6761
Raycoms <ray.neiheiser@gmail.com>
2019-08-05 10:10:43 -0300

    Adding the required API package information for publishing.

---
+++ src/api/java/com/minecolonies/api/entity/ai/statemachine/tickratestatemachine/package-info.java
@@ -0,0 +1,5 @@
+@API(owner = Constants.MOD_ID, apiVersion = Constants.VERSION, provides = "minecolonies-api|entity|ai|statemachine|tickratestatemachine")
+package com.minecolonies.api.entity.ai.statemachine.tickratestatemachine;
+
+import com.minecolonies.api.util.constant.Constants;
+import net.minecraftforge.fml.common.API;
---
+++ src/api/java/com/minecolonies/api/tileentities/package-info.java
@@ -0,0 +1,5 @@
+@API(owner = Constants.MOD_ID, apiVersion = Constants.VERSION, provides = "minecolonies-api|tileentities")
+package com.minecolonies.api.tileentities;
+
+import com.minecolonies.api.util.constant.Constants;
+import net.minecraftforge.fml.common.API;
