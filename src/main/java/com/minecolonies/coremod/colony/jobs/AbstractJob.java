10bab4f62680b59019f84a7d0d85a936f8499b4f
Raycoms <ray.neiheiser@gmail.com>
2019-10-20 12:13:11 -0300

    don't run the avoidance AI if guard (#3953)
    Improve guard AI from being stuck
    Fix rack crash

--- src/main/java/com/minecolonies/coremod/colony/jobs/AbstractJob.java
+++ src/main/java/com/minecolonies/coremod/colony/jobs/AbstractJob.java
@@ -432,4 +432,10 @@ public abstract class AbstractJob<AI extends AbstractAISkeleton<J>, J extends Ab
             workerAI.get().resetAI();
         }
     }
+
+    @Override
+    public boolean allowsAvoidance()
+    {
+        return true;
+    }
 }
