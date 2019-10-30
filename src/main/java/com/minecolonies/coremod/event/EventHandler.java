3a84def4b233af81686c0e66d383bc28249c46d0
someaddons <someaddons@github.com>
2019-10-30 16:25:09 +0100

    - fix render biped crash

--- src/main/java/com/minecolonies/coremod/event/EventHandler.java
+++ src/main/java/com/minecolonies/coremod/event/EventHandler.java
@@ -707,7 +707,7 @@ public class EventHandler
 
         // Global events
         // Halloween ghost mode
-        if (event.getWorld().isRemote && MineColonies.getConfig().getCommon().holidayFeatures.get() &&
+        if (event.getWorld().isRemote() && MineColonies.getConfig().getCommon().holidayFeatures.get() &&
               (LocalDateTime.now().getDayOfMonth() == 31 && LocalDateTime.now().getMonth() == Month.OCTOBER
                  || LocalDateTime.now().getDayOfMonth() == 1 && LocalDateTime.now().getMonth() == Month.NOVEMBER
                  || LocalDateTime.now().getDayOfMonth() == 2 && LocalDateTime.now().getMonth() == Month.NOVEMBER))
