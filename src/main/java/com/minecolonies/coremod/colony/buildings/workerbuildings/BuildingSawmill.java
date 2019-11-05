c241e22f0664bdbab8fcc3e6d92dec3a66041ef9
Robin de Jong <=>
2019-11-05 13:06:35 +0100

    Revert adding cactus to sawmill

--- src/main/java/com/minecolonies/coremod/colony/buildings/workerbuildings/BuildingSawmill.java
+++ src/main/java/com/minecolonies/coremod/colony/buildings/workerbuildings/BuildingSawmill.java
@@ -107,7 +107,7 @@ public class BuildingSawmill extends AbstractBuildingCrafter
         {
             if(!ItemStackUtils.isEmpty(stack))
             {
-                if (stack.getItem().isIn(ItemTags.PLANKS) || stack.getItem().isIn(ItemTags.LOGS) || stack.getItem().getRegistryName().getPath().contains("cactus"))
+                if (stack.getItem().isIn(ItemTags.PLANKS) || stack.getItem().isIn(ItemTags.LOGS))
                 {
                     amountOfValidBlocks++;
                     continue;
