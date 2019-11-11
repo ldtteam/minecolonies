f8e9635cce376393cb0fedaf90c0494b579866b8
Raycoms <ray.neiheiser@gmail.com>
2019-11-11 14:09:47 -0300

    hotfix restriction issues

--- src/main/java/com/minecolonies/coremod/colony/buildings/workerbuildings/BuildingLumberjack.java
+++ src/main/java/com/minecolonies/coremod/colony/buildings/workerbuildings/BuildingLumberjack.java
@@ -292,6 +292,14 @@ public class BuildingLumberjack extends AbstractFilterableListBuilding
      */
     public boolean shouldRestrict()
     {
+        if (restrict)
+        {
+            if (startRestriction == null || endRestriction == null)
+            {
+                restrict = false;
+                markDirty();
+            }
+        }
         return restrict;
     }
 
