85ca38519700278c828140227701675b1689dba8
someaddons <38401808+someaddons@users.noreply.github.com>
2019-11-20 23:55:49 +0100

    - dynamic state typing (#4091)
    - adding tickrate into statemachine
    - making statemachine faster

--- src/api/java/com/minecolonies/api/entity/ai/statemachine/states/IAIState.java
+++ src/api/java/com/minecolonies/api/entity/ai/statemachine/states/IAIState.java
@@ -4,7 +4,7 @@ package com.minecolonies.api.entity.ai.statemachine.states;
  * Interface type for IAIState enums
  * Implement this interface to add new statetypes
  */
-public interface IAIState
+public interface IAIState extends IState
 {
     /**
      * Check whether we can interrupt the current state to eat
