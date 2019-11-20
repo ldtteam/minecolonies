d290871706d6aa4f746de99a56a524a4cc545747
someaddons <someaddons@github.com>
2019-11-20 23:32:44 +0100

    - dynamic state typing - adding tickrate into statemachine - making statemachine faster

--- src/api/java/com/minecolonies/api/entity/ai/statemachine/basestatemachine/BasicEvent.java
+++ src/api/java/com/minecolonies/api/entity/ai/statemachine/basestatemachine/BasicEvent.java
@@ -1,7 +1,7 @@
 package com.minecolonies.api.entity.ai.statemachine.basestatemachine;
 
-import com.minecolonies.api.entity.ai.statemachine.states.IAIEventType;
 import com.minecolonies.api.entity.ai.statemachine.states.IAIState;
+import com.minecolonies.api.entity.ai.statemachine.states.IStateEventType;
 import com.minecolonies.api.entity.ai.statemachine.transitions.IStateMachineEvent;
 import org.jetbrains.annotations.NotNull;
 
@@ -17,10 +17,10 @@ public class BasicEvent extends BasicTransition implements IStateMachineEvent
     /**
      * The event type of this event
      */
-    private final IAIEventType eventType;
+    private final IStateEventType eventType;
 
     public BasicEvent(
-      @NotNull final IAIEventType eventType,
+      @NotNull final IStateEventType eventType,
       @NotNull final BooleanSupplier condition,
       @NotNull final Supplier<IAIState> nextState)
     {
@@ -33,7 +33,7 @@ public class BasicEvent extends BasicTransition implements IStateMachineEvent
      *
      * @return IAIEventType
      */
-    public IAIEventType getEventType()
+    public IStateEventType getEventType()
     {
         return eventType;
     }
