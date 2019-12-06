095795bb7e5ba289c167213d4208c09a86694943
Asherslab <asher.parry@icloud.com>
2019-12-06 16:59:11 +1000

    whoops stupid error in merge from version/1.12

--- src/api/java/com/minecolonies/api/util/constant/TranslationConstants.java
+++ src/api/java/com/minecolonies/api/util/constant/TranslationConstants.java
@@ -320,8 +320,6 @@ public final class TranslationConstants
     @NonNls
     public static final     String COM_MINECOLONIES_COREMOD_GENERAL_OFFBIG   = "com.minecolonies.coremod.general.offBig";
     @NonNls
-    public static final String COM_MINECOLONIES_CANT_TAKE_EQUIPPED                                 = "com.minecolonies.coremod.general.cantTakeEquipped";
-    @NonNls
     public static final String COM_MINECOLONIES_INFO_PREFIX = "com.minecolonies.coremod.info.";
     @NonNls
     public static final String COM_MINECOLONIES_CANT_TAKE_EQUIPPED  = "com.minecolonies.coremod.general.cantTakeEquipped";
