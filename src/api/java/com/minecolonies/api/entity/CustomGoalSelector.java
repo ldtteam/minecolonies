6f338349c42dfd6218f4b89a097aaf60272bf447
someaddons <someaddons@github.com>
2019-12-10 23:24:07 +0100

    Hotfix AI not using all goals

--- src/api/java/com/minecolonies/api/entity/CustomGoalSelector.java
+++ src/api/java/com/minecolonies/api/entity/CustomGoalSelector.java
@@ -1,17 +1,12 @@
 package com.minecolonies.api.entity;
 
 import com.google.common.collect.Sets;
-import com.minecolonies.api.util.Log;
-import net.minecraft.entity.MobEntity;
 import net.minecraft.entity.ai.goal.Goal;
 import net.minecraft.entity.ai.goal.GoalSelector;
 import net.minecraft.entity.ai.goal.PrioritizedGoal;
 import net.minecraft.profiler.IProfiler;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
 
-import java.lang.reflect.Field;
 import java.util.*;
 import java.util.stream.Stream;
 
@@ -40,7 +35,7 @@ public class CustomGoalSelector extends GoalSelector
     /**
      * By vanilla design there is max 1 running goal per flag, which is running is determined by priorities. This array contains the current goal for each flag.
      */
-    private PrioritizedGoal[] flagGoalsArray = new PrioritizedGoal[FLAG_COUNT];
+    private final PrioritizedGoal[] flagGoalsArray = new PrioritizedGoal[FLAG_COUNT];
 
     /**
      * All goals added to this selector
@@ -55,12 +50,12 @@ public class CustomGoalSelector extends GoalSelector
     /**
      * Array of flags, true if currently disabled
      */
-    private boolean[] disabledFlagsArray = new boolean[FLAG_COUNT];
+    private final boolean[] disabledFlagsArray = new boolean[FLAG_COUNT];
 
     /**
      * Amount of flags.
      */
-    private static int FLAG_COUNT = Goal.Flag.values().length;
+    private static final int FLAG_COUNT = Goal.Flag.values().length;
 
     /**
      * Tick counter
@@ -77,6 +72,7 @@ public class CustomGoalSelector extends GoalSelector
         super(old.profiler);
         importFrom(old);
         super.goals = this.goals;
+        super.profiler = this.profiler;
     }
 
     /**
@@ -89,7 +85,7 @@ public class CustomGoalSelector extends GoalSelector
         super(profiler);
         this.profiler = profiler;
         super.goals = this.goals;
-
+        super.profiler = this.profiler;
         for (Goal.Flag flag : Goal.Flag.values())
         {
             flagGoalsArray[flag.ordinal()] = DUMMY;
@@ -155,11 +151,14 @@ public class CustomGoalSelector extends GoalSelector
      */
     private boolean goalContainsDisabledFlag(final PrioritizedGoal goal)
     {
-        for (final Goal.Flag flag : goal.getMutexFlags())
+        for (int i = 0; i < FLAG_COUNT; i++)
         {
-            if (disabledFlagsArray[flag.ordinal()])
+            if (disabledFlagsArray[i])
             {
-                return true;
+                if (goal.getMutexFlags().contains(Goal.Flag.values()[i]))
+                {
+                    return true;
+                }
             }
         }
         return false;
@@ -167,15 +166,16 @@ public class CustomGoalSelector extends GoalSelector
 
     /**
      * Returns whether the given goal is higher priority in all flags it uses than existing running goals.
+     *
      * @param goal1 goal to check
      * @return true if it overrules the existing goal.
      */
     private boolean isPreemptedByAll(final PrioritizedGoal goal1)
     {
-        for (final Goal.Flag flag : goal1.getMutexFlags())
+        for (int i = 0; i < FLAG_COUNT; i++)
         {
-            final PrioritizedGoal compareGoal = flagGoalsArray[flag.ordinal()];
-            if (compareGoal.isRunning() && !compareGoal.isPreemptedBy(goal1))
+            final PrioritizedGoal compareGoal = flagGoalsArray[i];
+            if (compareGoal.isRunning() && !compareGoal.isPreemptedBy(goal1) && goal1.getMutexFlags().contains(Goal.Flag.values()[i]))
             {
                 return false;
             }
@@ -184,54 +184,48 @@ public class CustomGoalSelector extends GoalSelector
     }
 
     /**
-     * Ticks this selector, first checks running goals to stop, then checks all goals to see which should start running. Finally ticks all running goals
-     * Performance wise this is about 6 times faster, when checking at the same rate as the vanilla one, resulting in about 3-4 times less time spent updating and executing AI goals.
-     * When updating non-running goals only every 4 ticks it goes up to about 10% of vanilla's time spent for the whole update goals and their execution.
+     * Ticks this selector, first checks running goals to stop, then checks all goals to see which should start running. Finally ticks all running goals Performance wise this is
+     * about 6 times faster, when checking at the same rate as the vanilla one, resulting in about 3-4 times less time spent updating and executing AI goals. When updating
+     * non-running goals only every 4 ticks it goes up to about 10% of vanilla's time spent for the whole update goals and their execution.
      */
     public void tick()
     {
         this.profiler.startSection("goalUpdate");
-        for (int i = 0; i < FLAG_COUNT; i++)
-        {
-            final PrioritizedGoal goalToRun = flagGoalsArray[i];
-            if (goalToRun.isRunning() && (goalContainsDisabledFlag(goalToRun) || !goalToRun.shouldContinueExecuting()))
-            {
-                goalToRun.resetTask();
-            }
-        }
 
-        // Vanilla behaviour changed to checking it each tick with 1.14
-        if (counter++ == 1)
+        boolean hasFlags;
+        counter++;
+
+        for (final PrioritizedGoal currentGoal : goals)
         {
-            for (final PrioritizedGoal currentGoal : goals)
+            hasFlags = !currentGoal.getMutexFlags().isEmpty();
+
+            if (currentGoal.isRunning() && (hasFlags && goalContainsDisabledFlag(currentGoal) || !currentGoal.shouldContinueExecuting()))
             {
-                if (!currentGoal.isRunning() && !goalContainsDisabledFlag(currentGoal) && isPreemptedByAll(currentGoal) && currentGoal.shouldExecute())
+                currentGoal.resetTask();
+            }
+
+            // Vanilla behaviour changed to checking it each tick with 1.14
+            if (counter == 1 && !currentGoal.isRunning() &&
+                  ((!hasFlags && currentGoal.shouldExecute()) || (!goalContainsDisabledFlag(currentGoal) && isPreemptedByAll(currentGoal) && currentGoal.shouldExecute())))
+            {
+                for (Goal.Flag flag : currentGoal.getMutexFlags())
                 {
-                    for (Goal.Flag flag : currentGoal.getMutexFlags())
-                    {
-                        final PrioritizedGoal prioritizedgoal = flagGoalsArray[flag.ordinal()];
-                        prioritizedgoal.resetTask();
-                        flagGoalsArray[flag.ordinal()] = currentGoal;
-                    }
-                    currentGoal.startExecuting();
+                    final PrioritizedGoal prioritizedgoal = flagGoalsArray[flag.ordinal()];
+                    prioritizedgoal.resetTask();
+                    flagGoalsArray[flag.ordinal()] = currentGoal;
                 }
+                currentGoal.startExecuting();
+            }
+
+            if (currentGoal.isRunning())
+            {
+                currentGoal.tick();
             }
         }
 
         if (counter > 3)
         {
             counter = 0;
-        }
-        this.profiler.endSection();
-
-        this.profiler.startSection("goalTick");
-        for (int i = 0; i < FLAG_COUNT; i++)
-        {
-            final PrioritizedGoal goalToRun = flagGoalsArray[i];
-            if (goalToRun.isRunning())
-            {
-                goalToRun.tick();
-            }
         }
         this.profiler.endSection();
     }
@@ -243,7 +237,7 @@ public class CustomGoalSelector extends GoalSelector
      */
     public Stream<PrioritizedGoal> getRunningGoals()
     {
-        return this.flagGoals.values().stream().filter(PrioritizedGoal::isRunning);
+        return this.goals.stream().filter(PrioritizedGoal::isRunning);
     }
 
     /**
