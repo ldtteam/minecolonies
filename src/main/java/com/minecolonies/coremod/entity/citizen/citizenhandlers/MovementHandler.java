b5b79ce5062d81238a60599d61e21074867c0f8c
Ray  Neiheiser <ray.neiheiser@gmail.com>
2020-01-05 18:07:13 -0300

    some docu

--- src/main/java/com/minecolonies/coremod/entity/citizen/citizenhandlers/MovementHandler.java
+++ src/main/java/com/minecolonies/coremod/entity/citizen/citizenhandlers/MovementHandler.java
@@ -14,6 +14,9 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.shapes.VoxelShape;
 
+/**
+ * Custom movement handler for minecolonies citizens (avoid jumping so much).
+ */
 public class MovementHandler extends MovementController
 {
     public MovementHandler(MobEntity mob)
@@ -26,7 +29,7 @@ public class MovementHandler extends MovementController
     {
         if (this.action == net.minecraft.entity.ai.controller.MovementController.Action.STRAFE)
         {
-            float speedAtt = (float) this.mob.getAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).getValue();
+            final float speedAtt = (float) this.mob.getAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).getValue();
             float speed = (float) this.speed * speedAtt;
             float forward = this.moveForward;
             float strafe = this.moveStrafe;
@@ -39,13 +42,13 @@ public class MovementHandler extends MovementController
             totalMovement = speed / totalMovement;
             forward = forward * totalMovement;
             strafe = strafe * totalMovement;
-            float sinRotation = MathHelper.sin(this.mob.rotationYaw * ((float) Math.PI / 180F));
-            float cosRotation = MathHelper.cos(this.mob.rotationYaw * ((float) Math.PI / 180F));
-            float rot1 = forward * cosRotation - strafe * sinRotation;
-            float rot2 = strafe * cosRotation + forward * sinRotation;
-            PathNavigator pathnavigator = this.mob.getNavigator();
+            final float sinRotation = MathHelper.sin(this.mob.rotationYaw * ((float) Math.PI / 180F));
+            final float cosRotation = MathHelper.cos(this.mob.rotationYaw * ((float) Math.PI / 180F));
+            final float rot1 = forward * cosRotation - strafe * sinRotation;
+            final float rot2 = strafe * cosRotation + forward * sinRotation;
+            final PathNavigator pathnavigator = this.mob.getNavigator();
 
-            NodeProcessor nodeprocessor = pathnavigator.getNodeProcessor();
+            final NodeProcessor nodeprocessor = pathnavigator.getNodeProcessor();
             if (nodeprocessor.getPathNodeType(this.mob.world,
               MathHelper.floor(this.mob.posX + (double) rot1),
               MathHelper.floor(this.mob.posY),
@@ -64,26 +67,25 @@ public class MovementHandler extends MovementController
         else if (this.action == net.minecraft.entity.ai.controller.MovementController.Action.MOVE_TO)
         {
             this.action = net.minecraft.entity.ai.controller.MovementController.Action.WAIT;
-            double xDif = this.posX - this.mob.posX;
-            double zDif = this.posZ - this.mob.posZ;
-            double yDif = this.posY - this.mob.posY;
-            double dist = xDif * xDif + yDif * yDif + zDif * zDif;
+            final double xDif = this.posX - this.mob.posX;
+            final double zDif = this.posZ - this.mob.posZ;
+            final double yDif = this.posY - this.mob.posY;
+            final double dist = xDif * xDif + yDif * yDif + zDif * zDif;
             if (dist < (double) 2.5000003E-7F)
             {
                 this.mob.setMoveForward(0.0F);
                 return;
             }
 
-            float range = (float) (MathHelper.atan2(zDif, xDif) * (double) (180F / (float) Math.PI)) - 90.0F;
+            final float range = (float) (MathHelper.atan2(zDif, xDif) * (double) (180F / (float) Math.PI)) - 90.0F;
             this.mob.rotationYaw = this.limitAngle(this.mob.rotationYaw, range, 90.0F);
             this.mob.setAIMoveSpeed((float) (this.speed * this.mob.getAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).getValue()));
-            BlockPos blockpos = new BlockPos(this.mob);
-            BlockState blockstate = this.mob.world.getBlockState(blockpos);
-            Block block = blockstate.getBlock();
-            VoxelShape voxelshape = blockstate.getCollisionShape(this.mob.world, blockpos);
+            final BlockPos blockpos = new BlockPos(this.mob);
+            final BlockState blockstate = this.mob.world.getBlockState(blockpos);
+            final Block block = blockstate.getBlock();
+            final VoxelShape voxelshape = blockstate.getCollisionShape(this.mob.world, blockpos);
             if ((yDif > (double) this.mob.stepHeight && xDif * xDif + zDif * zDif < (double) Math.max(1.0F, this.mob.getWidth()))
-                  || (!voxelshape.isEmpty() && this.mob.posY < voxelshape.getEnd(Direction.Axis.Y) + (double) blockpos.getY() && !block.isIn(BlockTags.DOORS) && !block.isIn(
-              BlockTags.FENCES))
+                  || (!voxelshape.isEmpty() && this.mob.posY < voxelshape.getEnd(Direction.Axis.Y) + (double) blockpos.getY() && !block.isIn(BlockTags.DOORS) && !block.isIn(BlockTags.FENCES))
                        && !block.isLadder(blockstate, this.mob.world, blockpos, this.mob))
             {
                 this.mob.getJumpController().setJumping();
