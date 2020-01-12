811cf3961445be5ca52e89d838bdcf76f6804cf2
Ray  Neiheiser <ray.neiheiser@gmail.com>
2020-01-12 07:06:17 -0300

    1.15 gradle adaptations

--- src/api/java/com/minecolonies/api/util/constant/Constants.java
+++ src/api/java/com/minecolonies/api/util/constant/Constants.java
@@ -12,7 +12,7 @@ public final class Constants
     public static final String MOD_NAME                         = "MineColonies";
     public static final String VERSION                          = "@VERSION@";
     public static final String FORGE_VERSION                    = "required-after:Forge@[14.22.0.2459,)";
-    public static final String MC_VERSION                       = "[1.12,1.13,1.14]";
+    public static final String MC_VERSION                       = "[1.12,1.13,1.14,1.15]";
     public static final String CLIENT_PROXY_LOCATION            = "com.minecolonies.coremod.proxy.ClientProxy";
     public static final String SERVER_PROXY_LOCATION            = "com.minecolonies.coremod.proxy.ServerProxy";
     public static final String CONFIG_GUI_LOCATION              = "com.minecolonies.coremod.client.gui.GuiFactory";
