ae44a42e5afa570d566c9a78ce889e2a3b6de2ad
someaddons <someaddons@github.com>
2020-01-17 10:26:40 +0100

    Fix citizen limit calculation (#4228)
    * Fix citizen limit calculation.
    Now Calculates on building upgrade/destruction.
    Recalculates after colony load, instead of reading old values.
    Recruitment of additional guards over the citizen hut limit now works.
    Level 0 guard-buildings no longer count towards the limit.
    
    * Add missing calculation case & make it global for future use.

--- src/api/java/com/minecolonies/api/util/constant/NbtTagConstants.java
+++ src/api/java/com/minecolonies/api/util/constant/NbtTagConstants.java
@@ -9,8 +9,6 @@ public final class NbtTagConstants
     public static final String TAG_NAME                   = "name";
     public static final String TAG_DIMENSION              = "dimension";
     public static final String TAG_CENTER                 = "center";
-    public static final String TAG_MAX_CITIZENS           = "maxCitizens";
-    public static final String TAG_POTENTIAL_MAX_CITIZENS = "potentialMaxCitizens";
     public static final String TAG_BUILDINGS              = "buildings";
     public static final String TAG_BUILDING               = "building";
     public static final String TAG_BUILDINGS_CLAIM        = "buildingsClaim";
