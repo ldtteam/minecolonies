a0724edc1c251d1f0d04276958c956941a99bee9
someaddons <someaddons@github.com>
2020-02-03 19:29:50 +0100

    Hotfix pathing type crash

--- src/main/java/com/minecolonies/coremod/entity/pathfinding/MinecoloniesAdvancedPathNavigate.java
+++ src/main/java/com/minecolonies/coremod/entity/pathfinding/MinecoloniesAdvancedPathNavigate.java
@@ -280,7 +280,7 @@ public class MinecoloniesAdvancedPathNavigate extends AbstractAdvancedPathNaviga
     {
         final int pathLength = path.getCurrentPathLength();
         Path tempPath = null;
-        if (pathLength > 0 && !(path.getPathPointFromIndex(0) instanceof PathPointExtended))
+        if (pathLength > 0)
         {
             //  Fix vanilla PathPoints to be PathPointExtended
             @NotNull final PathPointExtended[] newPoints = new PathPointExtended[pathLength];
@@ -427,11 +427,6 @@ public class MinecoloniesAdvancedPathNavigate extends AbstractAdvancedPathNaviga
         final int curNodeNext = curNode + 1;
         if (curNodeNext < currentPath.getCurrentPathLength())
         {
-            if (!(currentPath.getPathPointFromIndex(curNode) instanceof PathPointExtended) || !(currentPath.getPathPointFromIndex(curNodeNext) instanceof PathPointExtended))
-            {
-                currentPath = convertPath(currentPath);
-            }
-            
             final PathPointExtended pEx = (PathPointExtended) currentPath.getPathPointFromIndex(curNode);
             final PathPointExtended pExNext = (PathPointExtended) currentPath.getPathPointFromIndex(curNodeNext);
 
