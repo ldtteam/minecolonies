3f2fb84ffd51a730c21ab78eb208bd6d607ecaee
Ray  Neiheiser <ray.neiheiser@gmail.com>
2020-03-15 20:39:37 +0000

    add jdoc

--- src/api/java/com/minecolonies/api/util/DebugUtil.java
+++ src/api/java/com/minecolonies/api/util/DebugUtil.java
@@ -2,13 +2,25 @@ package com.minecolonies.api.util;
 
 import net.minecraft.client.Minecraft;
 
-public final class DebugUtil {
-
-    private DebugUtil() {
+/**
+ * Debug utility class.
+ */
+public final class DebugUtil
+{
+    /**
+     * Private constructor to hide implicit one.
+     */
+    private DebugUtil()
+    {
         throw new IllegalStateException("Tried to initialize: DebugUtil but this is a Utility class.");
     }
 
-    public static boolean hasReleaseMouse() {
+    /**
+     * Called either in watchlist or debug condition to release the mouse automatically.
+     * @return true always on call.
+     */
+    public static boolean hasReleaseMouse()
+    {
         Minecraft.getInstance().mouseHelper.ungrabMouse();
         return true;
     }
