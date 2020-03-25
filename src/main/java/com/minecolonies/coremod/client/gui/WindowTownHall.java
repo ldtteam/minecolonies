c191b20d2c5d42bcab6549e72e55c2a891bb216d
Ray  Neiheiser <ray.neiheiser@gmail.com>
2020-03-25 15:27:42 +0000

    Revert "#4500"
    This reverts commit d55e6f5985193aebbeb69e94844371c7d9d24fff.

--- src/main/java/com/minecolonies/coremod/client/gui/WindowTownHall.java
+++ src/main/java/com/minecolonies/coremod/client/gui/WindowTownHall.java
@@ -163,7 +163,6 @@ public class WindowTownHall extends AbstractWindowBuilding<ITownHallView>
         registerButton(BUTTON_TOGGLE_HOUSING, this::toggleHousing);
         registerButton(BUTTON_TOGGLE_MOVE_IN, this::toggleMoveIn);
         registerButton(BUTTON_TOGGLE_PRINT_PROGRESS, this::togglePrintProgress);
-        registerButton(BUTTON_TOGGLE_RESET_RS, this::toggleRsRequest);
 
         registerButton(NAME_LABEL, this::fillCitizenInfo);
         registerButton(RECALL_ONE, this::recallOneClicked);
@@ -1049,11 +1048,6 @@ public class WindowTownHall extends AbstractWindowBuilding<ITownHallView>
             toggle = false;
         }
         Network.getNetwork().sendToServer(new ToggleMoveInMessage(this.building.getColony(), toggle));
-    }
-
-    private void toggleRsRequest()
-    {
-        Network.getNetwork().sendToServer(new ResetRequestSystemMessage(this.building.getColony()));
     }
 
     /**
--- src/main/java/com/minecolonies/coremod/network/NetworkChannel.java
+++ src/main/java/com/minecolonies/coremod/network/NetworkChannel.java
@@ -134,7 +134,6 @@ public class NetworkChannel
         registerMessage(++idx, TriggerServerResponseHandlerMessage.class, TriggerServerResponseHandlerMessage::new);
         registerMessage(++idx, TryResearchMessage.class, TryResearchMessage::new);
         registerMessage(++idx, HireSpiesMessage.class, HireSpiesMessage::new);
-        registerMessage(++idx, ResetRequestSystemMessage.class, ResetRequestSystemMessage::new);
 
         //Client side only
         registerMessage(++idx, BlockParticleEffectMessage.class, BlockParticleEffectMessage::new);
