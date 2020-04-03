cc9e971e7f0f3429d36b0ef23c8f9170f05fc5d1
Ray  Neiheiser <ray.neiheiser@gmail.com>
2020-04-03 21:51:34 +0100

    Revert "add some logging"
    This reverts commit 6524c2bc0a0a357d544b810c5856210217fc4a95.

--- src/main/java/com/minecolonies/coremod/colony/buildings/workerbuildings/PostBox.java
+++ src/main/java/com/minecolonies/coremod/colony/buildings/workerbuildings/PostBox.java
@@ -12,7 +12,6 @@ import com.minecolonies.api.colony.requestsystem.request.IRequest;
 import com.minecolonies.api.colony.requestsystem.request.RequestState;
 import com.minecolonies.api.colony.requestsystem.requestable.Stack;
 import com.minecolonies.api.colony.requestsystem.resolver.IRequestResolver;
-import com.minecolonies.api.util.Log;
 import com.minecolonies.coremod.client.gui.WindowPostBox;
 import com.minecolonies.coremod.colony.buildings.AbstractBuilding;
 import com.minecolonies.coremod.colony.buildings.views.AbstractBuildingView;
@@ -83,10 +82,6 @@ public class PostBox extends AbstractBuilding
             req.setCount(((Stack) request.getRequest()).getCount());
             createRequest(req, false);
         }
-        Log.getLogger().error("------------------------------------------------------------");
-        Log.getLogger().error("----------------Something got cancelled!!!!!----------------");
-        Log.getLogger().error("------------------------------------------------------------");
-
     }
 
     /**
