16a5f7d0870826eed2d7dc4385ac8a0c4fe2aa8a
someaddons <38401808+someaddons@users.noreply.github.com>
2020-04-13 13:15:00 +0200

    Colony creation gui (#4615)
    This PR changes how colony creation works, the colony is now created via a GUI you open by accessing a townhall, instead of upon townhall placement. The gui is looking like this:

---
+++ src/main/java/com/minecolonies/coremod/network/messages/ColonyDeleteOwnMessage.java
@@ -0,0 +1,61 @@
+package com.minecolonies.coremod.network.messages;
+
+import com.ldtteam.structurize.util.LanguageHandler;
+import com.minecolonies.api.colony.IColony;
+import com.minecolonies.api.colony.IColonyManager;
+import com.minecolonies.api.network.IMessage;
+import com.minecolonies.coremod.Network;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.network.PacketBuffer;
+import net.minecraftforge.fml.LogicalSide;
+import net.minecraftforge.fml.network.NetworkEvent;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * Message for deleting an owned colony
+ */
+public class ColonyDeleteOwnMessage implements IMessage
+{
+    @Override
+    public void toBytes(final PacketBuffer buf)
+    {
+
+    }
+
+    @Override
+    public void fromBytes(final PacketBuffer buf)
+    {
+
+    }
+
+    @Nullable
+    @Override
+    public LogicalSide getExecutionSide()
+    {
+        return LogicalSide.SERVER;
+    }
+
+    @Override
+    public void onExecute(final NetworkEvent.Context ctxIn, final boolean isLogicalServer)
+    {
+        ServerPlayerEntity player = ctxIn.getSender();
+
+        if (player == null)
+        {
+            return;
+        }
+
+        // Colony
+        final IColony colony = IColonyManager.getInstance().getIColonyByOwner(player.world, player);
+
+        if (colony == null)
+        {
+            LanguageHandler.sendPlayerMessage(player, "com.minecolonies.coremod.gui.colony.delete.notfound");
+            return;
+        }
+
+        IColonyManager.getInstance().deleteColonyByWorld(colony.getID(), false, player.world);
+        Network.getNetwork().sendToPlayer(new ColonyViewRemoveMessage(colony.getID(), colony.getDimension()), player);
+        LanguageHandler.sendPlayerMessage(player, "com.minecolonies.coremod.gui.colony.delete.success");
+    }
+}
