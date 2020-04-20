d16e3fbdad746fc46235e96216309c798606315f
Ray  Neiheiser <ray.neiheiser@gmail.com>
2020-04-20 18:16:38 +0100

    hf javadoc

--- src/main/java/com/minecolonies/coremod/colony/buildings/AbstractBuilding.java
+++ src/main/java/com/minecolonies/coremod/colony/buildings/AbstractBuilding.java
@@ -562,9 +562,9 @@ public abstract class AbstractBuilding extends AbstractBuildingContainer impleme
     /**
      * Regularly tick this building and check if we  got the minimum stock(like once a minute is still fine)
      * - If not: Check if there is a request for this already.
-     * -> If not: Create a request.
+     * -- If not: Create a request.
      * - If so: Check if there is a request for this still.
-     * -> If so: cancel it.
+     * -- If so: cancel it.
      */
     @Override
     public void onColonyTick(final IColony colony)
