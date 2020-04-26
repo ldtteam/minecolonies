8e19f7d22030cd16651f6e5529b6495b69b7c1fe
Ray  Neiheiser <ray.neiheiser@gmail.com>
2020-04-26 20:31:51 +0100

    miner attempt 1

--- src/main/java/com/minecolonies/coremod/client/model/ModelEntityMinerFemale.java
+++ src/main/java/com/minecolonies/coremod/client/model/ModelEntityMinerFemale.java
@@ -7,232 +7,123 @@ import org.jetbrains.annotations.NotNull;
 
 public class ModelEntityMinerFemale extends CitizenModel<AbstractEntityCitizen>
 {
-    //fields
-    ModelRenderer goggleLeft;
-    ModelRenderer goggleBase;
-    ModelRenderer goggleRight;
-    ModelRenderer goggleRightLens;
-    ModelRenderer goggleLeftLens;
-    ModelRenderer chest;
-    ModelRenderer helmetBase;
-    ModelRenderer helmetLight;
-    ModelRenderer helmetFront;
-    ModelRenderer helmetTop;
-    ModelRenderer helmetMiddle;
-    ModelRenderer bag;
-    ModelRenderer belt;
-    ModelRenderer torch;
-    ModelRenderer bagLock;
-    ModelRenderer beltBuckle;
-    ModelRenderer ponytailBase;
-    ModelRenderer ponytailTail;
-
     public ModelEntityMinerFemale()
     {
         textureWidth = 128;
         textureHeight = 64;
 
-        bipedRightArm = new ModelRenderer(this, 40, 16);
-        bipedRightArm.addBox(-3F, -2F, -2F, 4, 12, 4);
-        bipedRightArm.setRotationPoint(-5F, 2F, 0F);
-        bipedRightArm.setTextureSize(128, 64);
-        bipedRightArm.mirror = true;
-        setRotation(bipedRightArm, 0F, 0F, 0F);
+        bipedRightArm = new ModelRenderer(this);
+        bipedRightArm.setRotationPoint(-5.0F, 2.0F, 0.0F);
+        bipedRightArm.setTextureOffset(40, 16).addBox(-3.0F, -2.0F, -2.0F, 4.0F, 12.0F, 4.0F, 0.0F, true);
+        bipedRightArm.setTextureOffset(68, 11).addBox(-3.6F, 6.0333F, -2.5333F, 5.2F, 0.5F, 5.1333F, 0.0F, false);
+        bipedRightArm.setTextureOffset(68, 11).addBox(-3.2F, 6.5333F, -2.1333F, 4.4F, 4.0F, 4.3333F, 0.0F, false);
 
-        bipedLeftArm = new ModelRenderer(this, 40, 16);
-        bipedLeftArm.addBox(-1F, -2F, -2F, 4, 12, 4);
-        bipedLeftArm.setRotationPoint(5F, 2F, 0F);
-        bipedLeftArm.setTextureSize(128, 64);
-        bipedLeftArm.mirror = true;
-        setRotation(bipedLeftArm, 0F, 0F, 0F);
-        bipedLeftArm.mirror = false;
+        bipedLeftArm = new ModelRenderer(this);
+        bipedLeftArm.setRotationPoint(5.0F, 2.0F, 0.0F);
+        bipedLeftArm.setTextureOffset(40, 16).addBox(-1.0F, -2.0F, -2.0F, 4.0F, 12.0F, 4.0F, 0.0F, false);
+        bipedLeftArm.setTextureOffset(68, 11).addBox(-1.2F, 6.5333F, -2.1333F, 4.4F, 4.0F, 4.3333F, 0.0F, false);
+        bipedLeftArm.setTextureOffset(68, 11).addBox(-1.6F, 6.0333F, -2.5333F, 5.2F, 0.5F, 5.1333F, 0.0F, false);
 
-        bipedRightLeg = new ModelRenderer(this, 0, 16);
-        bipedRightLeg.addBox(-2F, 0F, -2F, 4, 12, 4);
-        bipedRightLeg.setRotationPoint(-2F, 12F, 0F);
-        bipedRightLeg.setTextureSize(128, 64);
-        bipedRightLeg.mirror = true;
-        setRotation(bipedRightLeg, 0F, 0F, 0F);
+        bipedRightLeg = new ModelRenderer(this);
+        bipedRightLeg.setRotationPoint(-2.0F, 12.0F, 0.0F);
+        bipedRightLeg.setTextureOffset(0, 16).addBox(-2.0F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, 0.0F, true);
 
-        bipedLeftLeg = new ModelRenderer(this, 0, 16);
-        bipedLeftLeg.addBox(-2F, 0F, -2F, 4, 12, 4);
-        bipedLeftLeg.setRotationPoint(2F, 12F, 0F);
-        bipedLeftLeg.setTextureSize(128, 64);
-        bipedLeftLeg.mirror = true;
-        setRotation(bipedLeftLeg, 0F, 0F, 0F);
-        bipedLeftLeg.mirror = false;
+        bipedLeftLeg = new ModelRenderer(this);
+        bipedLeftLeg.setRotationPoint(2.0F, 12.0F, 0.0F);
+        bipedLeftLeg.setTextureOffset(0, 16).addBox(-2.0F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, 0.0F, false);
 
-        bipedBody = new ModelRenderer(this, 16, 16);
-        bipedBody.addBox(-4F, 0F, -2F, 8, 12, 4);
-        bipedBody.setRotationPoint(0F, 0F, 0F);
-        bipedBody.setTextureSize(128, 64);
-        bipedBody.mirror = true;
-        setRotation(bipedBody, 0F, 0F, 0F);
+        bipedBody = new ModelRenderer(this);
+        bipedBody.setRotationPoint(0.0F, 0.0F, 0.0F);
+        bipedBody.setTextureOffset(16, 16).addBox(-4.0F, 0.0F, -2.0F, 8.0F, 12.0F, 4.0F, 0.0F, true);
+        bipedBody.setTextureOffset(44, 52).addBox(-1.0F, 9.0F, -2.7F, 2.0F, 2.0F, 1.0F, 0.0F, true);
+        bipedBody.setTextureOffset(0, 46).addBox(-4.5F, 9.5F, -2.5F, 9.0F, 1.0F, 5.0F, 0.0F, true);
+        bipedBody.setTextureOffset(78, 46).addBox(-3.0F, 8.1334F, 2.0F, 6.3333F, 1.0F, 3.0F, 0.0F, false);
+        bipedBody.setTextureOffset(78, 45).addBox(3.0F, 1.4F, 2.0F, 1.0F, 6.7333F, 3.0F, 0.0F, false);
+        bipedBody.setTextureOffset(77, 45).addBox(-4.0F, 1.4F, 2.0F, 1.0F, 6.7333F, 3.0F, 0.0F, false);
+        bipedBody.setTextureOffset(70, 30).addBox(3.1167F, 0.05F, 5.15F, 0.75F, 0.75F, 0.75F, 0.0F, false);
+        bipedBody.setTextureOffset(102, 36).addBox(3.25F, 1.25F, 5.2833F, 0.4833F, 3.55F, 0.4833F, 0.0F, false);
+        bipedBody.setTextureOffset(101, 32).addBox(2.9833F, 0.73F, 5.0F, 1.0167F, 0.6167F, 1.0167F, 0.0F, false);
+        bipedBody.setTextureOffset(81, 45).addBox(2.9833F, 2.0634F, 5.0F, 1.0167F, 0.6167F, 1.0167F, 0.0F, false);
+        bipedBody.setTextureOffset(101, 32).addBox(-4.0167F, 0.73F, 5.0F, 1.0167F, 0.6167F, 1.0167F, 0.0F, false);
+        bipedBody.setTextureOffset(81, 45).addBox(-4.0167F, 2.0634F, 5.0F, 1.0167F, 0.6167F, 1.0167F, 0.0F, false);
+        bipedBody.setTextureOffset(70, 30).addBox(-3.9F, 0.05F, 5.15F, 0.75F, 0.75F, 0.75F, 0.0F, false);
+        bipedBody.setTextureOffset(102, 36).addBox(-3.7083F, 1.25F, 5.2833F, 0.4833F, 3.55F, 0.4833F, 0.0F, false);
+        bipedBody.setTextureOffset(66, 3).addBox(-1.3481F, 0.6515F, 3.3085F, 1.0F, 1.0F, 1.0F, 0.0F, false);
+        bipedBody.setTextureOffset(66, 3).addBox(-1.3481F, 0.6515F, 2.2418F, 1.0F, 1.0F, 1.0F, 0.0F, false);
+        bipedBody.setTextureOffset(66, 3).addBox(-2.0148F, 0.9182F, 3.9752F, 1.0F, 1.0F, 1.0F, 0.0F, false);
+        bipedBody.setTextureOffset(66, 3).addBox(-2.0148F, 0.9182F, 2.9085F, 1.0F, 1.0F, 1.0F, 0.0F, false);
+        bipedBody.setTextureOffset(66, 3).addBox(-1.3481F, 0.5182F, 3.9752F, 1.0F, 1.0F, 1.0F, 0.0F, false);
+        bipedBody.setTextureOffset(66, 3).addBox(-0.2814F, 0.5182F, 3.5752F, 1.0F, 1.0F, 1.0F, 0.0F, false);
+        bipedBody.setTextureOffset(66, 3).addBox(-0.6814F, 1.0515F, 3.9752F, 1.0F, 1.0F, 1.0F, 0.0F, false);
+        bipedBody.setTextureOffset(66, 3).addBox(-0.6814F, 1.0515F, 2.9085F, 1.0F, 1.0F, 1.0F, 0.0F, false);
+        bipedBody.setTextureOffset(66, 3).addBox(-0.6814F, 0.6515F, 2.9333F, 1.0F, 1.0F, 1.0F, 0.0F, false);
+        bipedBody.setTextureOffset(66, 3).addBox(-0.6814F, 0.6515F, 1.8667F, 1.0F, 1.0F, 1.0F, 0.0F, false);
+        bipedBody.setTextureOffset(66, 2).addBox(-2.4514F, 0.7714F, 2.93F, 1.0F, 1.1333F, 1.0F, 0.0F, false);
+        bipedBody.setTextureOffset(66, 2).addBox(-2.4514F, 0.7714F, 1.8634F, 1.0F, 1.1333F, 1.0F, 0.0F, false);
+        bipedBody.setTextureOffset(68, 3).addBox(-2.9975F, 1.118F, 3.0916F, 1.0F, 1.1333F, 1.0F, 0.0F, false);
+        bipedBody.setTextureOffset(68, 3).addBox(-2.8642F, 0.9846F, 2.025F, 0.8667F, 1.2667F, 1.1333F, 0.0F, false);
+        bipedBody.setTextureOffset(68, 3).addBox(-2.9975F, 0.8513F, 3.8916F, 1.0F, 1.1333F, 1.0F, 0.0F, false);
+        bipedBody.setTextureOffset(68, 3).addBox(-0.3308F, 1.118F, 3.0916F, 1.0F, 1.1333F, 1.0F, 0.0F, false);
+        bipedBody.setTextureOffset(68, 3).addBox(-0.3308F, 1.118F, 2.025F, 1.0F, 1.1333F, 1.0F, 0.0F, false);
+        bipedBody.setTextureOffset(66, 2).addBox(0.2153F, 0.7714F, 2.93F, 1.0F, 1.1333F, 1.0F, 0.0F, false);
+        bipedBody.setTextureOffset(66, 2).addBox(0.2153F, 0.7714F, 1.8634F, 1.0F, 1.1333F, 1.0F, 0.0F, false);
+        bipedBody.setTextureOffset(66, 3).addBox(1.9852F, 0.7848F, 2.9333F, 1.0F, 1.0F, 1.0F, 0.0F, false);
+        bipedBody.setTextureOffset(66, 3).addBox(1.9852F, 1.0515F, 3.9752F, 1.0F, 1.0F, 1.0F, 0.0F, false);
+        bipedBody.setTextureOffset(66, 3).addBox(1.9852F, 1.0515F, 1.9752F, 1.0F, 1.0F, 1.0F, 0.0F, false);
+        bipedBody.setTextureOffset(66, 3).addBox(1.3186F, 0.5182F, 3.9752F, 1.0F, 1.0F, 1.0F, 0.0F, false);
+        bipedBody.setTextureOffset(66, 3).addBox(0.7852F, 0.5182F, 2.9085F, 1.0F, 1.0F, 1.0F, 0.0F, false);
+        bipedBody.setTextureOffset(66, 3).addBox(0.6519F, 0.9182F, 3.9752F, 1.0F, 1.0F, 1.0F, 0.0F, false);
+        bipedBody.setTextureOffset(66, 3).addBox(0.6519F, 0.9182F, 2.9085F, 1.0F, 1.0F, 1.0F, 0.0F, false);
+        bipedBody.setTextureOffset(66, 3).addBox(1.3186F, 0.6515F, 3.3085F, 1.0F, 1.0F, 1.0F, 0.0F, false);
+        bipedBody.setTextureOffset(66, 3).addBox(1.3186F, 0.6515F, 1.9752F, 1.0F, 1.0F, 1.0F, 0.0F, false);
+        bipedBody.setTextureOffset(68, 3).addBox(-0.3308F, 0.8513F, 3.8916F, 1.0F, 1.1333F, 1.0F, 0.0F, false);
+        bipedBody.setTextureOffset(68, 3).addBox(-0.3308F, 0.8513F, 2.825F, 1.0F, 1.1333F, 1.0F, 0.0F, false);
+        bipedBody.setTextureOffset(78, 46).addBox(-3.0F, 0.7334F, 5.0F, 6.0F, 7.4F, 1.0F, 0.0F, false);
 
-        bipedHead = new ModelRenderer(this, 0, 0);
-        bipedHead.addBox(-4F, -7F, -4F, 8, 7, 8);
-        bipedHead.setRotationPoint(0F, 0F, 0F);
-        bipedHead.setTextureSize(128, 64);
-        bipedHead.mirror = true;
-        setRotation(bipedHead, 0F, 0F, 0F);
+        bipedHead = new ModelRenderer(this);
+        bipedHead.setRotationPoint(0.0F, 0.0F, 0.0F);
+        bipedHead.setTextureOffset(0, 0).addBox(-4.0F, -7.0F, -4.0F, 8.0F, 7.0F, 8.0F, 0.0F, true);
+        bipedHead.setTextureOffset(52, 50).addBox(3.6667F, -5.0F, -1.3333F, 1.0F, 1.0F, 2.3333F, 0.0F, false);
+        bipedHead.setTextureOffset(52, 50).addBox(3.6667F, -5.0F, 0.9333F, 1.0F, 2.0667F, 3.1333F, 0.0F, false);
+        bipedHead.setTextureOffset(52, 50).addBox(3.1333F, -7.2F, -4.0F, 1.5333F, 2.2F, 8.0667F, 0.0F, false);
+        bipedHead.setTextureOffset(52, 50).addBox(-4.61F, -7.2F, -4.1333F, 1.5333F, 2.2F, 8.2F, 0.0F, false);
+        bipedHead.setTextureOffset(52, 50).addBox(-4.6F, -5.0667F, -1.0667F, 1.0F, 1.0F, 5.1333F, 0.0F, false);
+        bipedHead.setTextureOffset(52, 50).addBox(-4.61F, -4.1333F, 0.9333F, 1.0F, 1.1333F, 3.1333F, 0.0F, false);
+        bipedHead.setTextureOffset(52, 50).addBox(-4.4767F, -5.4667F, 3.6F, 9.0F, 2.4667F, 1.1333F, 0.0F, false);
+        bipedHead.setTextureOffset(52, 50).addBox(-2.8767F, -3.0667F, 3.61F, 5.8F, 1.0F, 1.1333F, 0.0F, false);
+        bipedHead.setTextureOffset(52, 50).addBox(-2.21F, -2.1333F, 3.6F, 4.3333F, 1.0F, 1.1333F, 0.0F, false);
+        bipedHead.setTextureOffset(52, 50).addBox(-3.01F, -6.1333F, -4.2667F, 6.2F, 1.0F, 1.2667F, 0.0F, false);
+        bipedHead.setTextureOffset(0, 57).addBox(-5.1667F, -8.3893F, -2.8607F, 10.7333F, 1.0F, 6.9333F, 0.0F, true);
+        bipedHead.setTextureOffset(0, 54).addBox(-3.6667F, -8.3893F, -4.7274F, 7.7333F, 1.0F, 9.9333F, 0.0F, true);
+        bipedHead.setTextureOffset(0, 54).addBox(-5.1333F, -5.4F, -0.1941F, 10.6667F, 1.0F, 5.4F, 0.0F, true);
+        bipedHead.setTextureOffset(0, 53).addBox(-5.1667F, -7.3641F, -4.7681F, 10.7333F, 2.0F, 9.9333F, 0.0F, true);
+        bipedHead.setTextureOffset(0, 57).addBox(-3.6667F, -9.3333F, -3.8404F, 7.7333F, 1.0F, 7.8667F, 0.0F, true);
+        bipedHead.setTextureOffset(40, 60).addBox(-1.6667F, -7.0803F, -5.2598F, 3.7333F, 1.6F, 2.1333F, 0.0F, true);
+        bipedHead.setTextureOffset(15, 39).addBox(1.3F, -4.6333F, -4.6F, 1.4F, 1.0F, 1.0F, 0.0F, true);
+        bipedHead.setTextureOffset(7, 39).addBox(-2.7F, -4.6333F, -4.6F, 1.4F, 1.0F, 1.0F, 0.0F, true);
+        bipedHead.setTextureOffset(0, 39).addBox(-3.0F, -5.1333F, -4.5F, 2.0F, 2.0F, 1.0F, 0.0F, true);
+        bipedHead.setTextureOffset(0, 33).addBox(-4.5F, -5.0333F, -4.35F, 9.0F, 1.0F, 4.0F, 0.0F, true);
+        bipedHead.setTextureOffset(20, 39).addBox(1.0F, -5.1333F, -4.5F, 2.0F, 2.0F, 1.0F, 0.0F, true);
 
-        chest = new ModelRenderer(this, 0, 55);
-        chest.addBox(-3.5F, 2.7F, -0.6F, 7, 3, 4);
-        chest.setRotationPoint(0F, 0F, 0F);
-        chest.setTextureSize(128, 64);
-        chest.mirror = true;
-        setRotation(chest, -0.5934119F, 0F, 0F);
+        ModelRenderer ponytailT = new ModelRenderer(this);
+        ponytailT.setRotationPoint(0.0F, 1.2F, 0.9333F);
+        setRotationAngle(ponytailT, 0.4363F, 0.0F, 0.0F);
+        ponytailT.setTextureOffset(52, 46).addBox(-1.0F, -2.905F, 4.6158F, 2.0F, 5.0F, 1.0F, 0.0F, true);
+        bipedHead.addChild(ponytailT);
 
-        goggleLeft = new ModelRenderer(this, 20, 39);
-        goggleLeft.addBox(1F, -5F, -4.5F, 2, 2, 1);
-        goggleLeft.setRotationPoint(0F, 0F, 0F);
-        goggleLeft.setTextureSize(128, 64);
-        goggleLeft.mirror = true;
-        setRotation(goggleLeft, 0F, 0F, 0F);
-
-        goggleBase = new ModelRenderer(this, 0, 33);
-        goggleBase.addBox(-4.5F, -4.9F, -4.35F, 9, 1, 4);
-        goggleBase.setRotationPoint(0F, 0F, 0F);
-        goggleBase.setTextureSize(128, 64);
-        goggleBase.mirror = true;
-        setRotation(goggleBase, 0F, 0F, 0F);
-
-        goggleRight = new ModelRenderer(this, 0, 39);
-        goggleRight.addBox(-3F, -5F, -4.5F, 2, 2, 1);
-        goggleRight.setRotationPoint(0F, 0F, 0F);
-        goggleRight.setTextureSize(128, 64);
-        goggleRight.mirror = true;
-        setRotation(goggleRight, 0F, 0F, 0F);
-
-        goggleRightLens = new ModelRenderer(this, 7, 39);
-        goggleRightLens.addBox(-2.3F, -4.5F, -4.6F, 1, 1, 1);
-        goggleRightLens.setRotationPoint(0F, 0F, 0F);
-        goggleRightLens.setTextureSize(128, 64);
-        goggleRightLens.mirror = true;
-        setRotation(goggleRightLens, 0F, 0F, 0F);
-
-        goggleLeftLens = new ModelRenderer(this, 15, 39);
-        goggleLeftLens.addBox(1.3F, -4.5F, -4.6F, 1, 1, 1);
-        goggleLeftLens.setRotationPoint(0F, 0F, 0F);
-        goggleLeftLens.setTextureSize(128, 64);
-        goggleLeftLens.mirror = true;
-        setRotation(goggleLeftLens, 0F, 0F, 0F);
-
-        helmetBase = new ModelRenderer(this, 32, 49);
-        helmetBase.addBox(-4.5F, -7.5F, -5.5F, 9, 2, 9);
-        helmetBase.setRotationPoint(0F, 0F, 0F);
-        helmetBase.setTextureSize(128, 64);
-        helmetBase.mirror = true;
-        setRotation(helmetBase, -0.122173F, 0F, 0F);
-
-        helmetFront = new ModelRenderer(this, 1, 43);
-        helmetFront.addBox(-3F, -8.5F, -5.5F, 6, 1, 9);
-        helmetFront.setRotationPoint(0F, 0F, 0F);
-        helmetFront.setTextureSize(128, 64);
-        helmetFront.mirror = true;
-        setRotation(helmetFront, 0F, 0F, 0F);
-
-        helmetLight = new ModelRenderer(this, 23, 54);
-        helmetLight.addBox(-1F, -8F, -6F, 2, 2, 2);
-        helmetLight.setRotationPoint(0F, 0F, 0F);
-        helmetLight.setTextureSize(128, 64);
-        helmetLight.mirror = true;
-        setRotation(helmetLight, 0F, 0F, 0F);
-
-        helmetMiddle = new ModelRenderer(this, 32, 41);
-        helmetMiddle.addBox(-4.5F, -8.5F, -3.5F, 9, 1, 6);
-        helmetMiddle.setRotationPoint(0F, 0F, 0F);
-        helmetMiddle.setTextureSize(128, 64);
-        helmetMiddle.mirror = true;
-        setRotation(helmetMiddle, 0F, 0F, 0F);
-
-        helmetTop = new ModelRenderer(this, 32, 33);
-        helmetTop.addBox(-3F, -9.5F, -3.5F, 6, 1, 6);
-        helmetTop.setRotationPoint(0F, 0F, 0F);
-        helmetTop.setTextureSize(128, 64);
-        helmetTop.mirror = true;
-        setRotation(helmetTop, 0F, 0F, 0F);
-
-        bag = new ModelRenderer(this, 57, 16);
-        bag.addBox(-3.5F, 7.5F, 1.4F, 4, 4, 1);
-        bag.setRotationPoint(0F, 0F, 0F);
-        bag.setTextureSize(128, 64);
-        bag.mirror = true;
-        setRotation(bag, 0F, 0F, 0F);
-
-        belt = new ModelRenderer(this, 57, 22);
-        belt.addBox(-4.5F, 9.5F, -2.5F, 9, 1, 5);
-        belt.setRotationPoint(0F, 0F, 0F);
-        belt.setTextureSize(128, 64);
-        belt.mirror = true;
-        setRotation(belt, 0F, 0F, 0F);
-
-        torch = new ModelRenderer(this, 57, 29);
-        torch.addBox(-3.5F, 7.4F, -2.4F, 1, 4, 1);
-        torch.setRotationPoint(0F, 0F, 0F);
-        torch.setTextureSize(128, 64);
-        torch.mirror = true;
-        setRotation(torch, 0F, 0F, 0F);
-
-        bagLock = new ModelRenderer(this, 68, 16);
-        bagLock.addBox(-2.5F, 8F, 1.5F, 2, 1, 1);
-        bagLock.setRotationPoint(0F, 0F, 0F);
-        bagLock.setTextureSize(128, 64);
-        bagLock.mirror = true;
-        setRotation(bagLock, 0F, 0F, 0F);
-
-        beltBuckle = new ModelRenderer(this, 68, 29);
-        beltBuckle.addBox(-1F, 9F, -2.7F, 2, 2, 1);
-        beltBuckle.setRotationPoint(0F, 0F, 0F);
-        beltBuckle.setTextureSize(128, 64);
-        beltBuckle.mirror = true;
-        setRotation(beltBuckle, 0F, 0F, 0F);
-
-        ponytailBase = new ModelRenderer(this, 80, 40);
-        ponytailBase.addBox(-0.5F, 2.4F, 3.7F, 1, 5, 1);
-        ponytailBase.setRotationPoint(0F, 0F, 0F);
-        ponytailBase.setTextureSize(128, 64);
-        ponytailBase.mirror = true;
-        setRotation(ponytailBase, 0.1047198F, 0F, 0F);
-
-        ponytailTail = new ModelRenderer(this, 79, 33);
-        ponytailTail.addBox(-1F, -2F, 3.4F, 2, 5, 1);
-        ponytailTail.setRotationPoint(0F, 0F, 0F);
-        ponytailTail.setTextureSize(128, 64);
-        ponytailTail.mirror = true;
-        setRotation(ponytailTail, 0.122173F, 0F, 0F);
-
-        bipedBody.addChild(chest);
-        bipedBody.addChild(belt);
-        bipedBody.addChild(bag);
-        bipedBody.addChild(torch);
-
-        belt.addChild(beltBuckle);
-        bag.addChild(bagLock);
-
-        bipedHead.addChild(goggleBase);
-        goggleBase.addChild(goggleLeft);
-        goggleBase.addChild(goggleRight);
-        goggleLeft.addChild(goggleLeftLens);
-        goggleRight.addChild(goggleRightLens);
-
-        bipedHead.addChild(ponytailBase);
-        ponytailBase.addChild(ponytailTail);
-
-        bipedHead.addChild(helmetBase);
-        helmetBase.addChild(helmetFront);
-        helmetFront.addChild(helmetLight);
-
-        helmetBase.addChild(helmetMiddle);
-        helmetMiddle.addChild(helmetTop);
+        ModelRenderer ponytailb = new ModelRenderer(this);
+        ponytailb.setRotationPoint(0.0F, 0.0F, 0.0F);
+        setRotationAngle(ponytailb, 0.1745F, 0.0F, 0.0F);
+        ponytailb.setTextureOffset(55, 47).addBox(-0.5F, 1.7538F, 5.6879F, 1.0F, 4.6F, 1.0F, 0.0F, true);
+        bipedHead.addChild(ponytailb);
 
         bipedHeadwear.showModel = false;
     }
 
-    private void setRotation(@NotNull final ModelRenderer model, final float x, final float y, final float z)
+    private void setRotationAngle(ModelRenderer modelRenderer, float x, float y, float z)
     {
-        model.rotateAngleX = x;
-        model.rotateAngleY = y;
-        model.rotateAngleZ = z;
+        modelRenderer.rotateAngleX = x;
+        modelRenderer.rotateAngleY = y;
+        modelRenderer.rotateAngleZ = z;
     }
 }
