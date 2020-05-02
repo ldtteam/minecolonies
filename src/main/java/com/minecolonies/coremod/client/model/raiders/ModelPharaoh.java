1f290547154347392e8209dbea8f75b084f472de
Ray  Neiheiser <ray.neiheiser@gmail.com>
2020-05-02 18:19:40 +0100

    commit model

--- src/main/java/com/minecolonies/coremod/client/model/raiders/ModelPharaoh.java
+++ src/main/java/com/minecolonies/coremod/client/model/raiders/ModelPharaoh.java
@@ -12,212 +12,149 @@ public class ModelPharaoh extends EgyptianModel<AbstractEntityEgyptian>
 {
     public ModelPharaoh()
     {
-        ModelRenderer armRightLayer;
-        ModelRenderer legRightPant;
-        ModelRenderer legRightLayer;
-        ModelRenderer bodyCollar;
-        ModelRenderer bodyBottom;
         ModelRenderer bodyJewel;
         ModelRenderer bodyGoldenStrip;
-        ModelRenderer bodyLayer;
-        ModelRenderer armLeftLayer;
-        ModelRenderer legLeftPant;
-        ModelRenderer legLeftLayer;
         ModelRenderer snakeBody;
         ModelRenderer snakeHead;
+        ModelRenderer headRightSideTop;
+        ModelRenderer headRightSideMiddle;
+        ModelRenderer headRightSideBottom;
         ModelRenderer headLeftSideTop;
         ModelRenderer headLeftSideMiddle;
         ModelRenderer headLeftSideBottom;
-        ModelRenderer headRightSideTop;
-        ModelRenderer headRightSideMiddle;
         ModelRenderer headTail;
         ModelRenderer headTop;
-        ModelRenderer headRightSideBottom;
         ModelRenderer headCap;
-        ModelRenderer headHat;
-        ModelRenderer headBack;
-        ModelRenderer headBackCap;
         ModelRenderer jaw;
-        ModelRenderer TeethTopFront;
-        ModelRenderer TeethTopRight;
-        ModelRenderer TeethTopLeft;
-        ModelRenderer TeethBottomFront;
-        ModelRenderer TeethBottomRight;
-        ModelRenderer TeethBottomLeft;
         
         textureWidth = 128;
         textureHeight = 64;
-        headBackCap = new ModelRenderer(this, 14, 59);
-        headBackCap.setRotationPoint(0.0F, 0.0F, 0.92F);
-        headBackCap.addBox(-4.5F, -2.85F, 0.0F, 9, 3, 0, 0.0F);
-        bipedRightArm = new ModelRenderer(this, 40, 16);
-        bipedRightArm.setRotationPoint(-5.0F, 2.0F, 0.0F);
-        bipedRightArm.addBox(-3.0F, -2.0F, -2.0F, 4, 12, 4, 0.0F);
-        TeethBottomFront = new ModelRenderer(this, 0, 15);
-        TeethBottomFront.setRotationPoint(0.0F, 0.0F, 0.0F);
-        TeethBottomFront.addBox(-2.5F, 0.0F, -4.0F, 5, 1, 0, 0.0F);
-        headRightSideMiddle = new ModelRenderer(this, 14, 51);
-        headRightSideMiddle.setRotationPoint(-7.82F, -7.11F, 0.86F);
-        headRightSideMiddle.addBox(0.0F, 0.0F, 0.19F, 5, 6, 2, 0.14F);
-        setRotateAngle(headRightSideMiddle, 0.0F, 0.0F, 0.26878070480712674F);
-        bodyLayer = new ModelRenderer(this, 0, 32);
-        bodyLayer.setRotationPoint(0.0F, 0.0F, 0.0F);
-        bodyLayer.addBox(-4.0F, 0.0F, -2.0F, 8, 12, 4, 0.3F);
-        bodyCollar = new ModelRenderer(this, 44, 0);
-        bodyCollar.setRotationPoint(0.0F, 0.0F, 0.0F);
-        bodyCollar.addBox(-5.5F, -0.2F, -2.5F, 11, 5, 5, 0.1F);
-        TeethBottomRight = new ModelRenderer(this, 10, 11);
-        TeethBottomRight.setRotationPoint(0.0F, 0.0F, 0.0F);
-        TeethBottomRight.addBox(-2.5F, 0.0F, -4.0F, 0, 1, 4, 0.0F);
-        legLeftPant = new ModelRenderer(this, 56, 20);
-        legLeftPant.mirror = true;
-        legLeftPant.setRotationPoint(0.0F, 0.0F, 0.0F);
-        legLeftPant.addBox(-2.4F, -0.5F, -2.5F, 5, 6, 5, 0.0F);
-        bipedRightLeg = new ModelRenderer(this, 24, 16);
-        bipedRightLeg.setRotationPoint(-1.9F, 12.0F, 0.1F);
-        bipedRightLeg.addBox(-2.0F, 0.0F, -2.0F, 4, 12, 4, 0.0F);
-        jaw = new ModelRenderer(this, 33, 54);
-        jaw.setRotationPoint(0.0F, -2.0F, 0.0F);
-        jaw.addBox(-2.5F, 1.0F, -4.0F, 5, 1, 5, 0.0F);
-        setRotateAngle(jaw, 0.5462880558742251F, 0.0F, 0.0F);
-        bipedBody = new ModelRenderer(this, 0, 16);
-        bipedBody.setRotationPoint(0.0F, 0.0F, 0.0F);
-        bipedBody.addBox(-4.0F, 0.0F, -2.0F, 8, 12, 4, 0.0F);
-        headTail = new ModelRenderer(this, 28, 51);
-        headTail.setRotationPoint(-1.5F, 0.4F, 2.5F);
-        headTail.addBox(0.0F, 0.0F, 0.0F, 3, 6, 2, -0.2F);
-        armRightLayer = new ModelRenderer(this, 40, 32);
-        armRightLayer.setRotationPoint(0.0F, 0.0F, 0.0F);
-        armRightLayer.addBox(-3.0F, -2.0F, -2.0F, 4, 12, 4, 0.3F);
-        headBack = new ModelRenderer(this, 38, 48);
-        headBack.setRotationPoint(0.0F, 0.0F, 0.0F);
-        headBack.addBox(-3.0F, -2.0F, 0.0F, 6, 2, 4, 0.0F);
-        armLeftLayer = new ModelRenderer(this, 24, 32);
-        armLeftLayer.mirror = true;
-        armLeftLayer.setRotationPoint(0.0F, 0.0F, 0.0F);
-        armLeftLayer.addBox(-1.0F, -2.0F, -2.0F, 4, 12, 4, 0.3F);
-        legLeftLayer = new ModelRenderer(this, 40, 32);
-        legLeftLayer.mirror = true;
-        legLeftLayer.setRotationPoint(0.0F, 0.0F, 0.0F);
-        legLeftLayer.addBox(-2.0F, 0.0F, -2.0F, 4, 12, 4, 0.2F);
-        headRightSideBottom = new ModelRenderer(this, 0, 57);
-        headRightSideBottom.setRotationPoint(-9.58F, -1.23F, 0.91F);
-        headRightSideBottom.addBox(0.0F, -1.0F, -4.0F, 3, 1, 4, 0.0F);
-        setRotateAngle(headRightSideBottom, 0.0F, -0.9147270609702279F, 0.26878070480712674F);
-        headLeftSideBottom = new ModelRenderer(this, 0, 57);
-        headLeftSideBottom.mirror = true;
-        headLeftSideBottom.setRotationPoint(9.58F, -1.23F, 0.91F);
-        headLeftSideBottom.addBox(-3.0F, -1.0F, -4.0F, 3, 1, 4, 0.0F);
-        setRotateAngle(headLeftSideBottom, 0.0F, 0.9147270609702279F, -0.26878070480712674F);
-        snakeHead = new ModelRenderer(this, 77, 3);
-        snakeHead.setRotationPoint(0.0F, -10.65F, -4.5F);
-        snakeHead.addBox(-1.5F, -1.0F, -2.0F, 3, 3, 3, -0.99F);
-        setRotateAngle(snakeHead, 0.2792526803190927F, 0.0F, 0.0F);
-        headCap = new ModelRenderer(this, 18, 49);
-        headCap.setRotationPoint(0.0F, -11.4F, 0.85F);
-        headCap.addBox(-4.5F, 0.15F, 0.19F, 9, 0, 2, 0.13F);
-        headLeftSideMiddle = new ModelRenderer(this, 14, 51);
-        headLeftSideMiddle.mirror = true;
-        headLeftSideMiddle.setRotationPoint(7.82F, -7.11F, 0.86F);
-        headLeftSideMiddle.addBox(-5.0F, 0.0F, 0.19F, 5, 6, 2, 0.14F);
-        setRotateAngle(headLeftSideMiddle, 0.0F, 0.0F, -0.26878070480712674F);
-        bipedLeftArm = new ModelRenderer(this, 24, 16);
-        bipedLeftArm.mirror = true;
+
+        bipedLeftArm = new ModelRenderer(this);
         bipedLeftArm.setRotationPoint(5.0F, 2.0F, 0.0F);
-        bipedLeftArm.addBox(-1.0F, -2.0F, -2.0F, 4, 12, 4, 0.0F);
-        legRightLayer = new ModelRenderer(this, 24, 32);
-        legRightLayer.setRotationPoint(0.0F, 0.0F, 0.0F);
-        legRightLayer.addBox(-2.0F, 0.0F, -2.0F, 4, 12, 4, 0.2F);
-        bodyBottom = new ModelRenderer(this, 52, 11);
-        bodyBottom.setRotationPoint(0.0F, 0.0F, 0.0F);
-        bodyBottom.addBox(-4.5F, 8.5F, -2.5F, 9, 4, 5, 0.0F);
-        headHat = new ModelRenderer(this, 80, 0);
-        headHat.setRotationPoint(0.0F, 0.0F, 0.0F);
-        headHat.addBox(-4.5F, -8.5F, -4.5F, 9, 9, 9, 0.1F);
-        headRightSideTop = new ModelRenderer(this, 0, 51);
-        headRightSideTop.setRotationPoint(-4.6F, -11.4F, 0.86F);
-        headRightSideTop.addBox(-5.15F, 0.13F, 0.19F, 5, 4, 2, 0.13F);
-        setRotateAngle(headRightSideTop, 0.0F, 0.0F, -0.890117918517108F);
-        bodyJewel = new ModelRenderer(this, 0, 0);
-        bodyJewel.setRotationPoint(0.0F, 10.0F, -2.0F);
-        bodyJewel.addBox(-1.5F, -1.5F, -1.0F, 3, 3, 1, -0.3F);
-        setRotateAngle(bodyJewel, 0.0F, 0.0F, 0.7853981633974483F);
-        bipedLeftLeg = new ModelRenderer(this, 40, 16);
-        bipedLeftLeg.mirror = true;
+        bipedLeftArm.setTextureOffset(24, 16).addBox(-1.0F, -2.0F, -2.0F, 4.0F, 12.0F, 4.0F, 0.0F, true);
+        bipedLeftArm.setTextureOffset(24, 32).addBox(-1.0F, -2.0F, -2.0F, 4.0F, 12.0F, 4.0F, 0.3F, true);
+
+        bipedLeftLeg = new ModelRenderer(this);
         bipedLeftLeg.setRotationPoint(1.9F, 12.0F, 0.1F);
-        bipedLeftLeg.addBox(-2.0F, 0.0F, -2.0F, 4, 12, 4, 0.0F);
-        headTop = new ModelRenderer(this, 76, 18);
-        headTop.setRotationPoint(0.0F, -8.55F, -4.45F);
-        headTop.addBox(-4.5F, 0.0F, 0.0F, 9, 4, 8, 0.09F);
-        setRotateAngle(headTop, 0.4714134309636684F, 0.0F, 0.0F);
-        TeethTopLeft = new ModelRenderer(this, 10, 10);
-        TeethTopLeft.setRotationPoint(0.0F, 0.0F, 0.0F);
-        TeethTopLeft.addBox(2.5F, -2.0F, -3.5F, 0, 1, 4, 0.0F);
-        snakeBody = new ModelRenderer(this, 76, 2);
-        snakeBody.setRotationPoint(0.0F, -7.5F, -4.6F);
-        snakeBody.addBox(-0.5F, -3.0F, -1.0F, 1, 3, 1, 0.0F);
-        setRotateAngle(snakeBody, -0.3490658503988659F, 0.0F, 0.0F);
-        legRightPant = new ModelRenderer(this, 56, 20);
-        legRightPant.setRotationPoint(0.0F, 0.0F, 0.0F);
-        legRightPant.addBox(-2.6F, -0.5F, -2.5F, 5, 6, 5, 0.0F);
-        bodyGoldenStrip = new ModelRenderer(this, 38, 12);
-        bodyGoldenStrip.setRotationPoint(0.0F, 10.0F, -2.6F);
-        bodyGoldenStrip.addBox(-1.5F, 0.0F, 0.0F, 3, 8, 0, 0.0F);
-        TeethTopFront = new ModelRenderer(this, 0, 14);
-        TeethTopFront.setRotationPoint(0.0F, 0.0F, 0.0F);
-        TeethTopFront.addBox(-2.5F, -2.0F, -3.5F, 5, 1, 0, 0.0F);
-        bipedHead = new ModelRenderer(this, 0, 0);
-        bipedHead.setRotationPoint(0.0F, 0.0F, -1.0F);
-        bipedHead.addBox(-4.0F, -8.0F, -4.0F, 8, 6, 8, 0.0F);
-        headLeftSideTop = new ModelRenderer(this, 0, 51);
-        headLeftSideTop.mirror = true;
-        headLeftSideTop.setRotationPoint(4.6F, -11.4F, 0.86F);
-        headLeftSideTop.addBox(0.15F, 0.13F, 0.19F, 5, 4, 2, 0.13F);
-        setRotateAngle(headLeftSideTop, 0.0F, 0.0F, 0.890117918517108F);
-        TeethBottomLeft = new ModelRenderer(this, 10, 11);
-        TeethBottomLeft.setRotationPoint(0.0F, 0.0F, 0.0F);
-        TeethBottomLeft.addBox(2.5F, 0.0F, -4.0F, 0, 1, 4, 0.0F);
-        TeethTopRight = new ModelRenderer(this, 10, 10);
-        TeethTopRight.setRotationPoint(0.0F, 0.0F, 0.0F);
-        TeethTopRight.addBox(-2.5F, -2.0F, -3.5F, 0, 1, 4, 0.0F);
-        bipedHead.addChild(headBackCap);
-        jaw.addChild(TeethBottomFront);
-        bipedHead.addChild(headRightSideMiddle);
-        bipedBody.addChild(bodyLayer);
-        bipedBody.addChild(bodyCollar);
-        jaw.addChild(TeethBottomRight);
-        bipedLeftLeg.addChild(legLeftPant);
-        bipedHead.addChild(jaw);
-        bipedHead.addChild(headTail);
-        bipedRightArm.addChild(armRightLayer);
-        bipedHead.addChild(headBack);
-        bipedLeftArm.addChild(armLeftLayer);
-        bipedLeftLeg.addChild(legLeftLayer);
-        bipedHead.addChild(headRightSideBottom);
-        bipedHead.addChild(headLeftSideBottom);
-        bipedHead.addChild(snakeHead);
-        bipedHead.addChild(headCap);
-        bipedHead.addChild(headLeftSideMiddle);
-        bipedRightLeg.addChild(legRightLayer);
-        bipedBody.addChild(bodyBottom);
-        bipedHead.addChild(headHat);
-        bipedHead.addChild(headRightSideTop);
+        bipedLeftLeg.setTextureOffset(40, 16).addBox(-2.0F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, 0.0F, true);
+        bipedLeftLeg.setTextureOffset(56, 20).addBox(-2.4F, -0.5F, -2.5F, 5.0F, 6.0F, 5.0F, 0.0F, true);
+        bipedLeftLeg.setTextureOffset(40, 32).addBox(-2.0F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, 0.2F, true);
+
+        bipedBody = new ModelRenderer(this);
+        bipedBody.setRotationPoint(0.0F, 0.0F, 0.0F);
+        bipedBody.setTextureOffset(0, 16).addBox(-4.0F, 0.0F, -2.0F, 8.0F, 12.0F, 4.0F, 0.0F, true);
+        bipedBody.setTextureOffset(44, 0).addBox(-5.5F, -0.2F, -2.5F, 11.0F, 5.0F, 5.0F, 0.15F, true);
+        bipedBody.setTextureOffset(0, 32).addBox(-4.0F, 0.0F, -2.0F, 8.0F, 12.0F, 4.0F, 0.3F, true);
+        bipedBody.setTextureOffset(52, 11).addBox(-4.5F, 8.5F, -2.5F, 9.0F, 4.0F, 5.0F, 0.0F, true);
+
+        bodyJewel = new ModelRenderer(this);
+        bodyJewel.setRotationPoint(0.0F, 10.0F, -2.0F);
         bipedBody.addChild(bodyJewel);
-        bipedHead.addChild(headTop);
-        bipedHead.addChild(TeethTopLeft);
-        bipedHead.addChild(snakeBody);
-        bipedRightLeg.addChild(legRightPant);
+        setRotationAngle(bodyJewel, 0.0F, 0.0F, 0.7854F);
+        bodyJewel.setTextureOffset(0, 0).addBox(-1.5F, -1.5F, -1.0F, 3.0F, 3.0F, 1.0F, -0.3F, true);
+
+        bodyGoldenStrip = new ModelRenderer(this);
+        bodyGoldenStrip.setRotationPoint(0.0F, 10.0F, -2.6F);
         bipedBody.addChild(bodyGoldenStrip);
-        bipedHead.addChild(TeethTopFront);
+        bodyGoldenStrip.setTextureOffset(38, 12).addBox(-1.5F, 0.0F, 0.0F, 3.0F, 8.0F, 0.0F, 0.0F, true);
+
+        bipedRightArm = new ModelRenderer(this);
+        bipedRightArm.setRotationPoint(-5.0F, 2.0F, 0.0F);
+        bipedRightArm.setTextureOffset(40, 16).addBox(-3.0F, -2.0F, -2.0F, 4.0F, 12.0F, 4.0F, 0.0F, false);
+        bipedRightArm.setTextureOffset(40, 32).addBox(-3.0F, -2.0F, -2.0F, 4.0F, 12.0F, 4.0F, 0.3F, false);
+
+        bipedRightLeg = new ModelRenderer(this);
+        bipedRightLeg.setRotationPoint(-1.9F, 12.0F, 0.1F);
+        bipedRightLeg.setTextureOffset(24, 16).addBox(-2.0F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, 0.0F, false);
+        bipedRightLeg.setTextureOffset(56, 20).addBox(-2.6F, -0.5F, -2.5F, 5.0F, 6.0F, 5.0F, 0.0F, false);
+        bipedRightLeg.setTextureOffset(24, 32).addBox(-2.0F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, 0.2F, false);
+
+        bipedHead = new ModelRenderer(this);
+        bipedHead.setRotationPoint(0.0F, 0.0F, -1.0F);
+        bipedHead.setTextureOffset(0, 0).addBox(-4.0F, -8.0F, -4.0F, 8.0F, 6.0F, 8.0F, 0.0F, true);
+        bipedHead.setTextureOffset(80, 0).addBox(-4.5F, -8.5F, -4.5F, 9.0F, 9.0F, 9.0F, 0.1F, true);
+        bipedHead.setTextureOffset(38, 48).addBox(-3.0F, -2.0F, 0.0F, 6.0F, 2.0F, 4.0F, 0.0F, true);
+        bipedHead.setTextureOffset(14, 59).addBox(-4.5F, -2.85F, 1.0F, 9.0F, 3.0F, 0.0F, 0.0F, true);
+        bipedHead.setTextureOffset(0, 14).addBox(-2.5F, -2.0F, -3.5F, 5.0F, 1.0F, 0.0F, 0.0F, true);
+        bipedHead.setTextureOffset(10, 10).addBox(-2.5F, -2.0F, -3.5F, 0.0F, 1.0F, 4.0F, 0.0F, true);
+        bipedHead.setTextureOffset(10, 10).addBox(2.5F, -2.0F, -3.5F, 0.0F, 1.0F, 4.0F, 0.0F, true);
+
+        snakeBody = new ModelRenderer(this);
+        snakeBody.setRotationPoint(0.0F, -7.5F, -4.6F);
+        bipedHead.addChild(snakeBody);
+        setRotationAngle(snakeBody, -0.3491F, 0.0F, 0.0F);
+        snakeBody.setTextureOffset(76, 2).addBox(-0.5F, -3.0F, -1.0F, 1.0F, 3.0F, 1.0F, 0.0F, true);
+
+        snakeHead = new ModelRenderer(this);
+        snakeHead.setRotationPoint(0.0F, -10.65F, -4.5F);
+        bipedHead.addChild(snakeHead);
+        setRotationAngle(snakeHead, 0.2793F, 0.0F, 0.0F);
+        snakeHead.setTextureOffset(77, 3).addBox(-1.5F, -1.0F, -2.0F, 3.0F, 3.0F, 3.0F, -0.99F, true);
+
+        headRightSideTop = new ModelRenderer(this);
+        headRightSideTop.setRotationPoint(-4.6F, -11.4F, 0.86F);
+        bipedHead.addChild(headRightSideTop);
+        setRotationAngle(headRightSideTop, 0.0F, 0.0F, -0.8901F);
+        headRightSideTop.setTextureOffset(0, 51).addBox(-5.15F, 0.13F, 0.19F, 5.0F, 4.0F, 2.0F, 0.13F, false);
+
+        headRightSideMiddle = new ModelRenderer(this);
+        headRightSideMiddle.setRotationPoint(-7.82F, -7.11F, 0.86F);
+        bipedHead.addChild(headRightSideMiddle);
+        setRotationAngle(headRightSideMiddle, 0.0F, 0.0F, 0.2688F);
+        headRightSideMiddle.setTextureOffset(14, 51).addBox(0.0F, 0.0F, 0.19F, 5.0F, 6.0F, 2.0F, 0.14F, false);
+
+        headRightSideBottom = new ModelRenderer(this);
+        headRightSideBottom.setRotationPoint(-9.58F, -1.23F, 0.91F);
+        bipedHead.addChild(headRightSideBottom);
+        setRotationAngle(headRightSideBottom, 0.0F, -0.9147F, 0.2688F);
+        headRightSideBottom.setTextureOffset(0, 57).addBox(0.0F, -1.0F, -4.0F, 3.0F, 1.0F, 4.0F, 0.0F, false);
+
+        headLeftSideTop = new ModelRenderer(this);
+        headLeftSideTop.setRotationPoint(4.6F, -11.4F, 0.86F);
         bipedHead.addChild(headLeftSideTop);
-        jaw.addChild(TeethBottomLeft);
-        bipedHead.addChild(TeethTopRight);
+        setRotationAngle(headLeftSideTop, 0.0F, 0.0F, 0.8901F);
+        headLeftSideTop.setTextureOffset(0, 51).addBox(0.15F, 0.13F, 0.19F, 5.0F, 4.0F, 2.0F, 0.13F, true);
+
+        headLeftSideMiddle = new ModelRenderer(this);
+        headLeftSideMiddle.setRotationPoint(7.82F, -7.11F, 0.86F);
+        bipedHead.addChild(headLeftSideMiddle);
+        setRotationAngle(headLeftSideMiddle, 0.0F, 0.0F, -0.2688F);
+        headLeftSideMiddle.setTextureOffset(14, 51).addBox(-5.0F, 0.0F, 0.19F, 5.0F, 6.0F, 2.0F, 0.14F, true);
+
+        headLeftSideBottom = new ModelRenderer(this);
+        headLeftSideBottom.setRotationPoint(9.58F, -1.23F, 0.91F);
+        bipedHead.addChild(headLeftSideBottom);
+        setRotationAngle(headLeftSideBottom, 0.0F, 0.9147F, -0.2688F);
+        headLeftSideBottom.setTextureOffset(0, 57).addBox(-3.0F, -1.0F, -4.0F, 3.0F, 1.0F, 4.0F, 0.0F, true);
+
+        headTail = new ModelRenderer(this);
+        headTail.setRotationPoint(1.5F, 0.4F, 2.5F);
+        bipedHead.addChild(headTail);
+        headTail.setTextureOffset(28, 51).addBox(-3.0F, 0.0F, 0.0F, 3.0F, 6.0F, 2.0F, -0.2F, true);
+
+        headTop = new ModelRenderer(this);
+        headTop.setRotationPoint(0.0F, -8.55F, -4.45F);
+        bipedHead.addChild(headTop);
+        setRotationAngle(headTop, 0.4714F, 0.0F, 0.0F);
+        headTop.setTextureOffset(76, 18).addBox(-4.5F, 0.0F, 0.0F, 9.0F, 4.0F, 8.0F, 0.09F, true);
+
+        headCap = new ModelRenderer(this);
+        headCap.setRotationPoint(0.0F, -11.4F, 0.85F);
+        bipedHead.addChild(headCap);
+        headCap.setTextureOffset(18, 49).addBox(-4.5F, 0.15F, 0.19F, 9.0F, 0.0F, 2.0F, 0.13F, true);
+
+        jaw = new ModelRenderer(this);
+        jaw.setRotationPoint(0.0F, -2.0F, 0.0F);
+        bipedHead.addChild(jaw);
+        setRotationAngle(jaw, 0.5463F, 0.0F, 0.0F);
+        jaw.setTextureOffset(33, 54).addBox(-2.5F, 1.0F, -4.0F, 5.0F, 1.0F, 5.0F, 0.0F, true);
+        jaw.setTextureOffset(10, 11).addBox(2.5F, 0.0F, -4.0F, 0.0F, 1.0F, 4.0F, 0.0F, true);
+        jaw.setTextureOffset(0, 15).addBox(-2.5F, 0.0F, -4.0F, 5.0F, 1.0F, 0.0F, 0.0F, true);
+        jaw.setTextureOffset(10, 11).addBox(-2.5F, 0.0F, -4.0F, 0.0F, 1.0F, 4.0F, 0.0F, true);
     }
 
-    /**
-     * This is a helper function from Tabula to set the rotation of model parts
-     */
-    public void setRotateAngle(ModelRenderer modelRenderer, float x, float y, float z)
+    private void setRotationAngle(ModelRenderer modelRenderer, float x, float y, float z)
     {
         modelRenderer.rotateAngleX = x;
         modelRenderer.rotateAngleY = y;
