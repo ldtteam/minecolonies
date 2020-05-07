e3d6caec51ddea7f7ff588a2646cd584d704abb2
someaddons <someaddons@github.com>
2020-05-07 14:34:51 +0200

    Fix colony delete own message

--- src/main/java/com/minecolonies/coremod/network/messages/server/colony/ColonyDeleteOwnMessage.java
+++ src/main/java/com/minecolonies/coremod/network/messages/server/colony/ColonyDeleteOwnMessage.java
@@ -3,51 +3,58 @@ package com.minecolonies.coremod.network.messages.server.colony;
 import com.ldtteam.structurize.util.LanguageHandler;
 import com.minecolonies.api.colony.IColony;
 import com.minecolonies.api.colony.IColonyManager;
-import com.minecolonies.api.colony.permissions.Action;
-import com.minecolonies.coremod.network.messages.server.AbstractColonyServerMessage;
+import com.minecolonies.api.network.IMessage;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.network.PacketBuffer;
+import net.minecraftforge.fml.LogicalSide;
 import net.minecraftforge.fml.network.NetworkEvent;
 import org.jetbrains.annotations.Nullable;
 
 /**
  * Message for deleting an owned colony
  */
-public class ColonyDeleteOwnMessage extends AbstractColonyServerMessage
+public class ColonyDeleteOwnMessage implements IMessage
 {
+    @Override
+    public void toBytes(final PacketBuffer buf)
+    {
+
+    }
+
+    @Override
+    public void fromBytes(final PacketBuffer buf)
+    {
+
+    }
 
     @Nullable
     @Override
-    public Action permissionNeeded()
+    public LogicalSide getExecutionSide()
     {
-        return null;
+        return LogicalSide.SERVER;
     }
 
     @Override
-    public boolean ownerOnly()
-    {
-        return true;
-    }
-
-    @Override
-    protected void onExecute(final NetworkEvent.Context ctxIn, final boolean isLogicalServer, final IColony colony)
+    public void onExecute(final NetworkEvent.Context ctxIn, final boolean isLogicalServer)
     {
         final ServerPlayerEntity player = ctxIn.getSender();
-        if (player == null) return;
+        if (player == null)
+        {
+            return;
+        }
 
-        IColonyManager.getInstance().deleteColonyByWorld(colony.getID(), false, player.world);
-        LanguageHandler.sendPlayerMessage(player, "com.minecolonies.coremod.gui.colony.delete.success");
-    }
-
-    @Override
-    protected void toBytesOverride(final PacketBuffer buf)
-    {
-
-    }
-
-    @Override
-    protected void fromBytesOverride(final PacketBuffer buf)
-    {
-
+        final IColony colony = IColonyManager.getInstance().getIColonyByOwner(player.world, player);
+        if (colony != null)
+        {
+            IColonyManager.getInstance().deleteColonyByDimension(colony.getID(), false, colony.getDimension());
+            LanguageHandler.sendPlayerMessage(player, "com.minecolonies.coremod.gui.colony.delete.success");
+        }
+        else
+        {
+            LanguageHandler.sendPlayerMessage(
+              player,
+              "com.minecolonies.command.nocolony", this.getClass().getSimpleName()
+            );
+        }
     }
 }
