7af13e995421eac5c58cacde42fc97d146290842
JiaYow <jiayow@gmail.com>
2020-05-07 21:43:05 +0200

    The lumberjack now fully and correctly decides between chopping wood and crafting stripped reciped :-D

--- src/main/java/com/minecolonies/coremod/colony/buildings/AbstractFilterableListIntrinsicCrafter.java
+++ src/main/java/com/minecolonies/coremod/colony/buildings/AbstractFilterableListIntrinsicCrafter.java
@@ -50,16 +50,6 @@ import static com.minecolonies.api.util.constant.NbtTagConstants.TAG_ID;
 public abstract class AbstractFilterableListIntrinsicCrafter extends AbstractFilterableListBuilding
 {
     /**
-     * Tag to store the item list.
-     */
-    private static final String TAG_ITEMLIST = "itemList";
-
-    /**
-     * List of allowed items.
-     */
-    private final Map<String, List<ItemStorage>> itemsAllowed = new HashMap<>();
-
-    /**
      * The constructor of the building.
      *
      * @param c the colony
@@ -69,134 +59,7 @@ public abstract class AbstractFilterableListIntrinsicCrafter extends AbstractFil
     {
         super(c, l);
     }
-    @Override
-    public void deserializeNBT(final CompoundNBT compound)
-    {
-        super.deserializeNBT(compound);
-        final ListNBT filterableList = compound.getList(TAG_ITEMLIST, Constants.NBT.TAG_COMPOUND);
-        for (int i = 0; i < filterableList.size(); ++i)
-        {
-            try
-            {
-                final CompoundNBT listItem = filterableList.getCompound(i);
-                final String id = listItem.getString(TAG_ID);
-                final ListNBT filterableItems = listItem.getList(TAG_ITEMLIST, Constants.NBT.TAG_COMPOUND);
-                final List<ItemStorage> items = new ArrayList<>();
-                for (int j = 0; j < filterableItems.size(); ++j)
-                {
-                    items.add(new ItemStorage(ItemStack.read(filterableItems.getCompound(j))));
-                }
-                if (!items.isEmpty())
-                {
-                    itemsAllowed.put(id, items);
-                }
-            }
-            catch (Exception e)
-            {
-                Log.getLogger().info("Removing incompatible stack");
-            }
-        }
-    }
 
-    @Override
-    public CompoundNBT serializeNBT()
-    {
-        final CompoundNBT compound = super.serializeNBT();
-        @NotNull final ListNBT filterableListCompound = new ListNBT();
-        for(@NotNull final Map.Entry<String, List<ItemStorage>> entry : itemsAllowed.entrySet())
-        {
-            @NotNull final CompoundNBT listCompound = new CompoundNBT();
-            listCompound.putString(TAG_ID, entry.getKey());
-            @NotNull final ListNBT filteredItems = new ListNBT();
-            for(@NotNull final ItemStorage item : entry.getValue())
-            {
-                @NotNull final CompoundNBT itemCompound = new CompoundNBT();
-                item.getItemStack().write(itemCompound);
-                filteredItems.add(itemCompound);
-            }
-            listCompound.put(TAG_ITEMLIST, filteredItems);
-            filterableListCompound.add(listCompound);
-        }
-        compound.put(TAG_ITEMLIST, filterableListCompound);
-        return compound;
-    }
-
-    /**
-     * Add a compostable item to the list.
-     * @param id the string id of the item type.
-     * @param item the item to add.
-     */
-    public void addItem(final String id, final ItemStorage item)
-    {
-        if(itemsAllowed.containsKey(id))
-        {
-            if (!itemsAllowed.get(id).contains(item))
-            {
-                final List<ItemStorage> list = itemsAllowed.get(id);
-                list.add(item);
-                itemsAllowed.put(id, list);
-            }
-        }
-        else
-        {
-            final List<ItemStorage> list = new ArrayList<>();
-            list.add(item);
-            itemsAllowed.put(id, list);
-        }
-        markDirty();
-    }
-
-    /**
-     * Check if the item is an allowed item.
-     * @param item the item to check.
-     * @param id the string id of the item type.
-     * @return true if so.
-     */
-    public boolean isAllowedItem(final String id, final ItemStorage item)
-    {
-        return itemsAllowed.containsKey(id) && itemsAllowed.get(id).contains(item);
-    }
-
-    /**
-     * Remove a compostable item from the list.
-     * @param id the string id of the item type.
-     * @param item the item to remove.
-     */
-    public void removeItem(final String id, final ItemStorage item)
-    {
-        if(itemsAllowed.containsKey(id) && itemsAllowed.get(id).contains(item))
-        {
-            final List<ItemStorage> list = itemsAllowed.get(id);
-            list.remove(item);
-            itemsAllowed.put(id, list);
-        }
-        markDirty();
-    }
-
-    /**
-     * Getter of copy of all allowed items.
-     * @return a copy.
-     */
-    public Map<String, List<ItemStorage>> getCopyOfAllowedItems()
-    {
-        return new HashMap<>(itemsAllowed);
-    }
-
-    @Override
-    public void serializeToView(@NotNull final PacketBuffer buf)
-    {
-        super.serializeToView(buf);
-        buf.writeInt(itemsAllowed.size());
-        for (final Map.Entry<String, List<ItemStorage>> entry : itemsAllowed.entrySet())
-        {
-            buf.writeString(entry.getKey());
-            buf.writeInt(entry.getValue().size());
-            for (final ItemStorage item : entry.getValue())
-            {
-                buf.writeItemStack(item.getItemStack());
-            }
-        }
-    }
 
     /* Intrinsic Crafter Functionality follows here */
 
@@ -268,5 +131,44 @@ public abstract class AbstractFilterableListIntrinsicCrafter extends AbstractFil
         final Map<Predicate<ItemStack>, Tuple<Integer, Boolean>> toKeep = new HashMap<>(keepX);
         toKeep.putAll(recipeOutputs.entrySet().stream().collect(Collectors.toMap(key -> (stack -> stack.isItemEqual(key.getKey().getItemStack())), Map.Entry::getValue)));
         return toKeep;
+    }
+
+    @Override
+    public boolean canCraftComplexRecipes()
+    {
+        return false;
+    }
+
+    @Override
+    public boolean canRecipeBeAdded(final IToken token)
+    {
+        return true;
+    }
+
+    /**
+     * Crafter building View.
+     */
+    public static class View extends AbstractBuildingWorker.View
+    {
+        /**
+         * Instantiate the crafter view.
+         *
+         * @param c the colonyview to put it in
+         * @param l the positon
+         */
+        public View(final IColonyView c, final BlockPos l)
+        {
+            super(c, l);
+        }
+
+        /**
+         * Check if an additional recipe can be added.
+         *
+         * @return true if so.
+         */
+        public boolean canRecipeBeAdded()
+        {
+            return false;
+        }
     }
 }
--- src/main/java/com/minecolonies/coremod/colony/buildings/workerbuildings/BuildingLumberjack.java
+++ src/main/java/com/minecolonies/coremod/colony/buildings/workerbuildings/BuildingLumberjack.java
@@ -106,18 +106,20 @@ public class BuildingLumberjack extends AbstractFilterableListIntrinsicCrafter
 
         keepX.put(itemStack -> ItemStackUtils.hasToolLevel(itemStack, ToolType.AXE, TOOL_LEVEL_WOOD_OR_GOLD, getMaxToolLevel()), new Tuple<>(1, true));
 
-        addStrippedWoodRecipe(Items.OAK_LOG, Items.STRIPPED_OAK_LOG);
-        addStrippedWoodRecipe(Items.SPRUCE_LOG, Items.STRIPPED_SPRUCE_LOG);
-        addStrippedWoodRecipe(Items.BIRCH_LOG, Items.STRIPPED_BIRCH_LOG);
-        addStrippedWoodRecipe(Items.JUNGLE_LOG, Items.STRIPPED_JUNGLE_LOG);
-        addStrippedWoodRecipe(Items.ACACIA_LOG, Items.STRIPPED_ACACIA_LOG);
-        addStrippedWoodRecipe(Items.DARK_OAK_LOG, Items.STRIPPED_DARK_OAK_LOG);
-        addStrippedWoodRecipe(Items.OAK_WOOD, Items.STRIPPED_OAK_WOOD);
-        addStrippedWoodRecipe(Items.SPRUCE_WOOD, Items.STRIPPED_SPRUCE_WOOD);
-        addStrippedWoodRecipe(Items.BIRCH_WOOD, Items.STRIPPED_BIRCH_WOOD);
-        addStrippedWoodRecipe(Items.JUNGLE_WOOD, Items.STRIPPED_JUNGLE_WOOD);
-        addStrippedWoodRecipe(Items.ACACIA_WOOD, Items.STRIPPED_ACACIA_WOOD);
-        addStrippedWoodRecipe(Items.DARK_OAK_WOOD, Items.STRIPPED_DARK_OAK_WOOD);
+        if (recipes.isEmpty()) {
+            addStrippedWoodRecipe(Items.OAK_LOG, Items.STRIPPED_OAK_LOG);
+            addStrippedWoodRecipe(Items.SPRUCE_LOG, Items.STRIPPED_SPRUCE_LOG);
+            addStrippedWoodRecipe(Items.BIRCH_LOG, Items.STRIPPED_BIRCH_LOG);
+            addStrippedWoodRecipe(Items.JUNGLE_LOG, Items.STRIPPED_JUNGLE_LOG);
+            addStrippedWoodRecipe(Items.ACACIA_LOG, Items.STRIPPED_ACACIA_LOG);
+            addStrippedWoodRecipe(Items.DARK_OAK_LOG, Items.STRIPPED_DARK_OAK_LOG);
+            addStrippedWoodRecipe(Items.OAK_WOOD, Items.STRIPPED_OAK_WOOD);
+            addStrippedWoodRecipe(Items.SPRUCE_WOOD, Items.STRIPPED_SPRUCE_WOOD);
+            addStrippedWoodRecipe(Items.BIRCH_WOOD, Items.STRIPPED_BIRCH_WOOD);
+            addStrippedWoodRecipe(Items.JUNGLE_WOOD, Items.STRIPPED_JUNGLE_WOOD);
+            addStrippedWoodRecipe(Items.ACACIA_WOOD, Items.STRIPPED_ACACIA_WOOD);
+            addStrippedWoodRecipe(Items.DARK_OAK_WOOD, Items.STRIPPED_DARK_OAK_WOOD);
+        }
     }
 
     public void addStrippedWoodRecipe(Item baseVariant, Item strippedVariant) {
