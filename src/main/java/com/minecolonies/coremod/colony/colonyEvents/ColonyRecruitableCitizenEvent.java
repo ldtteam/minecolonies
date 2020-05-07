9491f1c881831bc5cb9ca41b71f3a29e8355befd
someaddons <someaddons@github.com>
2020-05-07 22:58:20 +0200

    tavern

--- src/main/java/com/minecolonies/coremod/colony/colonyEvents/ColonyRecruitableCitizenEvent.java
+++ src/main/java/com/minecolonies/coremod/colony/colonyEvents/ColonyRecruitableCitizenEvent.java
@@ -1,12 +1,16 @@
 package com.minecolonies.coremod.colony.colonyEvents;
 
+import com.minecolonies.api.colony.ICitizenData;
 import com.minecolonies.api.colony.IColony;
 import com.minecolonies.api.colony.colonyEvents.EventStatus;
 import com.minecolonies.api.colony.colonyEvents.IColonyEntitySpawnEvent;
+import com.minecolonies.api.util.BlockPosUtil;
 import com.minecolonies.coremod.colony.CitizenData;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.nbt.INBT;
+import net.minecraft.nbt.ListNBT;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import org.jetbrains.annotations.NotNull;
@@ -14,18 +18,45 @@ import org.jetbrains.annotations.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.minecolonies.api.colony.colonyEvents.NBTTags.*;
 import static com.minecolonies.api.util.constant.Constants.MOD_ID;
+import static com.minecolonies.api.util.constant.Constants.TAG_COMPOUND;
+import static com.minecolonies.api.util.constant.NbtTagConstants.TAG_CITIZENS;
 
 /**
  * Event for recruitable citizens spawning in the tavern
  */
 public class ColonyRecruitableCitizenEvent implements IColonyEntitySpawnEvent
 {
+    /**
+     * Event registry id
+     */
     public static ResourceLocation RECRUITABLE_CITIZEN_EVENT = new ResourceLocation(MOD_ID, "recruit_citizen_event");
 
+    /**
+     * List of additional citizens
+     */
     private final List<CitizenData> externalCitizens = new ArrayList<>();
 
-    private final IColony colony;
+    /**
+     * The tavern's pos
+     */
+    private BlockPos spawnPos;
+
+    /**
+     * Colony of the event
+     */
+    private IColony colony;
+
+    /**
+     * The events status
+     */
+    private EventStatus status = EventStatus.STARTING;
+
+    /**
+     * The event's id
+     */
+    private int eventID = 0;
 
     public ColonyRecruitableCitizenEvent(final IColony colony)
     {
@@ -35,25 +66,25 @@ public class ColonyRecruitableCitizenEvent implements IColonyEntitySpawnEvent
     @Override
     public void setSpawnPoint(final BlockPos spawnPoint)
     {
-
+        this.spawnPos = spawnPoint;
     }
 
     @Override
     public BlockPos getSpawnPos()
     {
-        return null;
+        return spawnPos;
     }
 
     @Override
     public EventStatus getStatus()
     {
-        return null;
+        return getStatus();
     }
 
     @Override
     public void setStatus(final EventStatus status)
     {
-
+        this.status = status;
     }
 
     @Override
@@ -65,24 +96,45 @@ public class ColonyRecruitableCitizenEvent implements IColonyEntitySpawnEvent
     @Override
     public ResourceLocation getEventTypeID()
     {
-        return null;
+        return RECRUITABLE_CITIZEN_EVENT;
     }
 
     @Override
     public void setColony(@NotNull final IColony colony)
     {
-
+        this.colony = colony;
     }
 
     @Override
     public CompoundNBT writeToNBT(final CompoundNBT compound)
     {
-        return null;
+        compound.putInt(TAG_EVENT_ID, eventID);
+        BlockPosUtil.write(compound, TAG_SPAWN_POS, spawnPos);
+        compound.putInt(TAG_EVENT_STATUS, status.ordinal());
+        final ListNBT citizenList = new ListNBT();
+        for (final ICitizenData data : externalCitizens)
+        {
+            citizenList.add(data.serializeNBT());
+        }
+
+        compound.put(TAG_CITIZENS, citizenList);
+
+        return compound;
     }
 
     @Override
     public void readFromNBT(final CompoundNBT compound)
     {
+        eventID = compound.getInt(TAG_EVENT_ID);
+        spawnPos = BlockPosUtil.read(compound, TAG_SPAWN_POS);
+        final ListNBT citizenList = compound.getList(TAG_CITIZENS, TAG_COMPOUND);
+        for (final INBT data : citizenList)
+        {
+            CitizenData citizenData = new CitizenData(0, colony);
+            citizenData.deserializeNBT((CompoundNBT) data);
+            externalCitizens.add(citizenData);
+        }
+        status = EventStatus.values()[compound.getInt(TAG_EVENT_STATUS)];
 
     }
 
@@ -126,7 +178,7 @@ public class ColonyRecruitableCitizenEvent implements IColonyEntitySpawnEvent
      *
      * @param colony   colony to load into
      * @param compound NBTcompound with saved values
-     * @return the raid event.
+     * @return the event.
      */
     public static ColonyRecruitableCitizenEvent loadFromNBT(final IColony colony, final CompoundNBT compound)
     {
