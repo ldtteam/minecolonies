a666c45e26235ad836f14330637959f9983d7b85
Raycoms <ray.neiheiser@gmail.com>
2020-05-20 10:13:44 +0100

    Feature/amazons (#4940)
    Amazon Raiders, nuff said
    Fix desert raider trigger too.

---
+++ src/api/java/com/minecolonies/api/entity/mobs/amazons/IArcherAmazon.java
@@ -0,0 +1,8 @@
+package com.minecolonies.api.entity.mobs.amazons;
+
+import com.minecolonies.api.entity.mobs.IArcherMobEntity;
+
+public interface IArcherAmazon extends IAmazonEntity, IArcherMobEntity
+{
+
+}
---
+++ src/main/java/com/minecolonies/coremod/client/render/mobs/amazon/RendererChiefAmazon.java
@@ -0,0 +1,35 @@
+package com.minecolonies.coremod.client.render.mobs.amazon;
+
+import com.minecolonies.coremod.client.model.raiders.ModelAmazonChief;
+import net.minecraft.client.renderer.entity.EntityRendererManager;
+import net.minecraft.entity.MobEntity;
+import net.minecraft.util.ResourceLocation;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Renderer used for Chief amazons.
+ */
+public class RendererChiefAmazon extends AbstractRendererAmazon
+{
+    /**
+     * Texture of the entity.
+     */
+    private static final ResourceLocation TEXTURE = new ResourceLocation("minecolonies:textures/entity/raiders/amazon_chief.png");
+
+    /**
+     * Constructor method for renderer
+     *
+     * @param renderManagerIn the renderManager
+     */
+    public RendererChiefAmazon(final EntityRendererManager renderManagerIn)
+    {
+        super(renderManagerIn, new ModelAmazonChief(), 0.5F);
+    }
+
+    @NotNull
+    @Override
+    public ResourceLocation getEntityTexture(final MobEntity entity)
+    {
+        return TEXTURE;
+    }
+}
