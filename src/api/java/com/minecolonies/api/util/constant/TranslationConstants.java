35494a264a827b90e6158cb3805ecec9fd6ba44c
JiaYow <jiayow@gmail.com>
2020-05-30 10:14:55 +0200

    Added simple connector between banners and guard towers.

--- src/api/java/com/minecolonies/api/util/constant/TranslationConstants.java
+++ src/api/java/com/minecolonies/api/util/constant/TranslationConstants.java
@@ -154,6 +154,14 @@ public final class TranslationConstants
     @NonNls
     public static final String COM_MINECOLONIES_CLIPBOARD_COLONY_SET                               = "com.minecolonies.coremod.item.clipboard.registered";
     @NonNls
+    public static final String COM_MINECOLONIES_BANNER_RALLY_GUARDS_SELECTED                       = "com.minecolonies.coremod.item.bannerrallyguards.selected";
+    @NonNls
+    public static final String COM_MINECOLONIES_BANNER_RALLY_GUARDS_DESELECTED                     = "com.minecolonies.coremod.item.bannerrallyguards.deselected";
+    @NonNls
+    public static final String COM_MINECOLONIES_BANNER_RALLY_GUARDS_TOOLTIP                        = "com.minecolonies.coremod.item.bannerrallyguards.tooltip";
+    @NonNls
+    public static final String COM_MINECOLONIES_BANNER_RALLY_GUARDS_TOOLTIP_EMPTY                  = "com.minecolonies.coremod.item.bannerrallyguards.tooltipempty";
+    @NonNls
     public static final String CITIZEN_RENAME_SAME                                                 = "com.minecolonies.coremod.citizen.rename.same";
     @NonNls
     public static final String CITIZEN_RENAME_NOT_ALLOWED                                          = "com.minecolonies.coremod.citizen.rename.notallowed";
--- src/main/java/com/minecolonies/coremod/items/ItemBannerRallyGuards.java
+++ src/main/java/com/minecolonies/coremod/items/ItemBannerRallyGuards.java
@@ -1,32 +1,38 @@
 package com.minecolonies.coremod.items;
 
 import com.ldtteam.structurize.util.LanguageHandler;
-import com.minecolonies.api.colony.ICitizenData;
-import com.minecolonies.api.colony.IColony;
 import com.minecolonies.api.colony.IColonyManager;
 import com.minecolonies.api.colony.IColonyView;
-import com.minecolonies.api.colony.buildings.IBuilding;
-import com.minecolonies.api.colony.buildings.IGuardBuilding;
-import com.minecolonies.api.colony.buildings.views.IBuildingView;
 import com.minecolonies.api.creativetab.ModCreativeTabs;
-import com.minecolonies.api.entity.ai.citizen.guards.GuardTask;
+import com.minecolonies.api.tileentities.AbstractTileEntityColonyBuilding;
+import com.minecolonies.api.tileentities.TileEntityColonyBuilding;
 import com.minecolonies.api.util.BlockPosUtil;
-import com.minecolonies.coremod.client.gui.WindowGuardControl;
-import com.minecolonies.coremod.colony.buildings.AbstractBuildingGuards;
+import com.minecolonies.api.util.Log;
+import com.minecolonies.api.util.constant.TranslationConstants;
+import com.minecolonies.coremod.MineColonies;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUseContext;
 import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.nbt.ListNBT;
+import net.minecraft.nbt.NBTUtil;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.Style;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.NotNull;
 
-import static com.minecolonies.api.util.constant.NbtTagConstants.TAG_ID;
-import static com.minecolonies.api.util.constant.NbtTagConstants.TAG_POS;
+import javax.annotation.Nullable;
+import java.util.List;
+
+import static com.minecolonies.api.util.constant.NbtTagConstants.*;
 
 /**
  * Guard Scepter Item class. Used to give tasks to guards.
@@ -34,47 +40,77 @@ import static com.minecolonies.api.util.constant.NbtTagConstants.TAG_POS;
 public class ItemBannerRallyGuards extends AbstractItemMinecolonies
 {
     /**
-     * The compound tag for the last pos the tool has been clicked.
+     * The compound tag for the activity status of the banner
      */
-    private static final String TAG_LAST_POS = "lastPos";
+    private static final String TAG_IS_ACTIVE = "isActive";
+
+    private static final String TAG_GUARDTOWERS = "guardtowers";
+
+    @Nullable
+    private ITextComponent cachedTooltip = null;
 
     /**
-     * GuardScepter constructor. Sets max stack to 1, like other tools.
+     * Guard Scepter constructor. Sets max stack to 1, like other tools.
+     *
      * @param properties the properties.
      */
     public ItemBannerRallyGuards(final Properties properties)
     {
-        super("banner_rally_guards", properties.maxStackSize(1).maxDamage(2).group(ModCreativeTabs.MINECOLONIES));
+        super("banner_rally_guards", properties.maxStackSize(1).maxDamage(0).group(ModCreativeTabs.MINECOLONIES));
     }
 
     @NotNull
     @Override
     public ActionResultType onItemUse(final ItemUseContext ctx)
     {
-        // if server world, do nothing
-        if (ctx.getWorld().isRemote)
-        {
-            return ActionResultType.FAIL;
-        }
-
         final ItemStack scepter = ctx.getPlayer().getHeldItem(ctx.getHand());
-        if (!scepter.hasTag())
-        {
-            scepter.setTag(new CompoundNBT());
-        }
-        final CompoundNBT compound = scepter.getTag();
 
-        if (compound.keySet().contains(TAG_LAST_POS))
+        final CompoundNBT compound = checkForCompound(scepter);
+        final TileEntity entity = ctx.getWorld().getTileEntity(ctx.getPos());
+
+        if (entity instanceof TileEntityColonyBuilding && ((TileEntityColonyBuilding) entity).registryName.getPath().equalsIgnoreCase("guardtower"))
         {
-            final BlockPos lastPos = BlockPosUtil.read(compound, TAG_LAST_POS);
-            if (lastPos.equals(ctx.getPos()))
+            final BlockPos position = ((AbstractTileEntityColonyBuilding) entity).getPosition();
+            final ListNBT guardTowers = (ListNBT) compound.get(TAG_GUARDTOWERS);
+            int indexToRemove = -1;
+            for (int i = 0; i < guardTowers.size(); i++)
             {
-                ctx.getPlayer().inventory.removeStackFromSlot(ctx.getPlayer().inventory.currentItem);
-                LanguageHandler.sendPlayerMessage(ctx.getPlayer(), "com.minecolonies.coremod.job.guard.toolDoubleClick");
-                return ActionResultType.FAIL;
+                if (NBTUtil.readBlockPos(guardTowers.getCompound(i)).equals(position))
+                {
+                    indexToRemove = i;
+                    break;
+                }
             }
+
+            String message = null;
+            if (indexToRemove > -1)
+            {
+                guardTowers.remove(indexToRemove);
+                message = TranslationConstants.COM_MINECOLONIES_BANNER_RALLY_GUARDS_DESELECTED;
+            }
+            else
+            {
+                guardTowers.add(NBTUtil.writeBlockPos(position));
+                message = TranslationConstants.COM_MINECOLONIES_BANNER_RALLY_GUARDS_SELECTED;
+            }
+
+            if (!ctx.getWorld().isRemote)
+            {
+                LanguageHandler.sendPlayerMessage(ctx.getPlayer(),
+                  message,
+                  ((AbstractTileEntityColonyBuilding) entity).getDisplayName().getUnformattedComponentText(), BlockPosUtil.getString(position));
+            }
+            cachedTooltip = null; // Reset the tooltip here, it will be regenerated during the next tooltip update.
         }
-        return handleItemUsage(ctx.getWorld(), ctx.getPos(), compound, ctx.getPlayer());
+        else
+        {
+            // TODO: Toggle banner activation state
+            final int colonyId = compound.getInt(TAG_COLONY_ID);
+            final BlockPos builderPos = BlockPosUtil.read(compound, TAG_BUILDER);
+            MineColonies.proxy.openResourceScrollWindow(colonyId, builderPos);
+        }
+
+        return ActionResultType.SUCCESS;
     }
 
     @NotNull
@@ -97,16 +133,18 @@ public class ItemBannerRallyGuards extends AbstractItemMinecolonies
             final IColonyView colony = IColonyManager.getInstance().getColonyView(compound.getInt(TAG_ID), Minecraft.getInstance().world.getDimension().getType().getId());
             if (colony == null)
             {
-
                 return ActionResult.func_226251_d_(stack);
             }
-            final BlockPos guardTower = BlockPosUtil.read(compound, TAG_POS);
-            final IBuildingView hut = colony.getBuilding(guardTower);
 
-            if (hut instanceof AbstractBuildingGuards.View && playerIn.isShiftKeyDown())
+            if (playerIn.isShiftKeyDown())
             {
-                final WindowGuardControl window = new WindowGuardControl((AbstractBuildingGuards.View) hut);
-                window.open();
+                // TODO: Activate item here.
+                Log.getLogger().info("Item activation toggle");
+            }
+            else
+            {
+                // TODO: Open configuration window here.
+                Log.getLogger().info("Item configuration opened");
             }
         }
 
@@ -114,67 +152,53 @@ public class ItemBannerRallyGuards extends AbstractItemMinecolonies
     }
 
     /**
-     * Handles the usage of the item.
+     * Check for the compound and return it.
+     * If not available create and return it.
      *
-     * @param worldIn  the world it is used in.
-     * @param pos      the position.
-     * @param compound the compound.
-     * @param playerIn the player using it.
-     * @return if it has been successful.
+     * @param item the item to check in for.
+     * @return the compound of the item.
      */
-    @NotNull
-    private static ActionResultType handleItemUsage(final World worldIn, final BlockPos pos, final CompoundNBT compound, final PlayerEntity playerIn)
+    private static CompoundNBT checkForCompound(final ItemStack item)
     {
-        if (!compound.keySet().contains(TAG_ID))
+        if (!item.hasTag() || item.getTag().get(TAG_IS_ACTIVE) == null || item.getTag().get(TAG_GUARDTOWERS) == null)
         {
-            return ActionResultType.FAIL;
-        }
-        final IColony colony = IColonyManager.getInstance().getColonyByWorld(compound.getInt(TAG_ID), worldIn);
-        if (colony == null)
-        {
-            return ActionResultType.FAIL;
-        }
+            final CompoundNBT compound = new CompoundNBT();
 
-        final BlockPos guardTower = BlockPosUtil.read(compound, TAG_POS);
-        final IBuilding hut = colony.getBuildingManager().getBuilding(guardTower);
-        if (!(hut instanceof AbstractBuildingGuards))
-        {
-            return ActionResultType.FAIL;
+            compound.putBoolean(TAG_IS_ACTIVE, false);
+
+            @NotNull final ListNBT guardTowerList = new ListNBT();
+            compound.put(TAG_GUARDTOWERS, guardTowerList);
+            item.setTag(compound);
         }
-        final IGuardBuilding tower = (IGuardBuilding) hut;
+        return item.getTag();
+    }
 
-        if(BlockPosUtil.getDistance2D(pos, guardTower) > tower.getPatrolDistance())
+    @Override
+    public void addInformation(
+      final ItemStack stack, @Nullable final World worldIn, final List<ITextComponent> tooltip, final ITooltipFlag flagIn)
+    {
+        final CompoundNBT compound = checkForCompound(stack);
+        final ListNBT guardTowers = (ListNBT) compound.get(TAG_GUARDTOWERS);
+
+        // The isEmpty is in there because the tooltip is sometimes loaded before NBT is loaded.
+        // Worst case, the 0-towers-tooltip spams allocations, but compared to the rest of Minecraft, that's negligible.
+        if (guardTowers.isEmpty())
         {
-            LanguageHandler.sendPlayerMessage(playerIn, "com.minecolonies.coremod.job.guard.toolClickGuardTooFar");
-            return ActionResultType.FAIL;
-        }
-
-        final GuardTask task = GuardTask.values()[compound.getInt("task")];
-        final ICitizenData citizen = tower.getMainCitizen();
-
-        String name = "";
-        if (citizen != null)
-        {
-            name = " " + citizen.getName();
-        }
-
-        if (task.equals(GuardTask.GUARD))
-        {
-            LanguageHandler.sendPlayerMessage(playerIn, "com.minecolonies.coremod.job.guard.toolClickGuard", pos, name);
-            tower.setGuardPos(pos);
-            playerIn.inventory.removeStackFromSlot(playerIn.inventory.currentItem);
+            final ITextComponent emptyTooltip = LanguageHandler.buildChatComponent(TranslationConstants.COM_MINECOLONIES_BANNER_RALLY_GUARDS_TOOLTIP_EMPTY);
+            emptyTooltip.setStyle(new Style().setItalic(true).setColor(TextFormatting.DARK_AQUA));
+            tooltip.add(emptyTooltip);
         }
         else
         {
-            if (!compound.keySet().contains(TAG_LAST_POS))
+            if (cachedTooltip == null)
             {
-                tower.resetPatrolTargets();
+                cachedTooltip = LanguageHandler.buildChatComponent(TranslationConstants.COM_MINECOLONIES_BANNER_RALLY_GUARDS_TOOLTIP, guardTowers.size());
+                cachedTooltip.setStyle(new Style().setItalic(true).setColor(TextFormatting.DARK_AQUA));
             }
-            tower.addPatrolTargets(pos);
-            LanguageHandler.sendPlayerMessage(playerIn, "com.minecolonies.coremod.job.guard.toolClickPatrol", pos, name);
+            tooltip.add(cachedTooltip);
         }
-        BlockPosUtil.write(compound, TAG_LAST_POS, pos);
 
-        return ActionResultType.SUCCESS;
+
+        super.addInformation(stack, worldIn, tooltip, flagIn);
     }
 }
