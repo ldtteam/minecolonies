a5b4a6d0a9bfbbcc59b00cff29260bc619299b40
Ray  Neiheiser <ray.neiheiser@gmail.com>
2020-07-03 10:57:56 +0100

    this should be true

--- src/main/java/com/minecolonies/coremod/entity/ai/basic/AbstractEntityAIStructureWithWorkOrder.java
+++ src/main/java/com/minecolonies/coremod/entity/ai/basic/AbstractEntityAIStructureWithWorkOrder.java
@@ -199,7 +199,7 @@ public abstract class AbstractEntityAIStructureWithWorkOrder<J extends AbstractJ
         {
             getOwnBuilding().addNeededResource(stack, stack.getCount());
         }
-        getOwnBuilding().checkOrRequestBucket(getOwnBuilding().getRequiredResources(), worker.getCitizenData(), false);
+        getOwnBuilding().checkOrRequestBucket(getOwnBuilding().getRequiredResources(), worker.getCitizenData(), true);
     }
 
     @Override
