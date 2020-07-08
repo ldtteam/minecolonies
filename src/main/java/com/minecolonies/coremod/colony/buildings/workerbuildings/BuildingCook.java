460c5662c9f43fb860ea2a5dbe8bf74dd089a009
Ray  Neiheiser <ray.neiheiser@gmail.com>
2020-07-08 17:42:22 +0100

    same for fuel

--- src/main/java/com/minecolonies/coremod/colony/buildings/workerbuildings/BuildingCook.java
+++ src/main/java/com/minecolonies/coremod/colony/buildings/workerbuildings/BuildingCook.java
@@ -249,8 +249,8 @@ public class BuildingCook extends AbstractBuildingFurnaceUser
         }
 
         final Predicate<ItemStack> allowedFuel = theStack -> getAllowedFuel().stream().anyMatch(fuelStack -> fuelStack.isItemEqual(theStack));
-        if (allowedFuel.test(stack) && localAlreadyKept.stream().filter(storage -> allowedFuel.test(storage.getItemStack())).mapToInt(ItemStorage::getAmount).sum() < STACKSIZE
-              || !inventory)
+        if (allowedFuel.test(stack) && (localAlreadyKept.stream().filter(storage -> allowedFuel.test(storage.getItemStack())).mapToInt(ItemStorage::getAmount).sum() < STACKSIZE
+              || !inventory))
         {
             final ItemStorage kept = new ItemStorage(stack);
             if (localAlreadyKept.contains(kept))
