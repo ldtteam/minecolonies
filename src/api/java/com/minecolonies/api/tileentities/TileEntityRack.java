caef7c664d6674bf498b0e72db0ff1e35e4127c8
SomeAddons <someaddons@github.com>
2020-07-22 09:10:05 +0200

    Fix rack loading/acess unloaded

--- src/api/java/com/minecolonies/api/tileentities/TileEntityRack.java
+++ src/api/java/com/minecolonies/api/tileentities/TileEntityRack.java
@@ -440,6 +440,8 @@ public class TileEntityRack extends AbstractTileEntityRack
         {
             this.buildingPos = BlockPosUtil.read(compound, TAG_POS);
         }
+
+        notifyParentAboutInvChange();
     }
 
     @NotNull
