50787b3d4338a9d9f2be3410dd7044d80decde1a
carlansor <carlansor@gmail.com>
2020-08-05 00:12:08 -0300

    Lumberjack model improvements
    Improvements for better future customization

--- src/main/java/com/minecolonies/coremod/client/model/ModelEntityLumberjackFemale.java
+++ src/main/java/com/minecolonies/coremod/client/model/ModelEntityLumberjackFemale.java
@@ -1,3 +1,6 @@
+// Made with Blockbench 3.5.1
+// Exported for Minecraft version 1.15
+// Paste this class into your mod and generate all required imports
 package com.minecolonies.coremod.client.model;
 
 import com.minecolonies.api.client.render.modeltype.CitizenModel;
@@ -5,221 +8,156 @@ import com.minecolonies.api.entity.citizen.AbstractEntityCitizen;
 import net.minecraft.client.renderer.model.ModelRenderer;
 import org.jetbrains.annotations.NotNull;
 
-public class ModelEntityLumberjackFemale extends CitizenModel<AbstractEntityCitizen>
-{
-    ModelRenderer chest;
-    ModelRenderer ponytailBase;
-    ModelRenderer ponytailEnd;
-    ModelRenderer BasketBL;
-    ModelRenderer BasketTB;
-    ModelRenderer BasketBR;
-    ModelRenderer BasketTML;
-    ModelRenderer BasketBF;
-    ModelRenderer BasketMFR;
-    ModelRenderer BasketMFL;
-    ModelRenderer BasketMBL;
-    ModelRenderer BasketMBR;
-    ModelRenderer BasketTMR;
-    ModelRenderer BasketBB;
-    ModelRenderer logTop;
-    ModelRenderer logMiddle;
-    ModelRenderer logBottom;
+public class ModelEntityLumberjackFemale extends CitizenModel<AbstractEntityCitizen>{
+	bipedChestModelRenderer headDetail;
+	bipedChestModelRenderer ponytailBase;
+	bipedChestModelRenderer ponytailEnd;
+	bipedChestModelRenderer chest;
+	bipedChestModelRenderer logBottom;
+	bipedChestModelRenderer logMiddle;
+	bipedChestModelRenderer logTop;
+	bipedChestModelRenderer BasketBL;
+	bipedChestModelRenderer BasketTB;
+	bipedChestModelRenderer BasketBR;
+	bipedChestModelRenderer BasketTML;
+	bipedChestModelRenderer BasketBF;
+	bipedChestModelRenderer BasketMFR;
+	bipedChestModelRenderer BasketMFL;
+	bipedChestModelRenderer BasketMBL;
+	bipedChestModelRenderer BasketMBR;
+	bipedChestModelRenderer BasketTMR;
+	bipedChestModelRenderer BasketBB;
 
-    public ModelEntityLumberjackFemale()
-    {
-        textureWidth = 128;
-        textureHeight = 64;
+	public ModelEntityLumberjackFemale() {
+		textureWidth = 128;
+		textureHeight = 64;
 
-        bipedRightArm = new ModelRenderer(this, 40, 16);
-        bipedRightArm.addBox(-3F, -2F, -2F, 4, 12, 4);
-        bipedRightArm.setRotationPoint(-5F, 2F, 0F);
-        bipedRightArm.setTextureSize(128, 64);
-        bipedRightArm.mirror = true;
-        setRotation(bipedRightArm, 0F, 0F, 0F);
+		bipedRightArm = new ModelRenderer(this);
+		bipedRightArm.setRotationPoint(-5.0F, 2.0F, 0.0F);
+		bipedRightArm.setTextureOffset(40, 16).addBox(-3.0F, -2.0F, -2.0F, 4.0F, 12.0F, 4.0F, 0.0F, true);
 
-        bipedLeftArm = new ModelRenderer(this, 40, 16);
-        bipedLeftArm.addBox(-1F, -2F, -2F, 4, 12, 4);
-        bipedLeftArm.setRotationPoint(5F, 2F, 0F);
-        bipedLeftArm.setTextureSize(128, 64);
-        bipedLeftArm.mirror = true;
-        setRotation(bipedLeftArm, 0F, 0F, 0F);
-        bipedLeftArm.mirror = false;
+		bipedLeftArm = new ModelRenderer(this);
+		bipedLeftArm.setRotationPoint(5.0F, 2.0F, 0.0F);
+		bipedLeftArm.setTextureOffset(40, 16).addBox(-1.0F, -2.0F, -2.0F, 4.0F, 12.0F, 4.0F, 0.0F, false);
 
-        chest = new ModelRenderer(this, 17, 33);
-        chest.addBox(-3.5F, 1.7F, -1F, 7, 4, 4);
-        chest.setRotationPoint(0F, 0F, 0F);
-        chest.setTextureSize(128, 64);
-        chest.mirror = true;
-        setRotation(chest, -0.5934119F, 0F, 0F);
+		bipedRightLeg = new ModelRenderer(this);
+		bipedRightLeg.setRotationPoint(-2.0F, 12.0F, 0.0F);
+		bipedRightLeg.setTextureOffset(0, 16).addBox(-2.0F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, 0.0F, true);
 
-        bipedRightLeg = new ModelRenderer(this, 0, 16);
-        bipedRightLeg.addBox(-2F, 0F, -2F, 4, 12, 4);
-        bipedRightLeg.setRotationPoint(-2F, 12F, 0F);
-        bipedRightLeg.setTextureSize(128, 64);
-        bipedRightLeg.mirror = true;
-        setRotation(bipedRightLeg, 0F, 0F, 0F);
+		bipedLeftLeg = new ModelRenderer(this);
+		bipedLeftLeg.setRotationPoint(2.0F, 12.0F, 0.0F);
+		bipedLeftLeg.setTextureOffset(0, 16).addBox(-2.0F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, 0.0F, false);
 
-        bipedLeftLeg = new ModelRenderer(this, 0, 16);
-        bipedLeftLeg.addBox(-2F, 0F, -2F, 4, 12, 4);
-        bipedLeftLeg.setRotationPoint(2F, 12F, 0F);
-        bipedLeftLeg.setTextureSize(128, 64);
-        bipedLeftLeg.mirror = true;
-        setRotation(bipedLeftLeg, 0F, 0F, 0F);
-        bipedLeftLeg.mirror = false;
+		bipedHead = new ModelRenderer(this);
+		bipedHead.setRotationPoint(0.0F, 0.0F, 0.0F);
+		bipedHead.setTextureOffset(0, 0).addBox(-4.0F, -8.0F, -4.0F, 8.0F, 8.0F, 8.0F, 0.0F, true);
 
-        bipedHead = new ModelRenderer(this, 0, 0);
-        bipedHead.addBox(-4F, -8F, -4F, 8, 8, 8);
-        bipedHead.setRotationPoint(0F, 0F, 0F);
-        bipedHead.setTextureSize(128, 64);
-        bipedHead.mirror = true;
-        setRotation(bipedHead, 0F, 0F, 0F);
+		headDetail = new ModelRenderer(this);
+		headDetail.setRotationPoint(0.0F, 24.0F, 0.0F);
+		bipedHead.addChild(headDetail);
+		headDetail.setTextureOffset(32, 0).addBox(-4.0F, -32.0F, -4.0F, 8.0F, 8.0F, 8.0F, 0.5F, true);
 
-        bipedBody = new ModelRenderer(this, 16, 16);
-        bipedBody.addBox(-4F, 0F, -2F, 8, 12, 4);
-        bipedBody.setRotationPoint(0F, 0F, 0F);
-        bipedBody.setTextureSize(128, 64);
-        bipedBody.mirror = true;
-        setRotation(bipedBody, 0F, 0F, 0F);
+		ponytailBase = new ModelRenderer(this);
+		ponytailBase.setRotationPoint(0.0F, 0.0F, 0.0F);
+		bipedHead.addChild(ponytailBase);
+		setRotationAngle(ponytailBase, 0.5585F, 0.0F, 0.0F);
+		ponytailBase.setTextureOffset(58, 25).addBox(-0.5F, 3.2F, 3.8F, 1.0F, 5.0F, 1.0F, 0.0F, false);
 
-        ponytailBase = new ModelRenderer(this, 33, 6);
-        ponytailBase.addBox(-0.5F, 3.2F, 3.8F, 1, 5, 1);
-        ponytailBase.setRotationPoint(0F, 0F, 0F);
-        ponytailBase.setTextureSize(128, 64);
-        setRotation(ponytailBase, 0.5585054F, 0F, 0F);
+		ponytailEnd = new ModelRenderer(this);
+		ponytailEnd.setRotationPoint(0.0F, 0.0F, 0.0F);
+		ponytailBase.addChild(ponytailEnd);
+		setRotationAngle(ponytailEnd, -0.2296F, 0.0F, 0.0F);
+		ponytailEnd.setTextureOffset(57, 19).addBox(-1.0F, -2.0F, 4.2F, 2.0F, 5.0F, 1.0F, 0.0F, false);
 
-        ponytailEnd = new ModelRenderer(this, 32, 0);
-        ponytailEnd.addBox(-1F, -2F, 4.2F, 2, 5, 1);
-        ponytailEnd.setRotationPoint(0F, 0F, 0F);
-        ponytailEnd.setTextureSize(128, 64);
-        setRotation(ponytailEnd, -0.2296264F, 0F, 0F);
+		bipedBody = new ModelRenderer(this);
+		bipedBody.setRotationPoint(0.0F, 0.0F, 0.0F);
+		bipedBody.setTextureOffset(16, 16).addBox(-4.0F, 0.0F, -2.0F, 8.0F, 12.0F, 4.0F, 0.0F, true);
 
-        BasketBL = new ModelRenderer(this, 0, 33);
-        BasketBL.addBox(2F, 11F, 3F, 1, 1, 3);
-        BasketBL.setRotationPoint(0F, 0F, 0F);
-        BasketBL.setTextureSize(128, 64);
-        BasketBL.mirror = true;
-        setRotation(BasketBL, 0F, 0F, 0F);
+		chest = new ModelRenderer(this);
+		chest.setRotationPoint(0.0F, 0.0F, 0.0F);
+		bipedBody.addChild(chest);
+		setRotationAngle(chest, -0.5934F, 0.0F, 0.0F);
+		chest.setTextureOffset(17, 33).addBox(-3.5F, 1.7F, -1.0F, 7.0F, 4.0F, 4.0F, 0.0F, true);
 
-        BasketTB = new ModelRenderer(this, 0, 38);
-        BasketTB.addBox(-2F, 4F, 6F, 4, 1, 1);
-        BasketTB.setRotationPoint(0F, 0F, 0F);
-        BasketTB.setTextureSize(128, 64);
-        BasketTB.mirror = true;
-        setRotation(BasketTB, 0F, 0F, 0F);
+		logBottom = new ModelRenderer(this);
+		logBottom.setRotationPoint(0.0F, 0.0F, 0.0F);
+		bipedBody.addChild(logBottom);
+		setRotationAngle(logBottom, 0.0698F, 0.0F, 0.0F);
+		logBottom.setTextureOffset(17, 58).addBox(-5.3F, 8.5F, 2.5F, 10.0F, 3.0F, 3.0F, 0.0F, true);
 
-        BasketBR = new ModelRenderer(this, 0, 33);
-        BasketBR.addBox(-3F, 11F, 3F, 1, 1, 3);
-        BasketBR.setRotationPoint(0F, 0F, 0F);
-        BasketBR.setTextureSize(128, 64);
-        BasketBR.mirror = true;
-        setRotation(BasketBR, 0F, 0F, 0F);
+		logMiddle = new ModelRenderer(this);
+		logMiddle.setRotationPoint(0.0F, 0.0F, 0.0F);
+		bipedBody.addChild(logMiddle);
+		setRotationAngle(logMiddle, 0.6458F, 0.2967F, 0.0F);
+		logMiddle.setTextureOffset(17, 51).addBox(-1.3F, 6.7F, -1.0F, 5.0F, 3.0F, 3.0F, 0.0F, true);
 
-        BasketTML = new ModelRenderer(this, 11, 33);
-        BasketTML.addBox(3.1F, 1.4F, 0.6F, 1, 6, 1);
-        BasketTML.setRotationPoint(0F, 0F, 0F);
-        BasketTML.setTextureSize(128, 64);
-        BasketTML.mirror = true;
-        setRotation(BasketTML, 0.8080874F, -0.1745329F, 0F);
+		logTop = new ModelRenderer(this);
+		logTop.setRotationPoint(0.0F, 0.0F, 0.0F);
+		bipedBody.addChild(logTop);
+		setRotationAngle(logTop, 0.0F, 0.7854F, 0.2094F);
+		logTop.setTextureOffset(17, 41).addBox(-4.2F, 2.0F, 0.7F, 3.0F, 7.0F, 3.0F, 0.0F, true);
 
-        BasketBF = new ModelRenderer(this, 0, 38);
-        BasketBF.addBox(-2F, 11F, 2F, 4, 1, 1);
-        BasketBF.setRotationPoint(0F, 0F, 0F);
-        BasketBF.setTextureSize(128, 64);
-        BasketBF.mirror = true;
-        setRotation(BasketBF, 0F, 0F, 0F);
+		BasketBL = new ModelRenderer(this);
+		BasketBL.setRotationPoint(0.0F, 0.0F, 0.0F);
+		bipedBody.addChild(BasketBL);
+		BasketBL.setTextureOffset(0, 33).addBox(2.0F, 11.0F, 3.0F, 1.0F, 1.0F, 3.0F, 0.0F, true);
 
-        BasketMFR = new ModelRenderer(this, 11, 41);
-        BasketMFR.addBox(-3F, 0F, 2F, 1, 12, 1);
-        BasketMFR.setRotationPoint(0F, 0F, 0F);
-        BasketMFR.setTextureSize(128, 64);
-        BasketMFR.mirror = true;
-        setRotation(BasketMFR, 0F, 0F, 0F);
+		BasketTB = new ModelRenderer(this);
+		BasketTB.setRotationPoint(0.0F, 0.0F, 0.0F);
+		bipedBody.addChild(BasketTB);
+		BasketTB.setTextureOffset(0, 38).addBox(-2.0F, 4.0F, 6.0F, 4.0F, 1.0F, 1.0F, 0.0F, true);
 
-        BasketMFL = new ModelRenderer(this, 11, 41);
-        BasketMFL.addBox(2F, 0F, 2F, 1, 12, 1);
-        BasketMFL.setRotationPoint(0F, 0F, 0F);
-        BasketMFL.setTextureSize(128, 64);
-        BasketMFL.mirror = true;
-        setRotation(BasketMFL, 0F, 0F, 0F);
+		BasketBR = new ModelRenderer(this);
+		BasketBR.setRotationPoint(0.0F, 0.0F, 0.0F);
+		bipedBody.addChild(BasketBR);
+		BasketBR.setTextureOffset(0, 33).addBox(-3.0F, 11.0F, 3.0F, 1.0F, 1.0F, 3.0F, 0.0F, true);
 
-        BasketMBL = new ModelRenderer(this, 6, 41);
-        BasketMBL.addBox(2F, 4F, 6F, 1, 8, 1);
-        BasketMBL.setRotationPoint(0F, 0F, 0F);
-        BasketMBL.setTextureSize(128, 64);
-        BasketMBL.mirror = true;
-        setRotation(BasketMBL, 0F, 0F, 0F);
+		BasketTML = new ModelRenderer(this);
+		BasketTML.setRotationPoint(0.0F, 0.0F, 0.0F);
+		bipedBody.addChild(BasketTML);
+		setRotationAngle(BasketTML, 0.8081F, -0.1745F, 0.0F);
+		BasketTML.setTextureOffset(11, 33).addBox(3.1F, 1.4F, 0.6F, 1.0F, 6.0F, 1.0F, 0.0F, true);
 
-        BasketMBR = new ModelRenderer(this, 6, 41);
-        BasketMBR.addBox(-3F, 4F, 6F, 1, 8, 1);
-        BasketMBR.setRotationPoint(0F, 0F, 0F);
-        BasketMBR.setTextureSize(128, 64);
-        BasketMBR.mirror = true;
-        setRotation(BasketMBR, 0F, 0F, 0F);
+		BasketBF = new ModelRenderer(this);
+		BasketBF.setRotationPoint(0.0F, 0.0F, 0.0F);
+		bipedBody.addChild(BasketBF);
+		BasketBF.setTextureOffset(0, 38).addBox(-2.0F, 11.0F, 2.0F, 4.0F, 1.0F, 1.0F, 0.0F, true);
 
-        BasketTMR = new ModelRenderer(this, 11, 33);
-        BasketTMR.addBox(-4.1F, 1.4F, 0.5F, 1, 6, 1);
-        BasketTMR.setRotationPoint(0F, 0F, 0F);
-        BasketTMR.setTextureSize(128, 64);
-        BasketTMR.mirror = true;
-        setRotation(BasketTMR, 0.8080874F, 0.1745329F, 0F);
+		BasketMFR = new ModelRenderer(this);
+		BasketMFR.setRotationPoint(0.0F, 0.0F, 0.0F);
+		bipedBody.addChild(BasketMFR);
+		BasketMFR.setTextureOffset(11, 41).addBox(-3.0F, 0.0F, 2.0F, 1.0F, 12.0F, 1.0F, 0.0F, true);
 
-        BasketBB = new ModelRenderer(this, 0, 38);
-        BasketBB.addBox(-2F, 11F, 6F, 4, 1, 1);
-        BasketBB.setRotationPoint(0F, 0F, 0F);
-        BasketBB.setTextureSize(128, 64);
-        BasketBB.mirror = true;
-        setRotation(BasketBB, 0F, 0F, 0F);
+		BasketMFL = new ModelRenderer(this);
+		BasketMFL.setRotationPoint(0.0F, 0.0F, 0.0F);
+		bipedBody.addChild(BasketMFL);
+		BasketMFL.setTextureOffset(11, 41).addBox(2.0F, 0.0F, 2.0F, 1.0F, 12.0F, 1.0F, 0.0F, true);
 
-        logTop = new ModelRenderer(this, 17, 41);
-        logTop.addBox(-4.2F, 2F, 0.7F, 3, 7, 3);
-        logTop.setRotationPoint(0F, 0F, 0F);
-        logTop.setTextureSize(128, 64);
-        logTop.mirror = true;
-        setRotation(logTop, 0F, 0.7853982F, 0.2094395F);
+		BasketMBL = new ModelRenderer(this);
+		BasketMBL.setRotationPoint(0.0F, 0.0F, 0.0F);
+		bipedBody.addChild(BasketMBL);
+		BasketMBL.setTextureOffset(6, 41).addBox(2.0F, 4.0F, 6.0F, 1.0F, 8.0F, 1.0F, 0.0F, true);
 
-        logMiddle = new ModelRenderer(this, 17, 51);
-        logMiddle.addBox(-1.3F, 6.7F, -1F, 5, 3, 3);
-        logMiddle.setRotationPoint(0F, 0F, 0F);
-        logMiddle.setTextureSize(128, 64);
-        logMiddle.mirror = true;
-        setRotation(logMiddle, 0.6457718F, 0.296706F, 0F);
+		BasketMBR = new ModelRenderer(this);
+		BasketMBR.setRotationPoint(0.0F, 0.0F, 0.0F);
+		bipedBody.addChild(BasketMBR);
+		BasketMBR.setTextureOffset(6, 41).addBox(-3.0F, 4.0F, 6.0F, 1.0F, 8.0F, 1.0F, 0.0F, true);
 
-        logBottom = new ModelRenderer(this, 17, 58);
-        logBottom.addBox(-5.3F, 8.5F, 2.5F, 10, 3, 3);
-        logBottom.setRotationPoint(0F, 0F, 0F);
-        logBottom.setTextureSize(128, 64);
-        logBottom.mirror = true;
-        setRotation(logBottom, 0.0698132F, 0F, 0F);
+		BasketTMR = new ModelRenderer(this);
+		BasketTMR.setRotationPoint(0.0F, 0.0F, 0.0F);
+		bipedBody.addChild(BasketTMR);
+		setRotationAngle(BasketTMR, 0.8081F, 0.1745F, 0.0F);
+		BasketTMR.setTextureOffset(11, 33).addBox(-4.1F, 1.4F, 0.5F, 1.0F, 6.0F, 1.0F, 0.0F, true);
 
-        bipedHead.addChild(ponytailBase);
-        ponytailBase.addChild(ponytailEnd);
+		BasketBB = new ModelRenderer(this);
+		BasketBB.setRotationPoint(0.0F, 0.0F, 0.0F);
+		bipedBody.addChild(BasketBB);
+		BasketBB.setTextureOffset(0, 38).addBox(-2.0F, 11.0F, 6.0F, 4.0F, 1.0F, 1.0F, 0.0F, true);
+	}
 
-        bipedBody.addChild(chest);
-
-        bipedBody.addChild(logBottom);
-        bipedBody.addChild(logMiddle);
-        bipedBody.addChild(logTop);
-
-        //this could be better, but the basket is a mess, so I'm doing this for now
-        bipedBody.addChild(BasketBL);
-        bipedBody.addChild(BasketTB);
-        bipedBody.addChild(BasketBR);
-        bipedBody.addChild(BasketTML);
-        bipedBody.addChild(BasketBF);
-        bipedBody.addChild(BasketMFR);
-        bipedBody.addChild(BasketMFL);
-        bipedBody.addChild(BasketMBL);
-        bipedBody.addChild(BasketMBR);
-        bipedBody.addChild(BasketTMR);
-        bipedBody.addChild(BasketBB);
-
-        bipedHeadwear.showModel = false;
-    }
-
-    private void setRotation(@NotNull final ModelRenderer model, final float x, final float y, final float z)
-    {
-        model.rotateAngleX = x;
-        model.rotateAngleY = y;
-        model.rotateAngleZ = z;
-    }
+	public void setRotationAngle(ModelRenderer modelRenderer, float x, float y, float z) {
+		modelRenderer.rotateAngleX = x;
+		modelRenderer.rotateAngleY = y;
+		modelRenderer.rotateAngleZ = z;
+	}
 }
