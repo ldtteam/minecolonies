9d919497b0fd17522352a25f73923ab27d116b50
carlansor <carlansor@gmail.com>
2020-08-06 21:36:19 -0300

    again fixes
    rabbit herder model fix

--- src/main/java/com/minecolonies/coremod/client/model/ModelEntityRabbitHerderFemale.java
+++ src/main/java/com/minecolonies/coremod/client/model/ModelEntityRabbitHerderFemale.java
@@ -65,12 +65,12 @@ public class ModelEntityRabbitHerderFemale extends CitizenModel<AbstractEntityCi
 
         bipedLeftArm = new ModelRenderer(this);
         bipedLeftArm.setRotationPoint(5.0F, 2.0F, 0.0F);
-        bipedLeftArm.setTextureOffset(40, 16).addBox(0.0F, -2.0F, -2.0F, 3.0F, 12.0F, 4.0F, 0.0F, true);
+        bipedLeftArm.setTextureOffset(40, 16).addBox(-1.0F, -2.0F, -2.0F, 4.0F, 12.0F, 4.0F, 0.0F, true);
         bipedLeftArm.setTextureOffset(21, 32).addBox(-0.5F, 5.0F, -2.25F, 4.0F, 0.75F, 4.5F, 0.0F, false);
 
         bipedRightArm = new ModelRenderer(this);
         bipedRightArm.setRotationPoint(-5.0F, 2.0F, 0.0F);
-        bipedRightArm.setTextureOffset(40, 16).addBox(-2.0F, -2.0F, -2.0F, 3.0F, 12.0F, 4.0F, 0.0F, false);
+        bipedRightArm.setTextureOffset(40, 16).addBox(-3.0F, -2.0F, -2.0F, 4.0F, 12.0F, 4.0F, 0.0F, false);
         bipedRightArm.setTextureOffset(1, 32).addBox(-2.5F, 5.0F, -2.25F, 4.0F, 0.75F, 4.5F, 0.0F, true);
 
         bipedBody = new ModelRenderer(this);
