ee3f9443f45a6144e256f62913ae377a803ae2f9
Daniel Hensey <genesisstrain@gmail.com>
2020-08-20 00:59:30 -0700

    Added a display name based filter comparison into updateResources to bring that search box in line with how PostBox works (#5616)
    Added a display name based filter comparison into updateResources to bring that search box in line with how PostBox works
    Affects Warehouse and Restaurant at least

--- src/main/java/com/minecolonies/coremod/client/gui/WindowSelectRes.java
+++ src/main/java/com/minecolonies/coremod/client/gui/WindowSelectRes.java
@@ -150,7 +150,9 @@ public class WindowSelectRes extends AbstractWindowSkeleton
         this.allItems.addAll(ImmutableList.copyOf(StreamSupport.stream(Spliterators.spliteratorUnknownSize(ForgeRegistries.ITEMS.iterator(), Spliterator.ORDERED), false)
                                                     .map(ItemStack::new)
                                                     .filter((stack) -> (test.test(stack) && (this.filter.isEmpty() || stack.getTranslationKey().toLowerCase(Locale.US)
-                                                                                                                        .contains(this.filter.toLowerCase(Locale.US)))))
+                                                                                                                        .contains(this.filter.toLowerCase(Locale.US))
+                                                                                                                   || stack.getDisplayName().getFormattedText().toLowerCase(Locale.US)
+                                                                                                                        .contains(filter.toLowerCase(Locale.US)))))
                                                     .collect(Collectors.toList())));
         this.updateResourceList();
     }
