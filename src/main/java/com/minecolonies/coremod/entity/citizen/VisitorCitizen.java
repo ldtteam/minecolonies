a35e207a8dc5952941b1fc017f951bfa4276d820
Mark E. Schill <Mark.Schill@cmschill.net>
2020-08-29 17:25:55 -0400

    Adds location of visitor's death (#5645)
    * Adds location of visitor's death
    
    * Undoes file formatting
    
    Signed-off-by: Mark E. Schill <Mark.Schill@cmschill.net>
    
    * Utilizes utility for block position
    
    Signed-off-by: Mark E. Schill <Mark.Schill@cmschill.net>

--- src/main/java/com/minecolonies/coremod/entity/citizen/VisitorCitizen.java
+++ src/main/java/com/minecolonies/coremod/entity/citizen/VisitorCitizen.java
@@ -14,6 +14,7 @@ import com.minecolonies.api.entity.citizen.AbstractEntityCitizen;
 import com.minecolonies.api.entity.citizen.citizenhandlers.*;
 import com.minecolonies.api.inventory.InventoryCitizen;
 import com.minecolonies.api.inventory.container.ContainerCitizenInventory;
+import com.minecolonies.api.util.BlockPosUtil;
 import com.minecolonies.api.util.CompatibilityUtils;
 import com.minecolonies.api.util.ItemStackUtils;
 import com.minecolonies.api.util.constant.TypeConstants;
@@ -663,10 +664,13 @@ public class VisitorCitizen extends AbstractEntityCitizen
                     tavern.setNoVisitorTime(world.getRandom().nextInt(5000) + 30000);
                 }
 
+                final String deathLocation = BlockPosUtil.getString(getCitizenData().getLastPosition());
+
                 LanguageHandler.sendPlayersMessage(colony.getImportantMessageEntityPlayers(),
                   "com.minecolonies.coremod.gui.tavern.visitordeath",
                   getCitizenData().getName(),
-                  cause.getDamageType());
+                  cause.getDamageType(),
+                  deathLocation);
             }
         }
     }
