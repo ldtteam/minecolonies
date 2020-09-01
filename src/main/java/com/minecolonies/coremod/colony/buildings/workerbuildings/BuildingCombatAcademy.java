da5e4311aa37cc4f24a8e0e973d5bafaf3cd5b0e
Mike Garlick <37391233+Mekle001@users.noreply.github.com>
2020-09-01 08:05:45 -0700

    Fix sleeping in training facilities (#5692)
    Add in missing bed registration code
    Fix SleepAI to actually find the beds

--- src/main/java/com/minecolonies/coremod/colony/buildings/workerbuildings/BuildingCombatAcademy.java
+++ src/main/java/com/minecolonies/coremod/colony/buildings/workerbuildings/BuildingCombatAcademy.java
@@ -19,13 +19,16 @@ import com.minecolonies.coremod.client.gui.WindowHutWorkerPlaceholder;
 import com.minecolonies.coremod.colony.buildings.AbstractBuildingWorker;
 import com.minecolonies.coremod.colony.jobs.JobCombatTraining;
 import com.minecolonies.coremod.research.UnlockBuildingResearchEffect;
+import net.minecraft.block.BedBlock;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.CarvedPumpkinBlock;
 import net.minecraft.block.HayBlock;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.nbt.ListNBT;
 import net.minecraft.nbt.NBTUtil;
+import net.minecraft.state.properties.BedPart;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
@@ -347,6 +350,26 @@ public class BuildingCombatAcademy extends AbstractBuildingWorker implements IBu
             }
         }
     }
+
+    @Override
+    public void registerBlockPosition(@NotNull final BlockState blockState, @NotNull final BlockPos pos, @NotNull final World world)
+    {
+        super.registerBlockPosition(blockState, pos, world);
+
+        BlockPos registrationPosition = pos;
+        if (blockState.getBlock() instanceof BedBlock)
+        {
+            if (blockState.get(BedBlock.PART) == BedPart.FOOT)
+            {
+                registrationPosition = registrationPosition.offset(blockState.get(BedBlock.HORIZONTAL_FACING));
+            }
+
+            if (!bedList.contains(registrationPosition))
+            {
+                bedList.add(registrationPosition);
+            }
+        }
+    }    
 
     @Override
     public BuildingEntry getBuildingRegistryEntry()
