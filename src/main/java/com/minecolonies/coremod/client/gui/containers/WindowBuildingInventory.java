b24421a1011acd0fb3940d59fda3fde016af231f
Raycoms <ray.neiheiser@gmail.com>
2020-09-04 20:41:10 +0100

    Fix/several (#5710)
    Fix builder stuck
    Fix inv offset
    Improve baker consumption

--- src/main/java/com/minecolonies/coremod/client/gui/containers/WindowBuildingInventory.java
+++ src/main/java/com/minecolonies/coremod/client/gui/containers/WindowBuildingInventory.java
@@ -1,6 +1,7 @@
 package com.minecolonies.coremod.client.gui.containers;
 
 import com.minecolonies.api.inventory.container.ContainerBuildingInventory;
+import com.minecolonies.api.util.constant.Constants;
 import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.gui.screen.inventory.ContainerScreen;
 import net.minecraft.entity.player.PlayerInventory;
@@ -9,8 +10,35 @@ import net.minecraft.util.text.ITextComponent;
 
 public class WindowBuildingInventory extends ContainerScreen<ContainerBuildingInventory>
 {
-    private static final ResourceLocation CHEST_GUI_TEXTURE = new ResourceLocation("textures/gui/container/generic_54.png");
-    private final        int              inventoryRows;
+    /**
+     * Texture res loc.
+     */
+    private static final ResourceLocation TEXT = new ResourceLocation(Constants.MOD_ID, "textures/gui/generic_108.png");
+
+    /**
+     * Offset inside the texture to use.
+     */
+    private static final int TEXTURE_OFFSET = 126 * 2 - 17;
+
+    /**
+     * Offset of each slot.
+     */
+    private static final int SLOT_OFFSET = 18;
+
+    /**
+     * Size of the custom texture.
+     */
+    private static final int TEXTURE_SIZE = 350;
+
+    /**
+     * Offet of the screen for the texture.
+     */
+    private static final int TEXTURE_HEIGHT = 96;
+
+    /**
+     * In rows total.
+     */
+    private final int inventoryRows;
 
     public WindowBuildingInventory(final ContainerBuildingInventory container, final PlayerInventory playerInventory, final ITextComponent component)
     {
@@ -42,10 +70,11 @@ public class WindowBuildingInventory extends ContainerScreen<ContainerBuildingIn
     protected void drawGuiContainerBackgroundLayer(float partialTicks, int mouseX, int mouseY)
     {
         RenderSystem.color4f(1.0F, 1.0F, 1.0F, 1.0F);
-        this.minecraft.getTextureManager().bindTexture(CHEST_GUI_TEXTURE);
+        this.minecraft.getTextureManager().bindTexture(TEXT);
         int i = (this.width - this.xSize) / 2;
         int j = (this.height - this.ySize) / 2;
-        this.blit(i, j, 0, 0, this.xSize, this.inventoryRows * 18 + 17);
-        this.blit(i, j + this.inventoryRows * 18 + 17, 0, 126, this.xSize, 96);
+        blit(i, j, 0, 0, this.xSize, this.inventoryRows * SLOT_OFFSET + SLOT_OFFSET - 1, TEXTURE_SIZE, TEXTURE_SIZE);
+        blit(i, j + this.inventoryRows * SLOT_OFFSET + SLOT_OFFSET - 1, 0,
+          TEXTURE_OFFSET, this.xSize, TEXTURE_HEIGHT, TEXTURE_SIZE, TEXTURE_SIZE);
     }
 }
