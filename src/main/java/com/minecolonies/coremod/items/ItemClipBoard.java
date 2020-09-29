c346b4ffff0fd6b57d91a104d1fa8c1be65acc96
Ray  Neiheiser <ray.neiheiser@gmail.com>
2020-09-29 22:25:45 +0100

    fix #5845

--- src/main/java/com/minecolonies/coremod/items/ItemClipBoard.java
+++ src/main/java/com/minecolonies/coremod/items/ItemClipBoard.java
@@ -66,7 +66,10 @@ public class ItemClipBoard extends AbstractItemMinecolonies
             if (ctx.getWorld().isRemote)
             {
                 final IColonyView colonyView = IColonyManager.getInstance().getColonyView(compound.getInt(TAG_COLONY), ctx.getWorld().getDimensionKey().func_240901_a_());
-                MineColonies.proxy.openClipBoardWindow(colonyView);
+                if (colonyView != null)
+                {
+                    MineColonies.proxy.openClipBoardWindow(colonyView);
+                }
             }
         }
 
@@ -100,7 +103,10 @@ public class ItemClipBoard extends AbstractItemMinecolonies
         if (compound.keySet().contains(TAG_COLONY))
         {
             final IColonyView colonyView = IColonyManager.getInstance().getColonyView(compound.getInt(TAG_COLONY), worldIn.getDimensionKey().func_240901_a_());
-            MineColonies.proxy.openClipBoardWindow(colonyView);
+            if (colonyView != null)
+            {
+                MineColonies.proxy.openClipBoardWindow(colonyView);
+            }
         }
         else
         {
