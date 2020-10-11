3a6eeeaa3e390f90d02472fa7095255613ee7b04
someaddons <38401808+someaddons@users.noreply.github.com>
2020-10-11 11:44:57 +0200

    Improve some possible error for visitor spawn positions (#5902)
    Improve some possible error for visitor spawn positions
    Fix research citizen limit not allowing config limit to be reached

--- src/main/java/com/minecolonies/coremod/colony/managers/CitizenManager.java
+++ src/main/java/com/minecolonies/coremod/colony/managers/CitizenManager.java
@@ -286,10 +286,11 @@ public class CitizenManager implements ICitizenManager
             colony.getEventDescriptionManager().addEventDescription(new CitizenSpawnedEvent(spawnPoint, citizenData.getName()));
         }
         final EntityCitizen entity = (EntityCitizen) ModEntities.CITIZEN.create(world);
-        entity.getCitizenColonyHandler().registerWithColony(citizenData.getColony().getID(), citizenData.getId());
 
         entity.setPosition(spawnPoint.getX() + HALF_BLOCK, spawnPoint.getY() + SLIGHTLY_UP, spawnPoint.getZ() + HALF_BLOCK);
         world.addEntity(entity);
+
+        entity.getCitizenColonyHandler().registerWithColony(citizenData.getColony().getID(), citizenData.getId());
 
         colony.getProgressManager()
           .progressCitizenSpawn(citizens.size(), citizens.values().stream().filter(tempDate -> tempDate.getJob() != null).collect(Collectors.toList()).size());
@@ -460,6 +461,11 @@ public class CitizenManager implements ICitizenManager
         if (effect != null)
         {
             max += effect.getEffect();
+            // TODO research data rework
+            if (max >= 200)
+            {
+                return MineColonies.getConfig().getCommon().maxCitizenPerColony.get() - 25;
+            }
         }
         return max;
     }
