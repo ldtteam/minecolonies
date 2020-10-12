eb83c4fd8fc204dc887057b99e2ff77dad57d1f2
Raycoms <ray.neiheiser@gmail.com>
2020-10-12 21:13:59 +0100

    Fix/some (#5911)
    fix an exception with the container
    Also fix an issue with crashing with non resovled requests
    Also fix issue with style changes

--- src/main/java/com/minecolonies/coremod/network/messages/server/colony/building/BuildingSetStyleMessage.java
+++ src/main/java/com/minecolonies/coremod/network/messages/server/colony/building/BuildingSetStyleMessage.java
@@ -1,12 +1,22 @@
 package com.minecolonies.coremod.network.messages.server.colony.building;
 
+import com.ldtteam.structurize.util.PlacementSettings;
 import com.minecolonies.api.colony.IColony;
 import com.minecolonies.api.colony.buildings.IBuilding;
 import com.minecolonies.api.colony.buildings.views.IBuildingView;
+import com.minecolonies.api.util.LoadOnlyStructureHandler;
+import com.minecolonies.coremod.colony.workorders.WorkOrderBuildBuilding;
 import com.minecolonies.coremod.network.messages.server.AbstractBuildingServerMessage;
+import com.minecolonies.coremod.util.ColonyUtils;
+import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.network.PacketBuffer;
+import net.minecraft.util.Tuple;
+import net.minecraft.world.chunk.Chunk;
 import net.minecraftforge.fml.network.NetworkEvent;
+import net.minecraftforge.fml.network.PacketDistributor;
 import org.jetbrains.annotations.NotNull;
+
+import static com.ldtteam.structurize.blocks.interfaces.IBlueprintDataProvider.TAG_BLUEPRINTDATA;
 
 /**
  * Message to set the style of a building.
@@ -41,14 +51,12 @@ public class BuildingSetStyleMessage extends AbstractBuildingServerMessage<IBuil
     @Override
     public void fromBytesOverride(@NotNull final PacketBuffer buf)
     {
-
         style = buf.readString(32767);
     }
 
     @Override
     public void toBytesOverride(@NotNull final PacketBuffer buf)
     {
-
         buf.writeString(style);
     }
 
@@ -59,6 +67,26 @@ public class BuildingSetStyleMessage extends AbstractBuildingServerMessage<IBuil
         if (building.getTileEntity() != null)
         {
             building.getTileEntity().setStyle(style);
+
+            final WorkOrderBuildBuilding workOrder = new WorkOrderBuildBuilding(building, building.getBuildingLevel() + 1);
+            final LoadOnlyStructureHandler structure = new LoadOnlyStructureHandler(colony.getWorld(), building.getPosition(), workOrder.getStructureName(), new PlacementSettings(), true);
+
+            CompoundNBT teData = structure.getBluePrint().getTileEntityData(building.getTileEntity().getPos(), structure.getBluePrint().getPrimaryBlockOffset());
+            if (teData != null && teData.contains(TAG_BLUEPRINTDATA))
+            {
+                building.getTileEntity().readSchematicDataFromNBT(teData);
+                Chunk chunk = (Chunk) building.getTileEntity().getWorld().getChunk(building.getTileEntity().getPos());
+                PacketDistributor.TRACKING_CHUNK.with(() -> chunk).send(building.getTileEntity().getUpdatePacket());
+                building.getTileEntity().markDirty();
+                building.setHeight(structure.getBluePrint().getSizeY());
+                final Tuple<Tuple<Integer, Integer>, Tuple<Integer, Integer>> corners
+                  = ColonyUtils.calculateCorners(building.getPosition(),
+                  colony.getWorld(),
+                  structure.getBluePrint(),
+                  workOrder.getRotation(colony.getWorld()),
+                  workOrder.isMirrored());
+                building.setCorners(corners.getA().getA(), corners.getA().getB(), corners.getB().getA(), corners.getB().getB());
+            }
         }
     }
 }
