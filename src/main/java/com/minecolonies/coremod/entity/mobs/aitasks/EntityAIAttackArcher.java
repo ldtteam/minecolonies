61daaca308f211958021c78b01786a8cfd41e78f
someaddons <38401808+someaddons@users.noreply.github.com>
2020-11-01 09:35:18 +0100

    Fix speed modifiers, fix raider damage scaling (#6013)
    Reworked a bit how we handle our entities speed a bit, we use the entities movement speed attribute as base speed, and any speedvalues passed along for certain pathing/move to operations are speedFactors multiplied ontop. In some cases we already use speed like this, in other cases we treated the given speedFactors as basespeed and caused issues. I've also renamed all the speed parameters to speedFactor for clarity.
    
    Fixed raider difficulty scaling, it no longer scales base + scaling damage by difficulty, and also changed their damage to our custom damage source, which does not do additional world difficulty scaling with damage against players, as our raiders already do scale with world difficulty.

--- src/main/java/com/minecolonies/coremod/entity/mobs/aitasks/EntityAIAttackArcher.java
+++ src/main/java/com/minecolonies/coremod/entity/mobs/aitasks/EntityAIAttackArcher.java
@@ -33,15 +33,15 @@ public class EntityAIAttackArcher extends Goal
      */
     private static final double ARROW_PIERCE_DIFFICULTY = 3.0d;
 
-    private static final double                        PITCH_MULTIPLIER               = 0.4;
-    private static final double                        HALF_ROTATION                  = 180;
-    private static final double                        ATTACK_SPEED                   = 1.3;
-    private static final double                        AIM_HEIGHT                     = 2.0D;
-    private static final double                        ARROW_SPEED                    = 1.4D;
-    private static final double                        HIT_CHANCE                     = 10.0D;
-    private static final double                        AIM_SLIGHTLY_HIGHER_MULTIPLIER = 0.20000000298023224D;
-    private static final double                        BASE_PITCH                     = 0.8D;
-    private static final double                        PITCH_DIVIDER                  = 1.0D;
+    private static final double PITCH_MULTIPLIER               = 0.4;
+    private static final double HALF_ROTATION                  = 180;
+    private static final double ATTACK_SPEED                   = 1.1;
+    private static final double AIM_HEIGHT                     = 2.0D;
+    private static final double ARROW_SPEED                    = 1.4D;
+    private static final double HIT_CHANCE                     = 10.0D;
+    private static final double AIM_SLIGHTLY_HIGHER_MULTIPLIER = 0.20000000298023224D;
+    private static final double BASE_PITCH                     = 0.8D;
+    private static final double PITCH_DIVIDER                  = 1.0D;
     private static final double                        MAX_ATTACK_DISTANCE            = 20.0D;
     private static final double                        SPEED_FOR_DIST                 = 35;
     private final        AbstractEntityMinecoloniesMob entity;
