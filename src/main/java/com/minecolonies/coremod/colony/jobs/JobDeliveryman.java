0a4fa93295220e59f0598817244623408937ae74
Ray  Neiheiser <ray.neiheiser@gmail.com>
2020-11-04 11:24:00 +0000

    exact offset

--- src/main/java/com/minecolonies/coremod/colony/jobs/JobDeliveryman.java
+++ src/main/java/com/minecolonies/coremod/colony/jobs/JobDeliveryman.java
@@ -205,6 +205,7 @@ public class JobDeliveryman extends AbstractJob<EntityAIWorkDeliveryman, JobDeli
 
         LinkedList<IToken<?>> taskQueue = getTaskQueueFromDataStore();
 
+        int offset = 0;
         for (int i = insertionIndex; i < taskQueue.size(); i++)
         {
             final IToken theToken = taskQueue.get(i);
@@ -213,6 +214,7 @@ public class JobDeliveryman extends AbstractJob<EntityAIWorkDeliveryman, JobDeli
             {
                 taskQueue.remove(theToken);
                 i--;
+                offset--;
             }
             else
             {
@@ -220,7 +222,7 @@ public class JobDeliveryman extends AbstractJob<EntityAIWorkDeliveryman, JobDeli
             }
         }
 
-        getTaskQueueFromDataStore().add(Math.max(0, insertionIndex), token);
+        getTaskQueueFromDataStore().add(Math.max(0, insertionIndex + offset), token);
 
         if (newRequest instanceof StandardRequests.PickupRequest && newRequest.getRequest().getPriority() == getPlayerActionPriority(true))
         {
@@ -500,7 +502,7 @@ public class JobDeliveryman extends AbstractJob<EntityAIWorkDeliveryman, JobDeli
         for (int i = 0; i < requestTokens.size(); i++)
         {
             final IRequest<?> compareRequest = getColony().getRequestManager().getRequestForToken(requestTokens.get(i));
-            if (compareRequest == null || compareRequest.getState() == RequestState.COMPLETED)
+            if (compareRequest == null)
             {
                 continue;
             }
