b70993b6b6f0bdfaaf254b823d890cc3e12560a2
someaddons <someaddons@github.com>
2020-11-07 15:39:01 +0100

    hotfix building access too early

--- src/main/java/com/minecolonies/coremod/entity/ai/basic/AbstractEntityAIBasic.java
+++ src/main/java/com/minecolonies/coremod/entity/ai/basic/AbstractEntityAIBasic.java
@@ -416,6 +416,7 @@ public abstract class AbstractEntityAIBasic<J extends AbstractJob<?, J>, B exten
 
         if (getState() == INIT)
         {
+            this.getOwnBuilding().setJobDisplayName(job.getName());
             return IDLE;
         }
 
