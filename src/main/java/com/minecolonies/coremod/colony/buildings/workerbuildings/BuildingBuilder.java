47d4c3373749a1601727c80ff7b185dbb201efc7
ToMe25 <38815969+ToMe25@users.noreply.github.com>
2020-11-28 20:59:17 +0100

    Fix builders in manual mode unable to select orders created for them (#6165)
    Fix builders in manual mode not showing build orders that are created with them selected, but outside the normal builder range.

--- src/main/java/com/minecolonies/coremod/colony/buildings/workerbuildings/BuildingBuilder.java
+++ src/main/java/com/minecolonies/coremod/colony/buildings/workerbuildings/BuildingBuilder.java
@@ -154,9 +154,9 @@ public class BuildingBuilder extends AbstractBuildingStructureBuilder
             list.addAll(getColony().getWorkManager().getOrderedList(WorkOrderBuildDecoration.class, getPosition()));
 
             list.removeIf(order -> order instanceof WorkOrderBuildMiner);
-            list.removeIf(order -> order.isClaimed());
+            list.removeIf(order -> order.isClaimed() && !order.getClaimedBy().equals(getPosition()));
             list.removeIf(order -> order instanceof WorkOrderBuild && !(order instanceof WorkOrderBuildRemoval) &&
-                  !((WorkOrderBuild) order).canBuild(getMainCitizen()));
+                  !((WorkOrderBuild) order).canBuildIngoringDistance(getMainCitizen()));
 
             buf.writeInt(list.size());
 
@@ -329,7 +329,7 @@ public class BuildingBuilder extends AbstractBuildingStructureBuilder
         }
 
         IWorkOrder wo = getColony().getWorkManager().getWorkOrder(orderId);
-        if (wo == null || wo.getClaimedBy() != null)
+        if (wo == null || (wo.getClaimedBy() != null && !wo.getClaimedBy().equals(getPosition())))
         {
             return;
         }
