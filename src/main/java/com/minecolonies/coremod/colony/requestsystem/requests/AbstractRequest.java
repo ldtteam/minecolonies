0a99b6696ad7f9ba52ef6837961b3876fa7f5590
Ray  Neiheiser <ray.neiheiser@gmail.com>
2020-11-29 14:35:13 +0000

    Reset deliveries on failure

--- src/main/java/com/minecolonies/coremod/colony/requestsystem/requests/AbstractRequest.java
+++ src/main/java/com/minecolonies/coremod/colony/requestsystem/requests/AbstractRequest.java
@@ -412,6 +412,12 @@ public abstract class AbstractRequest<R extends IRequestable> implements IReques
         this.deliveries = InventoryUtils.processItemStackListAndMerge(this.deliveries);
     }
 
+    @Override
+    public void resetDeliveries()
+    {
+        this.deliveries.clear();
+    }
+
     @NotNull
     @Override
     public ITextComponent getLongDisplayString()
