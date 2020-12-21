dcc13aa99340cc18fdfded2bd8dceba9ff7bc62d
ravenbuilder934 <sabrinajamie@outlook.com>
2020-12-21 17:31:40 -0600

    Change to orange
    The yellow was too bright and hard to read

--- src/main/java/com/minecolonies/coremod/client/gui/WindowResourceList.java
+++ src/main/java/com/minecolonies/coremod/client/gui/WindowResourceList.java
@@ -219,9 +219,9 @@ public class WindowResourceList extends AbstractWindowSkeleton
                 neededLabel.setColor(RED, RED);
                 break;
             case NEED_MORE:
-                resourceLabel.setColor(YELLOW, YELLOW);
-                resourceMissingLabel.setColor(YELLOW, YELLOW);
-                neededLabel.setColor(YELLOW, YELLOW);
+                resourceLabel.setColor(ORANGE, ORANGE);
+                resourceMissingLabel.setColor(ORANGE, ORANGE);
+                neededLabel.setColor(ORANGE, ORANGE);
                 break;
             case HAVE_ENOUGH:
                 resourceLabel.setColor(DARKGREEN, DARKGREEN);
