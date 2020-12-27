77af1099c87a0ae44b787204cac238f20f2a1b39
ravenbuilder934 <sabrinajamie@outlook.com>
2020-12-27 11:36:47 -0600

    Wow, didn't even compile. Let's try this

--- src/api/java/com/minecolonies/api/items/ItemBlockHut.java
+++ src/api/java/com/minecolonies/api/items/ItemBlockHut.java
@@ -47,8 +47,8 @@ public class ItemBlockHut extends BlockItem
         super.addInformation(stack, worldIn, tooltip, flagIn);
         if (block.needsResearch())
         {
-            tooltip.add(new TranslationTextComponent(TranslationConstants.HUT_NEEDS_RESEARCH_TOOLTIP_1, block.getDisplayName()));
-            tooltip.add(new TranslationTextComponent(TranslationConstants.HUT_NEEDS_RESEARCH_TOOLTIP_2, block.getDisplayName()));
+            tooltip.add(new TranslationTextComponent(TranslationConstants.HUT_NEEDS_RESEARCH_TOOLTIP_1, block.getTranslationName()));
+            tooltip.add(new TranslationTextComponent(TranslationConstants.HUT_NEEDS_RESEARCH_TOOLTIP_2, block.getTranslationName()));
         }
     }
 
