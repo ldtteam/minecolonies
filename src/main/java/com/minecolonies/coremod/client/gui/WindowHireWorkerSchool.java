60fe9befbf92e73fd1b0effc0ea3dc87a9b9e7ef
Ray  Neiheiser <ray.neiheiser@gmail.com>
2021-02-17 16:50:51 +0000

    so this

--- src/main/java/com/minecolonies/coremod/client/gui/WindowHireWorkerSchool.java
+++ src/main/java/com/minecolonies/coremod/client/gui/WindowHireWorkerSchool.java
@@ -4,6 +4,7 @@ import com.minecolonies.api.colony.ICitizenDataView;
 import com.minecolonies.api.colony.IColonyView;
 import com.minecolonies.api.entity.citizen.Skill;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.Style;
 import net.minecraft.util.text.TextFormatting;
 
 import java.util.Comparator;
@@ -39,5 +40,15 @@ public class WindowHireWorkerSchool extends WindowHireWorker
                      .filter(citizen -> (citizen.getWorkBuilding() == null && building.getWorkerId().size() < building.getMaxInhabitants())
                                           || building.getPosition().equals(citizen.getWorkBuilding())).sorted(Comparator.comparing(ICitizenDataView::getName))
                      .collect(Collectors.toList());
+    }
+
+    @Override
+    protected Style createColor(final Skill primary, final Skill secondary, final Skill current)
+    {
+        if (current == Skill.Intelligence)
+        {
+            return Style.EMPTY.applyFormatting(TextFormatting.GREEN).applyFormatting(TextFormatting.BOLD);
+        }
+        return super.createColor(primary, secondary, current);
     }
 }
