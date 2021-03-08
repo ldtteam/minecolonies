a88cdb3bb73b32f2036001abf97ed9eaf5096e18
Simon Boigelot <sboigelot@gmail.com>
2021-03-09 00:56:00 +0100

    Improve physic shape and rotation of items

--- src/main/java/com/minecolonies/coremod/blocks/huts/BlockHutGraveyard.java
+++ src/main/java/com/minecolonies/coremod/blocks/huts/BlockHutGraveyard.java
@@ -7,6 +7,10 @@ import com.minecolonies.api.tileentities.MinecoloniesTileEntities;
 import com.minecolonies.coremod.tileentities.TileEntityWareHouse;
 import net.minecraft.block.BlockState;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.world.IBlockReader;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -17,10 +21,22 @@ import org.jetbrains.annotations.Nullable;
 
 public class BlockHutGraveyard extends AbstractBlockHut<BlockHutGraveyard>
 {
+    /**
+     * tall shape.
+     */
+    private static final VoxelShape SHAPE = VoxelShapes.create(0.1, 0.1, 0.1, 0.9, 1.9, 0.9);
+
     public BlockHutGraveyard()
     {
         //No different from Abstract parent
         super();
+    }
+
+    @NotNull
+    @Override
+    public VoxelShape getShape(final BlockState state, final IBlockReader worldIn, final BlockPos pos, final ISelectionContext context)
+    {
+        return SHAPE;
     }
 
     @NotNull
