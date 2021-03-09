fff780efe3f765e14134ae7d81fd54f5ac88a5cd
Sebastian Snoer <seb.snoer@gmail.com>
2021-03-09 16:11:07 +0100

    Changed hover text when first in queue

--- src/main/java/com/minecolonies/coremod/colony/requestsystem/requests/StandardRequests.java
+++ src/main/java/com/minecolonies/coremod/colony/requestsystem/requests/StandardRequests.java
@@ -275,7 +275,8 @@ public final class StandardRequests
 
             if (posInList >= 0)
             {
-                return ImmutableList.of(new TranslationTextComponent(FROM, requester), new TranslationTextComponent(IN_QUEUE+1, posInList));
+            	if(posInList == 0) 	return ImmutableList.of(new TranslationTextComponent(FROM, requester), new TranslationTextComponent(IN_PROGRESS, posInList+1));
+            	else				return ImmutableList.of(new TranslationTextComponent(FROM, requester), new TranslationTextComponent(IN_QUEUE, posInList+1));
             }
             else
             {
@@ -399,7 +400,8 @@ public final class StandardRequests
 
                 if (posInList >= 0)
                 {
-                    return ImmutableList.of(new TranslationTextComponent(AT, requester), new TranslationTextComponent(IN_QUEUE+1, posInList));
+                	if(posInList == 0) 	return ImmutableList.of(new TranslationTextComponent(AT, requester), new TranslationTextComponent(IN_PROGRESS, posInList+1));
+                	else				return ImmutableList.of(new TranslationTextComponent(AT, requester), new TranslationTextComponent(IN_QUEUE, posInList+1));
                 }
                 else if (getState() == RequestState.FOLLOWUP_IN_PROGRESS)
                 {
