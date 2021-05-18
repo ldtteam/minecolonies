eacdfdee03dfa9b6c74660ccdc8fb3d96766cf61
Ray  Neiheiser <ray.neiheiser@gmail.com>
2021-05-18 14:24:58 -0300

    fix issue 7117

--- src/api/java/com/minecolonies/api/blocks/types/RackType.java
+++ src/api/java/com/minecolonies/api/blocks/types/RackType.java
@@ -2,51 +2,26 @@ package com.minecolonies.api.blocks.types;
 
 import com.minecolonies.api.blocks.AbstractBlockMinecoloniesRack;
 import net.minecraft.util.IStringSerializable;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * Defines the types of Racks that the {@link AbstractBlockMinecoloniesRack} supports.
  */
 public enum RackType implements IStringSerializable
 {
-    DEFAULT(0, "blockrackemptysingle", "emptysingle"),
-    FULL(1, "blockrackfullsingle", "fullsingle"),
-    DEFAULTDOUBLE(2, "blockrackempty", "empty"),
-    FULLDOUBLE(3, "blockrackfull", "full"),
-    EMPTYAIR(4, "blockrackair", "dontrender");
+    DEFAULT("blockrackemptysingle", "emptysingle"),
+    FULL( "blockrackfullsingle", "fullsingle"),
+    DEFAULTDOUBLE( "blockrackempty", "empty"),
+    FULLDOUBLE( "blockrackfull", "full"),
+    EMPTYAIR( "blockrackair", "dontrender");
 
-    private static final RackType[] META_LOOKUP = new RackType[values().length];
-    static
-    {
-        for (final RackType blockRack : values())
-        {
-            META_LOOKUP[blockRack.getMetadata()] = blockRack;
-        }
-    }
-    private final int    meta;
     private final String name;
     private final String unlocalizedName;
 
-    RackType(final int meta, final String name, final String unlocalizedName)
+    RackType(final String name, final String unlocalizedName)
     {
-        this.meta = meta;
         this.name = name;
         this.unlocalizedName = unlocalizedName;
-    }
-
-    public static RackType byMetadata(final int meta)
-    {
-        int tempMeta = meta;
-        if (tempMeta < 0 || tempMeta >= META_LOOKUP.length)
-        {
-            tempMeta = 0;
-        }
-
-        return META_LOOKUP[tempMeta];
-    }
-
-    public int getMetadata()
-    {
-        return this.meta;
     }
 
     @Override
@@ -65,6 +40,7 @@ public enum RackType implements IStringSerializable
         return this.unlocalizedName;
     }
 
+    @NotNull
     @Override
     public String getString()
     {
