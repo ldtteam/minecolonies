338b13e61cff78325c1d7df06bba19d5e41a96c8
Ray  Neiheiser <ray.neiheiser@gmail.com>
2021-06-25 09:51:38 -0300

    derp

--- src/main/java/com/minecolonies/coremod/colony/buildings/moduleviews/CraftingModuleView.java
+++ src/main/java/com/minecolonies/coremod/colony/buildings/moduleviews/CraftingModuleView.java
@@ -142,7 +142,7 @@ public class CraftingModuleView extends AbstractBuildingModuleView
     @Override
     public String getDesc()
     {
-        return "com.minecolonies.coremod.gui.workerhuts." + id ;
+        return "com.minecolonies.coremod.gui.workerhuts.recipe." + id ;
     }
 
     public List<IRecipeStorage> getRecipes()
