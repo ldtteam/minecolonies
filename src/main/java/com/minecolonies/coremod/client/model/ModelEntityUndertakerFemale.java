b6650e57fd755342285911f80ece0db43ca44009
Raycoms <ray.neiheiser@gmail.com>
2021-06-26 12:03:13 +0100

    Update ModelEntityUndertakerFemale.java

--- src/main/java/com/minecolonies/coremod/client/model/ModelEntityUndertakerFemale.java
+++ src/main/java/com/minecolonies/coremod/client/model/ModelEntityUndertakerFemale.java
@@ -79,7 +79,7 @@ public class ModelEntityUndertakerFemale extends CitizenModel<AbstractEntityCiti
         bipedLeftLeg.setTextureOffset(16, 48).addBox(-2.0F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, 0.0F, true);
         bipedLeftLeg.setTextureOffset(0, 48).addBox(-2.0F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, 0.25F, false);
 		
-		bipedHeadwear.showModel = false;
+	bipedHeadwear.showModel = false;
     }
 
     public void setRotationAngle(ModelRenderer modelRenderer, float x, float y, float z)
