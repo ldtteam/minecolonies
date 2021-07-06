a995e85089bd22c22169693050fab5fbd6c78e03
Justen-N <losthalflingrogue@gmail.com>
2021-07-06 09:59:18 -0600

    Fix weird typo

--- src/api/java/com/minecolonies/api/blocks/AbstractBlockHut.java
+++ src/api/java/com/minecolonies/api/blocks/AbstractBlockHut.java
@@ -77,7 +77,7 @@ public abstract class AbstractBlockHut<B extends AbstractBlockHut<B>> extends Ab
     private static final VoxelShape SHAPE = VoxelShapes.create(0.1, 0.1, 0.1, 0.9, 0.9, 0.9);
 
     /**
-     * The hut's lower-case building-registry-compatible name.s
+     * The hut's lower-case building-registry-compatible name.
      */
     private final String name;
 
