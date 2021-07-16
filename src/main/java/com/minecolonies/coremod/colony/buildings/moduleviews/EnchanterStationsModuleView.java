d2a7d9a4853df6ef4a96d1526dc7c4b6ae238b27
Ray  Neiheiser <ray.neiheiser@gmail.com>
2021-07-16 19:16:50 -0300

    add missing

--- src/main/java/com/minecolonies/coremod/colony/buildings/moduleviews/EnchanterStationsModuleView.java
+++ src/main/java/com/minecolonies/coremod/colony/buildings/moduleviews/EnchanterStationsModuleView.java
@@ -74,7 +74,7 @@ public class EnchanterStationsModuleView extends AbstractBuildingModuleView
     @Override
     public String getIcon()
     {
-        return "info";
+        return "entity";
     }
 
     @Override
