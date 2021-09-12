324ee548180f131d4e6b21d1dd3d0fcc10da4fdd
Karthas077 <paranoidandroid077@gmail.com>
2021-09-11 18:39:46 -0700

    maven repo update
    Temporary usage of curse.maven
    Change to item handling on hive

--- src/api/java/com/minecolonies/api/compatibility/productivebees/ProductiveBeesCompat.java
+++ src/api/java/com/minecolonies/api/compatibility/productivebees/ProductiveBeesCompat.java
@@ -1,9 +1,11 @@
-package com.minecolonies.api.compatibility.resourcefulbees;
+package com.minecolonies.api.compatibility.productivebees;
 
 import cy.jdkdigital.productivebees.common.tileentity.AdvancedBeehiveTileEntity;
+import com.minecolonies.api.compatibility.resourcefulbees.IBeehiveCompat;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.items.CapabilityItemHandler;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,8 +21,13 @@ public class ProductiveBeesCompat implements IBeehiveCompat
             //If we have a productive bees hive, we care about the items inside
             AdvancedBeehiveTileEntity hive = (AdvancedBeehiveTileEntity) world.getBlockEntity(pos);
 
-            hive.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY).ifPresent(inv -> {
-                list = getItemHandlerAsList(inv);
+            //the actual IItemHandlerModifiable is protected, but this is the public method that
+            //Productive Bees uses to access the contents
+            hive.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY).ifPresent( inv -> {
+                for (int i = 0; i < inv.getSlots(); i++) {
+                    ItemStack itemstack = inv.getStackInSlot(i);
+                    list.add(inv.extractItem(i, itemstack.getCount(), false));
+                }
             });
 
             return list;
