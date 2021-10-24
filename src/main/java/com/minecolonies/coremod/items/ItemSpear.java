0dbac5c6a09b05bb316e7b1a6d3a4e6677f709ac
Tagan Hoyle <tagnum.elite@gmail.com>
2021-10-24 14:18:39 +0200

    Move SpearTileRenderer to its own class
    Also, cleanup ItemSpear class

--- src/main/java/com/minecolonies/coremod/items/ItemSpear.java
+++ src/main/java/com/minecolonies/coremod/items/ItemSpear.java
@@ -3,13 +3,8 @@ package com.minecolonies.coremod.items;
 import com.minecolonies.api.creativetab.ModCreativeTabs;
 import com.minecolonies.api.entity.SpearEntity;
 import com.minecolonies.api.util.constant.Constants;
-import com.minecolonies.coremod.client.model.ModelSpear;
-import com.mojang.blaze3d.matrix.MatrixStack;
+import com.minecolonies.coremod.client.render.SpearItemTileEntityRenderer;
 import mcp.MethodsReturnNonnullByDefault;
-import net.minecraft.client.renderer.IRenderTypeBuffer;
-import net.minecraft.client.renderer.ItemRenderer;
-import net.minecraft.client.renderer.model.ItemCameraTransforms;
-import net.minecraft.client.renderer.tileentity.ItemStackTileEntityRenderer;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.AbstractArrowEntity;
@@ -18,8 +13,6 @@ import net.minecraft.item.TridentItem;
 import net.minecraft.stats.Stats;
 import net.minecraft.util.*;
 import net.minecraft.world.World;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
 import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.ParametersAreNonnullByDefault;
@@ -30,7 +23,7 @@ public class ItemSpear extends TridentItem
 {
     public ItemSpear(final Properties properties)
     {
-        super(properties.durability(250).tab(ModCreativeTabs.MINECOLONIES).setISTER(() -> SpearTileRenderer::new));
+        super(properties.durability(250).tab(ModCreativeTabs.MINECOLONIES).setISTER(() -> SpearItemTileEntityRenderer::new));
         setRegistryName(new ResourceLocation(Constants.MOD_ID, "spear"));
     }
 
@@ -40,25 +33,25 @@ public class ItemSpear extends TridentItem
         if (entityLiving instanceof PlayerEntity)
         {
             PlayerEntity playerEntity = (PlayerEntity) entityLiving;
-            int i = this.getUseDuration(stack) - timeLeft;
-            if (i >= 10)
+            int usedForDuration = this.getUseDuration(stack) - timeLeft;
+            if (usedForDuration >= 10)
             {
                 if (!worldIn.isClientSide)
                 {
                     stack.hurtAndBreak(1, playerEntity, playerEntity1 -> playerEntity1.broadcastBreakEvent(entityLiving.getUsedItemHand()));
-                    SpearEntity spearProjectileEntity = new SpearEntity(worldIn, playerEntity, stack);
-                    spearProjectileEntity.shootFromRotation(playerEntity, playerEntity.xRot, playerEntity.yRot, 0.0F, 2.5F, 1.0F);
+                    SpearEntity spearEntity = new SpearEntity(worldIn, playerEntity, stack);
+                    spearEntity.shootFromRotation(playerEntity, playerEntity.xRot, playerEntity.yRot, 0.0F, 2.5F, 1.0F);
                     if (playerEntity.abilities.instabuild)
                     {
-                        spearProjectileEntity.pickup = AbstractArrowEntity.PickupStatus.CREATIVE_ONLY;
+                        spearEntity.pickup = AbstractArrowEntity.PickupStatus.CREATIVE_ONLY;
                     }
-
-                    worldIn.addFreshEntity(spearProjectileEntity);
-                    worldIn.playSound(null, spearProjectileEntity, SoundEvents.TRIDENT_THROW, SoundCategory.PLAYERS, 1.0F, 1.0F);
-                    if (!playerEntity.abilities.instabuild)
+                    else
                     {
                         playerEntity.inventory.removeItem(stack);
                     }
+
+                    worldIn.addFreshEntity(spearEntity);
+                    worldIn.playSound(null, spearEntity, SoundEvents.TRIDENT_THROW, SoundCategory.PLAYERS, 1.0F, 1.0F);
                 }
 
                 SoundEvent soundEvent = SoundEvents.TRIDENT_THROW;
@@ -80,27 +73,6 @@ public class ItemSpear extends TridentItem
         {
             playerEntity.startUsingItem(hand);
             return ActionResult.consume(itemstack);
-        }
-    }
-
-    @OnlyIn(Dist.CLIENT)
-    public static class SpearTileRenderer extends ItemStackTileEntityRenderer
-    {
-        private static final ResourceLocation TEXTURE = new ResourceLocation(Constants.MOD_ID, "textures/entity/spear.png");
-        private final        ModelSpear       model   = new ModelSpear();
-
-        @Override
-        public void renderByItem(
-          ItemStack stack,
-          @NotNull ItemCameraTransforms.TransformType transformType,
-          MatrixStack matrixStack,
-          @NotNull IRenderTypeBuffer buffer,
-          int combinedLight,
-          int combinedOverlay)
-        {
-            matrixStack.pushPose();
-            model.renderToBuffer(matrixStack, ItemRenderer.getFoilBuffer(buffer, model.renderType(TEXTURE), false, stack.hasFoil()), combinedLight, combinedOverlay, 1, 1, 1, 1);
-            matrixStack.popPose();
         }
     }
 }
