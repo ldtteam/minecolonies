2e96781a968b513d40f7e6b50e799bdcc0a85ec4
someaddons <38401808+someaddons@users.noreply.github.com>
2021-11-01 01:04:22 +0100

    Fix returning bad tileentities sometimes (#7735)
    Fix returning bad tileentities sometimes
    Fix Ship raids potentially beeing seen as not active while there is reloaded raid entities

--- src/main/java/com/minecolonies/coremod/colony/buildings/AbstractBuilding.java
+++ src/main/java/com/minecolonies/coremod/colony/buildings/AbstractBuilding.java
@@ -879,6 +879,11 @@ public abstract class AbstractBuilding extends AbstractBuildingContainer
     @Override
     public AbstractTileEntityColonyBuilding getTileEntity()
     {
+        if (tileEntity != null && tileEntity.isRemoved())
+        {
+            tileEntity = null;
+        }
+
         if ((tileEntity == null)
               && colony != null
               && colony.getWorld() != null
