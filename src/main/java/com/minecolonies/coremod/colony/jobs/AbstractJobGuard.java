3ff373f453803b8a703090571d42a384635c2406
someaddons <someaddons@github.com>
2022-01-03 19:16:58 +0100

    Fix citizen entity duplicating on backup load(can be automatic on restarts) Fix guards not always getting their modifiers added correctly

--- src/main/java/com/minecolonies/coremod/colony/jobs/AbstractJobGuard.java
+++ src/main/java/com/minecolonies/coremod/colony/jobs/AbstractJobGuard.java
@@ -1,12 +1,18 @@
 package com.minecolonies.coremod.colony.jobs;
 
 import com.minecolonies.api.colony.ICitizenData;
+import com.minecolonies.api.colony.buildings.IBuilding;
 import com.minecolonies.api.entity.citizen.AbstractEntityCitizen;
+import com.minecolonies.coremod.MineColonies;
 import com.minecolonies.coremod.colony.buildings.AbstractBuildingGuards;
 import com.minecolonies.coremod.entity.ai.citizen.guard.AbstractEntityAIGuard;
+import com.minecolonies.coremod.util.AttributeModifierUtils;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
 import net.minecraft.util.DamageSource;
 
 import static com.minecolonies.api.entity.ai.statemachine.states.AIWorkerState.GUARD_SLEEP;
+import static com.minecolonies.api.util.constant.CitizenConstants.GUARD_HEALTH_MOD_BUILDING_NAME;
+import static com.minecolonies.api.util.constant.CitizenConstants.GUARD_HEALTH_MOD_CONFIG_NAME;
 
 /**
  * Abstract Class for Guard Jobs.
@@ -51,5 +57,22 @@ public abstract class AbstractJobGuard<J extends AbstractJobGuard<J>> extends Ab
     public boolean isAsleep()
     {
         return getWorkerAI() != null && getWorkerAI().getState() == GUARD_SLEEP;
+    }
+
+    @Override
+    public void initEntityValues(AbstractEntityCitizen citizen)
+    {
+        super.initEntityValues(citizen);
+
+        final IBuilding workBuilding = citizen.getCitizenData().getWorkBuilding();
+        if (workBuilding instanceof AbstractBuildingGuards)
+        {
+            AttributeModifierUtils.addHealthModifier(citizen,
+              new AttributeModifier(GUARD_HEALTH_MOD_BUILDING_NAME, ((AbstractBuildingGuards) workBuilding).getBonusHealth(), AttributeModifier.Operation.ADDITION));
+            AttributeModifierUtils.addHealthModifier(citizen,
+              new AttributeModifier(GUARD_HEALTH_MOD_CONFIG_NAME,
+                MineColonies.getConfig().getServer().guardHealthMult.get() - 1.0,
+                AttributeModifier.Operation.MULTIPLY_TOTAL));
+        }
     }
 }
