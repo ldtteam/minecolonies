f0c4caf2fc857edf41d89f47793e688b6ecefdbf
Ray  Neiheiser <ray.neiheiser@gmail.com>
2022-01-30 10:36:53 -0300

    fix 7959 (#8005)
    Properly set the level before running the onUpgrade path. Making sure all down-paths are okay with that.

--- src/main/java/com/minecolonies/coremod/colony/buildings/AbstractBuilding.java
+++ src/main/java/com/minecolonies/coremod/colony/buildings/AbstractBuilding.java
@@ -912,11 +912,6 @@ public abstract class AbstractBuilding extends AbstractBuildingContainer
         return tileEntity;
     }
 
-    /**
-     * Called upon completion of an upgrade process. We suppress this warning since this parameter will be used in child classes which override this method.
-     *
-     * @param newLevel The new level.
-     */
     @Override
     public void onUpgradeComplete(final int newLevel)
     {
@@ -933,11 +928,6 @@ public abstract class AbstractBuilding extends AbstractBuildingContainer
         );
         calculateCorners();
         this.isBuilt = true;
-
-        if (newLevel > getBuildingLevel())
-        {
-            FireworkUtils.spawnFireworksAtAABBCorners(getCorners(), colony.getWorld(), newLevel);
-        }
 
         getModules(IBuildingEventsModule.class).forEach(module -> module.onUpgradeComplete(newLevel));
         colony.getResearchManager().checkAutoStartResearch();
