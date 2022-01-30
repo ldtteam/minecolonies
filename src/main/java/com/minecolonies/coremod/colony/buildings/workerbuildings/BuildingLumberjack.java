d52e542c75552cb1c0a054a65ef7afd9b9d981a6
Ray  Neiheiser <ray.neiheiser@gmail.com>
2022-01-30 10:36:53 -0300

    fix 7959 (#8005)
    Properly set the level before running the onUpgrade path. Making sure all down-paths are okay with that.

--- src/main/java/com/minecolonies/coremod/colony/buildings/workerbuildings/BuildingLumberjack.java
+++ src/main/java/com/minecolonies/coremod/colony/buildings/workerbuildings/BuildingLumberjack.java
@@ -152,12 +152,6 @@ public class BuildingLumberjack extends AbstractBuilding
     }
 
     @Override
-    public void onUpgradeComplete(final int newLevel)
-    {
-        super.onUpgradeComplete(newLevel);
-    }
-
-    @Override
     public void deserializeNBT(final CompoundNBT compound)
     {
         super.deserializeNBT(compound);
