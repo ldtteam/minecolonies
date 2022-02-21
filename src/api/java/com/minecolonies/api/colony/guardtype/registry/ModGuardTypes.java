17ad2fd60317d8dc77cf0aa5f0e8b28f0ea30ff6
Ray  Neiheiser <ray.neiheiser@gmail.com>
2022-02-20 21:03:17 -0300

    small adjustments

--- src/api/java/com/minecolonies/api/colony/guardtype/registry/ModGuardTypes.java
+++ src/api/java/com/minecolonies/api/colony/guardtype/registry/ModGuardTypes.java
@@ -9,7 +9,7 @@ public final class ModGuardTypes
 
     public static final ResourceLocation KNIGHT_ID = new ResourceLocation(Constants.MOD_ID, "knight");
     public static final ResourceLocation RANGER_ID = new ResourceLocation(Constants.MOD_ID, "ranger");
-    public static final ResourceLocation DRUID_ID  = new ResourceLocation(Constants.MOD_ID, "witch");
+    public static final ResourceLocation DRUID_ID  = new ResourceLocation(Constants.MOD_ID, "druid");
 
     public static       GuardType        knight;
     public static       GuardType ranger;
--- src/api/java/com/minecolonies/api/entity/combat/threat/ThreatTable.java
+++ src/api/java/com/minecolonies/api/entity/combat/threat/ThreatTable.java
@@ -4,7 +4,6 @@ import net.minecraft.entity.LivingEntity;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Predicate;
 
 import static com.minecolonies.api.util.constant.Constants.TICKS_SECOND;
 
