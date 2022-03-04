89c5e99c09f7a333d4abbe3c2c5c58af5e6f1b01
Ray  Neiheiser <ray.neiheiser@gmail.com>
2022-03-04 16:37:03 -0300

    Adjust codec

--- src/main/java/com/minecolonies/coremod/structures/MineColoniesStructures.java
+++ src/main/java/com/minecolonies/coremod/structures/MineColoniesStructures.java
@@ -1,8 +1,11 @@
 package com.minecolonies.coremod.structures;
 
 import com.minecolonies.api.util.constant.Constants;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.world.level.levelgen.feature.StructureFeature;
 import net.minecraft.world.level.levelgen.feature.configurations.JigsawConfiguration;
+import net.minecraft.world.level.levelgen.structure.pools.StructureTemplatePool;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
@@ -12,7 +15,11 @@ import net.minecraftforge.registries.RegistryObject;
  */
 public class MineColoniesStructures
 {
-    public static final DeferredRegister<StructureFeature<?>> DEFERRED_REGISTRY_STRUCTURE = DeferredRegister.create(ForgeRegistries.STRUCTURE_FEATURES, Constants.MOD_ID);
+    public static final Codec<JigsawConfiguration> COLONY_CODEC = RecordCodecBuilder.create((conf) -> conf.group(StructureTemplatePool.CODEC.fieldOf("start_pool")
+      .forGetter(JigsawConfiguration::startPool), Codec.intRange(0, 10).fieldOf("size")
+      .forGetter(JigsawConfiguration::maxDepth)).apply(conf, JigsawConfiguration::new));
 
-    public static final RegistryObject<StructureFeature<JigsawConfiguration>> EMPTY_COLONY = DEFERRED_REGISTRY_STRUCTURE.register("empty_colony", () -> (new EmptyColonyStructure(JigsawConfiguration.CODEC)));
+    public static final    DeferredRegister<StructureFeature<?>> DEFERRED_REGISTRY_STRUCTURE = DeferredRegister.create(ForgeRegistries.STRUCTURE_FEATURES, Constants.MOD_ID);
+
+    public static final RegistryObject<StructureFeature<JigsawConfiguration>> EMPTY_COLONY = DEFERRED_REGISTRY_STRUCTURE.register("empty_colony", () -> (new EmptyColonyStructure(COLONY_CODEC)));
 }
