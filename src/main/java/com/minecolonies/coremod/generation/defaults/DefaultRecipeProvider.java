357988e4444731243813ba6d31c0e6ed22af2d5f
Mirality <uecasm@users.noreply.github.com>
2022-03-24 21:05:56 +1300

    Fix dough recipes derp

--- src/main/java/com/minecolonies/coremod/generation/defaults/DefaultRecipeProvider.java
+++ src/main/java/com/minecolonies/coremod/generation/defaults/DefaultRecipeProvider.java
@@ -398,18 +398,18 @@ public class DefaultRecipeProvider extends RecipeProvider
                 .unlockedBy("has_dough", has(ModItems.breadDough))
                 .save(consumer, append(new ResourceLocation(MOD_ID, "baked_bread"), "_", method));
 
-        CookingRecipeBuilder.smelting(Ingredient.of(ModItems.cakeBatter),
-                Items.CAKE, 0.35f, 200)
+        CookingRecipeBuilder.cooking(Ingredient.of(ModItems.cakeBatter),
+                Items.CAKE, 0.35f, cookingTime, serializer)
                 .unlockedBy("has_dough", has(ModItems.cakeBatter))
                 .save(consumer, append(new ResourceLocation(MOD_ID, "baked_cake"), "_", method));
 
-        CookingRecipeBuilder.smelting(Ingredient.of(ModItems.cookieDough),
-                Items.COOKIE, 0.0475f, 25)
+        CookingRecipeBuilder.cooking(Ingredient.of(ModItems.cookieDough),
+                Items.COOKIE, 0.0475f, cookingTime / 8, serializer)
                 .unlockedBy("has_dough", has(ModItems.cookieDough))
                 .save(consumer, append(new ResourceLocation(MOD_ID, "baked_cookies"), "_", method));
 
-        CookingRecipeBuilder.smelting(Ingredient.of(ModItems.rawPumpkinPie),
-                Items.PUMPKIN_PIE, 0.35f, 200)
+        CookingRecipeBuilder.cooking(Ingredient.of(ModItems.rawPumpkinPie),
+                Items.PUMPKIN_PIE, 0.35f, cookingTime, serializer)
                 .unlockedBy("has_dough", has(ModItems.rawPumpkinPie))
                 .save(consumer, append(new ResourceLocation(MOD_ID, "baked_pumpkin_pie"), "_", method));
     }
