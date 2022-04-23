00cab5efdd368c4a49b5c0ec1be8f62fac55ff57
Mirality <uecasm@users.noreply.github.com>
2022-04-23 17:24:54 +1200

    Add brewing recipe enumeration

--- src/main/java/com/minecolonies/coremod/recipes/BrewingCraftingType.java
+++ src/main/java/com/minecolonies/coremod/recipes/BrewingCraftingType.java
@@ -1,15 +1,25 @@
 package com.minecolonies.coremod.recipes;
 
+import com.minecolonies.api.MinecoloniesAPIProxy;
+import com.minecolonies.api.compatibility.ICompatibilityManager;
+import com.minecolonies.api.crafting.GenericRecipe;
 import com.minecolonies.api.crafting.IGenericRecipe;
 import com.minecolonies.api.crafting.ModCraftingTypes;
 import com.minecolonies.api.crafting.registry.CraftingType;
+import net.minecraft.block.Blocks;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.RecipeManager;
 import net.minecraft.world.World;
+import net.minecraftforge.common.brewing.BrewingRecipeRegistry;
+import net.minecraftforge.common.brewing.IBrewingRecipe;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * A crafting type for brewing recipes
@@ -25,7 +35,38 @@ public class BrewingCraftingType extends CraftingType
     @NotNull
     public List<IGenericRecipe> findRecipes(@NotNull RecipeManager recipeManager, @Nullable World world)
     {
-        // TODO: enumerate vanilla brewing recipes
-        return new ArrayList<>();
+        final List<IGenericRecipe> recipes = new ArrayList<>();
+        final ICompatibilityManager compatibilityManager = MinecoloniesAPIProxy.getInstance().getColonyManager().getCompatibilityManager();
+
+        for (final IBrewingRecipe recipe : BrewingRecipeRegistry.getRecipes())
+        {
+            final List<ItemStack> inputs = compatibilityManager.getListOfAllItems().stream()
+                    .filter(recipe::isInput)
+                    .collect(Collectors.toList());
+            final List<ItemStack> ingredients = compatibilityManager.getListOfAllItems().stream()
+                    .filter(recipe::isIngredient)
+                    .collect(Collectors.toList());
+
+            for (final ItemStack input : inputs)
+            {
+                for (final ItemStack ingredient : ingredients)
+                {
+                    final ItemStack output = recipe.getOutput(input, ingredient);
+                    if (!output.isEmpty())
+                    {
+                        final ItemStack actualInput = input.copy();
+                        actualInput.setCount(3);
+                        final ItemStack actualOutput = output.copy();
+                        actualOutput.setCount(3);
+
+                        recipes.add(new GenericRecipe(null, actualOutput, Collections.emptyList(),
+                                Arrays.asList(Collections.singletonList(actualInput), Collections.singletonList(ingredient)),
+                                4, Blocks.BREWING_STAND, null, Collections.emptyList(), -1));
+                    }
+                }
+            }
+        }
+
+        return recipes;
     }
 }
