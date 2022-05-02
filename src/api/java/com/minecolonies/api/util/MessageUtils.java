ldtteam/minecolonies

commit 007d5ebe8dec950a643cba961415d1e27f4f6e68

hotfix

src/api/java/com/minecolonies/api/util/MessageUtils.java
@@ -57,7 +57,7 @@ public class MessageUtils
          */
         MessageBuilder(ITextComponent component)
         {
-            this.rootComponent = getFormattableComponent(component);
+            this.currentComponent = getFormattableComponent(component);
         }
 
         /**
@@ -176,11 +176,15 @@ public class MessageUtils
          */
         private void mergeComponents()
         {
-            if (currentComponent != null)
+            if (rootComponent == null)
+            {
+                rootComponent = currentComponent;
+            }
+            else
             {
                 rootComponent.append(currentComponent);
-                currentComponent = null;
             }
+            currentComponent = null;
         }
     }
 

commit b3d5de9f36d40c956169415f6ec9b961763c08a8

hotfix of hotfix (#8328)
Fix the compile error introduced in commit 007d5eb

src/api/java/com/minecolonies/api/util/MessageUtils.java
@@ -43,7 +43,7 @@ public class MessageUtils
         /**
          * The stored text component to use when sending the message.
          */
-        private final IFormattableTextComponent rootComponent;
+        private IFormattableTextComponent rootComponent;
 
         /**
          * The current working component.