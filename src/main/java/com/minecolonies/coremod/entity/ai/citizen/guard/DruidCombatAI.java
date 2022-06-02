d93847db6d0e6fe45855d23c54540aadafbaea20
Ray  Neiheiser <ray.neiheiser@gmail.com>
2022-06-02 19:48:49 -0300

    fix druid combat AI

--- src/main/java/com/minecolonies/coremod/entity/ai/citizen/guard/DruidCombatAI.java
+++ src/main/java/com/minecolonies/coremod/entity/ai/citizen/guard/DruidCombatAI.java
@@ -241,7 +241,7 @@ public class DruidCombatAI extends AttackMoveAI<EntityCitizen>
     {
         return (AbstractEntityAIGuard.isAttackableTarget(user, entity)
                   || (entity instanceof IThreatTableEntity && ((IThreatTableEntity) entity).getThreatTable().getTarget() != null )
-                  || (entity instanceof Player && entity.getLastHurtByMobTimestamp() != 0 && entity.tickCount - entity.getLastHurtByMobTimestamp() < 20 * 30))
+                  || (entity instanceof Player && entity.getLastHurtByMobTimestamp() != 0 && entity.tickCount > entity.getLastHurtByMobTimestamp() && entity.tickCount - entity.getLastHurtByMobTimestamp() < 20 * 30))
                  && !wasAffectedByDruid(entity);
     }
 
