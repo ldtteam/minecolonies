aa6a3774f4fb0cc7af88cd20abda1c090bf6c7f2
Raycoms <ray.neiheiser@gmail.com>
2023-01-22 13:58:44 +0100

    Fix NBT assignment corruption (#8905)
    In some cases it's possible that NBT data gets corrupted and causes citizens their jobs to no longer match the expected job in the building they are assigned to.
    This fix will prevent any loading issues by unassigning the worker when an improper link between the citizen it's job and building is found, to prevent any crashes.

--- src/api/java/com/minecolonies/api/util/constant/translation/DebugTranslationConstants.java
+++ src/api/java/com/minecolonies/api/util/constant/translation/DebugTranslationConstants.java
@@ -8,9 +8,11 @@ import org.jetbrains.annotations.NonNls;
 public class DebugTranslationConstants
 {
     @NonNls
-    public static final String DEBUG_NO_CLOSE_COLONY    = "com.minecolonies.coremod.gui.debugscreen.noclosecolony";
+    public static final String DEBUG_NO_CLOSE_COLONY              = "com.minecolonies.coremod.gui.debugscreen.noclosecolony";
     @NonNls
-    public static final String DEBUG_NEXT_COLONY        = "com.minecolonies.coremod.gui.debugscreen.nextcolony";
+    public static final String DEBUG_NEXT_COLONY                  = "com.minecolonies.coremod.gui.debugscreen.nextcolony";
     @NonNls
-    public static final String DEBUG_BLOCKS_FROM_CENTER = "com.minecolonies.coremod.gui.debugscreen.blocksfromcenter";
+    public static final String DEBUG_BLOCKS_FROM_CENTER           = "com.minecolonies.coremod.gui.debugscreen.blocksfromcenter";
+    @NonNls
+    public static final String DEBUG_WARNING_CITIZEN_LOAD_FAILURE = "com.minecolonies.coremod.debug.citizenloadfailure";
 }
