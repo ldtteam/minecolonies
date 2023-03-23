8ebdc4c2874813ff484fcc4f8ef42ee71018ba93
Mirality <uecasm@users.noreply.github.com>
2023-03-23 20:36:02 +1300

    le sigh

--- src/main/java/com/minecolonies/coremod/client/render/worldevent/RenderTypes.java
+++ src/main/java/com/minecolonies/coremod/client/render/worldevent/RenderTypes.java
@@ -1,9 +1,11 @@
 package com.minecolonies.coremod.client.render.worldevent;
 
+import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.DefaultVertexFormat;
 import com.mojang.blaze3d.vertex.VertexFormat;
 import net.minecraft.Util;
 import net.minecraft.client.renderer.RenderStateShard;
+import net.minecraft.client.renderer.RenderStateShard.DepthTestStateShard;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.resources.ResourceLocation;
 import org.lwjgl.opengl.GL11;
@@ -35,6 +37,7 @@ public class RenderTypes
 
     public static final class InnerRenderTypes extends RenderType
     {
+        private static final DepthTestStateShard ALWAYS_DEPTH_TEST = new AlwaysDepthTestStateShard();
         private static final DepthTestStateShard GREATER_DEPTH_TEST = new DepthTestStateShard(">", GL11.GL_GREATER);
 
         private InnerRenderTypes(final String nameIn,
@@ -61,7 +64,7 @@ public class RenderTypes
                     .setShaderState(POSITION_TEX_SHADER)
                     .setTextureState(new RenderStateShard.TextureStateShard(p_173202_, false, false))
                     .setTransparencyState(TRANSLUCENT_TRANSPARENCY)
-                    .setDepthTestState(NO_DEPTH_TEST)
+                    .setDepthTestState(ALWAYS_DEPTH_TEST)
                     .createCompositeState(false));
         });
 
@@ -105,5 +108,17 @@ public class RenderTypes
                         .setWriteMaskState(COLOR_WRITE)
                         .createCompositeState(false));
 
+    }
+
+    private static class AlwaysDepthTestStateShard extends DepthTestStateShard
+    {
+        private AlwaysDepthTestStateShard()
+        {
+            super("true_always", -1);
+            setupState = () -> {
+                RenderSystem.enableDepthTest();
+                RenderSystem.depthFunc(GL11.GL_ALWAYS);
+            };
+        }
     }
 }
