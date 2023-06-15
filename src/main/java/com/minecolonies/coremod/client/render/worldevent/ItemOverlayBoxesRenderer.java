9665c6253f333723904bf7f0fe7a331ee2d8a0ad
Mirality <uecasm@users.noreply.github.com>
2023-06-15 22:40:21 +1200

    Make the supply camp error boxes visible again

--- src/main/java/com/minecolonies/coremod/client/render/worldevent/ItemOverlayBoxesRenderer.java
+++ src/main/java/com/minecolonies/coremod/client/render/worldevent/ItemOverlayBoxesRenderer.java
@@ -1,9 +1,6 @@
 package com.minecolonies.coremod.client.render.worldevent;
 
-import com.ldtteam.structurize.util.WorldRenderMacros;
 import com.minecolonies.api.items.IBlockOverlayItem;
-import com.mojang.blaze3d.vertex.PoseStack;
-import com.mojang.blaze3d.vertex.VertexConsumer;
 
 import java.util.List;
 
@@ -30,17 +27,5 @@ public class ItemOverlayBoxesRenderer
 
             ColonyWorldRenderMacros.endRenderLineBox(ctx.bufferSource);
         }
-    }
-
-    private static void renderLineBox(final PoseStack poseStack, final VertexConsumer buffer,
-                                      final float minX, final float minY, final float minZ,
-                                      final float minX2, final float minY2, final float minZ2,
-                                      final float maxX, final float maxY, final float maxZ,
-                                      final float maxX2, final float maxY2, final float maxZ2,
-                                      final int red, final int green, final int blue, final int alpha)
-    {
-        buffer.defaultColor(red, green, blue, alpha);
-        WorldRenderMacros.populateRenderLineBox(minX, minY, minZ, minX2, minY2, minZ2, maxX, maxY, maxZ, maxX2, maxY2, maxZ2, poseStack.last().pose(), buffer);
-        buffer.unsetDefaultColor();
     }
 }
