425e057aea9b0675d6041086cc056dde2bf05a9d
Raycoms <ray.neiheiser@gmail.com>
2023-06-26 18:10:40 +0200

    Small fix (#9212)
    Fix interaction cancellation
    Fix indent
    Improve craftign code
    Improve dumping code

--- src/main/java/com/minecolonies/coremod/colony/interactionhandling/ServerCitizenInteraction.java
+++ src/main/java/com/minecolonies/coremod/colony/interactionhandling/ServerCitizenInteraction.java
@@ -28,6 +28,8 @@ import java.util.Set;
 import java.util.function.Predicate;
 
 import static com.minecolonies.api.util.constant.Constants.TICKS_SECOND;
+import static com.minecolonies.coremod.colony.interactionhandling.StandardInteraction.INTERACTION_R_IGNORE;
+import static com.minecolonies.coremod.colony.interactionhandling.StandardInteraction.INTERACTION_R_REMIND;
 
 /**
  * The server side interaction response handler.
@@ -129,13 +131,23 @@ public abstract class ServerCitizenInteraction extends AbstractInteractionRespon
     public void onServerResponseTriggered(final int responseId, final Player player, final ICitizenData data)
     {
         final Component response = getPossibleResponses().get(responseId);
+        tryHandleIgnoreResponse(response, player);
+    }
+
+    /**
+     * Check if the response was an ignore response.
+     * @param response the response to compare.
+     * @param player the player that triggered it.
+     */
+    private void tryHandleIgnoreResponse(final Component response, final Player player)
+    {
         if (response.getContents() instanceof TranslatableContents)
         {
-            if (((TranslatableContents) response.getContents()).getKey().equals("com.minecolonies.coremod.gui.chat.remindmelater"))
+            if (((TranslatableContents) response.getContents()).getKey().equals(INTERACTION_R_REMIND))
             {
                 displayAtWorldTick = (int) (player.level.getGameTime() + (TICKS_SECOND * 60 * 10));
             }
-            else if (((TranslatableContents) response.getContents()).getKey().equals("com.minecolonies.coremod.gui.chat.ignore"))
+            else if (((TranslatableContents) response.getContents()).getKey().equals(INTERACTION_R_IGNORE))
             {
                 displayAtWorldTick = (int) (player.level.getGameTime() + (TICKS_SECOND * 60 * 20));
             }
@@ -146,6 +158,8 @@ public abstract class ServerCitizenInteraction extends AbstractInteractionRespon
     @OnlyIn(Dist.CLIENT)
     public boolean onClientResponseTriggered(final int responseId, final Player player, final ICitizenDataView data, final BOWindow window)
     {
+        final Component response = getPossibleResponses().get(responseId);
+        tryHandleIgnoreResponse(response, player);
         if (((TranslatableContents) getPossibleResponses().get(responseId).getContents()).getKey().equals("com.minecolonies.coremod.gui.chat.skipchitchat"))
         {
             final MainWindowCitizen windowCitizen = new MainWindowCitizen(data);
