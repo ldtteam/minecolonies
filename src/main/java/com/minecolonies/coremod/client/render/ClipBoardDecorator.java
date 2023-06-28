738511c4f4fd876c915e926919bd18a35933d431
Raycoms <ray.neiheiser@gmail.com>
2023-06-28 18:29:04 +0200

    only if not 0

--- src/main/java/com/minecolonies/coremod/client/render/ClipBoardDecorator.java
+++ src/main/java/com/minecolonies/coremod/client/render/ClipBoardDecorator.java
@@ -72,18 +72,21 @@ public class ClipBoardDecorator implements IItemDecorator
                         final Set<IToken<?>> requestTokens = new HashSet<>();
                         requestTokens.addAll(resolver.getAllAssignedRequests());
                         requestTokens.addAll(retryingRequestResolver.getAllAssignedRequests());
-                        
-                        final PoseStack ps = new PoseStack();
 
-                        ps.pushPose();
-                        ps.translate(0, 0, 500);
-                        GuiComponent.drawCenteredString(ps, font,
-                          Component.literal(requestTokens.size() - asyncRequest.size() + ""),
-                          xOffset + 15,
-                          yOffset - 2,
-                          0xFF4500 | (255 << 24));
-                        ps.popPose();
-                        return true;
+                        if (requestTokens.size() - asyncRequest.size() > 0)
+                        {
+                            final PoseStack ps = new PoseStack();
+
+                            ps.pushPose();
+                            ps.translate(0, 0, 500);
+                            GuiComponent.drawCenteredString(ps, font,
+                              Component.literal(requestTokens.size() - asyncRequest.size() + ""),
+                              xOffset + 15,
+                              yOffset - 2,
+                              0xFF4500 | (255 << 24));
+                            ps.popPose();
+                            return true;
+                        }
                     }
                 }
             }
