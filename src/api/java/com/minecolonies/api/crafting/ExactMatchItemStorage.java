c0c6dcd810ca485d88c7459f353cc2201e99d89b
Raycoms <ray.neiheiser@gmail.com>
2023-10-09 19:54:16 +0200

    remove curr unneeded constructors

--- src/api/java/com/minecolonies/api/crafting/ExactMatchItemStorage.java
+++ src/api/java/com/minecolonies/api/crafting/ExactMatchItemStorage.java
@@ -13,56 +13,11 @@ public class ExactMatchItemStorage extends ItemStorage
     /**
      * Creates an instance of the storage.
      *
-     * @param stack             the stack.
-     * @param amount            the amount.
-     * @param ignoreDamageValue should the damage value be ignored?
-     */
-    public ExactMatchItemStorage(@NotNull final ItemStack stack, final int amount, final boolean ignoreDamageValue)
-    {
-        super(stack, amount, ignoreDamageValue);
-    }
-
-    /**
-     * Creates an instance of the storage.
-     *
-     * @param stack                the stack.
-     * @param ignoreDamageValue    should the damage value be ignored?
-     * @param shouldIgnoreNBTValue should the nbt value be ignored?
-     */
-    public ExactMatchItemStorage(@NotNull final ItemStack stack, final boolean ignoreDamageValue, final boolean shouldIgnoreNBTValue)
-    {
-        super(stack, ignoreDamageValue, shouldIgnoreNBTValue);
-    }
-
-    /**
-     * Creates an instance of the storage.
-     *
-     * @param stack             the stack.
-     * @param ignoreDamageValue should the damage value be ignored?
-     */
-    public ExactMatchItemStorage(@NotNull final ItemStack stack, final boolean ignoreDamageValue)
-    {
-        super(stack, ignoreDamageValue);
-    }
-
-    /**
-     * Creates an instance of the storage.
-     *
      * @param stack the stack.
      */
     public ExactMatchItemStorage(@NotNull final ItemStack stack)
     {
         super(stack);
-    }
-
-    /**
-     * Creates an instance of the storage from JSON
-     *
-     * @param jObject the JSON Object to parse
-     */
-    public ExactMatchItemStorage(@NotNull final JsonObject jObject)
-    {
-        super(jObject);
     }
 
     @Override
