3d9ce5f524b191edd2d93a5a8a18a34c299e6d00
Thom van den Akker <thodor20@gmail.com>
2023-10-12 09:16:08 +0200

    Register no damage firework entity

--- src/main/java/com/minecolonies/apiimp/initializer/EntityInitializer.java
+++ src/main/java/com/minecolonies/apiimp/initializer/EntityInitializer.java
@@ -2,7 +2,7 @@ package com.minecolonies.apiimp.initializer;
 
 import com.minecolonies.api.entity.MinecoloniesMinecart;
 import com.minecolonies.api.entity.ModEntities;
-import com.minecolonies.coremod.entity.SpearEntity;
+import com.minecolonies.api.util.NoDamageFireworkRocketEntity;
 import com.minecolonies.api.util.constant.Constants;
 import com.minecolonies.coremod.entity.*;
 import com.minecolonies.coremod.entity.citizen.EntityCitizen;
@@ -25,8 +25,8 @@ import com.minecolonies.coremod.entity.mobs.pirates.EntityCaptainPirate;
 import com.minecolonies.coremod.entity.mobs.pirates.EntityPirate;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.MobCategory;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.MobCategory;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -201,6 +201,10 @@ public class EntityInitializer
                 .setTrackingRange(ENTITY_TRACKING_RANGE)
                 .setUpdateInterval(ENTITY_UPDATE_FREQUENCY_FISHHOOK)
                 .sized(0.5F, 0.5F)
+                .setShouldReceiveVelocityUpdates(true));
+
+            ModEntities.NO_DAMAGE_FIREWORK_ENTITY = build(registry, "nodamagefirework",
+              EntityType.Builder.<NoDamageFireworkRocketEntity>of(NoDamageFireworkRocketEntity::new, MobCategory.MISC)
                 .setShouldReceiveVelocityUpdates(true));
         }
     }
