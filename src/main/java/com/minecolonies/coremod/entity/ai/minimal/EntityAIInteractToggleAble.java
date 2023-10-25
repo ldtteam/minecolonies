d21ed8a59eb34a443cb3deb71703efbf52a890b0
Raycoms <ray.neiheiser@gmail.com>
2023-10-25 16:41:04 +0200

    add rand

--- src/main/java/com/minecolonies/coremod/entity/ai/minimal/EntityAIInteractToggleAble.java
+++ src/main/java/com/minecolonies/coremod/entity/ai/minimal/EntityAIInteractToggleAble.java
@@ -1,6 +1,7 @@
 package com.minecolonies.coremod.entity.ai.minimal;
 
 import com.minecolonies.api.util.WorldUtil;
+import com.minecolonies.api.util.constant.ColonyConstants;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.ai.goal.Goal;
@@ -384,7 +385,7 @@ public class EntityAIInteractToggleAble extends Goal
         {
             return;
         }
-        updateTimer = 20 + offSet;
+        updateTimer = ColonyConstants.rand.nextInt(40 + offSet);
 
         if (!checkPath())
         {
