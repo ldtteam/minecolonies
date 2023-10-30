d5ee3417a09c6a38924aaba8e7494cf3ba7039bd
Thom van den Akker <thodor20@gmail.com>
2023-10-30 08:03:01 +0100

    Settings views rework (#9493)
    Settings views have been updated so the controls/inputs are on separate lines from the text itself
    Settings now always have a label above them to indicate what the setting is for, this was previously missing for a few settings (most notably the builder)
    Settings will (by default) no longer be hidden from the settings list, instead they will appear disabled, optionally also giving a reason as to why they are disabled (for example missing research, etc)
    Settings may still be hidden entirely by turning a different method it's return value to true, this is true for some settings like Dynamic tree compat for the forester, useful in case of dependencies on other mods
    Migration done from an existing colony against the current previous version, where parsing at least one setting of each setting type, all setting values retained properly

--- src/main/java/com/minecolonies/coremod/colony/buildings/modules/settings/StringSettingWithDesc.java
+++ src/main/java/com/minecolonies/coremod/colony/buildings/modules/settings/StringSettingWithDesc.java
@@ -1,23 +1,10 @@
 package com.minecolonies.coremod.colony.buildings.modules.settings;
 
-import com.ldtteam.blockui.Loader;
-import com.ldtteam.blockui.Pane;
-import com.ldtteam.blockui.controls.ButtonImage;
-import com.ldtteam.blockui.controls.Text;
-import com.ldtteam.blockui.views.View;
-import com.ldtteam.blockui.views.BOWindow;
-import com.minecolonies.api.colony.buildings.modules.settings.ISettingKey;
-import com.minecolonies.api.colony.buildings.modules.settings.ISettingsModuleView;
-import com.minecolonies.api.colony.buildings.views.IBuildingView;
-import net.minecraft.network.chat.Component;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
-
 import java.util.List;
 
 /**
  * Stores a string-list setting (Like enum, but easily serializable).
+ * TODO: Remove in future versions as this only exists right now for settings parsing purposes, this one is not necessary anymore because {@link StringSetting} contains a description by default now.
  */
 public class StringSettingWithDesc extends StringSetting
 {
@@ -40,25 +27,5 @@ public class StringSettingWithDesc extends StringSetting
     public StringSettingWithDesc(final List<String> settings, final int currentIndex)
     {
         super(settings, currentIndex);
-    }
-
-    @OnlyIn(Dist.CLIENT)
-    @Override
-    public void setupHandler(
-      final ISettingKey<?> key,
-      final Pane pane,
-      final ISettingsModuleView settingsModuleView,
-      final IBuildingView building, final BOWindow window)
-    {
-        Loader.createFromXMLFile(new ResourceLocation("minecolonies:gui/layouthuts/layoutstringsettingdesc.xml"), (View) pane);
-        pane.findPaneOfTypeByID("id", Text.class).setText(Component.literal(key.getUniqueId().toString()));
-        pane.findPaneOfTypeByID("desc", Text.class).setText(Component.translatable("com.minecolonies.coremod.setting." + key.getUniqueId().toString()));
-        pane.findPaneOfTypeByID("trigger", ButtonImage.class).setHandler(button -> settingsModuleView.trigger(key));
-    }
-
-    @Override
-    public void render(final ISettingKey<?> key, final Pane pane, final ISettingsModuleView settingsModuleView, final IBuildingView building, final BOWindow window)
-    {
-        pane.findPaneOfTypeByID("trigger", ButtonImage.class).setText(Component.translatable(getSettings().get(getCurrentIndex())));
     }
 }
