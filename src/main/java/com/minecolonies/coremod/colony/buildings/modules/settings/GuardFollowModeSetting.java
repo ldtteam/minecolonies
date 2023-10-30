2544faadb2b8c55f32333f04a1d5d59ef3d9c5df
Thom van den Akker <thodor20@gmail.com>
2023-10-30 08:03:01 +0100

    Settings views rework (#9493)
    Settings views have been updated so the controls/inputs are on separate lines from the text itself
    Settings now always have a label above them to indicate what the setting is for, this was previously missing for a few settings (most notably the builder)
    Settings will (by default) no longer be hidden from the settings list, instead they will appear disabled, optionally also giving a reason as to why they are disabled (for example missing research, etc)
    Settings may still be hidden entirely by turning a different method it's return value to true, this is true for some settings like Dynamic tree compat for the forester, useful in case of dependencies on other mods
    Migration done from an existing colony against the current previous version, where parsing at least one setting of each setting type, all setting values retained properly

--- src/main/java/com/minecolonies/coremod/colony/buildings/modules/settings/GuardFollowModeSetting.java
+++ src/main/java/com/minecolonies/coremod/colony/buildings/modules/settings/GuardFollowModeSetting.java
@@ -1,5 +1,6 @@
 package com.minecolonies.coremod.colony.buildings.modules.settings;
 
+import com.minecolonies.api.colony.buildings.modules.ISettingsModule;
 import com.minecolonies.api.colony.buildings.modules.settings.ISettingsModuleView;
 import com.minecolonies.coremod.colony.buildings.AbstractBuildingGuards;
 
@@ -8,35 +9,48 @@ import java.util.List;
 /**
  * Stores the follow mode setting.
  */
-public class FollowModeSetting extends StringSettingWithDesc
+public class GuardFollowModeSetting extends StringSettingWithDesc
 {
     /**
      * Different setting possibilities.
      */
-    public static String TIGHT =  "com.minecolonies.core.guard.setting.follow.tight";
-    public static String LOOSE =  "com.minecolonies.core.guard.setting.follow.loose";
+    public static final String TIGHT = "com.minecolonies.core.guard.setting.follow.tight";
+    public static final String LOOSE = "com.minecolonies.core.guard.setting.follow.loose";
 
     /**
      * Create a new follow mode setting.
      */
-    public FollowModeSetting()
+    public GuardFollowModeSetting()
     {
         super(TIGHT, LOOSE);
     }
 
     /**
      * Create a new follow mode setting.
-     * @param settings the overall list of settings.
+     *
+     * @param settings     the overall list of settings.
      * @param currentIndex the current selected index.
      */
-    public FollowModeSetting(final List<String> settings, final int currentIndex)
+    public GuardFollowModeSetting(final List<String> settings, final int currentIndex)
     {
         super(settings, currentIndex);
+    }
+
+    @Override
+    public boolean isActive(final ISettingsModule module)
+    {
+        return module.getSetting(AbstractBuildingGuards.GUARD_TASK).getValue().equals(GuardTaskSetting.FOLLOW);
     }
 
     @Override
     public boolean isActive(final ISettingsModuleView module)
     {
         return module.getSetting(AbstractBuildingGuards.GUARD_TASK).getValue().equals(GuardTaskSetting.FOLLOW);
+    }
+
+    @Override
+    public boolean shouldHideWhenInactive()
+    {
+        return true;
     }
 }
