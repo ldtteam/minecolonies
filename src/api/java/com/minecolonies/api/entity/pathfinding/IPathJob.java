0a35a8fb34b39219413c9afb79edca6170dce7c6
someaddons <someaddons@github.com>
2023-11-05 14:07:08 +0100

    add doc

--- src/api/java/com/minecolonies/api/entity/pathfinding/IPathJob.java
+++ src/api/java/com/minecolonies/api/entity/pathfinding/IPathJob.java
@@ -4,9 +4,20 @@ import net.minecraft.world.level.pathfinder.Path;
 
 import java.util.concurrent.Callable;
 
+/**
+ * Interface for path jobs
+ */
 public interface IPathJob extends Callable<Path>
 {
+    /**
+     * Get the path result holder for this job
+     * @return
+     */
     PathResult getResult();
 
+    /**
+     * Get the pathing options used for this job
+     * @return
+     */
     public PathingOptions getPathingOptions();
 }
