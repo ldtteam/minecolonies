81b5b823b60d95343a9acbbae370d5ed78e9cd26
Raycoms <ray.neiheiser@gmail.com>
2023-11-30 08:00:39 +0100

    Raiders search through buildings (#9509)
    Use the right spawn event
    Make env damage 0 until they walked away
    Make raiders walk around in buildings after arrival for a bit.
    Make citizens hide when raiders pass through their colony.
    Make citizens stop hiding when the ship is defeated but didnt sail away yet
    Print a different message on mercy end.
    Dont crash on custom arrow injection

--- src/main/java/com/minecolonies/coremod/entity/ai/basic/AbstractEntityAICrafting.java
+++ src/main/java/com/minecolonies/coremod/entity/ai/basic/AbstractEntityAICrafting.java
@@ -146,7 +146,7 @@ public abstract class AbstractEntityAICrafting<J extends AbstractJobCrafter<?, J
             {
                 if (building.isInBuilding(worker.blockPosition()))
                 {
-                    worker.getNavigation().moveToRandomPos(10, DEFAULT_SPEED, building.getCorners(), AbstractAdvancedPathNavigate.RestrictionType.XYZ);
+                    worker.getNavigation().moveToRandomPos(10, DEFAULT_SPEED, building.getCorners(), AbstractAdvancedPathNavigate.RestrictionType.XYZ, false);
                 }
                 else
                 {
