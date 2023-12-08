b9532df83ccb157d8680d47518c00f2c8eb3630e
Raycoms <ray.neiheiser@gmail.com>
2023-12-08 16:01:19 +0100

    properly handle child adjustment, and drop gear on death (#9609)
    Children when growing up should properly adjust dimensions/model
    When guards die, they drop their gear

--- src/main/java/com/minecolonies/coremod/entity/citizen/EntityCitizen.java
+++ src/main/java/com/minecolonies/coremod/entity/citizen/EntityCitizen.java
@@ -1110,11 +1110,13 @@ public class EntityCitizen extends AbstractEntityCitizen implements IThreatTable
         {
             if (!isChild && this.child)
             {
+                this.child = isChild;
                 getCitizenJobHandler().setModelDependingOnJob(citizenJobHandler.getColonyJob());
             }
         }
         this.child = isChild;
         this.getEntityData().set(DATA_IS_CHILD, isChild);
+        refreshDimensions();
         markDirty(0);
     }
 
