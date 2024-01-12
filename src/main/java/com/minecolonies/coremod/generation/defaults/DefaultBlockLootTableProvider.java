9f2db5764e152acb773ffb18643eb5bc8fb58a40
Thom van den Akker <thodor20@gmail.com>
2024-01-12 09:44:41 +0100

    Fix composted dirt loot dropping (#9633)
    Fix composted dirt loot dropping

--- src/main/java/com/minecolonies/coremod/generation/defaults/DefaultBlockLootTableProvider.java
+++ src/main/java/com/minecolonies/coremod/generation/defaults/DefaultBlockLootTableProvider.java
@@ -4,24 +4,32 @@ import com.minecolonies.api.blocks.AbstractBlockHut;
 import com.minecolonies.api.blocks.ModBlocks;
 import com.minecolonies.coremod.blocks.BlockMinecoloniesRack;
 import com.minecolonies.coremod.generation.SimpleLootTableProvider;
+import net.minecraft.advancements.critereon.EnchantmentPredicate;
+import net.minecraft.advancements.critereon.ItemPredicate;
+import net.minecraft.advancements.critereon.MinMaxBounds.Ints;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.storage.loot.LootPool;
+import net.minecraft.world.level.storage.loot.LootPool.Builder;
 import net.minecraft.world.level.storage.loot.LootTable;
+import net.minecraft.world.level.storage.loot.entries.AlternativesEntry;
 import net.minecraft.world.level.storage.loot.entries.LootItem;
 import net.minecraft.world.level.storage.loot.entries.LootPoolSingletonContainer;
 import net.minecraft.world.level.storage.loot.functions.CopyNameFunction;
 import net.minecraft.world.level.storage.loot.functions.CopyNbtFunction;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
 import net.minecraft.world.level.storage.loot.predicates.ExplosionCondition;
+import net.minecraft.world.level.storage.loot.predicates.MatchTool;
 import net.minecraft.world.level.storage.loot.providers.nbt.ContextNbtProvider;
 import net.minecraftforge.registries.ForgeRegistries;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class DefaultBlockLootTableProvider extends SimpleLootTableProvider
 {
@@ -55,6 +63,13 @@ public class DefaultBlockLootTableProvider extends SimpleLootTableProvider
         saveBlock(ModBlocks.blockColonyWallBanner, registrar);
         saveBlock(ModBlocks.blockIronGate, registrar);
         saveBlock(ModBlocks.blockWoodenGate, registrar);
+        saveBlock(ModBlocks.blockCompostedDirt, registrar,
+          lootPool -> lootPool.add(AlternativesEntry.alternatives()
+                                     .otherwise(LootItem.lootTableItem(ModBlocks.blockCompostedDirt)
+                                                  .when(MatchTool.toolMatches(ItemPredicate.Builder.item()
+                                                                                .hasEnchantment(new EnchantmentPredicate(Enchantments.SILK_TOUCH, Ints.exactly(1))))))
+                                     .otherwise(LootItem.lootTableItem(Blocks.DIRT)
+                                                  .when(ExplosionCondition.survivesExplosion()))));
 
         // intentionally no drops -- creative only
         //saveBlock(ModBlocks.blockDecorationPlaceholder, registrar);
@@ -104,23 +119,26 @@ public class DefaultBlockLootTableProvider extends SimpleLootTableProvider
 
     private void saveBlock(@NotNull final Block block, @NotNull final LootTableRegistrar registrar)
     {
+        final LootPoolSingletonContainer.Builder<?> item = LootItem.lootTableItem(block);
+        if (block instanceof AbstractBlockHut || block instanceof BlockMinecoloniesRack)
+        {
+            item.apply(CopyNameFunction.copyName(CopyNameFunction.NameSource.BLOCK_ENTITY));
+        }
+
+        this.saveBlock(block, registrar, lootPool -> lootPool.add(item).when(ExplosionCondition.survivesExplosion()));
+    }
+
+    private void saveBlock(@NotNull final Block block, @NotNull final LootTableRegistrar registrar, final Consumer<Builder> lootPoolConfigurer)
+    {
         final ResourceLocation location = ForgeRegistries.BLOCKS.getKey(block);
         if (location != null)
         {
             final ResourceLocation id = new ResourceLocation(location.getNamespace(),
-                    "blocks/" + location.getPath());
+              "blocks/" + location.getPath());
 
-            final LootPoolSingletonContainer.Builder<?> item = LootItem.lootTableItem(block);
-            if (block instanceof AbstractBlockHut || block instanceof BlockMinecoloniesRack)
-            {
-                item.apply(CopyNameFunction.copyName(CopyNameFunction.NameSource.BLOCK_ENTITY));
-            }
-
-            registrar.register(id, LootContextParamSets.BLOCK,
-                    LootTable.lootTable().withPool(LootPool.lootPool()
-                            .add(item)
-                            .when(ExplosionCondition.survivesExplosion())
-                    ));
+            final Builder lootPoolbuilder = LootPool.lootPool();
+            lootPoolConfigurer.accept(lootPoolbuilder);
+            registrar.register(id, LootContextParamSets.BLOCK, LootTable.lootTable().withPool(lootPoolbuilder));
         }
     }
 
