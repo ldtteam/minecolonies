7822e1be81336c39b1c2443f9d2b4d8f92989c72
Raycoms <ray.neiheiser@gmail.com>
2024-03-14 19:35:56 +0100

    bunch of fixes (#9832)
    Not so strict on guards not eating/sick handling (as we already got an override on the AI anyway)
    Make sure builders recalc on restart
    Try to improve compat for enchantments, etc
    Fix nametag rename
    Fix crash in research window

--- src/main/java/com/minecolonies/core/entity/citizen/EntityCitizen.java
+++ src/main/java/com/minecolonies/core/entity/citizen/EntityCitizen.java
@@ -385,7 +385,7 @@ public class EntityCitizen extends AbstractEntityCitizen implements IThreatTable
             return InteractionResult.FAIL;
         }
 
-        if (!ItemStackUtils.isEmpty(player.getItemInHand(hand)) && player.getItemInHand(hand).getItem() instanceof NameTagItem)
+        if (!ItemStackUtils.isEmpty(player.getItemInHand(hand)) && player.getItemInHand(hand).is(Items.NAME_TAG))
         {
             return super.checkAndHandleImportantInteractions(player, hand);
         }
@@ -1722,7 +1722,34 @@ public class EntityCitizen extends AbstractEntityCitizen implements IThreatTable
         {
             return citizenData.getInventory().getIterableArmorAndHandInv();
         }
+        else if (citizenDataView != null)
+        {
+            return citizenDataView.getInventory().getIterableArmorAndHandInv();
+        }
         return super.getAllSlots();
+    }
+
+    @NotNull
+    @Override
+    public ItemStack getItemBySlot(EquipmentSlot slotType)
+    {
+        switch (slotType.getType())
+        {
+            case HAND:
+                return super.getItemBySlot(slotType);
+            case ARMOR:
+                if (citizenData != null)
+                {
+                    return citizenData.getInventory().getArmorInSlot(slotType);
+                }
+                else if (citizenDataView != null)
+                {
+                    return citizenDataView.getInventory().getArmorInSlot(slotType);
+                }
+                return super.getItemBySlot(slotType);
+            default:
+                return ItemStack.EMPTY;
+        }
     }
 
     @Override
@@ -1833,6 +1860,7 @@ public class EntityCitizen extends AbstractEntityCitizen implements IThreatTable
     {
         if (citizenData != null && citizenColonyHandler.getColony() != null && name != null)
         {
+            citizenData.setName(name.getString());
             super.setCustomName(name);
         }
     }
