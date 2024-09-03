9720ad5f3cc7dfc30f551d2b8e6594a9ba3c8f49
wmaxlees <max.lees@gmail.com>
2024-09-03 16:28:47 -0600

    Reformat new files

--- src/main/java/com/minecolonies/api/items/ModToolTypes.java
+++ src/main/java/com/minecolonies/api/items/ModToolTypes.java
@@ -23,28 +23,28 @@ import java.util.function.Supplier;
 /**
  * Class used for storing and registering any ToolTypes.
  */
-public class ModToolTypes {
+public class ModToolTypes
+{
     /**
      * The Forge registry for tool types. Do not directly add to this registry. Prefer using the
      * register() function provided here.
      */
-    private final static DeferredRegister<ToolTypeEntry> DEFERRED_REGISTER = DeferredRegister.create(
-            new ResourceLocation(Constants.MOD_ID, "tooltypes"), Constants.MOD_ID);
+    private final static DeferredRegister<ToolTypeEntry> DEFERRED_REGISTER = DeferredRegister.create(new ResourceLocation(Constants.MOD_ID, "tooltypes"), Constants.MOD_ID);
 
-    public static String NONE_ID = "none";
-    public static String PICKAXE_ID = "pickaxe";
-    public static String SHOVEL_ID = "shovel";
-    public static String AXE_ID = "axe";
-    public static String HOE_ID = "hoe";
-    public static String SWORD_ID = "sword";
-    public static String BOW_ID = "bow";
-    public static String FISHING_ROD_ID = "rod";
-    public static String SHEARS_ID = "shears";
-    public static String SHIELD_ID = "shield";
-    public static String HELMET_ID = "helmet";
-    public static String LEGGINGS_ID = "leggings";
-    public static String CHESTPLATE_ID = "chestplate";
-    public static String BOOTS_ID = "boots";
+    public static String NONE_ID            = "none";
+    public static String PICKAXE_ID         = "pickaxe";
+    public static String SHOVEL_ID          = "shovel";
+    public static String AXE_ID             = "axe";
+    public static String HOE_ID             = "hoe";
+    public static String SWORD_ID           = "sword";
+    public static String BOW_ID             = "bow";
+    public static String FISHING_ROD_ID     = "rod";
+    public static String SHEARS_ID          = "shears";
+    public static String SHIELD_ID          = "shield";
+    public static String HELMET_ID          = "helmet";
+    public static String LEGGINGS_ID        = "leggings";
+    public static String CHESTPLATE_ID      = "chestplate";
+    public static String BOOTS_ID           = "boots";
     public static String FLINT_AND_STEEL_ID = "flintandsteel";
 
     public static RegistryObject<ToolTypeEntry> none;
@@ -67,12 +67,130 @@ public class ModToolTypes {
      * The list of all tool types.
      */
     public static List<RegistryObject<ToolTypeEntry>> toolTypes = new ArrayList<>();
+    static
+    {
+        none = register(NONE_ID,
+          () -> new ToolTypeEntry.Builder().setName("")
+                  .setDisplayName(Component.translatable(ToolTranslationConstants.TOOL_TYPE_NONE))
+                  .setIsTool(itemStack -> true)
+                  .setToolLevel(itemStack -> itemStack == null ? 0 : 1)
+                  .build());
+
+        pickaxe = register(PICKAXE_ID,
+          () -> new ToolTypeEntry.Builder().setName(PICKAXE_ID)
+                  .setDisplayName(Component.translatable(ToolTranslationConstants.TOOL_TYPE_PICKAXE))
+                  .setIsTool(itemStack -> canPerformDefaultActions(itemStack, ToolActions.DEFAULT_PICKAXE_ACTIONS) || Compatibility.isTinkersTool(itemStack, PICKAXE_ID))
+                  .setToolLevel(itemStack -> vanillaToolLevel(PICKAXE_ID, itemStack))
+                  .build());
+
+        shovel = register(SHOVEL_ID,
+          () -> new ToolTypeEntry.Builder().setName(SHOVEL_ID)
+                  .setDisplayName(Component.translatable(ToolTranslationConstants.TOOL_TYPE_SHOVEL))
+                  .setIsTool(itemStack -> canPerformDefaultActions(itemStack, ToolActions.DEFAULT_SHOVEL_ACTIONS) || Compatibility.isTinkersTool(itemStack, SHOVEL_ID))
+                  .setToolLevel(itemStack -> vanillaToolLevel(SHOVEL_ID, itemStack))
+                  .build());
+
+        axe = register(AXE_ID,
+          () -> new ToolTypeEntry.Builder().setName(AXE_ID)
+                  .setDisplayName(Component.translatable(ToolTranslationConstants.TOOL_TYPE_AXE))
+                  .setIsTool(itemStack -> canPerformDefaultActions(itemStack, ToolActions.DEFAULT_AXE_ACTIONS) || Compatibility.isTinkersTool(itemStack, AXE_ID))
+                  .setToolLevel(itemStack -> vanillaToolLevel(AXE_ID, itemStack))
+                  .build());
+
+        hoe = register(HOE_ID,
+          () -> new ToolTypeEntry.Builder().setName(HOE_ID)
+                  .setDisplayName(Component.translatable(ToolTranslationConstants.TOOL_TYPE_HOE))
+                  .setIsTool(itemStack -> canPerformDefaultActions(itemStack, ToolActions.DEFAULT_HOE_ACTIONS) || Compatibility.isTinkersTool(itemStack, HOE_ID))
+                  .setToolLevel(itemStack -> vanillaToolLevel(HOE_ID, itemStack))
+                  .build());
+
+        sword = register(SWORD_ID,
+          () -> new ToolTypeEntry.Builder().setName(SWORD_ID)
+                  .setDisplayName(Component.translatable(ToolTranslationConstants.TOOL_TYPE_SWORD))
+                  .setIsTool(itemStack -> canPerformDefaultActions(itemStack, ToolActions.DEFAULT_SWORD_ACTIONS) || Compatibility.isTinkersWeapon(itemStack))
+                  .setToolLevel(itemStack -> {
+                      final String toolId = SWORD_ID;
+                      if (Compatibility.isTinkersWeapon(itemStack))
+                      {
+                          return Compatibility.getToolLevel(itemStack);
+                      }
+                      else if (itemStack.getItem() instanceof final TieredItem tieredItem)  // most tools
+                      {
+                          return tieredItem.getTier().getLevel();
+                      }
+                      return -1;
+                  })
+                  .build());
+
+        bow = register(BOW_ID,
+          () -> new ToolTypeEntry.Builder().setName(BOW_ID)
+                  .setDisplayName(Component.translatable(ToolTranslationConstants.TOOL_TYPE_BOW))
+                  .setIsTool(itemStack -> itemStack.getItem() instanceof BowItem)
+                  .setToolLevel(itemStack -> durabilityBasedLevel(itemStack, Items.BOW.getMaxDamage()))
+                  .build());
+
+        fishing_rod = register(FISHING_ROD_ID,
+          () -> new ToolTypeEntry.Builder().setName(FISHING_ROD_ID)
+                  .setDisplayName(Component.translatable(ToolTranslationConstants.TOOL_TYPE_FISHING_ROD))
+                  .setIsTool(itemStack -> canPerformDefaultActions(itemStack, ToolActions.DEFAULT_FISHING_ROD_ACTIONS))
+                  .setToolLevel(itemStack -> durabilityBasedLevel(itemStack, Items.FISHING_ROD.getMaxDamage()))
+                  .build());
+
+        shears = register(SHEARS_ID,
+          () -> new ToolTypeEntry.Builder().setName(SHEARS_ID)
+                  .setDisplayName(Component.translatable(ToolTranslationConstants.TOOL_TYPE_SHEARS))
+                  .setIsTool(itemStack -> canPerformDefaultActions(itemStack, ToolActions.DEFAULT_SHEARS_ACTIONS))
+                  .setToolLevel(itemStack -> durabilityBasedLevel(itemStack, Items.SHEARS.getMaxDamage()))
+                  .build());
+
+        shield = register(SHIELD_ID,
+          () -> new ToolTypeEntry.Builder().setName(SHIELD_ID)
+                  .setDisplayName(Component.translatable(ToolTranslationConstants.TOOL_TYPE_SHIELD))
+                  .setIsTool(itemStack -> canPerformDefaultActions(itemStack, ToolActions.DEFAULT_SHIELD_ACTIONS))
+                  .setToolLevel(itemStack -> durabilityBasedLevel(itemStack, Items.SHIELD.getMaxDamage()))
+                  .build());
+
+        helmet = register(HELMET_ID,
+          () -> new ToolTypeEntry.Builder().setName(HELMET_ID)
+                  .setDisplayName(Component.translatable(ToolTranslationConstants.TOOL_TYPE_HELMET))
+                  .setIsTool(itemStack -> itemStack.getItem() instanceof ArmorItem armor && EquipmentSlot.HEAD.equals(armor.getEquipmentSlot()))
+                  .setToolLevel(ModToolTypes::armorLevel)
+                  .build());
+
+        leggings = register(LEGGINGS_ID,
+          () -> new ToolTypeEntry.Builder().setName(LEGGINGS_ID)
+                  .setDisplayName(Component.translatable(ToolTranslationConstants.TOOL_TYPE_LEGGINGS))
+                  .setIsTool(itemStack -> itemStack.getItem() instanceof ArmorItem armor && EquipmentSlot.LEGS.equals(armor.getEquipmentSlot()))
+                  .setToolLevel(ModToolTypes::armorLevel)
+                  .build());
+
+        chestplate = register(CHESTPLATE_ID,
+          () -> new ToolTypeEntry.Builder().setName(CHESTPLATE_ID)
+                  .setDisplayName(Component.translatable(ToolTranslationConstants.TOOL_TYPE_CHEST_PLATE))
+                  .setIsTool(itemStack -> itemStack.getItem() instanceof ArmorItem armor && EquipmentSlot.CHEST.equals(armor.getEquipmentSlot()))
+                  .setToolLevel(ModToolTypes::armorLevel)
+                  .build());
+
+        boots = register(BOOTS_ID,
+          () -> new ToolTypeEntry.Builder().setName(BOOTS_ID)
+                  .setDisplayName(Component.translatable(ToolTranslationConstants.TOOL_TYPE_BOOTS))
+                  .setIsTool(itemStack -> itemStack.getItem() instanceof ArmorItem armor && EquipmentSlot.FEET.equals(armor.getEquipmentSlot()))
+                  .setToolLevel(ModToolTypes::armorLevel)
+                  .build());
+
+        flint_and_steel = register(FLINT_AND_STEEL_ID,
+          () -> new ToolTypeEntry.Builder().setName(FLINT_AND_STEEL_ID)
+                  .setDisplayName(Component.translatable(ToolTranslationConstants.TOOL_TYPE_LIGHTER))
+                  .setIsTool(itemStack -> itemStack.getItem() instanceof FlintAndSteelItem)
+                  .setToolLevel(itemStack -> durabilityBasedLevel(itemStack, Items.FLINT_AND_STEEL.getMaxDamage()))
+                  .build());
+    }
 
     /**
      * Register a new tool type. This should be used rather than directly adding entries
      * to the registry.
      *
-     * @param id The tool type name
+     * @param id       The tool type name
      * @param supplier A function that provides the ToolTypeEntry
      * @return The new RegistryObject
      */
@@ -88,136 +206,20 @@ public class ModToolTypes {
      *
      * @param eventBus The event bus
      */
-    public static void init(IEventBus eventBus) {
+    public static void init(IEventBus eventBus)
+    {
         DEFERRED_REGISTER.register(eventBus);
-    }
-
-    static {
-        none = register(NONE_ID, () -> new ToolTypeEntry.Builder()
-                .setName("")
-                .setDisplayName(Component.translatable(ToolTranslationConstants.TOOL_TYPE_NONE))
-                .setIsTool(itemStack -> true)
-                .setToolLevel(itemStack -> itemStack == null ? 0 : 1)
-                .build());
-
-        pickaxe = register(PICKAXE_ID, () -> new ToolTypeEntry.Builder()
-                .setName(PICKAXE_ID)
-                .setDisplayName(Component.translatable(ToolTranslationConstants.TOOL_TYPE_PICKAXE))
-                .setIsTool(itemStack -> canPerformDefaultActions(itemStack, ToolActions.DEFAULT_PICKAXE_ACTIONS) || Compatibility.isTinkersTool(itemStack, PICKAXE_ID))
-                .setToolLevel(itemStack -> vanillaToolLevel(PICKAXE_ID, itemStack))
-                .build());
-
-        shovel = register(SHOVEL_ID, () -> new ToolTypeEntry.Builder()
-                .setName(SHOVEL_ID)
-                .setDisplayName(Component.translatable(ToolTranslationConstants.TOOL_TYPE_SHOVEL))
-                .setIsTool(itemStack -> canPerformDefaultActions(itemStack, ToolActions.DEFAULT_SHOVEL_ACTIONS) || Compatibility.isTinkersTool(itemStack, SHOVEL_ID))
-                .setToolLevel(itemStack -> vanillaToolLevel(SHOVEL_ID, itemStack))
-                .build());
-
-        axe = register(AXE_ID, () -> new ToolTypeEntry.Builder()
-                .setName(AXE_ID)
-                .setDisplayName(Component.translatable(ToolTranslationConstants.TOOL_TYPE_AXE))
-                .setIsTool(itemStack -> canPerformDefaultActions(itemStack, ToolActions.DEFAULT_AXE_ACTIONS) || Compatibility.isTinkersTool(itemStack, AXE_ID))
-                .setToolLevel(itemStack -> vanillaToolLevel(AXE_ID, itemStack))
-                .build());
-
-        hoe = register(HOE_ID, () -> new ToolTypeEntry.Builder()
-                .setName(HOE_ID)
-                .setDisplayName(Component.translatable(ToolTranslationConstants.TOOL_TYPE_HOE))
-                .setIsTool(itemStack -> canPerformDefaultActions(itemStack, ToolActions.DEFAULT_HOE_ACTIONS) || Compatibility.isTinkersTool(itemStack, HOE_ID))
-                .setToolLevel(itemStack -> vanillaToolLevel(HOE_ID, itemStack))
-                .build());
-
-        sword = register(SWORD_ID, () -> new ToolTypeEntry.Builder()
-                .setName(SWORD_ID)
-                .setDisplayName(Component.translatable(ToolTranslationConstants.TOOL_TYPE_SWORD))
-                .setIsTool(itemStack -> canPerformDefaultActions(itemStack, ToolActions.DEFAULT_SWORD_ACTIONS)  || Compatibility.isTinkersWeapon(itemStack))
-                .setToolLevel(itemStack -> {
-                    final String toolId = SWORD_ID;
-                    if (Compatibility.isTinkersWeapon(itemStack))
-                    {
-                        return Compatibility.getToolLevel(itemStack);
-                    }
-                    else if (itemStack.getItem() instanceof final TieredItem tieredItem)  // most tools
-                    {
-                        return tieredItem.getTier().getLevel();
-                    }
-                    return -1;
-                })
-                .build());
-
-        bow = register(BOW_ID, () -> new ToolTypeEntry.Builder()
-                .setName(BOW_ID)
-                .setDisplayName(Component.translatable(ToolTranslationConstants.TOOL_TYPE_BOW))
-                .setIsTool(itemStack -> itemStack.getItem() instanceof BowItem)
-                .setToolLevel(itemStack -> durabilityBasedLevel(itemStack, Items.BOW.getMaxDamage()))
-                .build());
-
-        fishing_rod = register(FISHING_ROD_ID, () -> new ToolTypeEntry.Builder()
-                .setName(FISHING_ROD_ID)
-                .setDisplayName(Component.translatable(ToolTranslationConstants.TOOL_TYPE_FISHING_ROD))
-                .setIsTool(itemStack -> canPerformDefaultActions(itemStack, ToolActions.DEFAULT_FISHING_ROD_ACTIONS))
-                .setToolLevel(itemStack -> durabilityBasedLevel(itemStack, Items.FISHING_ROD.getMaxDamage()))
-                .build());
-
-        shears = register(SHEARS_ID, () -> new ToolTypeEntry.Builder()
-                .setName(SHEARS_ID)
-                .setDisplayName(Component.translatable(ToolTranslationConstants.TOOL_TYPE_SHEARS))
-                .setIsTool(itemStack -> canPerformDefaultActions(itemStack, ToolActions.DEFAULT_SHEARS_ACTIONS))
-                .setToolLevel(itemStack -> durabilityBasedLevel(itemStack, Items.SHEARS.getMaxDamage()))
-                .build());
-
-        shield = register(SHIELD_ID, () -> new ToolTypeEntry.Builder()
-                .setName(SHIELD_ID)
-                .setDisplayName(Component.translatable(ToolTranslationConstants.TOOL_TYPE_SHIELD))
-                .setIsTool(itemStack -> canPerformDefaultActions(itemStack, ToolActions.DEFAULT_SHIELD_ACTIONS))
-                .setToolLevel(itemStack -> durabilityBasedLevel(itemStack, Items.SHIELD.getMaxDamage()))
-                .build());
-
-        helmet = register(HELMET_ID, () -> new ToolTypeEntry.Builder()
-                .setName(HELMET_ID)
-                .setDisplayName(Component.translatable(ToolTranslationConstants.TOOL_TYPE_HELMET))
-                .setIsTool(itemStack -> itemStack.getItem() instanceof ArmorItem armor && EquipmentSlot.HEAD.equals(armor.getEquipmentSlot()))
-                .setToolLevel(ModToolTypes::armorLevel)
-                .build());
-
-        leggings = register(LEGGINGS_ID, () -> new ToolTypeEntry.Builder()
-                .setName(LEGGINGS_ID)
-                .setDisplayName(Component.translatable(ToolTranslationConstants.TOOL_TYPE_LEGGINGS))
-                .setIsTool(itemStack -> itemStack.getItem() instanceof ArmorItem armor && EquipmentSlot.LEGS.equals(armor.getEquipmentSlot()))
-                .setToolLevel(ModToolTypes::armorLevel)
-                .build());
-
-        chestplate = register(CHESTPLATE_ID, () -> new ToolTypeEntry.Builder()
-                .setName(CHESTPLATE_ID)
-                .setDisplayName(Component.translatable(ToolTranslationConstants.TOOL_TYPE_CHEST_PLATE))
-                .setIsTool(itemStack -> itemStack.getItem() instanceof ArmorItem armor && EquipmentSlot.CHEST.equals(armor.getEquipmentSlot()))
-                .setToolLevel(ModToolTypes::armorLevel)
-                .build());
-
-        boots = register(BOOTS_ID, () -> new ToolTypeEntry.Builder()
-                .setName(BOOTS_ID)
-                .setDisplayName(Component.translatable(ToolTranslationConstants.TOOL_TYPE_BOOTS))
-                .setIsTool(itemStack -> itemStack.getItem() instanceof ArmorItem armor && EquipmentSlot.FEET.equals(armor.getEquipmentSlot()))
-                .setToolLevel(ModToolTypes::armorLevel)
-                .build());
-
-        flint_and_steel = register(FLINT_AND_STEEL_ID, () -> new ToolTypeEntry.Builder()
-                .setName(FLINT_AND_STEEL_ID)
-                .setDisplayName(Component.translatable(ToolTranslationConstants.TOOL_TYPE_LIGHTER))
-                .setIsTool(itemStack -> itemStack.getItem() instanceof FlintAndSteelItem)
-                .setToolLevel(itemStack -> durabilityBasedLevel(itemStack, Items.FLINT_AND_STEEL.getMaxDamage()))
-                .build());
     }
 
     /**
      * Get the tool level for vanilla tools.
      *
-     * @param toolId The type of vanilla tool
+     * @param toolId    The type of vanilla tool
      * @param itemStack The item stack to check
      * @return The tool level
      */
-    public static int vanillaToolLevel(final String toolId, ItemStack itemStack) {
+    public static int vanillaToolLevel(final String toolId, ItemStack itemStack)
+    {
         if (Compatibility.isTinkersTool(itemStack, toolId))
         {
             return Compatibility.getToolLevel(itemStack);
@@ -235,7 +237,8 @@ public class ModToolTypes {
      * @param itemStack The item stack to check
      * @return The armor level
      */
-    public static int armorLevel(ItemStack itemStack) {
+    public static int armorLevel(ItemStack itemStack)
+    {
         if (itemStack.getItem() instanceof final ArmorItem armorItem)
         {
             return ItemStackUtils.getArmorLevel(armorItem.getMaterial());
@@ -249,7 +252,8 @@ public class ModToolTypes {
      * @param itemStack The item stack to check
      * @return The item level
      */
-    public static int durabilityBasedLevel(ItemStack itemStack, int vanillaItemDurability) {
+    public static int durabilityBasedLevel(ItemStack itemStack, int vanillaItemDurability)
+    {
         if (!itemStack.isDamageableItem())
         {
             return 5;
@@ -262,12 +266,15 @@ public class ModToolTypes {
      * Determine whether an item stack can perform the default actions of a given tool.
      *
      * @param itemStack The item stack to check
-     * @param actions The set of actions to compare
+     * @param actions   The set of actions to compare
      * @return Whether the item stack can perform the actions
      */
-    public static Boolean canPerformDefaultActions(ItemStack itemStack, Set<ToolAction> actions) {
-        for (final ToolAction toolAction : actions) {
-            if (!itemStack.canPerformAction(toolAction)) {
+    public static boolean canPerformDefaultActions(ItemStack itemStack, Set<ToolAction> actions)
+    {
+        for (final ToolAction toolAction : actions)
+        {
+            if (!itemStack.canPerformAction(toolAction))
+            {
                 return false;
             }
         }
--- src/main/java/com/minecolonies/api/items/registry/ToolTypeEntry.java
+++ src/main/java/com/minecolonies/api/items/registry/ToolTypeEntry.java
@@ -3,7 +3,6 @@ package com.minecolonies.api.items.registry;
 import com.minecolonies.api.items.ModToolTypes;
 import com.minecolonies.api.util.constant.IToolType;
 import net.minecraft.network.chat.Component;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.registries.RegistryObject;
 
@@ -14,7 +13,8 @@ import java.util.function.Predicate;
  * An entry in the ToolType registry that defines the types of
  * tools within the colony.
  */
-public class ToolTypeEntry implements IToolType {
+public class ToolTypeEntry implements IToolType
+{
     /**
      * The name of the tool type.
      */
@@ -40,12 +40,13 @@ public class ToolTypeEntry implements IToolType {
     /**
      * Constructor.
      *
-     * @param name The name of the tool type
+     * @param name        The name of the tool type
      * @param displayName The human readable name of the tool type
-     * @param isTool A predicate for determining if an itemstack is the tool type
-     * @param itemLevel A function to return the item level of an item stack
+     * @param isTool      A predicate for determining if an itemstack is the tool type
+     * @param itemLevel   A function to return the item level of an item stack
      */
-    private ToolTypeEntry(final String name, final Component displayName, final Predicate<ItemStack> isTool, final Function<ItemStack, Integer> itemLevel) {
+    private ToolTypeEntry(final String name, final Component displayName, final Predicate<ItemStack> isTool, final Function<ItemStack, Integer> itemLevel)
+    {
         this.name = name;
         this.displayName = displayName;
         this.isTool = isTool;
@@ -53,16 +54,37 @@ public class ToolTypeEntry implements IToolType {
     }
 
     /**
+     * Get the registered ToolTypeEntry from a tool ID.
+     *
+     * @param toolId The target ID
+     * @return The ToolTypeEntry
+     */
+    public static IToolType getToolType(final String toolId)
+    {
+        for (RegistryObject<ToolTypeEntry> toolType : ModToolTypes.toolTypes)
+        {
+            if (toolType.get().getName().equals(toolId))
+            {
+                return toolType.get();
+            }
+        }
+
+        return ModToolTypes.none.get();
+    }
+
+    /**
      * @return The tool type's name
      */
-    public String getName() {
+    public String getName()
+    {
         return name;
     }
 
     /**
      * @return the component for the human readable name.
      */
-    public Component getDisplayName() {
+    public Component getDisplayName()
+    {
         return displayName;
     }
 
@@ -72,7 +94,8 @@ public class ToolTypeEntry implements IToolType {
      * @param itemStack to test
      * @return Whether the item stack can act as the tool.
      */
-    public Boolean checkIsTool(ItemStack itemStack) {
+    public Boolean checkIsTool(ItemStack itemStack)
+    {
         return isTool.test(itemStack);
     }
 
@@ -82,14 +105,16 @@ public class ToolTypeEntry implements IToolType {
      * @param itemStack to test
      * @return The item level
      */
-    public int getMiningLevel(ItemStack itemStack) {
+    public int getMiningLevel(ItemStack itemStack)
+    {
         return isTool.test(itemStack) ? itemLevel.apply(itemStack) : -1;
     }
 
     /**
      * A builder that can construct new ToolTypeEntries.
      */
-    public static class Builder {
+    public static class Builder
+    {
         /**
          * The name of the tool type.
          */
@@ -114,67 +139,60 @@ public class ToolTypeEntry implements IToolType {
 
         /**
          * Set the name for the new ToolTypeEntry
+         *
          * @param name the new name
          * @return this
          */
-        public Builder setName(final String name) {
+        public Builder setName(final String name)
+        {
             this.name = name;
             return this;
         }
 
         /**
          * Set the display name for the new ToolTypeEntry
+         *
          * @param displayName the new human readable name
          * @return this
          */
-        public Builder setDisplayName(final Component displayName) {
+        public Builder setDisplayName(final Component displayName)
+        {
             this.displayName = displayName;
             return this;
         }
 
         /**
          * Set the predicate for determining whether an item stack is the tool type
+         *
          * @param isTool The predicate
          * @return this
          */
-        public Builder setIsTool(final Predicate<ItemStack> isTool) {
+        public Builder setIsTool(final Predicate<ItemStack> isTool)
+        {
             this.isTool = isTool;
             return this;
         }
 
         /**
          * Set the function for getting the item level of an item stack for this tool type
+         *
          * @param itemLevel The function
          * @return this
          */
-        public Builder setToolLevel(final Function<ItemStack, Integer> itemLevel) {
+        public Builder setToolLevel(final Function<ItemStack, Integer> itemLevel)
+        {
             this.itemLevel = itemLevel;
             return this;
         }
 
         /**
          * Constructs the actual ToolTypeEntry
+         *
          * @return the new ToolTypeEntry
          */
-        public ToolTypeEntry build() {
+        public ToolTypeEntry build()
+        {
             return new ToolTypeEntry(name, displayName, isTool, itemLevel);
         }
-    }
-
-    /**
-     * Get the registered ToolTypeEntry from a tool ID.
-     *
-     * @param toolId The target ID
-     * @return The ToolTypeEntry
-     */
-    public static IToolType getToolType(final String toolId)
-    {
-        for (RegistryObject<ToolTypeEntry> toolType : ModToolTypes.toolTypes) {
-            if (toolType.get().getName().equals(toolId)) {
-                return toolType.get();
-            }
-        }
-
-        return ModToolTypes.none.get();
     }
 }
