832c60154575390f5d87a437ccf42e56e14bef66
someaddons <38401808+someaddons@users.noreply.github.com>
2024-09-29 14:33:20 +0200

    Various fixes and Improvements (#10242)
    Fix accuracy research to better fit its name, it now increases the guards chance to critically hit
    The guard sleep less research is now called coffee, and moved slightly down in the tree, its effect value also got fixed it incorrectly defaulted to 5 resulting in a negative chance.
    Research effect default level values are now 1 instead of 5 when no levels are declared. So value 1 = researched, 0 = not researched by default.
    Research Tree choices now display a tooltip to clarify that an "OR" really can only choose one of the two
    
    
    Sleeping guards no longer constantly move their head, nor look at nearby players anymore
    Sleeping guards now sleep for the intended duration, they no longer get woken up by unstuck
    
    
    Improve Itemlookup performance when searching for a stack within a list
    Improve citizen Interact goal performance, it now uses our already existing custom lookat goal
    
    
    Fix Ship raids no longer spawning, being always overtaken by underwater raiders, now it is 20% for underwater raiders chance(except for underwater spawnpoints ofc)
    Fix entity registration check, forge field is unreliable as it gets reset on player visual tracking radius
    Fix visitor NPE when they have no home building
    Fix some enchanter scrolls saving the colony name to nbt
    Improve backup loading
    Fix sleeping citizens spawning in bad locations
    Improved refreshdimension entity movement
    Improved Spawnlocation util

--- src/main/java/com/minecolonies/core/entity/visitor/VisitorCitizen.java
+++ src/main/java/com/minecolonies/core/entity/visitor/VisitorCitizen.java
@@ -24,13 +24,12 @@ import com.minecolonies.core.entity.ai.minimal.LookAtEntityGoal;
 import com.minecolonies.core.entity.ai.visitor.EntityAIVisitor;
 import com.minecolonies.core.entity.citizen.EntityCitizen;
 import com.minecolonies.core.entity.citizen.citizenhandlers.*;
-import com.minecolonies.core.entity.pathfinding.proxy.EntityCitizenWalkToProxy;
 import com.minecolonies.core.entity.pathfinding.navigation.MovementHandler;
+import com.minecolonies.core.entity.pathfinding.proxy.EntityCitizenWalkToProxy;
 import com.minecolonies.core.network.messages.client.ItemParticleEffectMessage;
 import com.minecolonies.core.network.messages.server.colony.OpenInventoryMessage;
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.network.chat.Component;
 import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.sounds.SoundEvent;
@@ -60,7 +59,8 @@ import static com.minecolonies.api.util.constant.CitizenConstants.TICKS_20;
 import static com.minecolonies.api.util.constant.Constants.*;
 import static com.minecolonies.api.util.constant.NbtTagConstants.TAG_CITIZEN;
 import static com.minecolonies.api.util.constant.NbtTagConstants.TAG_COLONY_ID;
-import static com.minecolonies.api.util.constant.TranslationConstants.*;
+import static com.minecolonies.api.util.constant.TranslationConstants.MESSAGE_INFO_COLONY_VISITOR_DIED;
+import static com.minecolonies.api.util.constant.TranslationConstants.MESSAGE_INTERACTION_VISITOR_FOOD;
 import static com.minecolonies.core.entity.ai.minimal.EntityAIInteractToggleAble.*;
 
 /**
@@ -177,7 +177,7 @@ VisitorCitizen extends AbstractEntityCitizen
             if (damageSource.getEntity() instanceof LivingEntity && damage > 1.01f)
             {
                 final IBuilding home = getCitizenData().getHomeBuilding();
-                if (home.hasModule(BuildingModules.TAVERN_VISITOR))
+                if (home != null && home.hasModule(BuildingModules.TAVERN_VISITOR))
                 {
                     final TavernBuildingModule module = home.getModule(BuildingModules.TAVERN_VISITOR);
                     for (final Integer id : module.getExternalCitizens())
