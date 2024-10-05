ldtteam/minecolonies

commit 4f972e8582b051902c4212a636dea7570f147b02

Fix npe with bad crafting recipes after e.g. mod removal (#10163)
Fix npe with bad crafting recipes after e.g. mod removal
Fix pathing to consider pathblocks again(no those are not diving)
Adjust cost calculation to add randomness first, so those cannot overshadow cost reductions
Improve builder work position to not go too close to the placed block, avoiding to place a block inside itself Improve Study and - Research pathing to bookcases, they now have a bigger valid range, still try to move close though

src/main/java/com/minecolonies/core/colony/buildings/modules/AbstractCraftingBuildingModule.java
@@ -653,7 +653,8 @@ public abstract class AbstractCraftingBuildingModule extends AbstractBuildingMod
                 continue;
             }
             final IRecipeStorage storage = IColonyManager.getInstance().getRecipeManager().getRecipes().get(token);
-            if (storage != null && (stackPredicate.test(storage.getPrimaryOutput()) || storage.getAlternateOutputs().stream().anyMatch(stackPredicate::test)))
+            if (storage != null && (stackPredicate.test(storage.getPrimaryOutput()) || storage.getAlternateOutputs().stream().anyMatch(stackPredicate::test))
+                  && storage.getClassicForMultiOutput(stackPredicate) != null)
             {
                 if(foundRecipe == null)
                 {

src/main/java/com/minecolonies/api/util/EntityUtils.java
@@ -3,10 +3,9 @@ package com.minecolonies.api.util;
 import com.ldtteam.structurize.util.BlockUtils;
 import com.minecolonies.api.crafting.ItemStorage;
 import com.minecolonies.api.entity.citizen.AbstractEntityCitizen;
-import com.minecolonies.core.entity.pathfinding.SurfaceType;
 import com.minecolonies.api.items.ModTags;
+import com.minecolonies.core.entity.pathfinding.SurfaceType;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Vec3i;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.util.Mth;
 import net.minecraft.world.entity.Entity;
@@ -325,7 +324,7 @@ public final class EntityUtils
     public static boolean isLivingAtSite(@NotNull final LivingEntity entityLiving, final int x, final int y, final int z, final int range)
     {
         final BlockPos pos = BlockPos.containing(entityLiving.getX(), entityLiving.getY(), entityLiving.getZ());
-        return pos.distSqr(new Vec3i(x, y, z)) < MathUtils.square(range);
+        return BlockPosUtil.distSqr(pos, x, y, z) < MathUtils.square(range);
     }
 
     /**

src/main/java/com/minecolonies/core/entity/ai/workers/builder/EntityAIStructureBuilder.java
@@ -212,10 +212,11 @@ public class EntityAIStructureBuilder extends AbstractEntityAIStructureWithWorkO
                 final PathJobMoveCloseToXNearY pathJob = new PathJobMoveCloseToXNearY(world,
                   currentBlock,
                   job.getWorkOrder().getLocation(),
-                  5,
+                  4,
                   worker);
                 gotoPath = ((MinecoloniesAdvancedPathNavigate) worker.getNavigation()).setPathJob(pathJob, currentBlock, 1.0, false);
                 pathJob.getPathingOptions().dropCost = 200;
+                pathJob.extraNodes = 0;
             }
             else if (gotoPath.isDone())
             {

src/main/java/com/minecolonies/core/entity/ai/workers/education/EntityAIStudy.java
@@ -10,10 +10,11 @@ import com.minecolonies.api.util.Tuple;
 import com.minecolonies.core.colony.buildings.workerbuildings.BuildingLibrary;
 import com.minecolonies.core.colony.jobs.JobStudent;
 import com.minecolonies.core.entity.ai.workers.AbstractEntityAISkill;
-import net.minecraft.sounds.SoundEvents;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.InteractionHand;
+import com.minecolonies.core.entity.pathfinding.navigation.PathfindingAIHelper;
 import net.minecraft.core.BlockPos;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import org.jetbrains.annotations.NotNull;
 
@@ -108,7 +109,7 @@ public class EntityAIStudy extends AbstractEntityAISkill<JobStudent, BuildingLib
             studyPos = building.getRandomBookShelf();
         }
 
-        if (walkToBlock(studyPos))
+        if (PathfindingAIHelper.walkCloseToXNearY(worker, studyPos, building.getPosition(), 7))
         {
             setDelay(WALK_DELAY);
             return getState();

src/main/java/com/minecolonies/core/entity/ai/workers/education/EntityAIWorkResearcher.java
@@ -7,6 +7,7 @@ import com.minecolonies.api.research.ILocalResearch;
 import com.minecolonies.core.colony.buildings.workerbuildings.BuildingUniversity;
 import com.minecolonies.core.colony.jobs.JobResearch;
 import com.minecolonies.core.entity.ai.workers.AbstractEntityAIInteract;
+import com.minecolonies.core.entity.pathfinding.navigation.PathfindingAIHelper;
 import net.minecraft.core.BlockPos;
 import net.minecraft.sounds.SoundEvents;
 import org.jetbrains.annotations.NotNull;
@@ -67,7 +68,7 @@ public class EntityAIWorkResearcher extends AbstractEntityAIInteract<JobResearch
             studyPos = building.getRandomBookShelf();
         }
 
-        if (walkToBlock(studyPos))
+        if (PathfindingAIHelper.walkCloseToXNearY(worker, studyPos, building.getPosition(), 7))
         {
             return getState();
         }

src/main/java/com/minecolonies/core/entity/pathfinding/navigation/AbstractAdvancedPathNavigate.java
@@ -4,12 +4,10 @@ import com.minecolonies.api.colony.IColony;
 import com.minecolonies.api.crafting.ItemStorage;
 import com.minecolonies.api.entity.pathfinding.IPathJob;
 import com.minecolonies.api.entity.pathfinding.IStuckHandler;
-import com.minecolonies.api.util.Tuple;
 import com.minecolonies.core.entity.pathfinding.PathingOptions;
 import com.minecolonies.core.entity.pathfinding.pathjobs.AbstractPathJob;
 import com.minecolonies.core.entity.pathfinding.pathresults.PathResult;
 import com.minecolonies.core.entity.pathfinding.pathresults.TreePathResult;
-import com.minecolonies.core.entity.pathfinding.pathresults.WaterPathResult;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.Mob;
@@ -216,4 +214,11 @@ public abstract class AbstractAdvancedPathNavigate extends GroundPathNavigation
      * @param pauseTicks
      */
     protected abstract void setPauseTicks(int pauseTicks);
+
+    /**
+     * Gets the current path result
+     *
+     * @return
+     */
+    public abstract PathResult getPathResult();
 }

src/main/java/com/minecolonies/core/entity/pathfinding/navigation/MinecoloniesAdvancedPathNavigate.java
@@ -1177,4 +1177,10 @@ public class MinecoloniesAdvancedPathNavigate extends AbstractAdvancedPathNaviga
             this.pauseTicks = pauseTicks;
         }
     }
+
+    @Override
+    public PathResult getPathResult()
+    {
+        return pathResult;
+    }
 }

src/main/java/com/minecolonies/core/entity/pathfinding/navigation/PathfindingAIHelper.java
@@ -1,0 +1,44 @@
+package com.minecolonies.core.entity.pathfinding.navigation;
+
+import com.minecolonies.api.entity.other.AbstractFastMinecoloniesEntity;
+import com.minecolonies.api.util.BlockPosUtil;
+import com.minecolonies.core.entity.pathfinding.pathjobs.PathJobMoveCloseToXNearY;
+import net.minecraft.core.BlockPos;
+
+public class PathfindingAIHelper
+{
+    /**
+     * Tries to walk close to a given pos, staying near another position.
+     *
+     * @param entity
+     * @param desiredPosition
+     * @param nearbyPosition
+     * @param distToDesired
+     * @return True while walking, false when reached
+     */
+    public static boolean walkCloseToXNearY(
+      final AbstractFastMinecoloniesEntity entity, final BlockPos desiredPosition,
+      final BlockPos nearbyPosition,
+      final int distToDesired)
+    {
+        final MinecoloniesAdvancedPathNavigate nav = ((MinecoloniesAdvancedPathNavigate) entity.getNavigation());
+
+        if (nav.isDone() || (nav.getPathResult() != null
+                               && !(nav.getPathResult().getJob() instanceof PathJobMoveCloseToXNearY job
+                                      && job.nearbyPosition.equals(nearbyPosition)
+                                      && job.desiredPosition.equals(desiredPosition)
+                                      && job.distToDesired == distToDesired)))
+        {
+            // Check distance once navigation is done, to let the entity walk
+            if (BlockPosUtil.dist(entity.blockPosition(), desiredPosition) < distToDesired)
+            {
+                return false;
+            }
+
+            PathJobMoveCloseToXNearY pathJob = new PathJobMoveCloseToXNearY(entity.level, desiredPosition, nearbyPosition, distToDesired, entity);
+            nav.setPathJob(pathJob, desiredPosition, 1.0, false);
+        }
+
+        return true;
+    }
+}

commit 7066c5cc40296ced6aa53d24cdbe11f3040d1164

Fix explicit requests for recipes (#10310)
Fix output-conditional recipe search (broken by 4f972e8); notably fixes nether miner not asking for resources.

src/main/java/com/minecolonies/core/colony/buildings/modules/AbstractCraftingBuildingModule.java
@@ -653,9 +653,13 @@ public abstract class AbstractCraftingBuildingModule extends AbstractBuildingMod
                 continue;
             }
             final IRecipeStorage storage = IColonyManager.getInstance().getRecipeManager().getRecipes().get(token);
-            if (storage != null && (stackPredicate.test(storage.getPrimaryOutput()) || InventoryUtils.getFirstMatch(storage.getAlternateOutputs(), stackPredicate) != null)
-                  && storage.getClassicForMultiOutput(stackPredicate) != null)
+            if (storage != null && (stackPredicate.test(storage.getPrimaryOutput()) || InventoryUtils.getFirstMatch(storage.getAlternateOutputs(), stackPredicate) != null))
             {
+                if (storage.getRecipeType() instanceof MultiOutputRecipe && storage.getClassicForMultiOutput(stackPredicate) == null)
+                {
+                    continue;
+                }
+
                 if(foundRecipe == null)
                 {
                     foundRecipe = storage;