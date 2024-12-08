d01a948584e6ef64e761d574e5cd05e92f43d3a7
Raycoms <ray.neiheiser@gmail.com>
2024-12-08 12:24:09 +0100

    fix 10505

--- src/main/java/com/minecolonies/core/entity/ai/workers/crafting/EntityAIConcreteMixer.java
+++ src/main/java/com/minecolonies/core/entity/ai/workers/crafting/EntityAIConcreteMixer.java
@@ -118,19 +118,19 @@ public class EntityAIConcreteMixer extends AbstractEntityAICrafting<JobConcreteM
         final BlockState blockToMine = world.getBlockState(posToMine);
         if (mineBlock(posToMine))
         {
-            incrementActionsDoneAndDecSaturation();
-
             if (currentRequest != null && currentRecipeStorage != null && blockToMine.getBlock().asItem().equals(currentRecipeStorage.getPrimaryOutput().getItem()))
             {
                 currentRequest.addDelivery(new ItemStack(blockToMine.getBlock(), 1));
                 job.setCraftCounter(job.getCraftCounter() + 1);
                 if (job.getCraftCounter() >= job.getMaxCraftingCount())
                 {
-                    job.finishRequest(true);
-                    worker.getCitizenExperienceHandler().addExperience(currentRequest.getRequest().getCount() / 2.0);
-                    currentRequest = null;
                     currentRecipeStorage = null;
                     resetValues();
+
+                    if (inventoryNeedsDump() && job.getMaxCraftingCount() == 0 && job.getProgress() == 0 && job.getCraftCounter() == 0 && currentRequest != null)
+                    {
+                        worker.getCitizenExperienceHandler().addExperience(currentRequest.getRequest().getCount() / 2.0);
+                    }
 
                     return START_WORKING;
                 }
