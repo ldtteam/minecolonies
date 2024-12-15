a8774b228690853419045a55fa85c626ada23f55
Raycoms <ray.neiheiser@gmail.com>
2024-12-15 23:19:25 +0100

    95% of pharao model

--- src/main/java/com/minecolonies/core/client/model/raiders/ModelPharaoh.java
+++ src/main/java/com/minecolonies/core/client/model/raiders/ModelPharaoh.java
@@ -46,13 +46,10 @@ public class ModelPharaoh extends EgyptianModel<AbstractEntityEgyptianRaider>
         PartDefinition bodyDefinition = partDefinition.addOrReplaceChild("body",
           CubeListBuilder.create()
             .texOffs(0, 16).addBox(-4.0F, 0.0F, -2.0F, 8.0F, 12.0F, 4.0F, new CubeDeformation(0.0F))
-            .texOffs(44, 0).addBox(-5.5F, -0.2F, -2.5F, 11.0F, 5.0F, 5.0F, new CubeDeformation( 0.15F)).mirror()
+            .texOffs(44, 0).addBox(-5.5F, -0.2F, -2.5F, 11.0F, 5.0F, 5.0F, new CubeDeformation( 0.15F))
             .texOffs(0, 32).addBox(-4.0F, 0.0F, -2.0F, 8.0F, 12.0F, 4.0F, new CubeDeformation( 0.3F))
-            .texOffs(52, 11).addBox(-4.5F, 8.5F, -2.5F, 9.0F, 4.0F, 5.0F).mirror()
-            .texOffs(0, 0).addBox(-1.5F, -1.5F, -1.0F, 3.0F, 3.0F, 1.0F, new CubeDeformation( -0.3F)).mirror()
-            .texOffs(38, 12).addBox(-1.5F, 0.0F, 0.0F, 3.0F, 8.0F, 0.0F).mirror()
+            .texOffs(52, 11).addBox(-4.5F, 8.5F, -2.5F, 9.0F, 4.0F, 5.0F, new CubeDeformation(0.0F))
           , PartPose.offset(0.0F, 0.0F, 0.0F));
-
 
         PartDefinition bodyJewelDefinition = bodyDefinition.addOrReplaceChild("bodyJewel",
           CubeListBuilder.create()
@@ -86,16 +83,7 @@ public class ModelPharaoh extends EgyptianModel<AbstractEntityEgyptianRaider>
             .texOffs(0, 14).addBox(-2.5F, -2.0F, -3.5F, 5.0F, 1.0F, 0.0F).mirror()
             .texOffs(10, 10).addBox(-2.5F, -2.0F, -3.5F, 0.0F, 1.0F, 4.0F).mirror()
             .texOffs(10, 10).addBox(2.5F, -2.0F, -3.5F, 0.0F, 1.0F, 4.0F).mirror()
-            .texOffs(0, 51).addBox(-5.15F, 0.13F, 0.19F, 5.0F, 4.0F, 2.0F, new CubeDeformation( 0.13F))
-            .texOffs(14, 51).addBox(0.0F, 0.0F, 0.19F, 5.0F, 6.0F, 2.0F, new CubeDeformation( 0.14F))
-            .texOffs(0, 57).addBox(0.0F, -1.0F, -4.0F, 3.0F, 1.0F, 4.0F)
-            .texOffs(0, 51).addBox(0.15F, 0.13F, 0.19F, 5.0F, 4.0F, 2.0F, new CubeDeformation( 0.13F)).mirror()
-            .texOffs(14, 51).addBox(-5.0F, 0.0F, 0.19F, 5.0F, 6.0F, 2.0F, new CubeDeformation( 0.14F)).mirror()
-            .texOffs(0, 57).addBox(-3.0F, -1.0F, -4.0F, 3.0F, 1.0F, 4.0F).mirror()
-            .texOffs(28, 51).addBox(-3.0F, 0.0F, 0.0F, 3.0F, 6.0F, 2.0F, new CubeDeformation( -0.2F)).mirror()
-            .texOffs(76, 18).addBox(-4.5F, 0.0F, 0.0F, 9.0F, 4.0F, 8.0F, new CubeDeformation( 0.09F)).mirror()
-            .texOffs(18, 49).addBox(-4.5F, 0.15F, 0.19F, 9.0F, 0.0F, 2.0F, new CubeDeformation( 0.13F)).mirror()
-          , PartPose.offset(0.0F, -11.4F, 0.85F));
+          , PartPose.offset(0.0F, 0.0F, -1.0F));
 
         PartDefinition snakeBodyDefinition = headDefinition.addOrReplaceChild("snakeBody",
           CubeListBuilder.create()
