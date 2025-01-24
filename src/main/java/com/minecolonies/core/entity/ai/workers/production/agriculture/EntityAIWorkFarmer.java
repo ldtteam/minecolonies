923af6cfad85c7aa74d99aae0a862274decf5431
Thom van den Akker <thodor20@gmail.com>
2025-01-24 22:05:25 +0100

    Remove handler method

--- src/main/java/com/minecolonies/core/entity/ai/workers/production/agriculture/EntityAIWorkFarmer.java
+++ src/main/java/com/minecolonies/core/entity/ai/workers/production/agriculture/EntityAIWorkFarmer.java
@@ -13,7 +13,6 @@ import com.minecolonies.api.entity.citizen.VisibleCitizenStatus;
 import com.minecolonies.api.equipment.ModEquipmentTypes;
 import com.minecolonies.api.items.ModItems;
 import com.minecolonies.api.util.InventoryUtils;
-import com.minecolonies.api.util.ItemStackUtils;
 import com.minecolonies.api.util.Tuple;
 import com.minecolonies.api.util.WorldUtil;
 import com.minecolonies.api.util.constant.Constants;
@@ -34,7 +33,6 @@ import com.minecolonies.core.util.AdvancementUtils;
 import com.minecolonies.core.util.citizenutils.CitizenItemUtils;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
-import net.minecraft.core.NonNullList;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
@@ -47,8 +45,6 @@ import net.minecraft.world.item.Items;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.storage.loot.LootParams;
-import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.common.ToolActions;
@@ -866,45 +862,6 @@ public class EntityAIWorkFarmer extends AbstractEntityAICrafting<JobFarmer, Buil
     public int getBreakSpeedLevel()
     {
         return getSecondarySkillLevel();
-    }
-
-    /**
-     * Harvest the crop (only if pams is installed).
-     *
-     * @param pos the position to harvest.
-     */
-    private void harvestCrop(@NotNull final BlockPos pos)
-    {
-        final ItemStack tool = worker.getMainHandItem();
-
-        final int fortune = ItemStackUtils.getFortuneOf(tool);
-        final BlockState state = world.getBlockState(pos);
-
-        final double chance = worker.getCitizenColonyHandler().getColonyOrRegister().getResearchManager().getResearchEffects().getEffectStrength(FARMING);
-
-        final NonNullList<ItemStack> drops = NonNullList.create();
-        state.getDrops(new LootParams.Builder((ServerLevel) world).withLuck(fortune)
-                         .withLuck(fortune)
-                         .withParameter(LootContextParams.ORIGIN, worker.position())
-                         .withParameter(LootContextParams.TOOL, tool)
-                         .withParameter(LootContextParams.THIS_ENTITY, getCitizen()));
-        for (final ItemStack item : drops)
-        {
-            final ItemStack drop = item.copy();
-            if (worker.getRandom().nextDouble() < chance)
-            {
-                drop.setCount(drop.getCount() * 2);
-            }
-            InventoryUtils.addItemStackToItemHandler(worker.getInventoryCitizen(), drop);
-        }
-
-        if (state.getBlock() instanceof final CropBlock crops)
-        {
-            world.setBlockAndUpdate(pos, crops.getStateForAge(0));
-        }
-
-        this.incrementActionsDone();
-        worker.decreaseSaturationForContinuousAction();
     }
 
     /**
