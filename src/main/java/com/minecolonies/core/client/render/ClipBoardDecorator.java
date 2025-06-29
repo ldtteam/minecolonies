bf28e2c416896eef15aedfc030bc6e1238664d29
Mirality <uecasm@users.noreply.github.com>
2025-06-29 20:10:33 +1200

    Swap clipboard and map decorators

--- src/main/java/com/minecolonies/core/client/render/ClipBoardDecorator.java
+++ src/main/java/com/minecolonies/core/client/render/ClipBoardDecorator.java
@@ -2,6 +2,10 @@ package com.minecolonies.core.client.render;
 
 import com.minecolonies.api.colony.ICitizenDataView;
 import com.minecolonies.api.colony.IColonyView;
+import com.minecolonies.api.colony.requestsystem.manager.IRequestManager;
+import com.minecolonies.api.colony.requestsystem.resolver.player.IPlayerRequestResolver;
+import com.minecolonies.api.colony.requestsystem.resolver.retrying.IRetryingRequestResolver;
+import com.minecolonies.api.colony.requestsystem.token.IToken;
 import com.minecolonies.api.items.component.ColonyId;
 import com.minecolonies.api.util.Log;
 import com.mojang.blaze3d.vertex.PoseStack;
@@ -11,6 +15,9 @@ import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.ItemStack;
 import net.neoforged.neoforge.client.IItemDecorator;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class ClipBoardDecorator implements IItemDecorator
 {
@@ -36,32 +43,52 @@ public class ClipBoardDecorator implements IItemDecorator
             {
                 try
                 {
-                    int count = 0;
+                    final Set<IToken<?>> asyncRequest = new HashSet<>();
                     for (final ICitizenDataView view : colonyView.getCitizens().values())
                     {
-                        if (view.hasBlockingInteractions())
+                        if (view.getJobView() != null)
                         {
-                            count++;
+                            asyncRequest.addAll(view.getJobView().getAsyncRequests());
                         }
                     }
 
-                    if (count > 0)
+                    final IRequestManager requestManager = colonyView.getRequestManager();
+                    if (requestManager != null)
                     {
-                        final PoseStack ps = graphics.pose();
-                        ps.pushPose();
-                        ps.translate(0, 0, 500);
-                        graphics.drawCenteredString(font,
-                            Component.literal(count + ""),
-                            xOffset + 15,
-                            yOffset - 2,
-                            0xFF4500 | (255 << 24));
-                        ps.popPose();
-                        return true;
+                        final IPlayerRequestResolver resolver = requestManager.getPlayerResolver();
+                        final IRetryingRequestResolver retryingRequestResolver = requestManager.getRetryingRequestResolver();
+
+                        final Set<IToken<?>> requestTokens = new HashSet<>();
+                        requestTokens.addAll(resolver.getAllAssignedRequests());
+                        requestTokens.addAll(retryingRequestResolver.getAllAssignedRequests());
+
+                        int count = 0;
+                        for (final IToken<?> reqId : requestTokens)
+                        {
+                            if (!asyncRequest.contains(reqId))
+                            {
+                                count++;
+                            }
+                        }
+
+                        if (count > 0)
+                        {
+                            final PoseStack ps = graphics.pose();
+                            ps.pushPose();
+                            ps.translate(0, 0, 500);
+                            graphics.drawCenteredString(font,
+                                Component.literal(count + ""),
+                                xOffset + 15,
+                                yOffset - 2,
+                                0xFF4500 | (255 << 24));
+                            ps.popPose();
+                            return true;
+                        }
                     }
                 }
                 catch (Exception e)
                 {
-                    Log.getLogger().error("Something went wrong with the colonymap item decorator", e);
+                    Log.getLogger().error("Something went wrong with the clipboard item decorator", e);
                 }
             }
         }
