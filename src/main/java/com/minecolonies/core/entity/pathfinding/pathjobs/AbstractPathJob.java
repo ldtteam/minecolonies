7a68e40c5669992206f3a6e80ddd92af794438d4
someaddons <someaddons@github.com>
2025-09-18 20:42:37 +0200

    Improve pathfinding by making citizens less likely to take expensive paths(e.g. jumping down) without exploring additional options

--- src/main/java/com/minecolonies/core/entity/pathfinding/pathjobs/AbstractPathJob.java
+++ src/main/java/com/minecolonies/core/entity/pathfinding/pathjobs/AbstractPathJob.java
@@ -157,6 +157,11 @@ public abstract class AbstractPathJob implements Callable<Path>, IPathJob
     private MNode startNode = null;
 
     /**
+     * Current best node
+     */
+    private MNode bestNode = null;
+
+    /**
      * Visited level
      */
     private int visitedLevel = 1;
@@ -327,7 +332,7 @@ public abstract class AbstractPathJob implements Callable<Path>, IPathJob
     @Nullable
     protected Path search()
     {
-        MNode bestNode = getAndSetupStartNode();
+        bestNode = getAndSetupStartNode();
         double bestNodeEndScore = getEndNodeScore(bestNode);
         // Node count since we found a better end node than the current one
         int nodesSinceEndNode = 0;
@@ -582,6 +587,12 @@ public abstract class AbstractPathJob implements Callable<Path>, IPathJob
         if (costPerEstimation <= 0.0)
         {
             return false;
+        }
+
+        // When reaching and never having done a heuristic rebalance and we did explore a high cost assume that we found a possibly too expensive path
+        if (reaches && maxCost > 20 && visitedLevel == 1 && totalNodesVisited < maxNodes * 0.5)
+        {
+            costPerEstimation *= 0.7;
         }
 
         // Detect an overstimating heuristic(not guranteed, but can check the found path)
@@ -1860,7 +1871,8 @@ public abstract class AbstractPathJob implements Callable<Path>, IPathJob
     @Override
     public String toString()
     {
-        return getClass().getSimpleName() + " start:" + start + " entity:" + entity + " maxNodes:" + maxNodes + " totalNodesVisited:" + totalNodesVisited + " h-rebalances:" + (
+        return getClass().getSimpleName() + " start:" + start + " entity:" + entity + " maxNodes:" + maxNodes + " totalNodesVisited:" + totalNodesVisited + " bestNodeCost:"
+            + bestNode.getCost() + " heuristicCostEstimate:" + startNode.getHeuristic() + " h-rebalances:" + (
             visitedLevel - 1) + " reaches:"
             + reachesDestination;
     }
