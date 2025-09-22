b8ced52ca279b7c5a963438834fc8f1d8507fca6
Raycoms <ray.neiheiser@gmail.com>
2025-09-22 14:45:05 +0200

    clean up braces

--- src/main/java/com/minecolonies/core/entity/ai/workers/CitizenAI.java
+++ src/main/java/com/minecolonies/core/entity/ai/workers/CitizenAI.java
@@ -112,7 +112,7 @@ public class CitizenAI implements IStateAI
     private IState decideAiTask()
     {
         IState next = calculateNextState();
-        if ((next == null || next == lastState))
+        if (next == null || next == lastState)
         {
             if (citizen.getCitizenAI().getState() == CitizenAIState.IDLE && next != CitizenAIState.IDLE)
             {
