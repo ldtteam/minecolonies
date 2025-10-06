6a8869c4f428cd8c514cfa5711fdf069e73ab46f
Gavin Lambert <uecasm@users.noreply.github.com>
2025-10-06 23:58:04 +1300

    Prevent recursive ore breaking (#11243)
    Prevent the smelter consider "breakable ore" that breaks into itself to actually be breakable. It's still potentially smeltable though.

--- src/main/java/com/minecolonies/api/compatibility/ICompatibilityManager.java
+++ src/main/java/com/minecolonies/api/compatibility/ICompatibilityManager.java
@@ -105,6 +105,14 @@ public interface ICompatibilityManager
     boolean isMineableOre(@NotNull ItemStack stack);
 
     /**
+     * Check if a stack belongs to a breakable ore.
+     *
+     * @param stack the stack to test.
+     * @return true if so.
+     */
+    boolean isBreakableOre(@NotNull ItemStack stack);
+
+    /**
      * Get a copy of the list of compost recipes.
      *
      * @return the list of compost recipes, indexed by input item.
