71a62ecf3e4724cd9a112cdd367bcb7d7f996008
Thom van den Akker <thodor20@gmail.com>
2025-10-25 10:14:13 +0200

    Add javadoc

--- src/main/java/com/minecolonies/core/commands/arguments/MultipleOptionsArgument.java
+++ src/main/java/com/minecolonies/core/commands/arguments/MultipleOptionsArgument.java
@@ -16,31 +16,70 @@ import net.minecraft.world.level.Level;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
+/**
+ * Base argument class that can serve to handle multiple different option values.
+ * <p>
+ * You pass one more {@link ArgumentOption} instances into the constructor.
+ * The parser will then run over these options, in the specified order, and determine which option matches the argument value first.
+ * <p>
+ * Make sure the order can actually be respected in a logical way, with the outermost fallback option at the last position.
+ * For example:
+ * <ol>
+ *     <li>Match against a specific keyword</li>
+ *     <li>Match against a known identifier (i.e. a number or UUID)</li>
+ *     <li>Match against any other string value</li>
+ * </ol>
+ * If not a single option matches the argument value, the parser will automatically fail with the missing selector warning.
+ *
+ * @param <TValue> the generic type of the value that the parser should return (this must be shared across all options).
+ */
 public abstract class MultipleOptionsArgument<TValue> implements ArgumentType<MultipleOptionsArgument.OptionContainer<TValue>>
 {
+    /**
+     * Container class carrying the chosen option + original input value to the resolver method.
+     *
+     * @param option   the chosen option in the parser.
+     * @param value    the original input value originating from the parser.
+     * @param <TValue> the generic type of the option parser.
+     */
     public record OptionContainer<TValue>(
         ArgumentOption<TValue> option,
         String value)
     {}
 
+    /**
+     * The list of all argument options specified.
+     */
     private final List<ArgumentOption<TValue>> allowedOptions;
 
+    /**
+     * Default constructor.
+     *
+     * @param allowedOptions the list of all argument options specified.
+     */
     protected MultipleOptionsArgument(final List<ArgumentOption<TValue>> allowedOptions)
     {
         this.allowedOptions = allowedOptions;
     }
 
+    /**
+     * Get the underlying value by running the resolver method on the chosen option parser.
+     *
+     * @param context  the command context.
+     * @param name     the name of the argument.
+     * @param <TValue> the generic type of the option parser.
+     * @return the resolved value from the option.
+     * @throws CommandSyntaxException when any of the resolvers fail to resolve the finalized value.
+     */
     @SuppressWarnings("unchecked")
-    protected static <TValue> TValue getValue(
-        final CommandContext<CommandSourceStack> context,
-        final String name) throws CommandSyntaxException
+    protected static <TValue> TValue getValue(final CommandContext<CommandSourceStack> context, final String name) throws CommandSyntaxException
     {
         final OptionContainer<TValue> container = context.getArgument(name, OptionContainer.class);
         return container.option.resolveValue(context.getSource(), container.value);
     }
 
     @Override
-    public OptionContainer<TValue> parse(final StringReader reader) throws CommandSyntaxException
+    public final OptionContainer<TValue> parse(final StringReader reader) throws CommandSyntaxException
     {
         if (reader.canRead())
         {
@@ -79,12 +118,40 @@ public abstract class MultipleOptionsArgument<TValue> implements ArgumentType<Mu
         return builder.buildFuture();
     }
 
+    /**
+     * A singular argument option, this is responsible for checking if the argument value matches the expected user input.
+     * This class is also responsible for resolving the finalized value after the matching succeeded.
+     *
+     * @param <TValue> the generic type of the option parser.
+     */
     public interface ArgumentOption<TValue>
     {
+        /**
+         * Check if the user defined input matches what this specific argument expects.
+         * If this method returns false the process will transfer to the next option.
+         *
+         * @param value the original user input.
+         * @return true if the option matches the user input, false if not.
+         */
         boolean matches(final String value);
 
+        /**
+         * Resolves a value from the command source during post execution of the command.
+         *
+         * @param source the command source stack.
+         * @param value  the original user input.
+         * @return the finalized value that the specific implementation expects.
+         * @throws CommandSyntaxException when the resolver fails to resolve the finalized value.
+         */
         TValue resolveValue(final CommandSourceStack source, final String value) throws CommandSyntaxException;
 
+        /**
+         * Build any potential suggestions that this option can list for the argument preview when typing.
+         *
+         * @param world              the world the command is being typed/executed in.
+         * @param suggestionProvider the suggestion provider, allowing access to excess suggestion data.
+         * @param builder            the suggestion builder.
+         */
         void createSuggestions(final Level world, final SharedSuggestionProvider suggestionProvider, final SuggestionsBuilder builder);
     }
 }
