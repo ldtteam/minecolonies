d466045e48130f202234147c16bd4aabcce53a93
Thom van den Akker <thodor20@gmail.com>
2025-11-25 11:29:56 +0100

    Fix name

--- src/main/java/com/minecolonies/core/colony/permissions/Permissions.java
+++ src/main/java/com/minecolonies/core/colony/permissions/Permissions.java
@@ -676,7 +676,7 @@ public class Permissions implements IPermissions
         }
         else if (action.allowsOperatorBypass() && player.hasPermissions(IMinecoloniesAPI.getInstance().getConfig().getServer().permissionEventMinBypassPermLevel.get()))
         {
-            Log.getLogger().debug("Permission check got bypassed, original event was. Player: {}, Name: {}, Action: {}", player.getUUID(), player.getName(), action);
+            Log.getLogger().debug("Permission check got bypassed, original event was. Player: {}, Name: {}, Action: {}", player.getUUID(), player.getName().getString(), action);
             return true;
         }
         return false;
