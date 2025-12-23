fe7c75795c1c85b5d6969e817f5ab9de27713658
someaddons <someaddons@github.com>
2025-12-23 17:05:50 +0100

    Migrate UI's to use the new unified selectresource window from structurize

--- src/main/java/com/minecolonies/core/client/gui/modules/building/MinimumStockModuleWindow.java
+++ src/main/java/com/minecolonies/core/client/gui/modules/building/MinimumStockModuleWindow.java
@@ -6,13 +6,14 @@ import com.ldtteam.blockui.controls.ButtonImage;
 import com.ldtteam.blockui.controls.ItemIcon;
 import com.ldtteam.blockui.controls.Text;
 import com.ldtteam.blockui.views.ScrollingList;
+import com.ldtteam.structurize.client.gui.WindowSelectRes;
+import com.minecolonies.api.colony.IColonyManager;
 import com.minecolonies.api.colony.buildings.modules.IMinimumStockModuleView;
 import com.minecolonies.api.crafting.ItemStorage;
 import com.minecolonies.api.util.Tuple;
 import com.minecolonies.api.util.constant.Constants;
 import com.minecolonies.core.Network;
 import com.minecolonies.core.client.gui.AbstractModuleWindow;
-import com.minecolonies.core.client.gui.WindowSelectRes;
 import com.minecolonies.core.network.messages.server.colony.building.AddMinimumStockToBuildingModuleMessage;
 import com.minecolonies.core.network.messages.server.colony.building.RemoveMinimumStockFromBuildingModuleMessage;
 import net.minecraft.network.chat.Component;
@@ -91,7 +92,13 @@ public class MinimumStockModuleWindow extends AbstractModuleWindow<IMinimumStock
     {
         if (!moduleView.hasReachedLimit())
         {
-            new WindowSelectRes(this, (stack) -> true, (stack, qty) -> Network.getNetwork().sendToServer(new AddMinimumStockToBuildingModuleMessage(buildingView, stack, qty)), true).open();
+            new WindowSelectRes(this,
+                Component.empty(),
+                null,
+                IColonyManager.getInstance().getCompatibilityManager().getListOfAllItems(),
+                (stack, qty) -> Network.getNetwork().sendToServer(new AddMinimumStockToBuildingModuleMessage(buildingView, stack, qty)),
+                true,
+                Component.translatable("com.minecolonies.coremod.gui.scan.select.stack")).open();
         }
     }
 
