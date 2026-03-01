094ad8adfa04b24bfcc3575d0483c97e80bcb867
someaddons <someaddons@github.com>
2026-03-01 22:17:03 +0100

    typo

--- src/main/java/com/minecolonies/core/placementhandlers/GeneralBlockPlacementHandler.java
+++ src/main/java/com/minecolonies/core/placementhandlers/GeneralBlockPlacementHandler.java
@@ -84,7 +84,7 @@ public class GeneralBlockPlacementHandler implements IPlacementHandler
             {
                 Log.getLogger()
                     .warn("Schematic with invalid blockentity data for block:" + blockState + " in pack:" + context.getBluePrint().getPackName() + " schematic:"
-                        + context.getBluePrint().getName() + " blokentitydata:" + tileEntityData);
+                        + context.getBluePrint().getName() + " blockentitydata:" + tileEntityData);
                 return ActionProcessingResult.PASS;
             }
 
