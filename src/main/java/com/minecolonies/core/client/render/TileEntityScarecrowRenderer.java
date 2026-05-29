16dfc2c7f8f0791f4ca4b861153a05b71b7ab2c1
Thom van den Akker <thodor20@gmail.com>
2026-05-29 23:37:12 +0200

    Remove PX

--- src/main/java/com/minecolonies/core/client/render/TileEntityScarecrowRenderer.java
+++ src/main/java/com/minecolonies/core/client/render/TileEntityScarecrowRenderer.java
@@ -144,9 +144,6 @@ public class TileEntityScarecrowRenderer implements BlockEntityRenderer<Abstract
         matrixStack.popPose();
     }
 
-    // Pixel-to-block scale used by the model
-    private static final float PX = 1f / 16f;
-
     private static void renderLantern(final PoseStack matrixStack, final MultiBufferSource buffer, final BlockPos pos, final net.minecraft.world.level.Level level)
     {
         matrixStack.pushPose();
