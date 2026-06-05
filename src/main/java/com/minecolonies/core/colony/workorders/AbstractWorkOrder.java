2b9d9e60f30e317ec40b87fcaf8647d495a5a9a3
Mirality <uecasm@users.noreply.github.com>
2026-06-06 10:56:41 +1200

    Fix wrong thread used after reloading world

--- src/main/java/com/minecolonies/core/colony/workorders/AbstractWorkOrder.java
+++ src/main/java/com/minecolonies/core/colony/workorders/AbstractWorkOrder.java
@@ -421,7 +421,7 @@ public abstract class AbstractWorkOrder implements IBuilderWorkOrder
     @Override
     public void loadBlueprint(final Level world, final Consumer<Blueprint> afterLoad)
     {
-        loadBlueprintFuture(world).thenAccept(afterLoad);
+        loadBlueprintFuture(world).thenAcceptAsync(afterLoad, WorldUtil.getMainThread(world));
     }
 
     @Override
